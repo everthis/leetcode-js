7570adba4794eeb722164e135ec8cdfb49111476
everthis <wwwaap@gmail.com>
2018-08-13 17:17:16 +0800

    add 406 496 scripts.

---
+++ 406-queue-reconstruction-by-height.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+const reconstructQueue = function(people) {
+    if (!people) return []
+    const peopledct = {}
+    let height = []
+    const res = []
+    people.forEach((el, idx) => {
+        if (peopledct.hasOwnProperty(el[0])) {
+            peopledct[el[0]].push([el[1], idx])
+        } else {
+            peopledct[el[0]] = [[el[1], idx]]
+            height.push(el[0])
+        }
+    })
+    height = height.sort((a, b) => b - a)
+
+    for (let i = 0; i < height.length; i++) {
+        peopledct[height[i]] = peopledct[height[i]].sort((a, b) => a[0] - b[0])
+        for (el of peopledct[height[i]]) {
+            res.splice(el[0],0, people[el[1]])
+        }
+    }
+    return res
+};
+
+console.log(reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]))
---
+++ 496-next-greater-element-I.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} findNums
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const nextGreaterElement = function(findNums, nums) {
+    const map = {}
+    const stack = []
+    for(let num of nums) {
+        while(stack.length && stack[stack.length - 1] < num) {
+            let tmp = stack.pop()
+            map[tmp] = num
+        }
+        stack.push(num)
+    }
+    for (let i = 0; i < findNums.length; i++) {
+        findNums[i] = map[findNums[i]] == null ? -1 : map[findNums[i]]
+    }
+
+    return findNums
+};
+
+console.log(nextGreaterElement([4,1,2], [1,3,4,2]))
+console.log(nextGreaterElement([2,4], [1,2,3,4]))
