a58fc6c403387daf2704ba749df2b9b835c8c249
everthis <wwwaap@gmail.com>
2018-08-21 15:17:57 +0800

    add 647 palindromic-substrings.

---
+++ 647-palindromic-substrings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = function(s) {
+    let count = 0
+
+    if (s == null || s.length === 0) {
+        return 0
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        extendPalindrome(s, i, i)
+        extendPalindrome(s, i, i + 1)
+    }
+
+    function extendPalindrome(str, left, right) {
+        while (left >= 0 && right < s.length && s.charAt(left) === s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
+    }
+    return count
+};
+
+console.log(countSubstrings("abc"))
+console.log(countSubstrings("aaa"))
