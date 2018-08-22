8924994adf62d91782c7d745b6f44617fb4fae69
everthis <wwwaap@gmail.com>
2018-08-22 19:39:56 +0800

    add 260, 485, 856 scripts.

---
+++ 260-single-number-III.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const singleNumber = function(nums) {
+    const hash = {}
+    nums.forEach((el, idx) => {
+        if(hash.hasOwnProperty(el)) {
+           hash[el] += 1
+           delete hash[el]
+        } else {
+           hash[el] = 1
+        }
+    })
+    return Object.keys(hash).map(el => +el)
+};
---
+++ 856-score-of-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} S
+ * @return {number}
+ */
+const scoreOfParentheses = function(S) {
+    let res = 0, bal = 0;
+    for(let i = 0; i < S.length; i++) {
+        if(S.charAt(i) === '(') {
+           bal += 1
+        } else {
+           bal -= 1
+           if(S.charAt(i - 1) === '(') {
+               res += 1 << bal
+           }
+        }
+    }
+    return res
+};
