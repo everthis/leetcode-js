8b18761d03ea60ccb208f8e9685dd2d6a4f180e1
everthis <wwwaap@gmail.com>
2018-09-11 21:36:47 +0800

    odd-even-linked-list

---
+++ 328-odd-even-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const oddEvenList = function(head) {
+  if (head === null) return null;
+  let odd = head,
+    even = head.next,
+    evenHead = even;
+  while (even !== null && even.next !== null) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
