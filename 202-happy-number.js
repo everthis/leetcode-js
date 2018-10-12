77459b95e9c6de49441c47a489b2feb17cc76ec7
everthis <wwwaap@gmail.com>
2018-10-12 21:57:01 +0800

    add 202 script.

---
+++ 202-happy-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isHappy = function(n) {
+  const arr = [];
+  let tmp = n;
+  while (arr.indexOf(tmp) === -1) {
+    arr.push(tmp);
+    let res = ("" + tmp)
+      .split("")
+      .reduce((ac, str) => ac + Math.pow(+str, 2), 0);
+    if (res === 1) {
+      return true;
+    }
+    tmp = res;
+  }
+  return false;
+};
+
+console.log(isHappy(2));
+console.log(isHappy(19));
