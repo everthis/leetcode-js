f80bb6f7163367ed2265b2d2f29f69db73ce6921
everthis <wwwaap@gmail.com>
2018-10-31 22:14:20 +0800

    add 873 script.

---
+++ 873-length-of-longest-fibonacci-subsequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+const lenLongestFibSubseq = function(A) {
+  const n = A.length;
+  let max = 0;
+  const dp = Array(n).map(el => []);
+  for (let i = 1; i < n; i++) {
+    let l = 0,
+      r = i - 1;
+    while (l < r) {
+      let sum = A[l] + A[r];
+      if (sum > A[i]) {
+        r--;
+      } else if (sum < A[i]) {
+        l++;
+      } else {
+        dp[r][i] = dp[l][r] + 1;
+        max = Math.max(max, dp[r][i]);
+        r--;
+        l++;
+      }
+    }
+  }
+  return max == 0 ? 0 : max + 2;
+};
