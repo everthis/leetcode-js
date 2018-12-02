02d6cb6aa48e797bee2bd1dc1f3fae2390faa374
everthis <wwwaap@gmail.com>
2018-12-02 14:58:22 +0800

    102-binary-tree-level-order-traversal.js

---
+++ 102-binary-tree-level-order-traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrder = function(root) {
+  const res = [];
+  if (root == null) return res;
+  let next = [root];
+  while (next.length > 0) {
+    next = tr(res, next);
+  }
+  return res;
+};
+
+function tr(res, nodeArr) {
+  const arr = [];
+  const nextLevelNodes = [];
+  for (let i = 0; i < nodeArr.length; i++) {
+    arr.push(nodeArr[i].val);
+    if (nodeArr[i].left) {
+      nextLevelNodes.push(nodeArr[i].left);
+    }
+    if (nodeArr[i].right) {
+      nextLevelNodes.push(nodeArr[i].right);
+    }
+  }
+  if (arr.length) res.push(arr);
+  return nextLevelNodes;
+}
