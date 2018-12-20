4959aae59623f6123803f14a06e5163d17efb530
everthis <wwwaap@gmail.com>
2018-12-20 18:24:33 +0800

    Update 75-sort-colors.js

--- 75-sort-colors.js
+++ 75-sort-colors.js
@@ -4,21 +4,20 @@
  */
 
 const sortColors = function(nums) {
-    const counts = {};
-    for (const n of nums) {
-        if (!counts[n]) counts[n] = 0;
-        counts[n]++;
-    }
-    
-    let val = 0;
-    for (let i = 0; i < nums.length;) {
-        let count = counts[val];
-        while (count > 0) {
-            nums[i] = val;
-            count--;
-            i++;
+    let i = 0
+    let j = 0
+    for(let k = 0; k < nums.length; k++) {
+        let v = nums[k]
+        nums[k] = 2
+        if(v < 2) {
+           nums[j] = 1
+           j += 1
         }
-        val+=1;
+        
+        if(v === 0) {
+           nums[i] = 0
+           i += 1
+        }
     }
     
     return nums;
