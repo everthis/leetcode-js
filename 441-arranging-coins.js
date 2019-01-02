3ade627c4b72d8affbfbe9853b1afb010e8f3674
everthis <wwwaap@gmail.com>
2019-01-02 16:18:28 +0800

    Create 441-arranging-coins.js

---
+++ 441-arranging-coins.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const arrangeCoins = function(n) {
+    if (n === 0) {
+        return 0
+    }
+    let num = 1
+    let sum = 1
+    while(n >= sum + num + 1) {
+        num += 1
+        sum += num
+    }
+
+    return num
+};
