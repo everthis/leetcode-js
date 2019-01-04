0ae1e931ea61760076c711e60433b1afc2c1bc39
everthis <wwwaap@gmail.com>
2019-01-04 12:39:24 +0800

    Create 438-find-all-anagrams-in-a-string.js

---
+++ 438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+const findAnagrams = function(s, p) {
+    const slen = s.length
+    const plen = p.length
+    if( plen > slen ) {
+        return []
+    }
+    const aCode = ('a').charCodeAt(0)
+    const count = new Array(26).fill(0)
+    for(let i = 0; i < plen; i++) {
+        count[ p.charCodeAt(i) - aCode ] += 1
+    }
+    const res = []
+    for(let i = 0; i < slen; i++) {
+        count[ s.charCodeAt(i) - aCode ] -= 1
+        if (i >= plen - 1) {
+            if(i - plen >= 0) count[ s.charCodeAt( i - plen ) - aCode ] += 1
+            if (allZero(count)) {
+                res.push(i - plen + 1)
+            }
+        }
+    }
+
+    return res
+};
+
+function allZero(count) {
+    for(let el of count) {
+        if(el !== 0) return false
+    }
+    return true
+}
