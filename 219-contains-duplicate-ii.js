4ecefe6994039ec648150719b3f09d0d33c9685b
everthis <wwwaap@gmail.com>
2019-01-07 13:52:44 +0800

    Create 219-contains-duplicate-ii.js

---
+++ 219-contains-duplicate-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+const containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            if (i - map.get(nums[i]) <= k) {
+                return true;
+            } else {
+                map.set(nums[i], i);
+            }
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+    return false;
+};
