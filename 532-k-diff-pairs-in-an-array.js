a22a964d8ea752455486bd6926ed5dd2c4dbf2f5
everthis <wwwaap@gmail.com>
2019-01-08 14:39:01 +0800

    Update 532-k-diff-pairs-in-an-array.js

--- 532-k-diff-pairs-in-an-array.js
+++ 532-k-diff-pairs-in-an-array.js
@@ -7,20 +7,13 @@ const findPairs = function(nums, k) {
     if(k < 0) return 0
     let count = 0
     const hash = {}
-    for(let el of nums) {
-        if (hash.hasOwnProperty(el)) {
-          if(k === 0 && hash[el] === 1) {
-            count++
-          }
-          hash[el] += 1
-        } else {
-          if (hash.hasOwnProperty(el - k)) {
-              count++
-          }
-          if (hash.hasOwnProperty(el + k)) {
-              count++
-          }
-          hash[el] = 1
+    for(let i = 0; i < nums.length; i++) {
+        hash[nums[i]] = i
+    }
+    for(let i = 0; i < nums.length; i++) {
+        if(hash.hasOwnProperty(nums[i] + k) && hash[nums[i] + k] !== i) {
+           count++
+           delete hash[nums[i] + k]
         }
     }
 
