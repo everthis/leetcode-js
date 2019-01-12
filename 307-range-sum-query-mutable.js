44e2b2dbb5a6ccdd61ba6c1ccd4cf9c2947d175d
everthis <wwwaap@gmail.com>
2019-01-12 17:10:13 +0800

    Create 307-range-sum-query-mutable.js

---
+++ 307-range-sum-query-mutable.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ */
+const NumArray = function(nums) {
+    this.arr = nums
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(i, val) {
+    this.arr[i] = val
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    let sum = 0;
+    for (let k = i; k <= j; k++) {
+        sum += this.arr[k];
+    }
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * obj.update(i,val)
+ * var param_2 = obj.sumRange(i,j)
+ */
