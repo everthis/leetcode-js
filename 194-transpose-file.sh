8d9d9edf7a4a009f64d076ff4a3845a537fcc3f2
everthis <wwwaap@gmail.com>
2019-01-14 10:10:19 +0800

    Create 194-transpose-file.sh

---
+++ 194-transpose-file.sh
@@ -0,0 +1,16 @@
+# Read from the file file.txt and print its transposed content to stdout.
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if(NR == 1) {
+            s[i] = $i;
+        } else {
+            s[i] = s[i] " " $i;
+        }
+    }
+}
+END {
+    for (i = 1; s[i] != ""; i++) {
+        print s[i];
+    }
+}' file.txt
