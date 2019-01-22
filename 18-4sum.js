da611d611ad29cc3d776c6e82217c43e63f00b9a
everthis <wwwaap@gmail.com>
2019-01-22 23:10:51 +0800

    Update 18-4sum.js

--- 18-4sum.js
+++ 18-4sum.js
@@ -9,16 +9,18 @@ const fourSum = function(nums, target) {
 
 function nSum(nums, target, k, start) {
   const res = [];
+  if (nums.length < k || k < 2 || target < nums[0] * k || target > nums[-1] * k)
+    return res;
   if (k == 2) {
     // 2 sum; ( improved to O(n) )
     let r = nums.length - 1;
     let l = start;
     while (l < r) {
-      if (nums[l] + nums[r] == target) {
+      if (nums[l] + nums[r] === target) {
         res.push([nums[l], nums[r]]);
         //skip duplication
-        while (l < r && nums[l] == nums[l + 1]) l++;
-        while (l < r && nums[r] == nums[r - 1]) r--;
+        while (l < r && nums[l] === nums[l + 1]) l++;
+        while (l < r && nums[r] === nums[r - 1]) r--;
         l++;
         r--;
       } else if (nums[l] + nums[r] < target) {
@@ -29,13 +31,13 @@ function nSum(nums, target, k, start) {
     }
   } else {
     for (let i = start; i < nums.length - k + 1; i++) {
-      let temp = nSum(nums, target - nums[i], k - 1, i + 1);
-      temp.forEach(t => {
-        t.push(nums[i]);
-        res.push(t);
-      });
-      //skip duplication
-      while (i < nums.length - 1 && nums[i] === nums[i + 1]) i++;
+      if (i === start || (i > start && nums[i] !== nums[i - 1])) {
+        let temp = nSum(nums, target - nums[i], k - 1, i + 1);
+        temp.forEach(t => {
+          t.push(nums[i]);
+          res.push(t);
+        });
+      }
     }
   }
   return res;
