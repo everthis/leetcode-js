9894352dad7f34de0515bff7ff4cf66a6918dcf2
everthis <wwwaap@gmail.com>
2019-01-29 17:56:01 +0800

    Create 60-permutation-sequence.js

---
+++ 60-permutation-sequence.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+const getPermutation = function(n, k) {
+    let sb = ''
+    const num = []
+    let fact = 1
+    for (let i = 1; i <= n; i++) {
+      fact *= i
+      num.push(i)
+    }
+    for(let i = 0, l = k - 1; i < n; i++ ) {
+      fact = Math.floor( fact / (n - i) )
+      let index = Math.floor( l / fact )
+      sb += num.splice(index, 1)[0]
+      l -= index * fact
+    }
+    return sb
+};
