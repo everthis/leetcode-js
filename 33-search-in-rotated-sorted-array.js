77b8a8275b08b9e05b11c272bb1535be336a9d4c
everthis <wwwaap@gmail.com>
2019-02-02 22:34:03 +0800

    Update 33-search-in-rotated-sorted-array.js

--- 33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array.js
@@ -26,3 +26,46 @@ const search = function(nums, target) {
     
     return -1
 };
+
+// another
+
+const search = function(nums, target) {
+  const len = nums.length
+  for(let i = 0; nums[i] <= target; i++){
+    if(nums[i] === target){
+      return i
+    }
+  }
+  for(let j = len - 1; nums[j] >= target; j--){
+    if(nums[j] === target){
+      return j
+    }
+  }
+  return -1 
+};
+
+// another 
+const search = function(nums, target) {
+  let low = 0
+  let high = nums.length - 1
+  while (low <= high) {
+    let mid = low + ((high - low) >> 1)
+    if (nums[mid] === target) return mid
+
+    if (nums[low] <= nums[mid] ) {
+      if (target < nums[mid] && target >= nums[low]) {
+        high = mid - 1
+      } else {
+        low = mid + 1
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[high]) {
+        low = mid + 1
+      } else {
+        high = mid - 1
+      }
+    }
+
+  }
+  return -1
+};
