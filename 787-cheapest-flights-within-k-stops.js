41d4db0120eccfe31c8cc20babfc5059958d30e7
everthis <wwwaap@gmail.com>
2019-02-03 21:36:22 +0800

    Update 787-cheapest-flights-within-k-stops.js

--- 787-cheapest-flights-within-k-stops.js
+++ 787-cheapest-flights-within-k-stops.js
@@ -6,6 +6,22 @@
  * @param {number} K
  * @return {number}
  */
+
+const findCheapestPrice = function(n, flights, src, dst, K) {
+    let mn = new Array(n + 1).fill(Infinity);
+    mn[src] = 0;
+    for(let k = 0; k < K + 1; k++){
+      let newmn = [].concat(mn);
+      for(let i = 0; i < flights.length; i++){
+        let f = flights[i], a = f[0], b = f[1], c = f[2];
+        newmn[b] = Math.min(newmn[b], mn[a] + c);
+      }
+      mn = [].concat(newmn);
+    }
+    return mn[dst] != Infinity ? mn[dst] : -1
+}
+
+// another
 const findCheapestPrice = function(n, flights, src, dst, K) {
     const map = [];
     flights.forEach(([s, d, p]) => {
