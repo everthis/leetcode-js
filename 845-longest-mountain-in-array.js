8b9f72a141c3dde09cf5d142a5cdbdabefbc52a3
everthis <wwwaap@gmail.com>
2019-02-07 00:19:54 +0800

    Create 845-longest-mountain-in-array.js

---
+++ 845-longest-mountain-in-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+const longestMountain = function(A) {
+        const N = A.length;
+        let ans = 0, base = 0;
+        while (base < N) {
+            let end = base;
+            // if base is a left-boundary
+            if (end + 1 < N && A[end] < A[end + 1]) {
+                // set end to the peak of this potential mountain
+                while (end + 1 < N && A[end] < A[end + 1]) end++;
+
+                // if end is really a peak..
+                if (end + 1 < N && A[end] > A[end + 1]) {
+                    // set end to the right-boundary of mountain
+                    while (end + 1 < N && A[end] > A[end + 1]) end++;
+                    // record candidate answer
+                    ans = Math.max(ans, end - base + 1);
+                }
+            }
+
+            base = Math.max(end, base + 1);
+        }
+
+        return ans;
+};
