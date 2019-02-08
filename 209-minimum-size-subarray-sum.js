bb5375022d487adce0c6d415f7f2f144c34badc5
everthis <wwwaap@gmail.com>
2019-02-08 14:40:06 +0800

    Update 209-minimum-size-subarray-sum.js

--- 209-minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum.js
@@ -4,17 +4,13 @@
  * @return {number}
  */
 const minSubArrayLen = function(s, nums) {
-  if (!nums || nums.length === 0) return 0;
-  const len = nums.length
-  let left = 0, right = 0, result = +Infinity, sum = 0;
-  while (right < len) {
-    sum += nums[right];
-    right++;
-    while (sum >= s) {
-      if (right - left < result) result = right - left;
-      sum -= nums[left];
-      left++;
+    let sum = 0, from = 0, win = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        while (sum >= s) {
+            win = Math.min(win, i - from + 1);
+            sum -= nums[from++];
+        }
     }
-  }
-  return result === +Infinity ? 0 : result;
+    return (win === Number.MAX_SAFE_INTEGER) ? 0 : win;
 };
