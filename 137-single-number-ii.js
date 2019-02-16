8333b7a4a70273f1a69df8228275863a15b5fba3
everthis <wwwaap@gmail.com>
2019-02-16 23:28:33 +0800

    Update 137-single-number-ii.js

--- 137-single-number-ii.js
+++ 137-single-number-ii.js
@@ -28,3 +28,29 @@ const singleNumber = (nums)=> {
   }
   return one;
 }
+
+// another
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = (nums)=> {
+    // Initialize result
+    let result = 0;
+    let x, sum;
+    const n = nums.length
+    // Iterate through every bit
+    for (let i = 0; i < 32; i++) {
+      // Find sum of set bits at ith position in all
+      // array elements
+      sum = 0;
+      x = (1 << i);
+      for (let j = 0; j < n; j++ ) {
+          if (nums[j] & x) sum++;
+      }
+      // The bits with sum not multiple of 3, are the
+      // bits of element with single occurrence.
+      if (sum % 3) result |= x;
+    }
+    return result;
+}
