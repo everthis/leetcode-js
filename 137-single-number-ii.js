83e05966a0dc2a1597955b6445467cd80071e98d
everthis <wwwaap@gmail.com>
2019-02-16 13:40:17 +0800

    Update 137-single-number-ii.js

--- 137-single-number-ii.js
+++ 137-single-number-ii.js
@@ -13,3 +13,18 @@ const singleNumber = function(nums) {
       if(hash[el] === 1) return +el
     }
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = (nums)=> {
+  let one=0, two=0;
+  for (let i=0; i<nums.length; i++) {
+    one = (one ^ nums[i]) & ~two;
+    two = (two ^ nums[i]) & ~one;
+  }
+  return one;
+}
