e95faa1ed371b146db53cc5c143a281300c090dc
everthis <wwwaap@gmail.com>
2019-02-18 22:08:32 +0800

    Update 236-lowest-common-ancestor-of-a-binary-tree.js

--- 236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree.js
@@ -49,13 +49,9 @@ function traverse(node, path = [], arr) {
 // another
 
 const lowestCommonAncestor = function(root, p, q) {
-    if(!root) return null;
-    if(root === p || root === q) return root;
-    
+    if(root === null || root === p || root === q) return root;
     const left = lowestCommonAncestor(root.left, p, q);
     const right = lowestCommonAncestor(root.right, p, q);
-    
     if(left && right) return root;
-    
     return left ? left : right;
 };
