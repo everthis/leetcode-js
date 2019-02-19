d3c2c0f79b850940023208e92eac8f16aa464f2e
everthis <wwwaap@gmail.com>
2019-02-19 16:33:57 +0800

    Update 34-find-first-and-last-position-of-element-in-sorted-array.js

--- 34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,18 +6,9 @@
 const searchRange = function(nums, target) {
   let len = nums.length;
   let start = 0;
-  let end = len > 0 ? len - 1 : 0;
-
+  let end = len - 1;
   let res = [];
   let idx;
-  if (len === 0) return [-1, -1];
-  if (len === 1) {
-    if (target === nums[0]) {
-      return [0, 0];
-    } else {
-      return [-1, -1];
-    }
-  }
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
     if (target === nums[mid]) {
@@ -25,7 +16,7 @@ const searchRange = function(nums, target) {
       break;
     }
     if (target < nums[mid]) end = mid - 1;
-    if (target > nums[mid]) start = mid + 1;
+    else start = mid + 1;
   }
   if (idx == null) return [-1, -1];
   let li = idx;
