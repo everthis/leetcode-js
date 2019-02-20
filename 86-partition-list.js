07242dde3e896c0499552169295c709e79bf5ba7
everthis <wwwaap@gmail.com>
2019-02-20 14:54:14 +0800

    Update 86-partition-list.js

--- 86-partition-list.js
+++ 86-partition-list.js
@@ -11,6 +11,44 @@
  * @return {ListNode}
  */
 const partition = function(head, x) {
+    const leftHead = new ListNode(); 
+    const rightHead = new ListNode(); 
+    let left = leftHead;
+    let right = rightHead;
+  
+    // split list into two sides, left if val < x, else right
+    for (let node = head; node !== null; node = node.next) {
+      if (node.val < x) {
+        left.next = node;
+        left = node;
+      } else {
+        right.next = node;
+        right = node;
+      }
+    }
+  
+    // combine the two sides
+    left.next = rightHead.next;
+    right.next = null;
+  
+    return leftHead.next;
+};
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+const partition = function(head, x) {
     const left = []
     const right = []
     let containX = false
