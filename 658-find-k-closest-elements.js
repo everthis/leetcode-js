79bcb0268e86c95a32f9e83a4fb22841f4018c71
everthis <wwwaap@gmail.com>
2019-02-20 16:01:16 +0800

    Update 658-find-k-closest-elements.js

--- 658-find-k-closest-elements.js
+++ 658-find-k-closest-elements.js
@@ -16,3 +16,27 @@ const findClosestElements = function(arr, k, x) {
       }     
       return arr.slice(lo, lo+k);
 };
+
+// another
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+const findClosestElements = function(arr, k, x) {
+    let lo=0,hi=arr.length-1
+    while(hi-lo>=k){
+        let left=Math.abs(x-arr[lo])
+        let right=Math.abs(x-arr[hi])
+        if(left<right){
+            hi--
+        }else if(left>right){
+            lo++
+        }else{
+            hi--
+        }
+    }
+    return arr.slice(lo,hi+1)
+};
