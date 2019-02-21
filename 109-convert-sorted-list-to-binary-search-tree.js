4e956d1817c3a196bd8b1f73e66691e88cddbda1
everthis <wwwaap@gmail.com>
2019-02-21 14:45:34 +0800

    Create 109-convert-sorted-list-to-binary-search-tree.js

---
+++ 109-convert-sorted-list-to-binary-search-tree.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+const sortedListToBST = function(head) {
+    if(head == null) return null
+    const arr = []
+    let cur = head
+    while(cur !== null) {
+        arr.push(cur)
+        cur = cur.next
+    }
+    return build(arr, null, '')
+};
+
+function build(arr, parent, type) {
+    if(arr.length === 0) return
+    let mid = Math.floor(arr.length / 2)
+    let left = arr.slice(0, mid)
+    let right = arr.slice(mid + 1)
+    const node = new TreeNode(arr[mid].val)
+    if(parent) parent[type] = node
+    build(left, node, 'left')
+    build(right, node, 'right')
+    return node
+}
