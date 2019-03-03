cb1d06ee15664f4d53d9b31257b1493a5a533090
everthis <wwwaap@gmail.com>
2019-03-03 10:58:44 +0800

    Create 988-smallest-string-starting-from-leaf.js

---
+++ 988-smallest-string-starting-from-leaf.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+const smallestFromLeaf = function(root) {
+    const res = []
+    chk(root, [], res)
+    res.sort()
+    return res[0]
+};
+
+function chk(node, path, res) {
+  if(node == null) return
+  path.push(node.val)
+  if(node.left == null && node.right == null) {
+    res.push(arrToStr( path.slice(0).reverse() ))
+    return
+  }
+  chk(node.left, path.slice(0), res)
+  chk(node.right, path.slice(0), res)
+}
+
+function numToChar(num) {
+  const str = 'abcdefghijklmnopqrstuvwxyz'
+  return str[num]
+}
+
+function arrToStr(arr) {
+  let res = ''
+  for(let i = 0; i < arr.length; i++) {
+    res += numToChar(arr[i])
+  }
+  return res
+}
