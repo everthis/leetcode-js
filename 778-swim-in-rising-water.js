8c175927e7fc3b8c31ba7533796f1996f9c5386e
everthis <wwwaap@gmail.com>
2019-03-07 21:43:14 +0800

    Update 778-swim-in-rising-water.js

--- 778-swim-in-rising-water.js
+++ 778-swim-in-rising-water.js
@@ -39,3 +39,51 @@ function dfs(grid, visited, dir, waterHeight, row, col, n) {
   }
   return false;
 }
+
+// another
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+class UnionFind {
+  constructor(N) {
+    this.id = [];
+    for (let i = 0; i < N; i++) {
+      this.id[i] = i;
+    }
+  }
+
+  root(i) {
+    while (i != this.id[i]) {
+      this.id[i] = this.id[this.id[i]];
+      i = this.id[i];
+    }
+    return i;
+  }
+  isConnected(p, q) {
+    return this.root(p) === this.root(q);
+  }
+  union(p, q) {
+    if (this.isConnected(p, q)) return;
+    this.id[this.root(p)] = this.root(q);
+  }
+}
+const swimInWater = grid => {
+  const N = grid.length;
+  const uf = new UnionFind(N * N);
+  let time = 0;
+  while (!uf.isConnected(0, N * N - 1)) {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        if (grid[i][j] > time) continue;
+        if (i < N - 1 && grid[i + 1][j] <= time)
+          uf.union(i * N + j, i * N + j + N);
+        if (j < N - 1 && grid[i][j + 1] <= time)
+          uf.union(i * N + j, i * N + j + 1);
+      }
+    }
+    time++;
+  }
+  return time - 1;
+};
