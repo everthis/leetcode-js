e94b59236738d91cb51bd55d1483ec0f1957c95c
everthis <wwwaap@gmail.com>
2019-03-22 13:41:06 +0800

    Update 376-wiggle-subsequence.js

--- 376-wiggle-subsequence.js
+++ 376-wiggle-subsequence.js
@@ -15,3 +15,24 @@ const wiggleMaxLength = function(nums) {
   }
   return count
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const wiggleMaxLength = function(nums) {
+  const len = nums.length
+  if (len === 0) return 0
+  let up = 1
+  let down = 1
+  for (let i = 1; i < len; i++) {
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1
+    } else if (nums[i] < nums[i - 1]) {
+      down = up + 1
+    }
+  }
+  return Math.max(up, down)
+}
