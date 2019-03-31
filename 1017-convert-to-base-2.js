11ba9b408a8c033595aaf26ef736a6298a4a73b3
everthis <wwwaap@gmail.com>
2019-03-31 21:30:55 +0800

    Update 1017-convert-to-base-2.js

--- 1017-convert-to-base-2.js
+++ 1017-convert-to-base-2.js
@@ -3,6 +3,31 @@
  * @return {string}
  */
 const baseNeg2 = function(N) {
+  return negBase(N, -2)
+};
+
+function negBase(val, base) {
+  if(val === 0) return '0'
+	let result = '';
+	while (val !== 0) {
+		let remainder = val % base;
+		val = Math.trunc(val / base);
+		if (remainder < 0) {
+			remainder += -base;
+			val += 1;
+		}
+		result = remainder + result;
+	}
+	return result;
+}
+
+// another
+
+/**
+ * @param {number} N
+ * @return {string}
+ */
+const baseNeg2 = function(N) {
   if (N === 0) return "0"; 
   let res = ''
   while(N !== 0) {
