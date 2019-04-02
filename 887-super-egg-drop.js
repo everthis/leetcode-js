c81de408aa477f261e7c2e3e7ad10ddb4a5f8452
everthis <wwwaap@gmail.com>
2019-04-02 22:24:24 +0800

    Update 887-super-egg-drop.js

--- 887-super-egg-drop.js
+++ 887-super-egg-drop.js
@@ -3,6 +3,33 @@
  * @param {number} N
  * @return {number}
  */
+
+const superEggDrop = function(K, N) {
+  let lo = 1,
+    hi = N,
+    mi
+  while (lo < hi) {
+    mi = ((lo + hi) / 2) >> 0
+    if (f(mi, K, N) < N) lo = mi + 1
+    else hi = mi
+  }
+  return lo
+}
+
+function f(x, K, N) {
+  let ans = 0,
+    r = 1,
+    i = 1
+  for (let i = 1; i <= K; ++i) {
+    r = ((r * (x - i + 1)) / i) >> 0
+    ans += r
+    if (ans >= N) break
+  }
+  return ans
+}
+
+// another
+
 const superEggDrop = function(K, N) {
   const dp = Array.from({ length: K + 1 }, () => new Array(N + 1).fill(0))
   return helper(K, N, dp)
