a56a21595904a8a4abee55f8ace5e4102606744c
everthis <wwwaap@gmail.com>
2019-04-03 15:59:38 +0800

    Update 32-longest-valid-parentheses.js

--- 32-longest-valid-parentheses.js
+++ 32-longest-valid-parentheses.js
@@ -18,3 +18,21 @@ const longestValidParentheses = function(s) {
   }
   return max
 }
+
+// another
+
+const longestValidParentheses = function(s) {
+  let longest = 0
+  let stack = [-1]
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i)
+    } else {
+      stack.pop()
+      if (!stack.length) stack.push(i)
+      else longest = Math.max(longest, i - stack[stack.length - 1])
+    }
+  }
+
+  return longest
+}
