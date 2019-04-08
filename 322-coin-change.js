74117edfdf1c240203894c277a5ad4ada709657e
everthis <wwwaap@gmail.com>
2019-04-08 16:06:30 +0800

    Update 322-coin-change.js

--- 322-coin-change.js
+++ 322-coin-change.js
@@ -4,15 +4,12 @@
  * @return {number}
  */
 const coinChange = function(coins, amount) {
-  let max = amount + 1
-  const dp = new Array(amount + 1).fill(max)
+  const dp = new Array(amount + 1).fill(amount + 1)
   dp[0] = 0
   for (let i = 1; i <= amount; i++) {
-    for (let j = 0; j < coins.length; j++) {
-      if (coins[j] <= i) {
-        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1)
-      }
+    for (let coin of coins) {
+      if (coin <= i) dp[i] = Math.min(dp[i], dp[i - coin] + 1)
     }
   }
-  return dp[amount] > amount ? -1 : dp[amount]
+  return dp[amount] === amount + 1 ? -1 : dp[amount]
 }
