61a4bbf940cf24b54adc23c56d87c3d690922636
everthis <wwwaap@gmail.com>
2019-04-21 22:29:14 +0800

    Update 1031-maximum-sum-of-two-non-overlapping-subarrays.js

--- 1031-maximum-sum-of-two-non-overlapping-subarrays.js
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays.js
@@ -5,6 +5,21 @@
  * @return {number}
  */
 const maxSumTwoNoOverlap = function(A, L, M) {
+  for(let i = 1, len = A.length; i < len; i++) {
+    A[i] += A[i - 1]
+  }
+  let LMax = A[L - 1], MMax = A[M - 1], res = A[L + M - 1]
+  for(let i = L + M, len = A.length; i < len; i++) {
+    LMax = Math.max(LMax, A[i - M] - A[i - M - L])
+    MMax = Math.max(MMax, A[i - L] - A[i - M - L])
+    res = Math.max(res, Math.max(LMax + A[i] - A[i - M], MMax + A[i] - A[i - L]))
+  }
+  return res
+}
+
+// another
+
+const maxSumTwoNoOverlap = function(A, L, M) {
   let n = A.length
   let sum = []
   sum[0] = 0
