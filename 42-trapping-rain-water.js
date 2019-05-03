347110a4c241c254da2574056360f9883f8236a8
everthis <wwwaap@gmail.com>
2019-05-03 18:00:46 +0800

    Update 42-trapping-rain-water.js

--- 42-trapping-rain-water.js
+++ 42-trapping-rain-water.js
@@ -21,3 +21,26 @@ const trap = function(height) {
     }
     return res
 };
+
+// another
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const trap = function(height) {
+  const len = height.length
+  if (len === 0) return 0
+  const leftMax = [height[0]]
+  const rightMax = []
+  rightMax[len - 1] = height[len - 1]
+  for (let i = len - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(height[i], rightMax[i + 1])
+  }
+  let res = 0
+  for (let i = 1; i < len; i++) {
+    leftMax[i] = Math.max(height[i], leftMax[i - 1])
+    res += Math.min(leftMax[i], rightMax[i]) - height[i]
+  }
+  return res
+}
