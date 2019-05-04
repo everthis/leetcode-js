2214b3f7470cdf41bd4d9c4cfbcaf3d9526d9b09
everthis <wwwaap@gmail.com>
2019-05-04 19:23:40 +0800

    Update 115-distinct-subsequences.js

--- 115-distinct-subsequences.js
+++ 115-distinct-subsequences.js
@@ -47,3 +47,24 @@ const numDistinct = function(s, t) {
   }
   return cur[m]
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+const numDistinct = function(s, t) {
+  const m = t.length,
+    n = s.length
+  const dp = new Array(m + 1).fill(0)
+  dp[0] = 1
+  for (let j = 1; j <= n; j++) {
+    for (let i = m; i > 0; i--) {
+      dp[i] = dp[i] + (t[i - 1] == s[j - 1] ? dp[i - 1] : 0)
+    }
+  }
+  return dp[m]
+}
+
