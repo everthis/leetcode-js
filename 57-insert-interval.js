2996923d35da48dede4231b35fee6b36bd4d5755
everthis <wwwaap@gmail.com>
2019-05-04 19:59:01 +0800

    Update 57-insert-interval.js

--- 57-insert-interval.js
+++ 57-insert-interval.js
@@ -7,10 +7,8 @@ const insert = function(intervals, newInterval) {
   let i = 0
   while (i < intervals.length && intervals[i][1] < newInterval[0]) i++
   while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-    newInterval = [
-      Math.min(intervals[i][0], newInterval[0]),
-      Math.max(intervals[i][1], newInterval[1])
-    ]
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1])
     intervals.splice(i, 1)
   }
   intervals.splice(i, 0, newInterval)
