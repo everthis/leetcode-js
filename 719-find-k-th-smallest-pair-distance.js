6f0fe0c5d649b66f0fc7519075c131723095cd22
everthis <wwwaap@gmail.com>
2019-05-06 18:54:30 +0800

    add 719

---
+++ 719-find-k-th-smallest-pair-distance.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const smallestDistancePair = function(nums, k) {
+  nums.sort((a, b) => a - b)
+  let lo = 0
+  let hi = nums[nums.length - 1] - nums[0]
+  while (lo < hi) {
+    let mi = Math.floor((lo + hi) / 2)
+    let count = 0,
+      left = 0
+    for (let right = 0; right < nums.length; ++right) {
+      while (nums[right] - nums[left] > mi) left++
+      count += right - left
+    }
+    //count = number of pairs with distance <= mi
+    if (count >= k) hi = mi
+    else lo = mi + 1
+  }
+  return lo
+}
