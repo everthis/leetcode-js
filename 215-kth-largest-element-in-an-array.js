4d3a4c0a077acb104d1660f043220603df3085dd
everthis <wwwaap@gmail.com>
2019-05-12 00:36:09 +0800

    Update 215-kth-largest-element-in-an-array.js

--- 215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array.js
@@ -58,3 +58,50 @@ function quickselect(arr, lo, hi, k) {
   if (pivtIdx < arr.length - k) return quickselect(arr, pivtIdx + 1, hi, k)
   return quickselect(arr, lo, pivtIdx - 1, k)
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findKthLargest = function(list, k) {
+  const len = list.length
+  let lo = 0
+  let hi = len - 1
+  let pivot = 0
+  let t = len - k
+  while(lo < hi) {
+    pivot = partition(list, lo, hi)
+    if(pivot === t) {
+      break
+    } else if(pivot < t) {
+      lo = pivot + 1
+    } else if(pivot > t) {
+      hi = pivot - 1
+    }
+  }
+  
+  return list[t]
+}
+
+function partition(arr, s, e) {
+  let t = arr[e]
+  let i = s
+  for(let j = s; j <= e - 1; j++) {
+    if(arr[j] <= t) {
+      swap(arr, i, j)
+      i++
+    }
+  }
+  swap(arr, i, e) 
+  return i
+}
+
+function swap(arr, i, j) {
+  let tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
+
