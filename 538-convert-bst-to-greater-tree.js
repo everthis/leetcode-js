f7b31d2a94361c688e638d76f62fa29f0e0a518b
everthis <wwwaap@gmail.com>
2019-05-20 20:43:27 +0800

    Update 538-convert-bst-to-greater-tree.js

--- 538-convert-bst-to-greater-tree.js
+++ 538-convert-bst-to-greater-tree.js
@@ -24,3 +24,24 @@ function traverse(node, arr) {
   arr.unshift(node)
   traverse(node.left, arr)
 }
+
+
+// another
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const convertBST = function(root) {
+  const obj = {sum: 0}
+  traverse(root, obj)
+  return root
+};
+
+function traverse(node, obj) {
+  if(node == null) return
+  traverse(node.right, obj)
+  node.val += obj.sum
+  obj.sum = node.val
+  traverse(node.left, obj)
+}
