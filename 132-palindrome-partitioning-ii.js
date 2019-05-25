c6aee052bf74e60f978567448a5d2fa699cb8c90
everthis <wwwaap@gmail.com>
2019-05-25 21:25:51 +0800

    Update 132-palindrome-partitioning-ii.js

--- 132-palindrome-partitioning-ii.js
+++ 132-palindrome-partitioning-ii.js
@@ -2,6 +2,37 @@
  * @param {string} s
  * @return {number}
  */
+
+const minCut = function(s) {
+  const n = s.length
+  if (n <= 1) return 0
+  const dp = new Array(n).fill(0)
+  for (let i = 1; i < n; i++) dp[i] = i
+  for (let i = 1; i < n; i++) {
+    // odd
+    for (
+      let start = i, end = i;
+      end < n && start >= 0 && s[end] === s[start];
+      start--, end++
+    ) {
+      dp[end] = Math.min(dp[end], start === 0 ? 0 : dp[start - 1] + 1)
+    }
+    // even
+    for (
+      let start = i - 1, end = i;
+      end < n && start >= 0 && s[end] === s[start];
+      start--, end++
+    ) {
+      dp[end] = Math.min(dp[end], start === 0 ? 0 : dp[start - 1] + 1)
+    }
+  }
+  return dp[n - 1]
+}
+
+
+
+// another
+
 const minCut = function(s) {
   const n = s.length
   const cut = new Array(n + 1).fill(0)
