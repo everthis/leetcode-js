dc6946fb07bfa570f9fa13e6f41f7ec192858f51
everthis <wwwaap@gmail.com>
2019-05-27 17:25:40 +0800

    Update 389-find-the-difference.js

--- 389-find-the-difference.js
+++ 389-find-the-difference.js
@@ -4,6 +4,20 @@
  * @return {character}
  */
 const findTheDifference = function(s, t) {
+  let xor = 0
+  for(let i = 0, len = s.length; i < len; i++) xor = xor ^ s.charCodeAt(i) ^ t.charCodeAt(i)
+  xor = xor ^ t.charCodeAt(t.length - 1)
+  return String.fromCharCode(xor)
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+const findTheDifference = function(s, t) {
   const arr = s.split("");
   let idx;
   for (let i = 0; i < t.length; i++) {
