0fc29b14aa9c06a4fd3f47cc9eb8d4f87a505b42
everthis <wwwaap@gmail.com>
2019-05-27 20:43:48 +0800

    Update 287-find-the-duplicate-number.js

--- 287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number.js
@@ -3,6 +3,31 @@
  * @return {number}
  */
 const findDuplicate = function(nums) {
+  const len = nums.length
+  if(len > 0) {
+    let slow = nums[0]
+    let fast = nums[nums[0]]
+    while(slow !== fast) {
+      slow = nums[slow]
+      fast = nums[nums[fast]]
+    }
+    slow = 0
+    while(slow !== fast) {
+      slow = nums[slow]
+      fast = nums[fast]
+    }
+    return slow
+  }
+  return -1;
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = function(nums) {
   let n = nums.length - 1,
     res = 0
   for (let p = 0; p < 32; ++p) {
