92f45073609df4235a4bddf60a714c4ba340cc93
everthis <wwwaap@gmail.com>
2019-05-28 21:52:57 +0800

    update 95

--- 95-unique-binary-search-trees-ii.js
+++ 95-unique-binary-search-trees-ii.js
@@ -10,24 +10,24 @@
  * @return {TreeNode[]}
  */
 const generateTrees = function(n) {
-    if(n === 0) return []
-    return genTreeList(1,n);
-};
+  if (n === 0) return []
+  return genTreeList(1, n)
+}
 
 function genTreeList(start, end) {
-    const list = []
-    if(start > end) list.push(null)
-    for(let idx = start; idx <= end; idx++) {
-        const leftList = genTreeList(start, idx - 1)
-        const rightList = genTreeList(idx + 1, end)
-        for(let left of leftList) {
-            for(let right of rightList) {
-                const root = new TreeNode(idx)
-                root.left = left
-                root.right = right
-                list.push(root)
-            }
-        }
+  const list = []
+  if (start > end) list.push(null)
+  for (let idx = start; idx <= end; idx++) {
+    const leftList = genTreeList(start, idx - 1)
+    const rightList = genTreeList(idx + 1, end)
+    for (let left of leftList) {
+      for (let right of rightList) {
+        const root = new TreeNode(idx)
+        root.left = left
+        root.right = right
+        list.push(root)
+      }
     }
-    return list
+  }
+  return list
 }
