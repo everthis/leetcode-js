9bfd337e3bd0acde8816eb22995b336860ad701a
everthis <wwwaap@gmail.com>
2019-05-30 15:28:13 +0800

    Update 41-first-missing-positive.js

--- 41-first-missing-positive.js
+++ 41-first-missing-positive.js
@@ -15,3 +15,28 @@ const firstMissingPositive = function(nums) {
    }
    return max < 0 ? 1 : max + 1
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function firstMissingPositive(nums) {
+  const A = nums
+  const n = nums.length
+  for (let i = 0; i < n; i++) {
+    while (A[i] > 0 && A[i] <= n && A[A[i] - 1] !== A[i]) swap(A, i, A[i] - 1)
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (A[i] !== i + 1) return i + 1
+  }
+  return n + 1
+}
+
+function swap(arr, i, j) {
+  let tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
