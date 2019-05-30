4b35d9ecd3e3c9d038d8299eb980796956fe8b15
everthis <wwwaap@gmail.com>
2019-05-30 22:07:21 +0800

    Update 493-reverse-pairs.js

--- 493-reverse-pairs.js
+++ 493-reverse-pairs.js
@@ -23,3 +23,44 @@ function sortSubArr(arr, s, e) {
   tmp.sort((a, b) => a - b);
   arr.splice(s, e - s, ...tmp);
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function merge(A, start, mid, end) {
+  let n1 = mid - start + 1
+  let n2 = end - mid
+  const L = new Array(n1).fill(0)
+  const R = new Array(n2).fill(0)
+
+  for (let i = 0; i < n1; i++) L[i] = A[start + i]
+  for (let j = 0; j < n2; j++) R[j] = A[mid + 1 + j]
+  let i = 0,
+    j = 0
+  for (let k = start; k <= end; k++) {
+    if (j >= n2 || (i < n1 && L[i] <= R[j])) A[k] = L[i++]
+    else A[k] = R[j++]
+  }
+}
+
+function mergesort_and_count(A, start, end) {
+  if (start < end) {
+    let mid = ((start + end) >> 1)
+    let count =
+      mergesort_and_count(A, start, mid) + mergesort_and_count(A, mid + 1, end)
+    let j = mid + 1
+    for (let i = start; i <= mid; i++) {
+      while (j <= end && A[i] > A[j] * 2) j++
+      count += j - (mid + 1)
+    }
+    merge(A, start, mid, end)
+    return count
+  } else return 0
+}
+
+function reversePairs(nums) {
+  return mergesort_and_count(nums, 0, nums.length - 1)
+}
