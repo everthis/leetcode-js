96e8ad7a08e0dc98897490c6ee302fb522792c71
everthis <wwwaap@gmail.com>
2019-06-04 21:38:13 +0800

    Update 188-best-time-to-buy-and-sell-stock-iv.js

--- 188-best-time-to-buy-and-sell-stock-iv.js
+++ 188-best-time-to-buy-and-sell-stock-iv.js
@@ -24,3 +24,35 @@ const maxProfit = function(k, prices) {
   }
   return sell[k]
 }
+
+
+ // another
+
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(k, prices) {
+  if (k >= prices.length / 2) {
+    let max = 0;
+    for(let i = 1; i < prices.length; i++) {
+      if (prices[i] > prices[i - 1]) {
+        max += prices[i] - prices[i - 1];
+      }
+    }
+    return max;
+  }
+  if (prices.length === 0) return 0;
+  let dp = new Array(k + 1);
+  dp[0] = new Array(prices.length).fill(0);
+  for (let t = 1; t <= k; t++) {
+    dp[t] = [0];
+    let max = dp[t - 1][0] - prices[0];
+    for (let day = 1; day < prices.length; day++) {
+      dp[t][day] = Math.max(dp[t][day - 1], max + prices[day]);
+      max = Math.max(max, dp[t - 1][day] - prices[day]);
+    }
+  }
+  return dp[k][prices.length - 1];
+}
