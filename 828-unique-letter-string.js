3f497fc4a74f4d7059f2348a993698514a1dabed
everthis <wwwaap@gmail.com>
2019-06-21 13:51:58 +0800

    Update 828-unique-letter-string.js

--- 828-unique-letter-string.js
+++ 828-unique-letter-string.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
 const uniqueLetterString = function(S) {
+  const s = S.split('')
+  let res = 0
+  for (let n = S.length, i = 0, l = 0, r = 0; i < n; i++) {
+    for (l = i - 1; l >= 0 && s[l] != s[i]; l--);
+    for (r = i + 1; r < n && s[r] != s[i]; r++);
+    res += (r - i) * (i - l)
+  }
+  return res % (10 ** 9 + 7)
+}
+
+// another
+
+/**
+ * @param {string} S
+ * @return {number}
+ */
+const uniqueLetterString = function(S) {
   const len = S.length
   if (len === 0) return 0
   if (len === 1) return 1
