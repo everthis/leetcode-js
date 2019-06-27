108867fa482de4369463b3f3335b5e4e8ce4fd15
everthis <wwwaap@gmail.com>
2019-06-27 16:47:35 +0800

    Update 99-recover-binary-search-tree.js

--- 99-recover-binary-search-tree.js
+++ 99-recover-binary-search-tree.js
@@ -10,6 +10,32 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 const recoverTree = function(root) {
+  let node1, node2;
+  let prev = new TreeNode(-Infinity);
+  traverse(root);
+  swap(node1, node2)
+    
+  function traverse(node) {
+    if (!node) return;
+    traverse(node.left);
+    if (node.val < prev.val) {
+      node2 = node;
+      if (!node1) node1 = prev;
+    }
+    prev = node;
+    traverse(node.right);
+  }
+
+  function swap(node1, node2) {
+    let temp = node1.val
+    node1.val = node2.val
+    node2.val = temp
+  }
+}
+
+// another
+
+const recoverTree = function(root) {
   const eNodes = [];
   if (root == null) return;
   let current = root;
