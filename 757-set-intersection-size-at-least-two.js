7845af3b0a7fadcd6d31b77722f5d45b71a440b0
everthis <wwwaap@gmail.com>
2019-07-02 14:05:53 +0800

    Update 757-set-intersection-size-at-least-two.js

--- 757-set-intersection-size-at-least-two.js
+++ 757-set-intersection-size-at-least-two.js
@@ -3,6 +3,32 @@
  * @return {number}
  */
 const intersectionSizeTwo = function(intervals) {
+  let highest = Number.NEGATIVE_INFINITY;
+  let secondHighest = Number.NEGATIVE_INFINITY;
+  return intervals
+      .sort((a, b) => a[1] - b[1])
+      .reduce((sum, interval) => {
+      if (interval[0] > secondHighest) {
+          secondHighest = interval[1];
+          highest = interval[1] - 1;
+          return sum + 2;
+      }
+      else if (interval[0] > highest) {
+          highest = secondHighest;
+          secondHighest = interval[1];
+          return sum + 1;
+      }
+      return sum;
+  }, 0);
+};
+
+// another
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const intersectionSizeTwo = function(intervals) {
   intervals.sort((a, b) => a[1] - b[1]);
 
   let n = intervals.length;
