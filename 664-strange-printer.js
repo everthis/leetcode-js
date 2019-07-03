20cb3a490ca8248b302739289800c8a459f6169d
everthis <wwwaap@gmail.com>
2019-07-03 14:01:04 +0800

    Create 664-strange-printer.js

---
+++ 664-strange-printer.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const strangePrinter = function(s) {
+  let n = s.length
+  let dp = new Array(n).fill(0).map(() => new Array(n).fill(0))
+
+  const help = (s, i, j) => {
+    if (i > j) return 0
+    if (dp[i][j] > 0) {
+      return dp[i][j]
+    }
+    let res = help(s, i, j - 1) + 1
+    for (let k = i; k < j; k++) {
+      if (s[k] == s[j]) {
+        res = Math.min(help(s, i, k) + help(s, k + 1, j - 1), res)
+      }
+    }
+    dp[i][j] = res
+    return res
+  }
+
+  return help(s, 0, n - 1)
+}
