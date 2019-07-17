1787fa8eea5ba169e10139ed21dd3cb0605f37f3
everthis <wwwaap@gmail.com>
2019-07-17 13:58:35 +0800

    Update 1123-lowest-common-ancestor-of-deepest-leaves.js

--- 1123-lowest-common-ancestor-of-deepest-leaves.js
+++ 1123-lowest-common-ancestor-of-deepest-leaves.js
@@ -24,3 +24,41 @@ const lcaDeepestLeaves = function(root) {
     return lcaDeepestLeaves(root.right)
   }
 }
+
+// BFS
+
+const lcaDeepestLeaves = function(root) {
+  let current = [root];
+  let level = 0;
+  let last = [];
+  while(current.length) {
+    let next = [];
+    for (var i = 0; i < current.length; i++) {
+      if (current[i].left) {
+        current[i].left.parent = current[i];
+        next.push(current[i].left);
+      }
+      if (current[i].right) {
+        current[i].right.parent = current[i];
+        next.push(current[i].right);
+      }
+    }
+    last = current;
+    current = next;
+  }
+  let parent = last[0].parent;
+  if (!parent) {
+    return last[0];
+  }
+  while(last.length > 1) {
+    let next = [];
+    for (var i = 0; i < last.length; i++) {
+      newParent = last[i].parent;
+      if (!next.includes(newParent)) {
+        next.push(newParent);
+      }
+    }
+    last = next;
+  }
+  return last[0]; 
+};
