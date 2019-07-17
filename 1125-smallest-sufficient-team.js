c833d1322a31929ae789c01767064c6e83fc50dd
everthis <wwwaap@gmail.com>
2019-07-17 15:09:53 +0800

    Update 1125-smallest-sufficient-team.js

--- 1125-smallest-sufficient-team.js
+++ 1125-smallest-sufficient-team.js
@@ -49,3 +49,106 @@ const smallestSufficientTeam = function(req_skills, people) {
   }
   return []
 }
+
+// another
+
+/**
+ * @param {string[]} req_skills
+ * @param {string[][]} people
+ * @return {number[]}
+ */
+const smallestSufficientTeam = function(req_skills, people) {
+  let skill_len = req_skills.length
+
+  // 将people转换为id的模式
+  let id_people = []
+  let hash = {}
+  for (let i = 0; i < skill_len; i++) {
+    hash[req_skills[i]] = i
+  }
+  for (let i = 0; i < people.length; i++) {
+    id_people[i] = []
+    for (let j = 0; j < people[i].length; j++) {
+      id_people[i][j] = hash[people[i][j]]
+    }
+  }
+
+  // 过滤掉不可能的选取的人员
+  let skip = {}
+  for (let i = 0; i < id_people.length; i++) {
+    if (skip[i]) continue
+    let skills = Array(skill_len).fill(0)
+    for (let j = 0; j < id_people[i].length; j++) {
+      let curId = id_people[i][j]
+      skills[curId]++
+    }
+    for (let k = i + 1; k < id_people.length; k++) {
+      if (skip[k]) continue
+      let needSkip = true
+      for (let l = 0; l < id_people[k].length; l++) {
+        let id = id_people[k][l]
+        if (skills[id] === 0) {
+          needSkip = false
+          break
+        }
+      }
+      if (needSkip) {
+        skip[k] = true
+      }
+    }
+  }
+
+  // 构造精简后的人员，并且保存对应的index关系
+  let slim_people = []
+  let idHash = {}
+  for (let i = 0; i < id_people.length; i++) {
+    if (skip[i]) continue
+    idHash[slim_people.length] = i
+    slim_people.push(id_people[i])
+  }
+
+  // 执行回溯
+  let res = Infinity
+  let remain = {}
+  let ans = null
+  for (let i = 0; i < slim_people.length; i++) {
+    remain[i] = false
+  }
+  let init_select = Array(skill_len).fill(0)
+
+  backtrack(0, init_select, 0, remain)
+
+  return ans
+
+  function backtrack(id, select, count, remain) {
+    if (count >= res) return
+    let done = true
+    for (let i = 0; i < select.length; i++) {
+      if (select[i] === 0) {
+        done = false
+      }
+    }
+    if (done) {
+      res = count
+      let _res_ = []
+      for (let k in remain) {
+        if (remain[k]) _res_.push(idHash[k])
+      }
+      ans = _res_
+      return
+    }
+    for (let k = id; k < slim_people.length; k++) {
+      let arr = slim_people[k]
+      for (let i = 0; i < arr.length; i++) {
+        select[arr[i]]++
+      }
+      remain[k] = true
+      backtrack(k + 1, select, count + 1, remain)
+      remain[k] = false
+      for (let i = 0; i < arr.length; i++) {
+        select[arr[i]]--
+      }
+    }
+  }
+}
+
