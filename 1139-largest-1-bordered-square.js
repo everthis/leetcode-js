e155687fd15ab071a0d8ecdaafcba76148bd13d6
everthis <wwwaap@gmail.com>
2019-07-29 21:08:10 +0800

    Update 1139-largest-1-bordered-square.js

--- 1139-largest-1-bordered-square.js
+++ 1139-largest-1-bordered-square.js
@@ -3,28 +3,30 @@
  * @return {number}
  */
 const largest1BorderedSquare = function(grid) {
-  let A = grid
+  let A = grid;
   let m = A.length,
-    n = A[0].length
-  const left = Array.from(Array(m)).map(() => Array(n).fill(0))
-  const top = Array.from(Array(m)).map(() => Array(n).fill(0))
+    n = A[0].length;
+  let max = 0;
+  const hori = Array.from(Array(m)).map(() => Array(n).fill(0));
+  const ver = Array.from(Array(m)).map(() => Array(n).fill(0));
   for (let i = 0; i < m; ++i) {
     for (let j = 0; j < n; ++j) {
       if (A[i][j] > 0) {
-        left[i][j] = j > 0 ? left[i][j - 1] + 1 : 1
-        top[i][j] = i > 0 ? top[i - 1][j] + 1 : 1
+        hori[i][j] = j > 0 ? hori[i][j - 1] + 1 : 1;
+        ver[i][j] = i > 0 ? ver[i - 1][j] + 1 : 1;
       }
     }
   }
-  for (let l = Math.min(m, n); l > 0; --l)
-    for (let i = 0; i < m - l + 1; ++i)
-      for (let j = 0; j < n - l + 1; ++j)
-        if (
-          top[i + l - 1][j] >= l &&
-          top[i + l - 1][j + l - 1] >= l &&
-          left[i][j + l - 1] >= l &&
-          left[i + l - 1][j + l - 1] >= l
-        )
-          return l * l
-  return 0
-}
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      let small = Math.min(hori[i][j], ver[i][j]);
+      while (small > max) {
+        if (ver[i][j - small + 1] >= small && hori[i - small + 1][j] >= small)
+          max = small;
+        small--;
+      }
+    }
+  }
+  return max * max;
+};
+
