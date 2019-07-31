29da762f05beffd83a0f3aaf428e2aaed81670b8
everthis <wwwaap@gmail.com>
2019-07-31 20:45:26 +0800

    Update 1137-n-th-tribonacci-number.js

--- 1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number.js
@@ -10,3 +10,23 @@ const tribonacci = function(n) {
   let tmp = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
   return hash[n] = tmp
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const tribonacci = function(n) {
+  if (n < 2) return n
+  let prev0 = 0
+  let prev1 = 1
+  let prev2 = 1
+  for (let count = 3; count <= n; count++) {
+    let next = prev2 + prev1 + prev0
+    prev0 = prev1
+    prev1 = prev2
+    prev2 = next
+  }
+  return prev2
+}
