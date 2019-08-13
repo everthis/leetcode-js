1202a3d0c56437efb027f7ab1fc05724a2e69048
everthis <wwwaap@gmail.com>
2019-08-13 22:46:14 +0800

    Update 1155-number-of-dice-rolls-with-target-sum.js

--- 1155-number-of-dice-rolls-with-target-sum.js
+++ 1155-number-of-dice-rolls-with-target-sum.js
@@ -24,3 +24,23 @@ const numRollsToTarget = function(d, f, target) {
 
   return dp[target]
 }
+
+// another
+
+const numRollsToTarget = function(d, f, target) {
+  const MOD = 10 ** 9 + 7;
+  const dp = Array.from({ length: d + 1 }, () => new Array(target + 1).fill(0));
+  dp[0][0] = 1;
+  for (let i = 1; i <= d; i++) {
+    for (let j = 1; j <= target; j++) {
+      if (j > i * f) {
+        continue;
+      } else {
+        for (let k = 1; k <= f && k <= j; k++) {
+          dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+        }
+      }
+    }
+  }
+  return dp[d][target];
+};
