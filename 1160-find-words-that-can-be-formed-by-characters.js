599250386a15e954ae005c8fe2cb18d1c62575a9
Nothing <wwwaap@gmail.com>
2019-08-19 21:48:44 +0800

    Update 1160-find-words-that-can-be-formed-by-characters.js

--- 1160-find-words-that-can-be-formed-by-characters.js
+++ 1160-find-words-that-can-be-formed-by-characters.js
@@ -4,21 +4,30 @@
  * @return {number}
  */
 const countCharacters = function(words, chars) {
-  const cnt = Array(26)
-  cnt.fill(0)
-  for (let c of chars) cnt[c.charCodeAt() - 97]++
-
-  let res = 0
-  for (let word of words) {
-    let tmp = cnt.slice()
-    let match = true
-    for (let c of word) {
-      if (--tmp[c.charCodeAt() - 97] < 0) {
-        match = false
+  let letters = new Array(26).fill(0),
+    a = 'a'.charCodeAt(0),
+    z = 'z'.charCodeAt(0)
+  let count = 0
+  for (let i = 0; i < chars.length; i++) {
+    let l = chars[i].charCodeAt(0) - a
+    letters[l]++
+  }
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i]
+    let tmp = letters.slice()
+    let tCount = 0
+    for (let j = 0; j < word.length; j++) {
+      let l = word[j].charCodeAt(0) - a
+      tmp[l]--
+      if (tmp[l] < 0) {
         break
+      } else {
+        tCount++
       }
     }
-    if (match) res += word.length
+    if (tCount == word.length) {
+      count += word.length
+    }
   }
-  return res
+  return count
 }
