7ed140e1cc99ecf6b459c93f30806508348f216e
Nothing <wwwaap@gmail.com>
2019-09-28 19:37:53 +0800

    Update 13-roman-to-integer.js

--- 13-roman-to-integer.js
+++ 13-roman-to-integer.js
@@ -32,3 +32,26 @@ const romanToInt = function(s) {
   }
   return res
 }
+
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = function(s) {
+  const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+  let result = 0
+  let index = s.length - 1
+  let preInt = 0
+  while (index >= 0) {
+    let ch = s[index]
+    let curInt = map[ch]
+    if (curInt >= preInt) result += curInt
+    else result -= curInt
+    preInt = curInt
+    index--
+  }
+  return result
+}
