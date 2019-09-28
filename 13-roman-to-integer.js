715384fc6fd8f68a12b61b4022a3546b5777fafc
Nothing <wwwaap@gmail.com>
2019-09-28 10:45:55 +0800

    Create 13-roman-to-integer.js

---
+++ 13-roman-to-integer.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = function(s) {
+  let res = 0
+  for (let i = s.length - 1; i >= 0; i--) {
+    let c = s.charAt(i)
+    switch (c) {
+      case 'I':
+        res += res >= 5 ? -1 : 1
+        break
+      case 'V':
+        res += 5
+        break
+      case 'X':
+        res += 10 * (res >= 50 ? -1 : 1)
+        break
+      case 'L':
+        res += 50
+        break
+      case 'C':
+        res += 100 * (res >= 500 ? -1 : 1)
+        break
+      case 'D':
+        res += 500
+        break
+      case 'M':
+        res += 1000
+        break
+    }
+  }
+  return res
+}
