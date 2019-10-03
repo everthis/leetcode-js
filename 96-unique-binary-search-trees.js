0dc8a29b2c86e5d725000d035b27f8940a0c3046
Nothing <wwwaap@gmail.com>
2019-10-03 19:50:05 +0800

    Update 96-unique-binary-search-trees.js

--- 96-unique-binary-search-trees.js
+++ 96-unique-binary-search-trees.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
 const numTrees = function(n) {
+  const arr = new Array(n + 1).fill(0)
+  arr[0] = arr[1] = 1
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      arr[i] += arr[j - 1] * arr[i - j]
+    }
+  }
+  return arr[n]
+}
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const numTrees = function(n) {
   const hash = {
     0: 1,
     1: 1
