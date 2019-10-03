461282ef046504b835223779bd4ccd5e94caf225
Nothing <wwwaap@gmail.com>
2019-10-03 20:41:50 +0800

    Create 707-design-linked-list.js

---
+++ 707-design-linked-list.js
@@ -0,0 +1,73 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyLinkedList = function() {
+  this.arr = []
+}
+
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+  if (this.arr[index] !== undefined) {
+    return this.arr[index]
+  } else {
+    return -1
+  }
+}
+
+/**
+ * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+  this.arr.unshift(val)
+}
+
+/**
+ * Append a node of value val to the last element of the linked list.
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+  this.arr.push(val)
+}
+
+/**
+ * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+  if (this.arr.length >= index) {
+    this.arr.splice(index, 0, val)
+  }
+  if (index < 0) {
+    this.arr.unshift(val)
+  }
+}
+
+/**
+ * Delete the index-th node in the linked list, if the index is valid.
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+  if (index >= 0 && index < this.arr.length) {
+    this.arr.splice(index, 1)
+  }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
