fd97bed481577ac597cdb283942c5554ba3a4b6c
Nothing <wwwaap@gmail.com>
2019-10-07 12:33:37 +0800

    Update 89-gray-code.js

--- 89-gray-code.js
+++ 89-gray-code.js
@@ -22,6 +22,24 @@ const grayCode = function(n) {
  * @return {number[]}
  */
 const grayCode = function(n) {
+  const arr = []
+  arr.push(0)
+  for (let i = 0; i < n; i++) {
+    let inc = 1 << i
+    for (let j = arr.length - 1; j >= 0; j--) {
+      arr.push(arr[j] + inc)
+    }
+  }
+  return arr
+}
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const grayCode = function(n) {
   return n
     ? (x => [...x, ...x.map((v, i) => x[x.length - 1 - i] + x.length)])(
         grayCode(n - 1)
