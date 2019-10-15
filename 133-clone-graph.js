474880c927547e58033144bb6144a6468305038a
Nothing <wwwaap@gmail.com>
2019-10-15 11:02:10 +0800

    Update 133-clone-graph.js

--- 133-clone-graph.js
+++ 133-clone-graph.js
@@ -1,4 +1,14 @@
 /**
+ *   - if(!node) return node. Graphs can also have null neighbors
+ *   - using a Set doesn't work because we are dealing with objects not primitives
+ *       - and when encountering an already-cloned node, you are supposed to return the copied node
+ *       (otherwise you are linking back to the original)
+ *       - so, map = {} is correct
+ *   - the only "trick" is that you must set the current node as "already copied" before DFS-ing its neighbors
+ *   - declaring new variable for copying a node is actually extra O(n) space
+ */
+
+/**
  * // Definition for a Node.
  * function Node(val,neighbors) {
  *    this.val = val;
@@ -14,6 +24,7 @@ const cloneGraph = function(node) {
   const map = {}
   return traverse(node)
   function traverse(node) {
+    if(!node) return node;
     if (!map[node.val]) {
       const newNode = new Node(node.val)
       map[node.val] = newNode
