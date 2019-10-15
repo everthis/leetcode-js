99e082e8631750559f91919d4bb0c73bc253da7b
Nothing <wwwaap@gmail.com>
2019-10-15 22:09:21 +0800

    Update 134-gas-station.js

--- 134-gas-station.js
+++ 134-gas-station.js
@@ -17,3 +17,23 @@ const canCompleteCircuit = function(gas, cost) {
   }
   return total < 0 ? -1 : curtIdx
 }
+
+// another
+
+const canCompleteCircuit = function(gas, cost) {
+  const len = gas.length
+  let tank = 0
+  let count = 0
+  for (let i = 0; i < len * 2; i++) {
+    let idx = i % len
+    if (count === len) return idx
+    count += 1
+    tank += gas[idx] - cost[idx]
+    if (tank < 0) {
+      tank = 0
+      count = 0
+    }
+  }
+  return -1
+}
+
