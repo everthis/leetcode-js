26c19804882cdea43419d5ff13eec5883fc1da26
Nothing <wwwaap@gmail.com>
2019-10-17 21:31:49 +0800

    Update 189-rotate-array.js

--- 189-rotate-array.js
+++ 189-rotate-array.js
@@ -16,3 +16,22 @@ const rotate = function(nums, k) {
     i--
   }
 };
+
+// another
+
+const rotate = function(nums, k) {
+  k %= nums.length
+  reverse(nums, 0, nums.length - 1)
+  reverse(nums, 0, k - 1)
+  reverse(nums, k, nums.length - 1)
+}
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    var temp = nums[start]
+    nums[start] = nums[end]
+    nums[end] = temp
+    start++
+    end--
+  }
+}
