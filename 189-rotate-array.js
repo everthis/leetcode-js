c94b18b4e53b16c4f43416a97db2dcbd2d05d168
Nothing <wwwaap@gmail.com>
2019-10-17 21:28:03 +0800

    Update 189-rotate-array.js

--- 189-rotate-array.js
+++ 189-rotate-array.js
@@ -4,5 +4,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const rotate = function(nums, k) {
-  nums.unshift(...nums.splice(nums.length - k));
+  nums.unshift(...nums.splice(nums.length - k))
+};
+
+// another
+
+const rotate = function(nums, k) {
+  let i = k
+  while (i > 0) {
+    nums.unshift(nums.pop())
+    i--
+  }
 };
