b057fec07025c042839e3e9b7854bdeab0464632
Nothing <wwwaap@gmail.com>
2019-10-17 21:25:04 +0800

    Create 189-rotate-array.js

---
+++ 189-rotate-array.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function(nums, k) {
+  nums.unshift(...nums.splice(nums.length - k));
+};
