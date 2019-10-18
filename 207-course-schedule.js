b0bc5c09821fea85efd91704ba8aa3e3ac6d73be
Nothing <wwwaap@gmail.com>
2019-10-18 15:47:09 +0800

    Create 207-course-schedule.js

---
+++ 207-course-schedule.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+const canFinish = function(numCourses, prerequisites) {
+  const seen = new Set()
+  const seeing = new Set()
+  const adj = [...Array(numCourses)].map(r => [])
+  for (let [u, v] of prerequisites) {
+    adj[v].push(u)
+  }
+  for (let c = 0; c < numCourses; c++) {
+    if (!dfs(c)) {
+      return false
+    }
+  }
+  return true
+  function dfs(v) {
+    if (seen.has(v)) return true
+    if (seeing.has(v)) return false
+    seeing.add(v)
+    for (let nv of adj[v]) {
+      if (!dfs(nv)) {
+        return false
+      }
+    }
+    seeing.delete(v)
+    seen.add(v)
+    return true
+  }
+}
