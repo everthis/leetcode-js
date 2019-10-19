1f687d0b67603d23734a42d56496520de72cb328
Nothing <wwwaap@gmail.com>
2019-10-19 17:38:51 +0800

    Update 208-implement-trie-prefix-tree.js

--- 208-implement-trie-prefix-tree.js
+++ 208-implement-trie-prefix-tree.js
@@ -51,3 +51,37 @@ Trie.prototype.traverse = function(word) {
   }
   return curr
 }
+
+// another
+
+class Trie {
+  constructor() {
+    this.links = new Map();
+    this.isWord = false;
+  }
+  insert(word) {
+    let node = this;
+    for (const c of word) {
+      if (!node.links.has(c)) node.links.set(c, new Trie());
+      node = node.links.get(c);
+    }
+    node.isWord = true;
+  }
+  search(word) {
+    const node = this.traverse(word);
+    return node ? node.isWord : false;
+  }
+  startsWith(prefix) {
+    const node = this.traverse(prefix);
+    return node !== null;
+  }
+  traverse(word) {
+    let node = this;
+    for (const c of word) {
+      if (node.links.has(c)) node = node.links.get(c);
+      else return null;
+    }
+    return node;
+  }
+}
+
