982ae69bc0a4cdd7f6b8a177e51437764d11b5ec
Nothing <wwwaap@gmail.com>
2019-10-21 12:17:37 +0800

    Update 220-contains-duplicate-iii.js

--- 220-contains-duplicate-iii.js
+++ 220-contains-duplicate-iii.js
@@ -4,6 +4,38 @@
  * @param {number} t
  * @return {boolean}
  */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+  if (k < 1 || t < 0) {
+    return false
+  }
+  const array = new Map()
+  const num = 10 ** 10
+  for (let i = 0, iL = nums.length; i < iL; ++i) {
+    const noNegative = nums[i] + num
+    const factor = Math.floor(noNegative / (t + 1))
+    if (
+      array.has(factor) ||
+      (array.has(factor - 1) && noNegative - array.get(factor - 1) <= t) ||
+      (array.has(factor + 1) && array.get(factor + 1) - noNegative <= t)
+    ) {
+      return true
+    }
+    if (array.size >= k) {
+      array.delete(Math.floor((nums[i - k] + num) / (t + 1)))
+    }
+    array.set(factor, noNegative)
+  }
+  return false
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
 const containsNearbyAlmostDuplicate = function(nums, k, t) {
   const map = nums
     .map((val, idx) => ({ val, idx }))
