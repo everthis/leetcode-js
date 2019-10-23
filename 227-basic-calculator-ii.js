63df4eadeb9c37de8818e79aa96c097669f97b08
Nothing <wwwaap@gmail.com>
2019-10-23 15:13:35 +0800

    Update 227-basic-calculator-ii.js

--- 227-basic-calculator-ii.js
+++ 227-basic-calculator-ii.js
@@ -3,29 +3,24 @@
  * @return {number}
  */
 const calculate = function(s) {
-  if (s.length === 0) {
-    return 0
-  }
+  if (!s || !s.length) return 0
+  let n = 0
   let op = '+'
-  let stack = []
-  for (let i = 0, n = 0; i <= s.length; ++i) {
+  const stack = []
+  for (let i = 0; i < s.length; i++) {
     let c = s.charAt(i)
-    if (c === ' ') continue
-    if (c >= '0' && c <= '9') {
-      n = n * 10 + parseInt(c)
-      continue
+    let isNumber = '0' <= c && c <= '9'
+    if (isNumber) {
+      n = n * 10 + +c
     }
-    if (op === '+') {
-      stack.push(n)
-    } else if (op === '-') {
-      stack.push(-n)
-    } else if (op === '*') {
-      stack.push(stack.pop() * n)
-    } else if (op === '/') {
-      stack.push(Math.trunc(stack.pop() / n))
+    if ((!isNumber && c !== ' ') || i == s.length - 1) {
+      if (op === '+') stack.push(n)
+      else if (op === '-') stack.push(-n)
+      else if (op === '*') stack.push(stack.pop() * n)
+      else if (op === '/') stack.push(~~(stack.pop() / n))
+      op = c
+      n = 0
     }
-    op = c
-    n = 0
   }
-  return stack.reduce((n, acc) => n + acc, 0)
+  return stack.reduce((a, b) => a + b, 0)
 }
