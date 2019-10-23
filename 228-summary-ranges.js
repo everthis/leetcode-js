62e97800f8da5fa0b8f01c672447b4637aed849e
Nothing <wwwaap@gmail.com>
2019-10-23 16:11:55 +0800

    Update 228-summary-ranges.js

--- 228-summary-ranges.js
+++ 228-summary-ranges.js
@@ -33,3 +33,25 @@ function insert(arr, start, end) {
     arr.push(`${start}->${end}`)
   }
 }
+
+// another
+
+const summaryRanges = nums => {
+  if (!nums || nums.length === 0) {
+    return [];
+  }
+  const returnArray = [];
+  let tempIdx = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + 1 !== nums[i + 1]) {
+      if (tempIdx === i) {
+        returnArray.push(nums[tempIdx].toString());
+      } else {
+        returnArray.push(nums[tempIdx].toString() + "->" + nums[i].toString());
+      }
+      tempIdx = i + 1;
+    }
+  }
+
+  return returnArray;
+};
