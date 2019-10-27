3e0f5fb793fce9799f3954114a808f122162c517
Nothing <wwwaap@gmail.com>
2019-10-27 16:46:10 +0800

    Update 316-remove-duplicate-letters.js

--- 316-remove-duplicate-letters.js
+++ 316-remove-duplicate-letters.js
@@ -3,6 +3,31 @@
  * @return {string}
  */
 const removeDuplicateLetters = function(s) {
+  const last = {}
+  for (let i = 0; i < s.length; i++) last[s.charAt(i)] = i
+  const added = {}
+  const stack = []
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i)
+    if (added[char]) continue
+    while (stack.length && char < stack[0] && last[stack[0]] > i) {
+      added[stack[0]] = false
+      stack.shift()
+    }
+    stack.unshift(char)
+    added[char] = true
+  }
+  return stack.reverse().join('')
+}
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const removeDuplicateLetters = function(s) {
   const m = new Array(26)
   const a = 'a'.charCodeAt(0)
   for (let i = 0; i < s.length; i++) {
