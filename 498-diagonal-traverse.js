4fe11790412ec9adf1ce16dd7cb27674b97e1294
Nothing <wwwaap@gmail.com>
2019-10-28 21:33:23 +0800

    Update 498-diagonal-traverse.js

--- 498-diagonal-traverse.js
+++ 498-diagonal-traverse.js
@@ -36,3 +36,46 @@ function isValid(i, j, m, n) {
   }
   return true
 }
+
+// another
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+const findDiagonalOrder = function(matrix) {
+  if (matrix.length == 0) return []
+  let r = 0,
+    c = 0,
+    m = matrix.length,
+    n = matrix[0].length,
+    arr = new Array(m * n)
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = matrix[r][c]
+    if ((r + c) % 2 === 0) {
+      // moving up
+      if (c === n - 1) {
+        r++
+      } else if (r === 0) {
+        c++
+      } else {
+        r--
+        c++
+      }
+    } else {
+      // moving down
+      if (r === m - 1) {
+        c++
+      } else if (c === 0) {
+        r++
+      } else {
+        r++
+        c--
+      }
+    }
+  }
+  return arr
+}
+
+
+
