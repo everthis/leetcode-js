16540ccc36539b9c6fe418bccc63a6eed3d988d7
Nothing <wwwaap@gmail.com>
2019-10-30 10:26:17 +0800

    Update 289-game-of-life.js

--- 289-game-of-life.js
+++ 289-game-of-life.js
@@ -1,57 +1,56 @@
-const isValid = (arr, row, col) => {
-  const isRow = row >= 0 && row < arr.length;
-  const isCol = isRow && col >= 0 && col < arr[row].length;
-  return isRow && isCol;
-};
-
-const moves = [
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, -1],
-  [0, 1],
-  [1, 1],
-  [1, 0],
-  [1, -1]
-];
-
-const num = (arr, row, col, val) => {
-  let count = 0;
-  moves.forEach(move => {
-    const r = row + move[0];
-    const c = col + move[1];
-    if (isValid(arr, r, c) && arr[r][c] === val) {
-      count++;
-    }
-  });
-  return count;
-};
-
 /**
  * @param {number[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
 const gameOfLife = function(board) {
-  const newArr = Array(board.length)
-    .fill(null)
-    .map(() => Array(board[0].length).fill(0));
-  for (let row = 0; row < board.length; row++) {
-    for (let col = 0; col < board[row].length; col++) {
-      const ones = num(board, row, col, 1);
+  const DIRECTIONS = [
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1]
+  ];
 
-      newArr[row][col] = board[row][col];
-      if (board[row][col] === 1) {
-        if (ones < 2 || ones > 3) {
-          newArr[row][col] = 0;
-        }
-      } else if (ones === 3) {
-        newArr[row][col] = 1;
+  const isValid = function(x, y) {
+    if (x >= 0 && y >= 0 && x < board.length && y < board[0].length)
+      return true;
+    else return false;
+  };
+
+  const getAliveNeighbors = function(x, y) {
+    let aliveNeighs = 0;
+    for (let dir of DIRECTIONS) {
+      let newX = x + dir[0];
+      let newY = y + dir[1];
+      if (!isValid(newX, newY)) continue;
+      if (board[newX][newY] === 1 || board[newX][newY] === -1) {
+        aliveNeighs++;
+      }
+    }
+
+    return aliveNeighs;
+  };
+
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      let aliveNeighbors = getAliveNeighbors(row, col);
+      if (board[row][col] === 0) {
+        if (aliveNeighbors === 3) board[row][col] = 2;
+        else board[row][col] = 0;
+      } else {
+        if (aliveNeighbors === 2 || aliveNeighbors === 3) board[row][col] = 1;
+        else board[row][col] = -1;
       }
     }
   }
+
   for (let row = 0; row < board.length; row++) {
-    for (let col = 0; col < board[row].length; col++) {
-      board[row][col] = newArr[row][col];
+    for (let col = 0; col < board[0].length; col++) {
+      if (board[row][col] > 0) board[row][col] = 1;
+      else board[row][col] = 0;
     }
   }
 };
