62655d01d593eff4469ff753cc543484d58625b8
Nothing <wwwaap@gmail.com>
2019-11-06 16:08:06 +0800

    Update 452-minimum-number-of-arrows-to-burst-balloons.js

--- 452-minimum-number-of-arrows-to-burst-balloons.js
+++ 452-minimum-number-of-arrows-to-burst-balloons.js
@@ -16,3 +16,23 @@ const findMinArrowShots = function(points) {
   }
   return ans
 }
+
+// another
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+const findMinArrowShots = function(points) {
+  if(points == null || points.length === 0) return 0
+  points.sort((a, b) => a[1] - b[1])
+  let ans = 1
+  let lastX = points[0][1]
+  for (let i = 1; i < points.length; i++) {
+    if(points[i][0] <= lastX) continue
+    ans++
+    lastX = points[i][1]
+  }
+  return ans
+}
+
