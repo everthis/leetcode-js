2f4f3c16780d8cc350770f00bdfd8331e4a9cba1
Nothing <wwwaap@gmail.com>
2019-11-15 10:59:14 +0800

    Update 429-n-ary-tree-level-order-traversal.js

--- 429-n-ary-tree-level-order-traversal.js
+++ 429-n-ary-tree-level-order-traversal.js
@@ -12,6 +12,35 @@
 const levelOrder = function(root) {
   const res = []
   if(root == null) return res
+  helper(root, 0, res)
+  return res
+};
+
+function helper(node, index, res) {
+  if(node == null) return
+  if(res[index] == null) res[index] = []
+  res[index].push(node.val)
+  for(let i = 0, len = node.children.length; i < len; i++) {
+    helper(node.children[i], index + 1, res)
+  }
+}
+
+// another
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[][]}
+ */
+const levelOrder = function(root) {
+  const res = []
+  if(root == null) return res
   const q = []
   q.push(root)
   while(q.length) {
