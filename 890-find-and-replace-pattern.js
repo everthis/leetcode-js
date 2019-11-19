dbfcb14a0c7697512d151ecd9605911b9e080329
Nothing <wwwaap@gmail.com>
2019-11-19 11:36:44 +0800

    Update 890-find-and-replace-pattern.js

--- 890-find-and-replace-pattern.js
+++ 890-find-and-replace-pattern.js
@@ -25,3 +25,38 @@ const findAndReplacePattern = (words, pattern) => {
     return m.size === s.size;
   }
 };
+
+// anoother
+
+/**
+ * @param {string[]} words
+ * @param {string} pattern
+ * @return {string[]}
+ */
+const findAndReplacePattern = function(words, pattern) {
+  const p = helper(pattern);
+  const res = [];
+  for (let w of words) {
+    if (arrEqual(helper(w), p)) res.push(w);
+  }
+  return res;
+};
+
+function arrEqual(a, b) {
+  if (a.length !== b.length) return false;
+  for (let i = 0, len = a.length; i < len; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+function helper(w) {
+  const m = new Map();
+  const n = w.length;
+  const res = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    if (!m.has(w[i])) m.set(w[i], m.size);
+    res[i] = m.get(w[i]);
+  }
+  return res;
+}
