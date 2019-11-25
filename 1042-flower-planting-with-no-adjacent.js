b92dfa6182607b58f42658bc48f055c9fd47c7bd
Nothing <wwwaap@gmail.com>
2019-11-25 16:53:54 +0800

    Update 1042-flower-planting-with-no-adjacent.js

--- 1042-flower-planting-with-no-adjacent.js
+++ 1042-flower-planting-with-no-adjacent.js
@@ -19,15 +19,15 @@ const gardenNoAdj = function(N, paths) {
     let colors = new Array(4).fill(false);
     for (let neighbor of map[i]) {
       if (result[neighbor] !== -1) {
-        colors[result[neighbor] - 1] = true;
+        colors[result[neighbor]] = true;
       }
     }
     for (let j = 0; j < colors.length; j++) {
       if (!colors[j]) {
-        result[i] = j + 1;
+        result[i] = j;
         break;
       }
     }
   }
-  return result;
+  return result.map(i => ++i);
 };
