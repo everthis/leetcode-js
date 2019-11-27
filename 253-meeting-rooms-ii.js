1fd18e555cdcd45b16f81ecfda479e4319e3a858
Nothing <wwwaap@gmail.com>
2019-11-27 13:30:30 +0800

    Update 253-meeting-rooms-ii.js

--- 253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii.js
@@ -1,4 +1,21 @@
 /**
+
+Given an array of meeting time intervals consisting of
+start and end times [[s1,e1],[s2,e2],...] (si < ei),
+find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+ 
+ */
+
+/**
  * @param {number[][]} intervals
  * @return {number}
  */
