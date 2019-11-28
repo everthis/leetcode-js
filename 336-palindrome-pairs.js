3b77f63e7a0d175b02919c40242703c71bd200a5
Nothing <wwwaap@gmail.com>
2019-11-28 10:54:45 +0800

    Update 336-palindrome-pairs.js

--- 336-palindrome-pairs.js
+++ 336-palindrome-pairs.js
@@ -1,3 +1,21 @@
+/**
+
+Given a list of unique words, find all pairs of distinct indices (i, j) in the given list,
+so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+
+Example 1:
+
+Input: ["abcd","dcba","lls","s","sssll"]
+Output: [[0,1],[1,0],[3,2],[2,4]] 
+Explanation: The palindromes are ["dcbaabcd","abcddcba","slls","llssssll"]
+Example 2:
+
+Input: ["bat","tab","cat"]
+Output: [[0,1],[1,0]] 
+Explanation: The palindromes are ["battab","tabbat"]
+
+*/
+
 const reverseStr = s => {
   let str = ''
   for (let i = 0; i < s.length; i++) {
