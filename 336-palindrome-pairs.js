692d23b7dcf6a9acffd409f3cad50e0b1fd84b6a
Nothing <wwwaap@gmail.com>
2019-11-29 10:47:05 +0800

    Update 336-palindrome-pairs.js

--- 336-palindrome-pairs.js
+++ 336-palindrome-pairs.js
@@ -16,6 +16,65 @@ Explanation: The palindromes are ["battab","tabbat"]
 
 */
 
+/**
+ * @param {string[]} words
+ * @return {number[][]}
+ */
+const palindromePairs = function(words) {
+  const root = new Trie();
+  const pairs = [];
+  words.forEach((word, index) => addWord(word, index, root));
+  words.forEach((word, index) => searchWord(word, index, root, pairs));
+  return pairs;
+};
+
+const addWord = (word, wordIndex, root) => { 
+  const length = word.length;
+  let curr = root;
+  for (let i = length - 1; i >= 0; i--) {
+    let char = word.charAt(i);
+    if (!curr.children[char]) curr.children[char] = new Trie();
+    if (isPalindrome(0, i, word)) curr.words.push(wordIndex);
+    curr = curr.children[char];
+  }
+  curr.wordIndex = wordIndex;
+  curr.words.push(wordIndex);
+}
+
+const searchWord = (word, wordIndex, root, pairs) => {
+  const length = word.length;
+  let curr = root;
+  for (let i = 0; i < length; i++) {
+    let char = word.charAt(i);
+    if (curr.wordIndex >= 0 && curr.wordIndex !== wordIndex && isPalindrome(i, length - 1, word)) {
+      pairs.push([wordIndex, curr.wordIndex]);
+    }
+    curr = curr.children[char];
+    if (!curr) return;
+  }
+  
+  curr.words.forEach((suffix) => {
+    if (suffix !== wordIndex) pairs.push([wordIndex, suffix]);
+  })
+}
+
+const isPalindrome = (left, right, word) => {
+  while (left < right) {
+    if (word.charAt(left++) !== word.charAt(right--)) return false;
+  }
+  return true;
+}
+
+class Trie {
+  constructor() {
+    this.wordIndex = -1;
+    this.children = {};
+    this.words = [];
+  }
+}
+
+// another
+
 const reverseStr = s => {
   let str = ''
   for (let i = 0; i < s.length; i++) {
