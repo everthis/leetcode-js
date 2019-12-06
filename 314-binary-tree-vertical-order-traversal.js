cc94ac5e652be54c4b070ea2977f44fc453da0a7
Nothing <wwwaap@gmail.com>
2019-12-06 13:50:33 +0800

    Update 314-binary-tree-vertical-order-traversal.js

--- 314-binary-tree-vertical-order-traversal.js
+++ 314-binary-tree-vertical-order-traversal.js
@@ -120,3 +120,25 @@ const verticalOrder = function(root) {
   }
   return res
 };
+
+// another
+
+const verticalOrder = function(root) {
+  if (!root) return []
+  let result = []
+  function recurse(root, col, row) {
+    if (!root) return
+    recurse(root.left, col - 1, row + 1)
+    recurse(root.right, col + 1, row + 1)
+    result[col] = result[col] || []
+    result[col][row] = result[col][row] || []
+    result[col][row].push(root.val)
+  }
+
+  recurse(root, 100, 0)
+  return result
+    .filter(x => x)
+    .map(row => row.reduce((acc, val) => acc.concat(val), []))
+}
+
+
