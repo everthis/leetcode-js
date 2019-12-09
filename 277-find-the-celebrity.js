62abef22e2d306c82281d8a1a9e2113d6bbbef5a
Nothing <wwwaap@gmail.com>
2019-12-09 12:29:37 +0800

    Update 277-find-the-celebrity.js

--- 277-find-the-celebrity.js
+++ 277-find-the-celebrity.js
@@ -39,3 +39,41 @@ const solution = function(knows) {
     return celebrity
   }
 }
+
+// another
+
+/**
+ * Definition for knows()
+ *
+ * @param {integer} person a
+ * @param {integer} person b
+ * @return {boolean} whether a knows b
+ * knows = function(a, b) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} knows()
+ * @return {function}
+ */
+const solution = function(knows) {
+  /**
+   * @param {integer} n Total people
+   * @return {integer} The celebrity
+   */
+  return function(n) {
+    if (n < 1) return -1
+    let candidate = 0
+    for (let i = 1; i < n; i++) {
+      if (knows(candidate, i)) candidate = i
+    }
+    for (let i = 0; i < n; i++) {
+      if (i < candidate && (knows(candidate, i) || !knows(i, candidate)))
+        return -1
+      if (i > candidate && !knows(i, candidate)) return -1
+    }
+    return candidate
+  }
+}
+
