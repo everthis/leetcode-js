745dd1e0b776e9901d00bdba26e923c78cfc3cd7
Nothing <wwwaap@gmail.com>
2019-12-11 12:05:25 +0800

    Update 286-walls-and-gates.js

--- 286-walls-and-gates.js
+++ 286-walls-and-gates.js
@@ -55,3 +55,44 @@ const wallsAndGates = function(rooms) {
   }
 };
 
+// another
+
+/**
+ * @param {number[][]} rooms
+ * @return {void} Do not return anything, modify rooms in-place instead.
+ */
+const wallsAndGates = function(rooms) {
+  const dirs = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+  ];
+  const rows = rooms.length;
+  const cols = rows === 0 ? 0 : rooms[0].length;
+  const q = [];
+  const INF = 2147483647;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (rooms[i][j] === 0) dfs(dirs, rooms, i, j, rows, cols, 0);
+    }
+  }
+};
+
+function dfs(dirs, rooms, i, j, rows, cols, dis) {
+  if (
+    i < 0 ||
+    j < 0 ||
+    i >= rows ||
+    j >= cols ||
+    rooms[i][j] === -1 ||
+    rooms[i][j] < dis
+  ) {
+    return;
+  }
+  rooms[i][j] = dis;
+  for (let dir of dirs) {
+    dfs(dirs, rooms, i + dir[0], j + dir[1], rows, cols, dis + 1);
+  }
+}
+
