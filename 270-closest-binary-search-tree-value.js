db413f6d6c3262579fcd6b1ad6770857de6ba556
Nothing <wwwaap@gmail.com>
2019-12-12 14:12:00 +0800

    Create 270-closest-binary-search-tree-value.js

---
+++ 270-closest-binary-search-tree-value.js
@@ -0,0 +1,61 @@
+/**
+
+Given a non-empty binary search tree and a target value,
+find the value in the BST that is closest to the target.
+
+Note:
+
+Given target value is a floating point.
+You are guaranteed to have only one unique value
+in the BST that is closest to the target.
+
+Example:
+
+Input: root = [4,2,5,1,3], target = 3.714286
+
+    4
+   / \
+  2   5
+ / \
+1   3
+
+Output: 4
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+const closestValue = function(root, target) {
+  if(root == null) return -1
+  let node = root
+  const stack = []
+  const res = []
+  const K = 1
+  while(node || stack.length) {
+    if(node) {
+      stack.push(node)
+      node = node.left
+    } else {
+      node = stack.pop()
+      if(res.length === K) {
+        if(Math.abs(res[0] - target) < Math.abs(node.val - target)) {
+          return res[0]   
+        }
+        res.shift()
+      }
+      res.push(node.val)
+      node = node.right
+    }
+  }
+  return res[0]
+};
