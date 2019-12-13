2818931e2a012b762197905292276ff49f6b28b0
Nothing <wwwaap@gmail.com>
2019-12-13 10:03:51 +0800

    Update 333-largest-bst-subtree.js

--- 333-largest-bst-subtree.js
+++ 333-largest-bst-subtree.js
@@ -53,3 +53,22 @@ function helper(node) {
     return [-Number.MAX_VALUE, Number.MAX_VALUE, Math.max(left[2], right[2])]
   }
 }
+
+// another
+
+const largestBSTSubtree = function(root) {
+  function dfs(node) {
+    if (!node) return [0, 0, Number.MAX_VALUE, -Number.MAX_VALUE]
+    const [N1, n1, min1, max1] = dfs(node.left)
+    const [N2, n2, min2, max2] = dfs(node.right)
+    const n =
+      max1 < node.val && min2 > node.val ? n1 + 1 + n2 : -Number.MAX_VALUE
+    return [
+      Math.max(N1, N2, n),
+      n,
+      Math.min(min1, node.val),
+      Math.max(max2, node.val)
+    ]
+  }
+  return dfs(root)[0]
+}
