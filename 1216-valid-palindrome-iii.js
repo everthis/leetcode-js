8c55b37cdc77cbeb598ddb511afe4dbe4a531be2
Nothing <wwwaap@gmail.com>
2019-12-20 12:45:36 +0800

    Update 1216-valid-palindrome-iii.js

--- 1216-valid-palindrome-iii.js
+++ 1216-valid-palindrome-iii.js
@@ -72,3 +72,18 @@ function chk(s, l, r, cache) {
   return num
 }
 
+// another
+
+const isValidPalindrome = function(s, k) {
+  const len = s.length
+  const dp = Array.from({ length: len }, () => new Array(len).fill(0))
+  for(let i = len - 1; i >= 0; i--) {
+    dp[i][i] = 1
+    for(let j = i + 1; j < len; j++) {
+      if(s[i] === s[j]) dp[i][j] = dp[i + 1][j - 1] + 2
+      else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
+    }
+  }
+  return len <= dp[0][len - 1] + k
+};
+
