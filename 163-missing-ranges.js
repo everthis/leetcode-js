9b79a8645dabce3889804a7a8752f42733e47bf0
Nothing <wwwaap@gmail.com>
2020-01-04 14:09:08 +0800

    Update 163-missing-ranges.js

--- 163-missing-ranges.js
+++ 163-missing-ranges.js
@@ -5,6 +5,27 @@
  * @return {string[]}
  */
 const findMissingRanges = function(nums, lower, upper) {
+  const list = []
+  for (let n of nums) {
+    let justBelow = n - 1
+    if (lower === justBelow) list.push(lower + '')
+    else if (lower < justBelow) list.push(lower + '->' + justBelow)
+    lower = n + 1
+  }
+  if (lower === upper) list.push(lower + '')
+  else if (lower < upper) list.push(lower + '->' + upper)
+  return list
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {string[]}
+ */
+const findMissingRanges = function(nums, lower, upper) {
   const res = [];
   let next = lower;
   for (let i = 0; i < nums.length; i++) {
