7cbb085666a33c0de43c8fee16dd661eda1009a3
Nothing <wwwaap@gmail.com>
2020-01-08 09:58:34 +0800

    Update 251-flatten-2d-vector.js

--- 251-flatten-2d-vector.js
+++ 251-flatten-2d-vector.js
@@ -27,3 +27,38 @@ Vector2D.prototype.hasNext = function() {
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
+
+// another
+
+/**
+ * @param {number[][]} v
+ */
+const Vector2D = function(v) {
+  this.iterator = v[Symbol.iterator]()
+  this.row = this.iterator.next()
+  this.idx = 0
+}
+/**
+ * @return {number}
+ */
+Vector2D.prototype.next = function() {
+  if (this.hasNext()) {
+    return this.row.value[this.idx++]
+  }
+}
+/**
+ * @return {boolean}
+ */
+Vector2D.prototype.hasNext = function() {
+  while (this.row.done == false && this.idx == this.row.value.length) {
+    this.row = this.iterator.next()
+    this.idx = 0
+  }
+  return this.row.done == false
+}
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * var obj = new Vector2D(v)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
