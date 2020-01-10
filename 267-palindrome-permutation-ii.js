03ea010b48fcd09a25849d031580d543c4370aaa
Nothing <wwwaap@gmail.com>
2020-01-11 00:27:55 +0800

    Update 267-palindrome-permutation-ii.js

--- 267-palindrome-permutation-ii.js
+++ 267-palindrome-permutation-ii.js
@@ -1,4 +1,20 @@
 /**
+
+Given a string s, return all the palindromic permutations (without duplicates) of it.
+Return an empty list if no palindromic permutation could be form.
+
+Example 1:
+
+Input: "aabb"
+Output: ["abba", "baab"]
+Example 2:
+
+Input: "abc"
+Output: []
+
+*/
+
+/**
  * @param {string} s
  * @return {string[]}
  */
