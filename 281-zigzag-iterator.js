169387b09d38c30e78523295b595c4d9f6cebe53
Nothing <wwwaap@gmail.com>
2020-01-22 11:44:41 +0800

    Update 281-zigzag-iterator.js

--- 281-zigzag-iterator.js
+++ 281-zigzag-iterator.js
@@ -96,3 +96,47 @@ ZigzagIterator.prototype.next = function next() {
  * while (i.hasNext()) a.push(i.next());
  */
 
+// another
+
+/**
+ * @constructor
+ * @param {Integer[]} v1
+ * @param {Integer[]} v1
+ */
+const ZigzagIterator = function ZigzagIterator(v1, v2) {
+  this.queue = []
+  if (v1.length > 0) {
+    const it = v1[Symbol.iterator]()
+    const res = it.next()
+    this.queue.push({ it, res })
+  }
+  if (v2.length > 0) {
+    const it = v2[Symbol.iterator]()
+    const res = it.next()
+    this.queue.push({ it, res })
+  }
+}
+/**
+ * @this ZigzagIterator
+ * @returns {boolean}
+ */
+ZigzagIterator.prototype.hasNext = function hasNext() {
+  return this.queue.length > 0
+}
+/**
+ * @this ZigzagIterator
+ * @returns {integer}
+ */
+ZigzagIterator.prototype.next = function next() {
+  const { it, res } = this.queue.shift()
+  const { value } = res
+  const res1 = it.next()
+  if (!res1.done) this.queue.push({ it, res: res1 })
+  return value
+}
+/**
+ * Your ZigzagIterator will be called like this:
+ * var i = new ZigzagIterator(v1, v2), a = [];
+ * while (i.hasNext()) a.push(i.next());
+ */
+
