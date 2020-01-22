88e5a436488e99ea8e1fc1f33b7f37271a4c0b9e
Nothing <wwwaap@gmail.com>
2020-01-22 14:21:24 +0800

    Update 302-smallest-rectangle-enclosing-black-pixels.js

--- 302-smallest-rectangle-enclosing-black-pixels.js
+++ 302-smallest-rectangle-enclosing-black-pixels.js
@@ -33,3 +33,42 @@ const minArea = function(image, x, y) {
     return i;
   }
 };
+
+// another
+
+/**
+ * @param {character[][]} image
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const minArea = function(image, x, y) {
+  let top = x,
+    bottom = x
+  let left = y,
+    right = y
+  function dfs(x, y) {
+    if (
+      x < 0 ||
+      y < 0 ||
+      x >= image.length ||
+      y >= image[0].length ||
+      image[x][y] === '0'
+    )
+      return
+    image[x][y] = '0'
+    top = Math.min(top, x)
+    bottom = Math.max(bottom, x + 1)
+    left = Math.min(left, y)
+    right = Math.max(right, y + 1)
+    dfs(x + 1, y)
+    dfs(x - 1, y)
+    dfs(x, y - 1)
+    dfs(x, y + 1)
+  }
+
+  if (image.length == 0 || image[0].length == 0) return 0
+  dfs(x, y)
+  return (right - left) * (bottom - top)
+}
+
