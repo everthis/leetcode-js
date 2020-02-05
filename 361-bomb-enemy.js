dd38ca665f73fe846710527e51304d495ea71dc5
Nothing <wwwaap@gmail.com>
2020-02-05 16:47:16 +0800

    Update 361-bomb-enemy.js

--- 361-bomb-enemy.js
+++ 361-bomb-enemy.js
@@ -25,3 +25,66 @@ const maxKilledEnemies = function(grid) {
   }
   return result
 }
+
+// another
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const maxKilledEnemies = function(grid) {
+  if (grid == null || grid.length === 0 || grid[0].length === 0) return 0
+  const rows = grid.length
+  const cols = grid[0].length
+  let max = 0
+  const dp = Array.from({ length: rows }, () => new Array(cols).fill(0))
+  //travel each column twice: from left and from right
+  for (let i = 0; i < rows; i++) {
+    let cnt = 0
+    for (let k = 0; k < cols; k++) {
+      if (grid[i][k] === '0') {
+        dp[i][k] = cnt
+      } else if (grid[i][k] === 'E') {
+        cnt++
+      } else {
+        cnt = 0
+      }
+    }
+    cnt = 0
+    for (let k = cols - 1; k >= 0; k--) {
+      if (grid[i][k] === '0') {
+        dp[i][k] += cnt
+      } else if (grid[i][k] === 'E') {
+        cnt++
+      } else {
+        cnt = 0
+      }
+    }
+  }
+  //travel each row twice: from top and from bottom
+  for (let i = 0; i < cols; i++) {
+    let cnt = 0
+    for (let k = 0; k < rows; k++) {
+      if (grid[k][i] === '0') {
+        dp[k][i] += cnt
+      } else if (grid[k][i] === 'E') {
+        cnt++
+      } else {
+        cnt = 0
+      }
+    }
+    cnt = 0
+    for (let k = rows - 1; k >= 0; k--) {
+      if (grid[k][i] === '0') {
+        dp[k][i] += cnt
+        max = Math.max(max, dp[k][i])
+      } else if (grid[k][i] === 'E') {
+        cnt++
+      } else {
+        cnt = 0
+      }
+    }
+  }
+  return max
+}
+
