a4c67bd32090801a572276548cdc3adcde28141b
Nothing <wwwaap@gmail.com>
2020-02-05 19:29:08 +0800

    Update 361-bomb-enemy.js

--- 361-bomb-enemy.js
+++ 361-bomb-enemy.js
@@ -1,4 +1,29 @@
 /**
+
+Given a 2D grid, each cell is either a wall 'W',
+an enemy 'E' or empty '0' (the number zero),
+return the maximum enemies you can kill using one bomb.
+The bomb kills all the enemies in the same row and column
+from the planted point until it hits the wall since the wall
+is too strong to be destroyed.
+
+Note: You can only put the bomb at an empty cell.
+
+Example:
+
+Input: [["0","E","0","0"],["E","0","W","E"],["0","E","0","0"]]
+Output: 3 
+Explanation: For the given grid,
+
+0 E 0 0 
+E 0 W E 
+0 E 0 0
+
+Placing a bomb at (1,1) kills 3 enemies.
+
+*/
+
+/**
  * @param {character[][]} grid
  * @return {number}
  */
