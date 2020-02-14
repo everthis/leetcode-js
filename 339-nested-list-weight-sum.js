807b4098a8a27bb2d9a30e86c5e0b3e3dfa683b6
Nothing <wwwaap@gmail.com>
2020-02-14 15:51:29 +0800

    Update 339-nested-list-weight-sum.js

--- 339-nested-list-weight-sum.js
+++ 339-nested-list-weight-sum.js
@@ -55,3 +55,22 @@ function h(arr, level) {
   }
   return sum
 }
+
+// another
+
+const depthSum = function(nestedList) {
+  if(nestedList == null) return 0
+  let sum = 0
+  let level = 1
+  const q = [...nestedList]
+  while(q.length) {
+    const len = q.length
+    for(let i = 0; i < len; i++) {
+      const el = q.shift()
+      if(el.isInteger()) sum += el.getInteger() * level
+      else q.push(...(el.getList()))
+    }
+    level++
+  }
+  return sum
+};
