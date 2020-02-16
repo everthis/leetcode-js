8c3f028dbcb7858376e67d05e3110aca2473aaba
Nothing <wwwaap@gmail.com>
2020-02-16 22:16:23 +0800

    Update 320-generalized-abbreviation.js

--- 320-generalized-abbreviation.js
+++ 320-generalized-abbreviation.js
@@ -17,16 +17,16 @@ Output:
  */
 const generateAbbreviations = function(word) {
   const arr = []
-  bt(arr, word, 0, '', 0)
+  dfs(arr, word, 0, '', 0)
   return arr
 };
 
-function bt(res, word, pos, cur, cnt) {
+function dfs(res, word, pos, cur, cnt) {
   if(pos === word.length) {
     if(cnt > 0) cur += cnt
     res.push(cur)
   } else {
-    bt(res, word, pos + 1, cur, cnt + 1)
-    bt(res, word, pos + 1, cur + (cnt > 0 ? cnt : '') + word.charAt(pos), 0)
+    dfs(res, word, pos + 1, cur, cnt + 1)
+    dfs(res, word, pos + 1, cur + (cnt > 0 ? cnt : '') + word.charAt(pos), 0)
   }
 }
