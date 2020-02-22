70fa39223782bcd8433c9a307d98e5f49938c96a
Nothing <wwwaap@gmail.com>
2020-02-22 21:57:37 +0800

    Update 499-the-maze-iii.js

--- 499-the-maze-iii.js
+++ 499-the-maze-iii.js
@@ -62,6 +62,59 @@ The maze contains at least 2 empty spaces, and the width and the height of the m
 
 */
 
+/**
+ * @param {number[][]} maze
+ * @param {number[]} ball
+ * @param {number[]} hole
+ * @return {string}
+ */
+function findShortestWay(maze, ball, hole) {
+  const H = maze.length
+  const W = maze[0].length
+  const costs = [...Array(H)].map(r => Array(W).fill(Number.MAX_VALUE))
+  let minRoute = ''
+  dfs(ball[0], ball[1], 0, '')
+  return minRoute || 'impossible'
+
+  function dfs(r, c, cost, route) {
+    if (cost >= costs[r][c]) return
+    costs[r][c] = cost
+    for (let [dr, dc, d] of [
+      [1, 0, 'd'],
+      [0, -1, 'l'],
+      [0, 1, 'r'],
+      [-1, 0, 'u']
+    ]) {
+      let rr = r
+      let cc = c
+      let steps = 0
+      for (
+        ;
+        rr + dr >= 0 &&
+        rr + dr < H &&
+        cc + dc >= 0 &&
+        cc + dc < W &&
+        maze[rr + dr][cc + dc] !== 1;
+
+      ) {
+        rr += dr
+        cc += dc
+        steps++
+        if (rr === hole[0] && cc === hole[1]) {
+          if (cost + steps < costs[hole[0]][hole[1]]) {
+            costs[hole[0]][hole[1]] = cost + steps
+            minRoute = route + d
+          }
+          return
+        }
+      }
+      dfs(rr, cc, cost + steps, route + d)
+    }
+  }
+}
+
+// another
+
 const dirs = [
   [1, 0, 'd'],
   [0, -1, 'l'],
