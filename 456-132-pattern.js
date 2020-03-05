b9ed12fcfe5819606b9450c286c697fbfc562825
Nothing <wwwaap@gmail.com>
2020-03-05 11:17:41 +0800

    Update 456-132-pattern.js

--- 456-132-pattern.js
+++ 456-132-pattern.js
@@ -15,3 +15,23 @@ const find132pattern = function(nums) {
   }
   return false
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const find132pattern = function(nums) {
+  let idx = nums.length
+  let s3 = Number.NEGATIVE_INFINITY
+  for(let len = nums.length, i = len - 1; i >= 0; i--) {
+    if(nums[i] < s3) return true
+    while(idx < nums.length && nums[i] > nums[idx]) {
+      s3 = nums[idx++]
+    }
+    nums[--idx] = nums[i] 
+  }
+  return false
+}
+
