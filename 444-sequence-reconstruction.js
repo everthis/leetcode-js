2a2f0e48fefbac1b33cf057ce3f3903fb12eb7be
Nothing <wwwaap@gmail.com>
2020-03-09 13:35:48 +0800

    Update 444-sequence-reconstruction.js

--- 444-sequence-reconstruction.js
+++ 444-sequence-reconstruction.js
@@ -21,3 +21,45 @@ const sequenceReconstruction = function(org, seqs) {
 
   return true
 }
+
+// another
+
+/**
+ * @param {number[]} org
+ * @param {number[][]} seqs
+ * @return {boolean}
+ */
+const sequenceReconstruction = function(org, seqs) {
+  const graph = new Map()
+  const indegree = new Map()
+  seqs.forEach(seq => {
+    for (let i = 0; i < seq.length; i++) {
+      if (!graph.has(seq[i])) graph.set(seq[i], [])
+      if (!indegree.has(seq[i])) indegree.set(seq[i], 0)
+      if (i > 0) {
+        graph.get(seq[i - 1]).push(seq[i])
+        indegree.set(seq[i], indegree.get(seq[i]) + 1)
+      }
+    }
+  })
+  if (org.length !== graph.size) return false
+  const array = []
+  for (let [key, val] of indegree.entries()) {
+    if (val === 0) array.push(key)
+  }
+  let index = 0
+  while (array.length > 0) {
+    if (array.length > 1) return false
+    const current = array.shift()
+    if (org[index] !== current) {
+      return false
+    }
+    index++
+    graph.get(current).forEach(next => {
+      indegree.set(next, indegree.get(next) - 1)
+      if (indegree.get(next) === 0) array.push(next)
+    })
+  }
+  return index === org.length
+}
+
