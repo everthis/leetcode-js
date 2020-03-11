551157c33f880a4f36ecd092f39eff8d23e5f68d
Nothing <wwwaap@gmail.com>
2020-03-11 21:52:04 +0800

    Update 436-find-right-interval.js

--- 436-find-right-interval.js
+++ 436-find-right-interval.js
@@ -3,34 +3,27 @@
  * @return {number[]}
  */
 const findRightInterval = function(intervals) {
-  const len = intervals.length
-  if (len === 0) return []
-  if (len === 1) return [-1]
-  intervals = intervals
-    .map((item, idx) => {
-      item.idx = idx
-      return item
-    })
-    .sort((a, b) => a.start - b.start)
-  const results = []
-  for (const item of intervals) {
-    results[item.idx] = binarySearch(item, intervals)
-  }
-  return results
-}
-
-function binarySearch(target, intervals) {
-  let left = 0
-  let right = intervals.length - 1
-  let solution = -1
-  while (left <= right) {
-    const mid = ((left + right) / 2) | 0
-    if (target.end > intervals[mid].start) {
-      left = mid + 1
+  const res = []
+  const arrary = intervals
+    .map((interval, idx) => ({ interval, idx }))
+    .sort((obj1, obj2) => obj1.interval[0] - obj2.interval[0])
+  for (let interval of intervals) {
+    const val = interval[interval.length - 1]
+    let left = 0,
+      right = intervals.length
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2)
+      if (arrary[mid].interval[0] >= val) {
+        right = mid
+      } else {
+        left = mid + 1
+      }
+    }
+    if (left >= intervals.length) {
+      res.push(-1)
     } else {
-      solution = intervals[mid].idx
-      right = mid - 1
+      res.push(arrary[left].idx)
     }
   }
-  return solution
+  return res
 }
