10e1b07dde43d89749e1aaf7c9996ce22dc08f07
Nothing <wwwaap@gmail.com>
2020-03-13 22:43:55 +0800

    Create 425-word-squares.js

---
+++ 425-word-squares.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} words
+ * @return {string[][]}
+ */
+const wordSquares = function(words) {
+  const length = words[0].length
+  const createPrefixMap = function(words) {
+    const result = new Map()
+    for (let word of words) {
+      for (let i = 0; i < word.length - 1; ++i) {
+        const prefix = word.substr(0, i + 1)
+        const array = result.get(prefix)
+        if (array) {
+          array.push(word)
+        } else {
+          result.set(prefix, [word])
+        }
+      }
+    }
+    return result
+  }
+  const backtracking = function(step, result, martix, wordsList) {
+    if (step === length) {
+      result.push([...martix])
+      return
+    }
+    const visited = []
+    for (let word of wordsList) {
+      if (visited.indexOf(word) == -1) {
+        martix.push(word)
+        let prefix = ''
+        for (let i = 0; i < step + 1; ++i) {
+          prefix += martix[i][step + 1]
+        }
+        let newLists = dictMap.get(prefix)
+        newLists = newLists ? newLists : []
+        backtracking(step + 1, result, martix, newLists)
+        martix.pop()
+        visited.push(word)
+      }
+    }
+  }
+  const result = []
+  const dictMap = createPrefixMap(words)
+  backtracking(0, result, [], words)
+  return result
+}
