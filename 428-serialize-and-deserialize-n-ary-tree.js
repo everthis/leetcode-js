69bd6b2c34d0d3f5b2fd39190cb15dfa52444a41
Nothing <wwwaap@gmail.com>
2020-03-13 15:19:00 +0800

    Update 428-serialize-and-deserialize-n-ary-tree.js

--- 428-serialize-and-deserialize-n-ary-tree.js
+++ 428-serialize-and-deserialize-n-ary-tree.js
@@ -58,3 +58,67 @@ class Codec {
  *    this.children = children;
  * };
  */
+
+// another
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+class Codec {
+  constructor() {}
+  /**
+   * @param {Node} root
+   * @return {string}
+   */
+  // Encodes a tree to a single string.
+  serialize = function(root) {
+    const ans = []
+    const stack = root ? [root] : []
+    while (stack.length) {
+      const cur = stack.pop()
+      ans.push(cur.val, cur.children.length)
+      for (let i = cur.children.length - 1; i >= 0; i--) {
+        stack.push(cur.children[i])
+      }
+    }
+    return ans.join(',')
+  }
+  /**
+   * @param {string} data
+   * @return {Node}
+   */
+  // Decodes your encoded data to tree.
+  deserialize = function(data) {
+    if (!data) return null
+    const arr = data.split(',')
+    const helper = (index = 0, parent) => {
+      const node = new Node(arr[index++], [])
+      parent.children.push(node)
+      let childCount = arr[index++]
+      while (childCount--) {
+        index = helper(index, node)
+      }
+      return index
+    }
+    const fakeRoot = new Node(null, [])
+    helper(0, fakeRoot)
+    return fakeRoot.children[0]
+  }
+}
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
