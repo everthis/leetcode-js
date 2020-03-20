040f09aa824e308c385b98cade290775e941b4bb
Nothing <wwwaap@gmail.com>
2020-03-20 21:15:33 +0800

    Update 531-lonely-pixel-i.js

--- 531-lonely-pixel-i.js
+++ 531-lonely-pixel-i.js
@@ -29,3 +29,32 @@ const findLonelyPixel = function(picture) {
   }
   return res
 };
+
+// another
+
+/**
+ * @param {character[][]} picture
+ * @return {number}
+ */
+const findLonelyPixel = function(picture) {
+  if(picture == null || picture[0].length === 0) return 0
+  const rows = picture.length
+  const cols = picture[0].length
+  const c = new Array(cols).fill(0)
+  for(let i = 0; i < rows; i++) {
+    for(let j = 0; j < cols; j++) {
+      if(picture[i][j] === 'B') c[j]++
+    }
+  }
+  let res = 0
+  for(let i = 0; i < rows; i++) {
+    let cnt = 0, pos = -1
+    for(let j = 0; j < cols; j++) {
+      if(picture[i][j] !== 'B') continue
+      if(++cnt === 1) pos = j
+      else break
+    }
+    if(cnt === 1 && c[pos] === 1) res++
+  }
+  return res
+};
