6fbe7439dc682b3c4e88e5dd3fbd0ca0689eb75c
Nothing <wwwaap@gmail.com>
2020-03-21 21:16:07 +0800

    Update 533-lonely-pixel-ii.js

--- 533-lonely-pixel-ii.js
+++ 533-lonely-pixel-ii.js
@@ -33,3 +33,49 @@ const findBlackPixel = function(picture, N) {
   }
   return ans
 }
+
+// another
+
+/**
+ * @param {character[][]} picture
+ * @param {number} N
+ * @return {number}
+ */
+const findBlackPixel = function(picture, N) {
+  const rows = picture.length
+  if(rows === 0) return 0
+  const cols = picture[0].length
+  if(cols === 0) return 0
+  const m = new Map()
+  const colCnt = new Array(cols).fill(0)
+  for(let i = 0; i < rows; i++) {
+    const k = scanRow(picture, i, cols, N, colCnt)
+    if(k) {
+      m.set(k, (m.get(k) || 0) + 1)
+    }
+  }
+  let res = 0
+  for(let [k, v] of m) {
+    if(v === N) {
+      for(let i = 0; i < cols; i++) {
+        if(k[i] === 'B' && colCnt[i] === N) res += N
+      }
+    }
+  }
+  return res
+}
+
+function scanRow(p, r, cols, N, arr) {
+  let str = ''
+  let cnt = 0
+  for(let i = 0; i < cols; i++) {
+    if(p[r][i] === 'B') {
+      cnt++
+      arr[i] += 1
+    }
+    str += p[r][i]
+  }
+  if(cnt === N) return str
+  return ''
+}
+
