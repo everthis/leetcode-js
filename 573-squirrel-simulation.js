fd76d898a88157e60defb7033c68552541085b35
Nothing <wwwaap@gmail.com>
2020-03-28 21:33:31 +0800

    Update 573-squirrel-simulation.js

--- 573-squirrel-simulation.js
+++ 573-squirrel-simulation.js
@@ -20,3 +20,27 @@ const minDistance = function(height, width, tree, squirrel, nuts) {
 function distance(p1, p2) {
   return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1])
 }
+
+// another
+
+/**
+ * @param {number} height
+ * @param {number} width
+ * @param {number[]} tree
+ * @param {number[]} squirrel
+ * @param {number[][]} nuts
+ * @return {number}
+ */
+const minDistance = function (height, width, tree, squirrel, nuts) {
+  let sum = 0,
+    maxDiff = -Number.MAX_VALUE
+  for (let nut of nuts) {
+    const dist = Math.abs(tree[0] - nut[0]) + Math.abs(tree[1] - nut[1])
+    sum += 2 * dist
+    maxDiff = Math.max(
+      maxDiff,
+      dist - Math.abs(squirrel[0] - nut[0]) - Math.abs(squirrel[1] - nut[1])
+    )
+  }
+  return sum - maxDiff
+}
