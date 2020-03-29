6b01d977a36d54b158956737213fd1dfe42c0357
Nothing <wwwaap@gmail.com>
2020-03-29 15:28:55 +0800

    Update 578-get-highest-answer-rate-question.sql

--- 578-get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 select question_id as survey_log
-from survey_log
-group by question_id
-order by count(answer_id) desc
-limit 1;
+from (
+  select question_id, count(answer_id)/count(*) as count
+  from survey_log group by question_id
+  order by count desc 
+  limit 1
+) as p;
