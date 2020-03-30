c3c9581efe97711d6991a0209c3f34b4487a51d5
Nothing <wwwaap@gmail.com>
2020-03-30 14:27:53 +0800

    Create 585-investments-in-2016.sql

---
+++ 585-investments-in-2016.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select sum(TIV_2016) TIV_2016
+from insurance a
+where 1 = (select count(*) from insurance b where a.LAT = b.LAT and a.LON = b.LON) 
+and 1 < (select count(*) from insurance c where a.TIV_2015 = c.TIV_2015);
