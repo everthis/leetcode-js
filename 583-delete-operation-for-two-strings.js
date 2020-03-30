98431e472741a4b643ae4c4e16b2873cd818e2f4
Nothing <wwwaap@gmail.com>
2020-03-30 13:15:50 +0800

    Update 583-delete-operation-for-two-strings.js

--- 583-delete-operation-for-two-strings.js
+++ 583-delete-operation-for-two-strings.js
@@ -23,3 +23,24 @@ const minDistance = function (word1, word2, memo = new Map()) {
   memo.set(`${word1}-${word2}`, res)
   return res
 }
+
+// another
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const minDistance = function (word1, word2) {
+  const len1 = word1.length
+  const len2 = word2.length
+  const dp = Array.from({ length: len1 + 1 }, () => new Array(len2 + 1).fill(0))
+  for(let i = 1; i <= len1; i++) {
+    for(let j = 1; j<= len2; j++) {
+      if(word1[i - 1] === word2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1
+      else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+    }
+  }
+  return len1 + len2 - dp[len1][len2] * 2
+}
+
