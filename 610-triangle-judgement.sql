a7deff5e65bb5b898bb1c4cd85b94ceb73e80558
Nothing <wwwaap@gmail.com>
2020-04-02 17:53:27 +0800

    Create 610-triangle-judgement.sql

---
+++ 610-triangle-judgement.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT x
+     , y
+     , z
+     , CASE WHEN 
+            x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+            ELSE 'No'
+       END AS triangle
+  FROM triangle;
