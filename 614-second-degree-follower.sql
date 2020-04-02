f98fda8c97c08e96e754991ac2a8700a0c7ea946
Nothing <wwwaap@gmail.com>
2020-04-02 21:48:48 +0800

    Update 614-second-degree-follower.sql

--- 614-second-degree-follower.sql
+++ 614-second-degree-follower.sql
@@ -6,3 +6,12 @@ FROM follow
 WHERE followee IN (SELECT follower FROM follow)
 GROUP BY followee
 ORDER BY followee;
+
+# another
+
+select distinct follower, num
+from follow, 
+(select followee, count(distinct follower) as num from follow 
+group by followee) as t
+where follower = t.followee
+order by follower;
