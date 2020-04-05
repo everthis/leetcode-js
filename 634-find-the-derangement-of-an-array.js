2ac9392777e83a06f9846e4ee5817a24997811b0
Nothing <wwwaap@gmail.com>
2020-04-05 23:08:11 +0800

    Update 634-find-the-derangement-of-an-array.js

--- 634-find-the-derangement-of-an-array.js
+++ 634-find-the-derangement-of-an-array.js
@@ -4,12 +4,12 @@
  */
 const findDerangement = function (n) {
   if ([0, 1].includes(n)) return 0
-  if (n == 2) return 1
+  if (n === 2) return 1
   let prev = 1
-  let MOD = 10 ** 9 + 7
+  const MOD = 10 ** 9 + 7
   let result = 0
   for (let i = 3; i <= n; i++) {
-    result = (prev * i + (i % 2 == 1 ? -1 : 1)) % MOD
+    result = (prev * i + (i % 2 === 1 ? -1 : 1)) % MOD
     prev = result
   }
   return result
