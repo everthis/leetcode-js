f3d54cd056f2d578a3b1692f388ab2b893b9160a
Nothing <wwwaap@gmail.com>
2020-04-06 18:19:39 +0800

    Update 49-group-anagrams.js

--- 49-group-anagrams.js
+++ 49-group-anagrams.js
@@ -2,6 +2,29 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+const groupAnagrams = function (strs) {
+  const hash = new Map()
+  for (let str of strs) {
+    let key = 0
+    for (let char of str) {
+      const idx = char.charCodeAt(0)
+      key += Math.pow(idx, 4)
+    }
+    if (!hash.has(key)) hash.set(key, [str])
+    else {
+      hash.set(key, hash.get(key).concat(str))
+    }
+  }
+  return Array.from(hash.values())
+}
+
+
+// another
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 const groupAnagrams = function(strs) {
   if (strs.length === 0) {
     return [];
@@ -19,6 +42,5 @@ const groupAnagrams = function(strs) {
       hash[sel] = [el];
     }
   }
-
   return Object.values(hash);
 };
