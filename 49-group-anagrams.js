40569be6ffac264b8b18780fa1546a730afe5fb0
Nothing <wwwaap@gmail.com>
2020-04-06 18:27:01 +0800

    Update 49-group-anagrams.js

--- 49-group-anagrams.js
+++ 49-group-anagrams.js
@@ -18,6 +18,66 @@ const groupAnagrams = function (strs) {
   return Array.from(hash.values())
 }
 
+// another
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = (strs) => {
+  const resp = new Array(),
+    termsGrouped = new Map()
+  strs.forEach((term) => {
+    const hashed = hash(term)
+    if (!termsGrouped.has(hashed)) termsGrouped.set(hashed, new Array())
+    termsGrouped.get(hashed).push(term)
+  })
+  termsGrouped.forEach((terms) => {
+    resp.push(terms)
+  })
+  return resp
+}
+
+const hash = (term) => {
+  const primeLetterNumbers = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+    101,
+  ]
+  let accum = 1
+  for (let letter of term) {
+    const primeIndex = letter.charCodeAt(0) - 'a'.charCodeAt(0)
+    const primeMapping = primeLetterNumbers[primeIndex]
+    accum *= primeMapping
+  }
+  return accum
+}
+
+
+
 
 // another
 
