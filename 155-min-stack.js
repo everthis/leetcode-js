30ef87ecc6328d039dabbee34bf345827ffe2c64
Nothing <wwwaap@gmail.com>
2020-04-10 15:08:49 +0800

    Update 155-min-stack.js

--- 155-min-stack.js
+++ 155-min-stack.js
@@ -1,46 +1,52 @@
 /**
  * initialize your data structure here.
  */
-const MinStack = function() {
-    this.arr = []
-};
+const MinStack = function () {
+  this.stack = []
+  this.min = null
+}
 
-/** 
+/**
  * @param {number} x
  * @return {void}
  */
-MinStack.prototype.push = function(x) {
-    this.arr.push(x)
-};
+MinStack.prototype.push = function (x) {
+  if (this.min === null) {
+    this.min = x
+  } else {
+    this.min = Math.min(x, this.min)
+  }
+  return this.stack.push(x)
+}
 
 /**
  * @return {void}
  */
-MinStack.prototype.pop = function() {
-    this.arr.pop()
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-    return this.arr[this.arr.length - 1]
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-  let min = Number.MAX_VALUE
-  for(let el of this.arr) {
-    if(el < min) min = el
+MinStack.prototype.pop = function () {
+  let removed = this.stack.pop()
+  if (this.min === removed) {
+    this.min = this.stack.length > 0 ? Math.min(...this.stack) : null
   }
-  return min
-};
+  return this.stack
+}
 
-/** 
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1]
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.min
+}
+
+/**
  * Your MinStack object will be instantiated and called as such:
- * var obj = Object.create(MinStack).createNew()
+ * var obj = new MinStack()
  * obj.push(x)
  * obj.pop()
  * var param_3 = obj.top()
