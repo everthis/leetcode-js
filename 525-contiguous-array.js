68880a9bd02734a3d818574feb09792cf8d4ef47
Nothing <wwwaap@gmail.com>
2020-04-13 16:03:15 +0800

    Update 525-contiguous-array.js

--- 525-contiguous-array.js
+++ 525-contiguous-array.js
@@ -2,4 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findMaxLength = function(nums) {}
+const findMaxLength = function (nums) {
+  const map = new Map()
+  map.set(0, -1)
+  let count = 0
+  let max = 0
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    if (num === 0) {
+      count -= 1
+    }
+    if (num === 1) {
+      count += 1
+    }
+    if (map.has(count)) {
+      max = Math.max(max, i - map.get(count))
+    } else {
+      map.set(count, i)
+    }
+  }
+  return max
+}
