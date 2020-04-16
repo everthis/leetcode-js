b0a6be7f0acd3aef612d4493b1bb5f3feec83f18
Nothing <wwwaap@gmail.com>
2020-04-16 08:17:21 +0800

    Update 671-second-minimum-node-in-a-binary-tree.js

--- 671-second-minimum-node-in-a-binary-tree.js
+++ 671-second-minimum-node-in-a-binary-tree.js
@@ -29,3 +29,38 @@ const findSecondMinimumValue = function(root) {
   }
   return min2nd === Number.MAX_VALUE ? -1 : min2nd
 };
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const findSecondMinimumValue = function (root) {
+  if (root === null) return -1;
+  if (root.left === null && root.right === null) return -1;
+  let left = root.left.val;
+  let right = root.right.val;
+  if (left === root.val) {
+    left = findSecondMinimumValue(root.left);
+  }
+  if (right === root.val) {
+    right = findSecondMinimumValue(root.right);
+  }
+  if (right !== -1 && left !== -1) {
+    return Math.min(left, right);
+  }
+  if (right === -1) {
+    return left;
+  }
+  if (left === -1) {
+    return right;
+  }
+};
