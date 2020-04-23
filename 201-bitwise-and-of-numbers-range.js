ed33b513b99ef4f2327418d11494a1340ec25a52
Nothing <wwwaap@gmail.com>
2020-04-23 21:54:01 +0800

    Update 201-bitwise-and-of-numbers-range.js

--- 201-bitwise-and-of-numbers-range.js
+++ 201-bitwise-and-of-numbers-range.js
@@ -7,3 +7,20 @@ const rangeBitwiseAnd = function(m, n) {
   while(m<n) n = n & (n-1);
   return n;
 };
+
+// another
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const rangeBitwiseAnd = function(m, n) {
+  let s = 0
+  while(m !== n) {
+    m >>= 1
+    n >>= 1
+    s++
+  }
+  return m << s
+};
