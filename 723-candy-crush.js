ca265f5f5d4d2441da6036965116154c86f250c8
Nothing <wwwaap@gmail.com>
2020-04-26 08:40:18 +0800

    Update 723-candy-crush.js

--- 723-candy-crush.js
+++ 723-candy-crush.js
@@ -82,3 +82,58 @@ const inflictGravity = function (board) {
     }
   }
 }
+
+// another
+
+/**
+ * @param {number[][]} board
+ * @return {number[][]}
+ */
+const candyCrush = function (board) {
+  const N = board.length,
+    M = board[0].length
+  let found = true
+  while (found) {
+    found = false
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < M; j++) {
+        const val = Math.abs(board[i][j])
+        if (val === 0) continue
+        if (
+          j < M - 2 &&
+          Math.abs(board[i][j + 1]) == val &&
+          Math.abs(board[i][j + 2]) == val
+        ) {
+          found = true
+          let ind = j
+          while (ind < M && Math.abs(board[i][ind]) == val)
+            board[i][ind++] = -val
+        }
+        if (
+          i < N - 2 &&
+          Math.abs(board[i + 1][j]) == val &&
+          Math.abs(board[i + 2][j]) == val
+        ) {
+          found = true
+          let ind = i
+          while (ind < N && Math.abs(board[ind][j]) == val)
+            board[ind++][j] = -val
+        }
+      }
+    }
+    if (found) {
+      // move positive values to the bottom, then set the rest to 0
+      for (let j = 0; j < M; j++) {
+        let storeInd = N - 1
+        for (let i = N - 1; i >= 0; i--) {
+          if (board[i][j] > 0) {
+            board[storeInd--][j] = board[i][j]
+          }
+        }
+        for (let k = storeInd; k >= 0; k--) board[k][j] = 0
+      }
+    }
+  }
+  return board
+}
+
