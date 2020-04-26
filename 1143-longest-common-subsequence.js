bf47088fccdd6812a7cec48771deb393cb0edbce
Nothing <wwwaap@gmail.com>
2020-04-26 15:54:44 +0800

    Update 1143-longest-common-subsequence.js

--- 1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence.js
@@ -15,3 +15,30 @@ const longestCommonSubsequence = function(text1, text2) {
   }
   return dp[dp.length - 1].pop()
 }
+
+// another
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+const longestCommonSubsequence = function(text1, text2) {
+  const len1 = text1.length
+  const len2 = text2.length
+  if(len1 === 0 || len2 === 0) return 0
+  if(len1 < len2) return longestCommonSubsequence(text2, text1)
+  const dp = Array(len1 + 1).fill(0)
+  for(let i = 1; i <= len2; i++) {
+    let prev = 0
+    for(let j = 1; j <= len1; j++) {
+      const tmp = dp[j]
+      if(text1[j - 1] === text2[i - 1]) dp[j] = Math.max(dp[j], prev + 1)
+      else {
+        dp[j] = Math.max(dp[j - 1], dp[j])
+      }
+      prev = tmp
+    }
+  }
+  return dp[len1]
+};
