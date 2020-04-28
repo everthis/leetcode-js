4f7962d20088b94d89cc8044e745224b71b729b3
Nothing <wwwaap@gmail.com>
2020-04-28 16:41:00 +0800

    Update 744-find-smallest-letter-greater-than-target.js

--- 744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target.js
@@ -3,12 +3,15 @@
  * @param {character} target
  * @return {character}
  */
-const nextGreatestLetter = function(letters, target) {
-  const arr = Array.from(new Set(letters))
-  const len = arr.length
-  const t = target.charCodeAt(0)
-  for(let i = 0, len = arr.length; i < len; i++) {
-    if(arr[i].charCodeAt(0) - t > 0) return arr[i]
+const nextGreatestLetter = function (letters, target) {
+  const n = letters.length
+  if (target < letters[0] || target >= letters[n - 1]) return letters[0]
+  let left = 0
+  let right = n - 1
+  while (left < right) {
+    let mid = left + ((right - left) >> 1)
+    if (letters[mid] <= target) left = mid + 1
+    else right = mid
   }
-  return arr[0]
-};
+  return letters[right]
+}
