9796c23ce3ca649c48d2353f7ed60a5406379ab5
Nothing <wwwaap@gmail.com>
2020-05-01 14:25:14 +0800

    Update 750-number-of-corner-rectangles.js

--- 750-number-of-corner-rectangles.js
+++ 750-number-of-corner-rectangles.js
@@ -15,3 +15,40 @@ const countCornerRectangles = function (grid) {
   }
   return ans
 }
+
+// another
+
+// optimized
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const countCornerRectangles = function (grid) {
+  let ans = 0
+  let largeLoopLen, smLoopLen, r
+  if(grid.length > grid[0].length) {
+    r = false
+    largeLoopLen = grid.length
+    smLoopLen = grid[0].length
+  } else {
+    r = true
+    largeLoopLen = grid[0].length
+    smLoopLen = grid.length
+  }
+  for (let i = 0; i < smLoopLen - 1; i++) {
+    for (let j = i + 1; j < smLoopLen; j++) {
+      let counter = 0
+      for (let k = 0; k < largeLoopLen; k++) {
+        if(r) {
+          if (grid[i][k] === 1 && grid[j][k] === 1) counter++
+        } else {
+          if (grid[k][i] === 1 && grid[k][j] === 1) counter++
+        }
+        
+      }
+      ans += (counter * (counter - 1)) / 2
+    }
+  }
+  return ans
+}
