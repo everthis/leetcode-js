39620df9b1912159748723814937cecfefb0a1cc
Nothing <wwwaap@gmail.com>
2020-05-02 09:10:50 +0800

    Update 755-pour-water.js

--- 755-pour-water.js
+++ 755-pour-water.js
@@ -18,7 +18,6 @@ const pourWater = function (heights, V, K) {
     heights[bottom]++
     return pourWater(heights, V - 1, K)
   }
-
   for (let i = K + 1; i < heights.length; i++) {
     if (heights[i] > heights[bottom]) break
     if (heights[i] < heights[bottom]) bottom = i
@@ -26,3 +25,32 @@ const pourWater = function (heights, V, K) {
   heights[bottom]++
   return pourWater(heights, V - 1, K)
 }
+
+// another
+
+/**
+ * @param {number[]} heights
+ * @param {number} V
+ * @param {number} K
+ * @return {number[]}
+ */
+const pourWater = function (heights, V, K) {
+  let cur = K
+  for (let i = 0; i < V; i++) {
+    // Move left
+    while (cur > 0 && heights[cur - 1] <= heights[cur]) {
+      cur--
+    }
+    // Move right
+    while (cur < heights.length - 1 && heights[cur + 1] <= heights[cur]) {
+      cur++
+    }
+    // Move left to K
+    while(cur > K && heights[cur - 1] === heights[cur]) {
+      cur--
+    }
+    heights[cur]++
+  }
+  return heights
+}
+
