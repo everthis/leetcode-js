3cfbd1597874b16167220d2bb41a4fcd10beaf94
Nothing <wwwaap@gmail.com>
2020-05-02 08:35:29 +0800

    Create 755-pour-water.js

---
+++ 755-pour-water.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} heights
+ * @param {number} V
+ * @param {number} K
+ * @return {number[]}
+ */
+const pourWater = function (heights, V, K) {
+  if (!V) return heights
+  let bottom = K
+  //iterate through from k to find thee lowest bottom
+  for (let i = K; i >= 0; i--) {
+    if (heights[i] > heights[bottom]) break
+    if (heights[i] < heights[bottom]) bottom = i
+  }
+  //if bottom is not k increase height of bottom
+  //and run again but decrease water droplet V by one
+  if (bottom !== K) {
+    heights[bottom]++
+    return pourWater(heights, V - 1, K)
+  }
+
+  for (let i = K + 1; i < heights.length; i++) {
+    if (heights[i] > heights[bottom]) break
+    if (heights[i] < heights[bottom]) bottom = i
+  }
+  heights[bottom]++
+  return pourWater(heights, V - 1, K)
+}
