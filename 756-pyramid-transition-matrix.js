896ebfb5d76e3d2778d7d562318d3404ceb5c775
Nothing <wwwaap@gmail.com>
2020-05-03 00:23:46 +0800

    Update 756-pyramid-transition-matrix.js

--- 756-pyramid-transition-matrix.js
+++ 756-pyramid-transition-matrix.js
@@ -4,29 +4,20 @@
  * @return {boolean}
  */
 const pyramidTransition = function (bottom, allowed) {
-  const map = new Map()
-  allowed.map((item) => {
-    const t = map.get(item[0] + item[1]) || new Array()
-    t.push(item[2])
-    map.set(item[0] + item[1], t)
-  })
-  const memo = new Map()
-  const solve = function (cur, ind, next) {
-    if (memo.has(cur)) return memo.get(cur)
-    if (cur.length === 1) return true
-    if (ind >= cur.length - 1) {
-      const res = solve(next, 0, '')
-      memo.set(next, res)
-      return res
-    }
-    if (!map.has(cur.slice(ind, ind + 2))) {
-      memo.set(cur, false)
-      return false
-    }
-    for (let char of map.get(cur.slice(ind, ind + 2))) {
-      if (solve(cur, ind + 1, next + char)) return true
-    }
-    return false
+  const m = new Map()
+  for (let e of allowed) {
+    const p = e.slice(0, 2)
+    if (!m.has(p)) m.set(p, new Set())
+    m.get(p).add(e[2])
   }
-  return solve(bottom, 0, '')
+  return dfs(bottom, '', m, 0)
 }
+
+function dfs(row, next, m, i) {
+  if (row.length === 1) return true
+  if (next.length + 1 === row.length) return dfs(next, '', m, 0)
+  for (let c of m.get(row.slice(i, i + 2)) || new Set())
+    if (dfs(row, next + c, m, i + 1)) return true
+  return false
+}
+
