07cb037c561d9c65eba2071b182a21f291c3f3ce
Nothing <wwwaap@gmail.com>
2020-05-05 19:12:15 +0800

    Update 387-first-unique-character-in-a-string.js

--- 387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string.js
@@ -26,3 +26,23 @@ const firstUniqChar = function(s) {
   }
   return res[0] == null ? -1 : res[0];
 };
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = function(s) {
+    if(s === '') return -1
+    const map = new Map()
+    for(let i = 0, len = s.length; i < len; i++) {
+      if(!map.has(s[i])) map.set(s[i], [i, 0])
+      map.get(s[i])[1] += 1
+    }
+    for(let [key, val] of map) {
+      if(val[1] === 1) return val[0]
+    }
+    return -1
+    
+};
