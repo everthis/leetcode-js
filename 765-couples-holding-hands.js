b43b1801c5ae1ab7635420b0e5fb1f562d5eb4a9
Nothing <wwwaap@gmail.com>
2020-05-09 09:59:15 +0800

    Update 765-couples-holding-hands.js

--- 765-couples-holding-hands.js
+++ 765-couples-holding-hands.js
@@ -24,3 +24,51 @@ const minSwapsCouples = function (row) {
 function swap(arr, i, j) {
   ;[arr[i], arr[j]] = [arr[j], arr[i]]
 }
+
+// another
+
+/**
+ * @param {number[]} row
+ * @return {number}
+ */
+const minSwapsCouples = function (row) {
+  const parents = Array.from({ length: row.length / 2 }, (_, i) => i)
+  const positions = new Map()
+  for (let i = 0; i < row.length / 2; i++) {
+    const left = Math.floor(row[i * 2] / 2)
+    const right = Math.floor(row[i * 2 + 1] / 2)
+    if (positions.has(left)) {
+      union(i, positions.get(left))
+    } else {
+      positions.set(left, i)
+    }
+    if (positions.has(right)) {
+      union(i, positions.get(right))
+    } else {
+      positions.set(right, i)
+    }
+  }
+
+  const uniqueRoots = new Set()
+  for (const parent of parents) {
+    uniqueRoots.add(find(parent))
+  }
+  return parents.length - uniqueRoots.size
+
+  function union(a, b) {
+    const aRoot = find(a)
+    const bRoot = find(b)
+    parents[aRoot] = bRoot
+  }
+  function root(x) {
+    while (x !== parents[x]) {
+      parents[x] = parents[parents[x]]
+      x = parents[x]
+    }
+    return x
+  }
+  function find(node) {
+    return root(node)
+  }
+}
+
