d553e2af8f445c25337dda9a79decc525c7eb146
Nothing <wwwaap@gmail.com>
2020-05-10 16:54:08 +0800

    Update 997-find-the-town-judge.js

--- 997-find-the-town-judge.js
+++ 997-find-the-town-judge.js
@@ -4,6 +4,25 @@
  * @return {number}
  */
 const findJudge = function(N, trust) {
+  const arr = new Array(N + 1).fill(0)
+  for(let [t, ted] of trust) {
+    arr[t]--
+    arr[ted]++
+  }
+  for(let i = 1; i <= N; i++) {
+    if(arr[i] === N - 1) return i
+  }
+  return -1
+};
+
+// another
+
+/**
+ * @param {number} N
+ * @param {number[][]} trust
+ * @return {number}
+ */
+const findJudge = function(N, trust) {
   const m = new Map()
   for(let i = 1; i<= N; i++) {
     const e = new Map()
