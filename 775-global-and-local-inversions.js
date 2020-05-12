2864c99792928c91b93f7d32241787386bfa48b8
Nothing <wwwaap@gmail.com>
2020-05-12 14:21:05 +0800

    Update 775-global-and-local-inversions.js

--- 775-global-and-local-inversions.js
+++ 775-global-and-local-inversions.js
@@ -6,8 +6,22 @@ const isIdealPermutation = function(A) {
   if(A.length === 1 || A.length === 2) return true
   let max = A[0]
   for(let i = 0, len = A.length; i < len - 2; i++) {
+    max = Math.max(max, A[i])
     if(max > A[i + 2]) return false
-    else max = Math.max(max, A[i + 1])
+  }
+  return true;
+};
+
+// another
+
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+const isIdealPermutation = function(A) {
+  if(A.length === 1 || A.length === 2) return true
+  for(let i = 0, len = A.length; i < len; i++) {
+    if(Math.abs(A[i] - i) > 1) return false
   }
   return true;
 };
