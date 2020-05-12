5d1abcb66931647050d67e7f903e024cb58aed1c
Nothing <wwwaap@gmail.com>
2020-05-12 15:08:56 +0800

    Update 540-single-element-in-a-sorted-array.js

--- 540-single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array.js
@@ -12,3 +12,18 @@ const singleNonDuplicate = function(nums) {
     }
   }
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNonDuplicate = function(nums) {
+  if(nums.length === 0) return 0
+  let res = nums[0]
+  for(let i = 1, len = nums.length; i < len; i++) {
+    res ^= nums[i]
+  }
+  return res
+};
