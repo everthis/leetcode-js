a90454051ca6f863d6e5780a24d9870980001618
Nothing <wwwaap@gmail.com>
2020-05-14 20:02:37 +0800

    Update 208-implement-trie-prefix-tree.js

--- 208-implement-trie-prefix-tree.js
+++ 208-implement-trie-prefix-tree.js
@@ -85,3 +85,68 @@ class Trie {
   }
 }
 
+// another
+/**
+ * Initialize your data structure here.
+ */
+const Trie = function () {
+  this.root = new Node(null)
+}
+
+/**
+ * Inserts a word into the trie.
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  let cur = this.root
+  for (let i = 0, len = word.length; i < len; i++) {
+    if (!cur.children.has(word[i])) cur.children.set(word[i], new Node(null))
+    cur = cur.children.get(word[i])
+    if (i === len - 1) cur.word = true
+  }
+}
+
+/**
+ * Returns if the word is in the trie.
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+  let cur = this.root
+  for (let i = 0, len = word.length; i < len; i++) {
+    if (cur.children.has(word[i])) cur = cur.children.get(word[i])
+    else return false
+    if (i === len - 1) return cur.word === true
+  }
+}
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix.
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  let cur = this.root
+  for (let i = 0, len = prefix.length; i < len; i++) {
+    if (cur.children.has(prefix[i])) cur = cur.children.get(prefix[i])
+    else return false
+    if (i === len - 1) return true
+  }
+}
+
+class Node {
+  constructor(v) {
+    this.val = v
+    this.word = false
+    this.children = new Map()
+  }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
