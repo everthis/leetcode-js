f4640a44414c7dd795dec36487218709bcc21053
Nothing <wwwaap@gmail.com>
2020-05-15 16:21:59 +0800

    Update 918-maximum-sum-circular-subarray.js

--- 918-maximum-sum-circular-subarray.js
+++ 918-maximum-sum-circular-subarray.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 const maxSubarraySumCircular = function(A) {
-    let total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
-    for (let a of A) {
-        curMax = Math.max(curMax + a, a);
-        maxSum = Math.max(maxSum, curMax);
-        curMin = Math.min(curMin + a, a);
-        minSum = Math.min(minSum, curMin);
-        total += a;
-    }
-    return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
-}
+  let minSum = Infinity, sum = 0, maxSum = -Infinity, curMax = 0, curMin = 0
+  for(let a of A) {
+    sum += a
+    curMax = Math.max(curMax + a, a);
+    maxSum = Math.max(maxSum, curMax);
+    curMin = Math.min(curMin + a, a);
+    minSum = Math.min(minSum, curMin);
+  }
+  return  maxSum > 0 ? Math.max(maxSum, sum - minSum) : maxSum;
+};
