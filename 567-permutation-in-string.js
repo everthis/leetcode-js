f48aa2fd4c7a295e3a251ae9041283ab79287d6f
Nothing <wwwaap@gmail.com>
2020-05-18 17:01:36 +0800

    Update 567-permutation-in-string.js

--- 567-permutation-in-string.js
+++ 567-permutation-in-string.js
@@ -31,3 +31,36 @@ function matches(s1map, s2map) {
   }
   return true
 }
+
+
+// another
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+const checkInclusion = function(s1, s2) {
+  const arr = Array(26).fill(0)
+  const a = 'a'.charCodeAt(0)
+  const s1l = s1.length
+  for(let c of s1) arr[c.charCodeAt(0) - a] += 1
+  for(let i = 0, len = s2.length; i < len; i++) {
+    const tmp = s2[i]
+    arr[tmp.charCodeAt(0) - a]--
+    if(i >= s1l - 1) {
+      if(allZeros(arr)) return true
+      arr[s2.charCodeAt(i - s1l + 1) - a]++
+    }
+    
+  }
+  
+  return false
+};
+
+function allZeros(arr) {
+  for(let e of arr) {
+    if(e !== 0) return false
+  }
+  return true
+}
