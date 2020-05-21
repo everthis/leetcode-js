7d54d6f50f5f4cd5d17745b72efdd96ecb3e7608
Nothing <wwwaap@gmail.com>
2020-05-21 15:37:26 +0800

    Update 1277-count-square-submatrices-with-all-ones.js

--- 1277-count-square-submatrices-with-all-ones.js
+++ 1277-count-square-submatrices-with-all-ones.js
@@ -5,10 +5,13 @@
 const countSquares = function (A) {
   const [M, N] = [A.length, A[0].length]
   let ans = 0
-  for (let i = 1; i < M; ++i)
-    for (let j = 1; j < N; ++j)
-      if (A[i][j] && A[i - 1][j] && A[i][j - 1] && A[i - 1][j - 1])
+  for (let i = 0; i < M; ++i) {
+    for (let j = 0; j < N; ++j) {
+      if (A[i][j] && i > 0 && j > 0) {
         A[i][j] = 1 + Math.min(A[i - 1][j], A[i][j - 1], A[i - 1][j - 1])
-  for (let row of A) ans += row.reduce((a, b) => a + b)
+      }
+      ans += A[i][j]
+    }
+  }
   return ans
 }
