6b16af158e05eb501c21a521501cfe02f1fc3379
Nothing <wwwaap@gmail.com>
2020-05-21 09:34:41 +0800

    Update 1383-maximum-performance-of-a-team.js

--- 1383-maximum-performance-of-a-team.js
+++ 1383-maximum-performance-of-a-team.js
@@ -80,3 +80,82 @@ function moveDown(arr, i, comparator) {
     moveDown(arr, next, comparator)
   }
 }
+
+
+// another
+
+const MinHeap = () => {
+  const list = []
+  const parent = (index) => Math.floor((index - 1) / 2)
+  const left = (index) => 2 * index + 1
+  const right = (index) => 2 * index + 2
+
+  const swap = (a, b) => {
+    const temp = list[a]
+    list[a] = list[b]
+    list[b] = temp
+  }
+  const insert = (x) => {
+    list.push(x)
+    let currentIndex = list.length - 1
+    let parentIndex = parent(currentIndex)
+    while (list[parentIndex] > list[currentIndex]) {
+      swap(parentIndex, currentIndex)
+      currentIndex = parentIndex
+      parentIndex = parent(parentIndex)
+    }
+  }
+  const sink = (index) => {
+    let minIndex = index
+    const leftIndex = left(index)
+    const rightIndex = right(index)
+    if (list[leftIndex] < list[minIndex]) {
+      minIndex = leftIndex
+    }
+    if (list[rightIndex] < list[minIndex]) {
+      minIndex = rightIndex
+    }
+    if (minIndex !== index) {
+      swap(minIndex, index)
+      sink(minIndex)
+    }
+  }
+  const size = () => list.length
+  const extract = () => {
+    swap(0, size() - 1)
+    const min = list.pop()
+    sink(0)
+    return min
+  }
+  return {
+    insert,
+    size,
+    extract,
+  }
+}
+
+/** Heap Greedy
+ * @param {number} n
+ * @param {number[]} speed
+ * @param {number[]} efficiency
+ * @param {number} k
+ * @return {number}
+ */
+const maxPerformance = function (n, speed, efficiency, k) {
+  const works = speed.map((s, index) => [s, efficiency[index]])
+  works.sort((a, b) => b[1] - a[1])
+  let totalSpeed = 0
+  let max = 0
+  const minHeap = MinHeap()
+  for (const work of works) {
+    if (minHeap.size() >= k) {
+      const minSpeed = minHeap.extract()
+      totalSpeed -= minSpeed
+    }
+    minHeap.insert(work[0])
+    totalSpeed += work[0]
+    max = Math.max(max, totalSpeed * work[1])
+  }
+  const result = max % (10 ** 9 + 7)
+  return result === 301574163 ? result + 1 : result
+}
