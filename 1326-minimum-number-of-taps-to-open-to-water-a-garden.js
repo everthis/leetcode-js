87538379165822564fc5d7f9256dde5cad08e030
Nothing <wwwaap@gmail.com>
2020-06-01 21:05:30 +0800

    Update 1326-minimum-number-of-taps-to-open-to-water-a-garden.js

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden.js
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden.js
@@ -23,3 +23,19 @@ const minTaps = function (n, ranges) {
   }
   return count
 }
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[]} ranges
+ * @return {number}
+ */
+const minTaps = function (n, ranges) {
+  const dp = new Array(n + 1).fill(n + 2)
+  dp[0] = 0
+  for (let i = 0; i <= n; ++i)
+    for (let j = Math.max(i - ranges[i] + 1, 0); j <= Math.min(i + ranges[i], n); ++j)
+      dp[j] = Math.min(dp[j], dp[Math.max(0, i - ranges[i])] + 1)
+  return dp[n] < n + 2 ? dp[n] : -1
+}
