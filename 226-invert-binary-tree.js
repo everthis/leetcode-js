474e7c6515549e9e604a88b3d29ab33431c69e60
Nothing <wwwaap@gmail.com>
2020-06-01 15:34:07 +0800

    Create 226-invert-binary-tree.js

---
+++ 226-invert-binary-tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function (root) {
+  if (root) {
+    ;[root.left, root.right] = [invertTree(root.right), invertTree(root.left)]
+  }
+  return root
+}
