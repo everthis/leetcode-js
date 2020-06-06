724be3804c0fcef7cb826f73bbfe279f9063f930
Nothing <jihe@conviva.com>
2020-06-06 16:26:54 +0800

    Update 406-queue-reconstruction-by-height.js

--- 406-queue-reconstruction-by-height.js
+++ 406-queue-reconstruction-by-height.js
@@ -2,6 +2,23 @@
  * @param {number[][]} people
  * @return {number[][]}
  */
+const reconstructQueue = function (people) {
+  const h = 0
+  const k = 1
+  people.sort((a, b) => (a[h] == b[h] ? a[k] - b[k] : b[h] - a[h]))
+  let queue = []
+  for (let person of people) {
+    queue.splice(person[k], 0, person)
+  }
+  return queue
+}
+
+// another
+
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
 const reconstructQueue = function(people) {
   if (!people) return [];
   const peopledct = {};
