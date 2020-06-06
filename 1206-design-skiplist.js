902497570c3623cd773c491c11f2272ede22e360
Nothing <jihe@conviva.com>
2020-06-06 20:03:41 +0800

    Update 1206-design-skiplist.js

--- 1206-design-skiplist.js
+++ 1206-design-skiplist.js
@@ -1,3 +1,66 @@
+class Skiplist {
+  constructor() {
+    this.head = { down: null, right: null, val: -Infinity }
+  }
+  search(val) {
+    let curr = this.head
+    while (curr) {
+      while (curr.right && curr.right.val <= val) {
+        curr = curr.right
+      }
+      if (curr.val == val) {
+        return true
+      }
+      curr = curr.down
+    }
+    return false
+  }
+  add(val) {
+    let curr = this.head
+    const insertion_positions = []
+    while (curr) {
+      while (curr.right && curr.right.val < val) {
+        curr = curr.right
+      }
+      insertion_positions.push(curr)
+      curr = curr.down
+    }
+    let insert = true
+    let down = null
+    while (insert && insertion_positions.length) {
+      const position = insertion_positions.pop()
+      const node = { down, val, right: position.right }
+      position.right = node
+      down = node
+      insert = Math.random() < 0.5
+    }
+    if (insert) {
+      const node = { val, down }
+      this.head = { val: -Infinity, right: node, down: this.head }
+    }
+  }
+  erase(val) {
+    let curr = this.head
+    const erase_positions = []
+    while (curr) {
+      while (curr.right && curr.right.val < val) {
+        curr = curr.right
+      }
+      if (curr.right && curr.right.val == val) {
+        erase_positions.push(curr)
+      }
+      curr = curr.down
+    }
+    const seen = erase_positions.length > 0
+    for (const position of erase_positions) {
+      position.right = position.right && position.right.right
+    }
+    return seen
+  }
+}
+
+// another
+
 const Skiplist = function () {
   this.maxLvl = ~~Math.log2(20000)
   this.levels = [...Array(this.maxLvl)].map(() => new Node(-1))
