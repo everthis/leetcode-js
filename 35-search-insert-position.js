a0638eac534a5da17bf24f7ed861537d4d8b456c
Nothing <wwwaap@gmail.com>
2020-06-10 20:45:46 +0800

    Update 35-search-insert-position.js

--- 35-search-insert-position.js
+++ 35-search-insert-position.js
@@ -14,3 +14,22 @@ const searchInsert = function(nums, target) {
     }
   }
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target) {
+  const n = nums.length
+  let l = 0, r = n - 1
+  while(l <= r) {
+    const mid = l + ((r - l) >> 1)
+    if(nums[mid] === target) return mid
+    if(nums[mid] > target) r = mid - 1
+    else l = mid + 1
+  }
+  return l
+};
