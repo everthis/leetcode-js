ea348e53882f10a84c456dccc96b7d0c9faed964
Nothing <wwwaap@gmail.com>
2020-06-11 16:02:42 +0800

    Update 75-sort-colors.js

--- 75-sort-colors.js
+++ 75-sort-colors.js
@@ -2,23 +2,19 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-
 const sortColors = function(nums) {
-    let i = 0
-    let j = 0
-    for(let k = 0; k < nums.length; k++) {
-        let v = nums[k]
-        nums[k] = 2
-        if(v < 2) {
-           nums[j] = 1
-           j += 1
-        }
-        
-        if(v === 0) {
-           nums[i] = 0
-           i += 1
-        }
+  let j = 0;
+  let k = nums.length - 1;
+  const swap = (a, b) => {
+    const t = nums[a];
+    nums[a] = nums[b];
+    nums[b] = t;
+  };
+  for (let i = 0; i <= k; i++) {
+    if (nums[i] === 2) {
+      swap(i--, k--);
+    } else if (nums[i] === 0) {
+      swap(i, j++);
     }
-    
-    return nums;
+  }
 };
