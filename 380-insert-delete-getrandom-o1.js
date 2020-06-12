4dc4b5cc27b1449be73d290f3b55ad5b50d92f7a
Nothing <wwwaap@gmail.com>
2020-06-12 16:29:55 +0800

    Update 380-insert-delete-getrandom-o1.js

--- 380-insert-delete-getrandom-o1.js
+++ 380-insert-delete-getrandom-o1.js
@@ -1,51 +1,53 @@
 /**
  * Initialize your data structure here.
  */
-const RandomizedSet = function() {
-    this.data = []
-};
+const RandomizedSet = function () {
+  this.map = new Map()
+  this.array = []
+}
 
 /**
- * Inserts a value to the set. Returns true if the set did not already contain the specified element. 
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element.
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.insert = function(val) {
-    if(this.data.indexOf(val) === -1) {
-        this.data.push(val)
-        return true
-    }
-    return false
-};
+RandomizedSet.prototype.insert = function (val) {
+  const { array, map } = this
+  if (map.has(val)) return false
+  array.push(val)
+  map.set(val, array.length - 1)
+  return true
+}
 
 /**
- * Removes a value from the set. Returns true if the set contained the specified element. 
+ * Removes a value from the set. Returns true if the set contained the specified element.
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function(val) {
-    let idx = this.data.indexOf(val)
-    if(idx !== -1) {
-        this.data.splice(idx, 1)
-        return true
-    }
-    return false
-    
-};
+RandomizedSet.prototype.remove = function (val) {
+  const { array, map } = this
+  if (!map.has(val)) return false
+  const [last, index] = [array[array.length - 1], map.get(val)]
+  array[index] = last
+  map.set(last, index)
+  array.pop()
+  map.delete(val)
+  return true
+}
 
 /**
  * Get a random element from the set.
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function() {
-    const len = this.data.length
-    const idx = Math.floor(Math.random() * len)
-    return this.data[idx]
-};
+RandomizedSet.prototype.getRandom = function () {
+  const { array } = this
+  const r = Math.floor(array.length * Math.random())
+  return array[r]
+}
 
-/** 
+/**
  * Your RandomizedSet object will be instantiated and called as such:
- * var obj = Object.create(RandomizedSet).createNew()
+ * var obj = new RandomizedSet()
  * var param_1 = obj.insert(val)
  * var param_2 = obj.remove(val)
  * var param_3 = obj.getRandom()
