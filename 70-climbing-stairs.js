b0321a30b5390ceb348990560a4368c8f0dc2fa7
Nothing <wwwaap@gmail.com>
2020-06-15 19:59:13 +0800

    Update 70-climbing-stairs.js

--- 70-climbing-stairs.js
+++ 70-climbing-stairs.js
@@ -22,3 +22,26 @@ function single(i, hash) {
   hash[i] = single(i - 1, hash) + single(i - 2, hash);
   return hash[i];
 }
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function (n) {
+  const dp = new Array(n + 1).fill(0)
+  if (n === 1) {
+    return 1
+  }
+  if (n === 2) {
+    return 2
+  }
+  dp[0] = 0
+  dp[1] = 1
+  dp[2] = 2
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp[n]
+}
