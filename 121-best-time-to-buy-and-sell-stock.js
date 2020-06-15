502f8e78166b88d8cae96ecc49a8cb14a302242e
Nothing <wwwaap@gmail.com>
2020-06-15 20:37:18 +0800

    Update 121-best-time-to-buy-and-sell-stock.js

--- 121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock.js
@@ -14,3 +14,19 @@ const maxProfit = function(prices) {
   }
   return maxP;
 };
+
+// another
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function (prices) {
+  let maxCur = 0,
+    maxSoFar = 0
+  for (let i = 1; i < prices.length; i++) {
+    maxCur = Math.max(0, (maxCur += prices[i] - prices[i - 1]))
+    maxSoFar = Math.max(maxCur, maxSoFar)
+  }
+  return maxSoFar
+}
