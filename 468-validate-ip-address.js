9cbc46fc0b3b7ba3644575c148b66b36c31bbe7f
Nothing <wwwaap@gmail.com>
2020-06-16 21:38:10 +0800

    Update 468-validate-ip-address.js

--- 468-validate-ip-address.js
+++ 468-validate-ip-address.js
@@ -2,6 +2,40 @@
  * @param {string} IP
  * @return {string}
  */
+const validIPAddress = function (IP) {
+  if (IP.indexOf('.') > 0) return validIPv4(IP) ? 'IPv4' : 'Neither'
+  else return validIPv6(IP) ? 'IPv6' : 'Neither'
+}
+
+const validIPv4 = function (IP) {
+  const strs = IP.split('.')
+  if (strs.length !== 4) return false
+  for (let str of strs) {
+    if (str.length === 0) return false
+    if (str.match(/[^0-9]/)) return false
+    if (str.length > 1 && str.charAt(0) === '0') return false
+    if (+str > 255) return false
+  }
+  return true
+}
+
+const validIPv6 = function (IP) {
+  const strs = IP.split(':')
+  if (strs.length !== 8) return false
+  for (let str of strs) {
+    if (str.length === 0) return false
+    if (str.length > 4) return false
+    if (str.match(/[^0-9a-fA-F]/g)) return false
+  }
+  return true
+}
+
+// another
+
+/**
+ * @param {string} IP
+ * @return {string}
+ */
 const validIPAddress = function(IP) {
   const ipv4 = /^((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.){4}$/
   const ipv6 = /^([\da-f]{1,4}:){8}$/i
