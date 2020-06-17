27964bc1b6ab23e2c8548558787616d021f4c260
Nothing <wwwaap@gmail.com>
2020-06-17 17:09:50 +0800

    Update 130-surrounded-regions.js

--- 130-surrounded-regions.js
+++ 130-surrounded-regions.js
@@ -31,3 +31,64 @@ function search(board, i, j) {
   search(board, i, j + 1);
   search(board, i, j - 1);
 }
+
+// another
+
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+const solve = (board) => {
+  if (!board || board.length === 0 || board[0].length === 0) return;
+  const n = board.length;
+  const m = board[0].length;
+    const dirs = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+  const bfs = (board, n, m, i, j) => {
+    const queue = [];
+    queue.push([i, j]);
+    board[i][j] = "1";
+    while (queue.length > 0) {
+      const pos = queue.shift();
+      for (let k = 0; k < 4; k++) {
+        i = pos[0] + dirs[k][0];
+        j = pos[1] + dirs[k][1];
+        if (i >= 0 && i < n && j >= 0 && j < m && board[i][j] === "O") {
+          board[i][j] = "1";
+          queue.push([i, j]);
+        }
+      }
+    }
+  };
+  // scan the borders and mark the 'O's to '1'
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (
+        (i === 0 || i === n - 1 || j === 0 || j === m - 1) &&
+        board[i][j] === "O"
+      ) {
+        bfs(board, n, m, i, j);
+      }
+    }
+  }
+  // scan the inner area and mark the 'O's to 'X'
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      if (board[i][j] === "O") {
+        board[i][j] = "X";
+      }
+    }
+  }
+  // reset all the '1's to 'O's
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (board[i][j] === "1") {
+        board[i][j] = "O";
+      }
+    }
+  }
+};
