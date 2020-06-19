f746b5ba964977d217339099b602cff0d338102b
Nothing <wwwaap@gmail.com>
2020-06-19 21:29:46 +0800

    Update 1044-longest-duplicate-substring.js

--- 1044-longest-duplicate-substring.js
+++ 1044-longest-duplicate-substring.js
@@ -3,14 +3,14 @@
  * @return {string}
  */
 const longestDupSubstring = function(S) {
-  let R = 26,
+  const R = 26,
     MOD = 1e9 + 7
   let lo = 0,
     hi = S.length - 1,
     res = ''
   while (lo < hi) {
-    let len = Math.ceil((lo + hi) / 2)
-    let sub = rabinKarp(S, len)
+    const len = Math.ceil((lo + hi) / 2)
+    const sub = rabinKarp(S, len)
     if (sub !== '') {
       lo = len
       res = sub
@@ -28,23 +28,23 @@ const longestDupSubstring = function(S) {
     for (let i = 1; i < len; i++) {
       RM = (RM * R) % MOD
     }
-    let map = new Map()
+    const map = new Map()
     let num = 0
     // 计算前len个字符串的散列值
     for (let i = 0; i < len; i++) {
-      let code = str.charCodeAt(i) - aCode
+      const code = str.charCodeAt(i) - aCode
       num = (num * R + code) % MOD
     }
     map.set(num, 0)
     // 后续计算散列值
     for (let i = 0; i < str.length - len; i++) {
-      let preCode = str.charCodeAt(i) - aCode,
+      const preCode = str.charCodeAt(i) - aCode,
         curCode = str.charCodeAt(i + len) - aCode
       num = (num + MOD - ((preCode * RM) % MOD)) % MOD
       num = (num * R + curCode) % MOD
       if (map.has(num)) {
-        let sub = str.substring(i + 1, i + 1 + len)
-        let preId = map.get(num),
+        const sub = str.substring(i + 1, i + 1 + len)
+        const preId = map.get(num),
           preSub = str.substring(preId, preId + len)
         if (sub === preSub) return sub
       }
