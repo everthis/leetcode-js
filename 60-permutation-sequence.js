2c053475c5fca9d6a4df2b5711ac01d646db8115
Nothing <wwwaap@gmail.com>
2020-06-20 16:48:20 +0800

    Update 60-permutation-sequence.js

--- 60-permutation-sequence.js
+++ 60-permutation-sequence.js
@@ -3,19 +3,19 @@
  * @param {number} k
  * @return {string}
  */
-const getPermutation = function(n, k) {
-    let sb = ''
-    const num = []
-    let fact = 1
-    for (let i = 1; i <= n; i++) {
-      fact *= i
-      num.push(i)
-    }
-    for(let i = 0, l = k - 1; i < n; i++ ) {
-      fact = Math.floor( fact / (n - i) )
-      let index = Math.floor( l / fact )
-      sb += num.splice(index, 1)[0]
-      l -= index * fact
-    }
-    return sb
-};
+const getPermutation = function (n, k) {
+  let sb = ''
+  const num = []
+  let fact = 1
+  for (let i = 1; i <= n; i++) {
+    fact *= i
+    num.push(i)
+  }
+  for (let i = 0, l = k - 1; i < n; i++) {
+    fact = Math.floor(fact / (n - i))
+    const index = Math.floor(l / fact)
+    sb += num.splice(index, 1)[0]
+    l -= index * fact
+  }
+  return sb
+}
