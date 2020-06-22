1a27d7155c75db4a31e0c831f549a098937db131
Nothing <wwwaap@gmail.com>
2020-06-22 20:24:35 +0800

    Update 174-dungeon-game.js

--- 174-dungeon-game.js
+++ 174-dungeon-game.js
@@ -17,3 +17,24 @@ const calculateMinimumHP = function(dungeon) {
   }
   return dungeon[0][0]
 };
+
+// another
+
+/**
+ * @param {number[][]} dungeon
+ * @return {number}
+ */
+const calculateMinimumHP = function (dungeon) {
+  const n = dungeon.length,
+    m = dungeon[0].length
+  const dp = Array(n + 1).fill(Number.MAX_VALUE)
+  dp[n - 1] = 1
+  for (let j = m - 1; j >= 0; j--) {
+    for (let i = n - 1; i >= 0; i--) {
+      dp[i] = Math.min(dp[i], dp[i + 1]) - dungeon[i][j]
+      dp[i] = Math.max(1, dp[i])
+    }
+  }
+  return dp[0]
+}
+
