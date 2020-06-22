6426abfb4a24187ea8947e44f14148ea34a01d29
Nothing <wwwaap@gmail.com>
2020-06-22 20:36:33 +0800

    Update 174-dungeon-game.js

--- 174-dungeon-game.js
+++ 174-dungeon-game.js
@@ -2,21 +2,25 @@
  * @param {number[][]} dungeon
  * @return {number}
  */
-const calculateMinimumHP = function(dungeon) {
-  if(dungeon.length === 0) return 1 
-  const rows = dungeon.length
-  const cols = dungeon[0].length
-
-  for(let i = rows - 1 ; i >= 0; i--) {
-    for(let j = cols - 1; j >= 0; j--) {
-      if(i==rows-1 && j==cols-1) dungeon[i][j]=Math.max(1, 1-dungeon[i][j]);
-      else if(i==rows-1) dungeon[i][j]=Math.max(1, dungeon[i][j+1]-dungeon[i][j]);
-      else if(j==cols-1) dungeon[i][j]=Math.max(1, dungeon[i+1][j]-dungeon[i][j]);
-      else dungeon[i][j]=Math.max(1, Math.min(dungeon[i+1][j], dungeon[i][j+1])-dungeon[i][j]);
+const calculateMinimumHP = function (dungeon) {
+  const M = dungeon.length
+  const N = dungeon[0].length
+  // hp[i][j] represents the min hp needed at position (i, j)
+  // Add dummy row and column at bottom and right side
+  const hp = Array.from({ length: M + 1 }, () =>
+    Array(N + 1).fill(Number.MAX_VALUE)
+  )
+  hp[M][N - 1] = 1
+  hp[M - 1][N] = 1
+  for (let i = M - 1; i >= 0; i--) {
+    for (let j = N - 1; j >= 0; j--) {
+      const need = Math.min(hp[i + 1][j], hp[i][j + 1]) - dungeon[i][j]
+      hp[i][j] = need <= 0 ? 1 : need
     }
   }
-  return dungeon[0][0]
-};
+  return hp[0][0]
+}
+
 
 // another
 
