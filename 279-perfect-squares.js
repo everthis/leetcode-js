0249fc40ba8f087153a0a04d1d7d117455b60689
Nothing <wwwaap@gmail.com>
2020-06-27 20:49:36 +0800

    Update 279-perfect-squares.js

--- 279-perfect-squares.js
+++ 279-perfect-squares.js
@@ -39,3 +39,42 @@ const numSquares = function (n) {
   }
   return cntPerfectSquares[n]
 }
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const numSquares = function (n) {
+  // Based on Lagrange's Four Square theorem, there
+  // are only 4 possible results: 1, 2, 3, 4.
+  // If n is a perfect square, return 1.
+  if (is_square(n)) {
+    return 1
+  }
+  // The result is 4 if and only if n can be written in the
+  // form of 4^k*(8*m + 7). Please refer to
+  // Legendre's three-square theorem.
+  while ((n & 3) === 0) {
+    // n%4 == 0
+    n >>= 2
+  }
+  if ((n & 7) === 7) {
+    // n%8 == 7
+    return 4
+  }
+  // Check whether 2 is the result.
+  let sqrt_n = Math.sqrt(n) >> 0
+  for (let i = 1; i <= sqrt_n; i++) {
+    if (is_square(n - i * i)) {
+      return 2
+    }
+  }
+  return 3
+  function is_square(n) {
+    const sqrt_n = Math.sqrt(n) >> 0
+    return sqrt_n * sqrt_n == n
+  }
+}
+
