c9d257edcb0e799cb280c32d724f7912525a9239
Nothing <wwwaap@gmail.com>
2020-07-01 16:47:00 +0800

    Update 441-arranging-coins.js

--- 441-arranging-coins.js
+++ 441-arranging-coins.js
@@ -15,3 +15,13 @@ const arrangeCoins = function(n) {
 
     return num
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const arrangeCoins = function(n) {
+  return (-1 + Math.sqrt(1+4*2*n)) >> 1
+};
