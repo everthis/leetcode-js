9380f84c8f9da35630fc05958311e4e1cf6c4b12
Nothing <wwwaap@gmail.com>
2020-07-05 22:55:19 +0800

    Update 461-hamming-distance.js

--- 461-hamming-distance.js
+++ 461-hamming-distance.js
@@ -12,3 +12,17 @@ const hammingDistance = function (x, y) {
   }
   return d
 }
+
+// another
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const hammingDistance = function (x, y) {
+  let n = x ^ y
+  n = n - ((n >> 1) & 0x55555555)
+  n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+  return (((n + (n >> 4)) & 0xf0f0f0f) * 0x1010101) >> 24
+}
