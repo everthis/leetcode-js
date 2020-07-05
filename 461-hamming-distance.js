88f886a3b82c34b014a5ca9bc4afdc8789787f6d
Nothing <wwwaap@gmail.com>
2020-07-05 22:59:52 +0800

    Update 461-hamming-distance.js

--- 461-hamming-distance.js
+++ 461-hamming-distance.js
@@ -26,3 +26,18 @@ const hammingDistance = function (x, y) {
   n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
   return (((n + (n >> 4)) & 0xf0f0f0f) * 0x1010101) >> 24
 }
+
+// another
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const hammingDistance = function (x, y) {
+  let n = x ^ y
+  let tmp = n - ((n >> 1) & 033333333333) - ((n >> 2) & 011111111111);
+  return ((tmp + (tmp >> 3)) & 030707070707) % 63;
+}
+
+// https://tech.liuchao.me/2016/11/count-bits-of-integer/
