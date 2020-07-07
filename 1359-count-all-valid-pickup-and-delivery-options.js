86f294cdec45ee9e824b970699b035df48a6da3c
Nothing <wwwaap@gmail.com>
2020-07-07 10:52:32 +0800

    Update 1359-count-all-valid-pickup-and-delivery-options.js

--- 1359-count-all-valid-pickup-and-delivery-options.js
+++ 1359-count-all-valid-pickup-and-delivery-options.js
@@ -10,3 +10,17 @@ const countOrders = function(n) {
   }
   return res
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const countOrders = function(n) {
+  let res = 1
+  const MOD = 10 ** 9 + 7
+  for(let i = 1; i <= n; i++) res = res * i % MOD
+  for(let i = 1; i < 2 * n; i += 2) res = res * i % MOD
+  return res
+};
