4189e0cb7f451f030b38ee2c485700bba3582ffa
Nothing <wwwaap@gmail.com>
2020-07-08 22:39:51 +0800

    Update 1340-jump-game-v.js

--- 1340-jump-game-v.js
+++ 1340-jump-game-v.js
@@ -4,6 +4,39 @@
  * @return {number}
  */
 const maxJumps = function (arr, d) {
+  const map = {}
+  let max = 1
+  for (let i = 0; i < arr.length; i++) {
+    max = Math.max(max, calc(i))
+  }
+  return max
+
+  function calc(i) {
+    if (map[i]) return map[i]
+    let max = 1
+    const left = Math.max(0, i - d)
+    for (let j = i - 1; j >= left; j--) {
+      if (arr[j] >= arr[i]) break
+      if (arr[j] < arr[i]) max = Math.max(max, calc(j) + 1)
+    }
+    const right = Math.min(arr.length - 1, i + d)
+    for (let j = i + 1; j <= right; j++) {
+      if (arr[j] >= arr[i]) break
+      if (arr[j] < arr[i]) max = Math.max(max, calc(j) + 1)
+    }
+    map[i] = max
+    return map[i]
+  }
+}
+
+// another
+
+/**
+ * @param {number[]} arr
+ * @param {number} d
+ * @return {number}
+ */
+const maxJumps = function (arr, d) {
   const cache = new Array(arr.length)
   const diffs = [1, -1]
   const dfs = (i) => {
