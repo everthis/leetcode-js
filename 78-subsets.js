5047cd2826ddf6b48ced058983ee0f557ebb088f
Nothing <wwwaap@gmail.com>
2020-07-11 20:07:39 +0800

    Update 78-subsets.js

--- 78-subsets.js
+++ 78-subsets.js
@@ -21,3 +21,22 @@ function subsets(nums) {
 }
 
 console.log(subsets([1, 2, 3]));
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+function subsets(nums) {
+  const subs = [[]]
+  for (let num of nums) {
+    const n = subs.length
+    for (let i = 0; i < n; i++) {
+      subs.push(subs[i].slice(0))
+      subs[subs.length - 1].push(num)
+    }
+  }
+  return subs
+}
