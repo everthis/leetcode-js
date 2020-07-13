a682549a2427a6ba2eaa7a23916a5f3cd7cf1142
Nothing <wwwaap@gmail.com>
2020-07-13 16:17:28 +0800

    Update 100-same-tree.js

--- 100-same-tree.js
+++ 100-same-tree.js
@@ -16,3 +16,24 @@ const isSameTree = function(p, q) {
   if(p == null || q == null || p.val !== q.val) return false
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 };
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = function(p, q) {
+  if(p == null || q == null) return p === q
+  if(p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
