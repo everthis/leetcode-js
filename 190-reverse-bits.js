1f3fc0a4e60446bb1ce806e27065d2e16da3c8b6
Nothing <wwwaap@gmail.com>
2020-07-13 08:32:24 +0800

    Update 190-reverse-bits.js

--- 190-reverse-bits.js
+++ 190-reverse-bits.js
@@ -13,3 +13,17 @@ const reverseBits = function(n) {
   }
   return count;
 };
+
+// another
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+const reverseBits = function(n) {
+  const b = n.toString(2)
+  const leadingZeroes = b.padStart(32,'0')
+  const rev = leadingZeroes.split('')
+  rev.reverse()
+  return parseInt(rev.join(''), 2)
+};
