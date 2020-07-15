a16a08648d7042a820f96f9e0a0fb1f3a7118674
Nothing <wwwaap@gmail.com>
2020-07-15 20:18:06 +0800

    Update 151-reverse-words-in-a-string.js

--- 151-reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string.js
@@ -9,3 +9,28 @@ const reverseWords = function(str) {
     .reverse()
     .join(" ");
 };
+
+// another
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+const reverseWords = function (s) {
+  let sb = ''
+  const n = s.length
+  let i = n - 1
+  while (i >= 0) {
+    if (s.charAt(i) == ' ') {
+      i--
+      continue
+    }
+    let j = i - 1
+    while (j >= 0 && s.charAt(j) != ' ') j--
+    sb += ' '
+    sb += s.slice(j + 1, i + 1)
+    i = j - 1
+  }
+  if (sb.length > 0) sb = sb.slice(1)
+  return sb
+}
