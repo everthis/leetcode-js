e32bad1733262d7b6a1dc2f29553c55cce602439
Nothing <wwwaap@gmail.com>
2020-07-20 22:36:56 +0800

    Update 203-remove-linked-list-elements.js

--- 203-remove-linked-list-elements.js
+++ 203-remove-linked-list-elements.js
@@ -27,3 +27,23 @@ const removeElements = function(head, val) {
   }
   return dummy.next
 };
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+const removeElements = function(head, val) {
+  if (head === null) return null;
+  head.next = removeElements(head.next, val);
+  return head.val === val ? head.next : head;
+};
