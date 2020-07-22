747f202799d59f2983c0a81516655787400e8931
Nothing <wwwaap@gmail.com>
2020-07-22 19:39:15 +0800

    Update 103-binary-tree-zigzag-level-order-traversal.js

--- 103-binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal.js
@@ -75,3 +75,34 @@ const zigzagLevelOrder = function (root) {
 
   return zigzag;
 };
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const zigzagLevelOrder = function (root) {
+  const res = []
+  dfs(root, res, 0)
+  return res
+  
+  function dfs(node, res, level) {
+    if(node == null) return
+    if(res.length <= level) res.push([])
+    const tmp = res[level]
+    if(level % 2 === 0) tmp.push(node.val)
+    else tmp.unshift(node.val)
+    
+    dfs(node.left, res, level + 1)
+    dfs(node.right, res, level + 1)
+  }
+};
