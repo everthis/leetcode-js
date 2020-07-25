2f2580eacfebd15b4c2719945c5d367773c564ce
Nothing <wwwaap@gmail.com>
2020-07-25 17:30:30 +0800

    Update 153-find-minimum-in-rotated-sorted-array.js

--- 153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array.js
@@ -2,12 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findMin = function(nums) {
-    if(nums.length === 1) return nums[0]
-    for(let i = 1; i < nums.length; i++) {
-        if(nums[i] - nums[i - 1] < 0) {
-            return nums[i]
-        }
-    }
-    return nums[0]
-};
+const findMin = function (nums) {
+  let low = 0,
+    high = nums.length - 1
+  // loop invariant: 1. low < high
+  //                 2. mid != high and thus A[mid] != A[high] (no duplicate exists)
+  //                 3. minimum is between [low, high]
+  // The proof that the loop will exit: after each iteration either the 'high' decreases
+  // or the 'low' increases, so the interval [low, high] will always shrink.
+  while (low < high) {
+    const mid = low + ((high - low) >> 1)
+    if (nums[mid] < nums[high]) high = mid
+    else if (nums[mid] > nums[high]) low = mid + 1
+  }
+
+  return nums[low]
+}
