45e16462f73c58d155f0d1cea02344aeeff37080
Nothing <wwwaap@gmail.com>
2020-07-26 22:39:46 +0800

    Update 258-add-digits.js

--- 258-add-digits.js
+++ 258-add-digits.js
@@ -13,5 +13,15 @@ const addDigits = function(num) {
   return +res;
 };
 
+// another
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const addDigits = function(num) {
+  return 1 + (num - 1) % 9;
+};
+
 console.log(addDigits(0));
 console.log(addDigits(38));
