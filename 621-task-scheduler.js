3bc0635b2c9f2d74d2c75f192baad3f64805e16e
Nothing <wwwaap@gmail.com>
2020-07-28 20:32:01 +0800

    Update 621-task-scheduler.js

--- 621-task-scheduler.js
+++ 621-task-scheduler.js
@@ -15,3 +15,27 @@ const leastInterval = function(tasks, n) {
   }
   return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
 };
+
+// another
+
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+const leastInterval = function(tasks, n) {
+  const hash = {};
+  for(let task of tasks) {
+    hash[task] = hash[task] + 1 || 1
+  }
+  let max = 0, count = 0;
+  for(let el in hash) {
+    if(hash[el] > max) {
+      max = hash[el];
+      count = 1
+    } else if(hash[el] === max) {
+      count++;
+    }
+  }
+  return Math.max((max - 1) *  (n + 1)  +  count, tasks.length)
+};
