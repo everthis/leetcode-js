629eae4103c932088259d0787925e285d093acb9
Nothing <wwwaap@gmail.com>
2020-07-29 20:23:50 +0800

    Update 309-best-time-to-buy-and-sell-stock-with-cooldown.js

--- 309-best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -9,14 +9,14 @@ const maxProfit = function(prices) {
 
   const length = prices.length;
   // buy[i]: max profit if the first "i" days end with a "buy" day
-  let buy = Array(length + 1).fill(0); // new int[length + 1];
+  const buy = Array(length + 1).fill(0); 
   // buy[i]: max profit if the first "i" days end with a "sell" day
-  let sell = Array(length + 1).fill(0); // new int[length + 1];
+  const sell = Array(length + 1).fill(0); 
 
   buy[1] = -prices[0];
 
   for (let i = 2; i <= length; i++) {
-    let price = prices[i - 1];
+    const price = prices[i - 1];
     buy[i] = Math.max(buy[i - 1], sell[i - 2] - price);
     sell[i] = Math.max(sell[i - 1], buy[i - 1] + price);
   }
