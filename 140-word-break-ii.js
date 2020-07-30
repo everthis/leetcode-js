5ce9854e150cfc2f7eea38dd6743b37656bfd0ab
Nothing <wwwaap@gmail.com>
2020-07-30 21:00:16 +0800

    Update 140-word-break-ii.js

--- 140-word-break-ii.js
+++ 140-word-break-ii.js
@@ -24,3 +24,36 @@ function backTrack(s, wordDict, mem) {
     mem[s] = result
     return result
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+const wordBreak = function (s, wordDict) {
+  const dictSet = new Set(wordDict)
+  const memo = {}
+  function dfs(start) {
+    if (start > s.length - 1) {
+      return [[]]
+    }
+    if (memo[start] !== undefined) {
+      return memo[start]
+    }
+    const out = []
+    for (let i = start; i < s.length; i++) {
+      const substr = s.substring(start, i + 1)
+      if (dictSet.has(substr)) {
+        let next = dfs(i + 1)
+        for (let n of next) {
+          out.push([substr, ...n])
+        }
+      }
+    }
+    return (memo[start] = out)
+  }
+  const res = dfs(0)
+  return res.filter((a) => a.join('') === s).map((a) => a.join(' '))
+}
