cba8825eb7d80fbc080cf16e07b8059a85621c21
Nothing <wwwaap@gmail.com>
2020-08-06 16:53:07 +0800

    Update 442-find-all-duplicates-in-an-array.js

--- 442-find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array.js
@@ -19,3 +19,19 @@ const findDuplicates = function(nums) {
 
 console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1]));
 console.log(findDuplicates([10, 2, 5, 10, 9, 1, 1, 4, 3, 7]));
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const findDuplicates = function(nums) {
+  const res = []
+  for(let i = 0, len = nums.length; i < len; i++) {
+    const idx = Math.abs(nums[i]) - 1
+    if(nums[idx] < 0) res.push(idx + 1)
+    nums[idx] = -nums[idx]
+  }
+  return res;
+};
