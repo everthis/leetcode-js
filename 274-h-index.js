29fe46f5592b49ab317aa9d16fdd3995326e6828
Nothing <wwwaap@gmail.com>
2020-08-11 15:32:03 +0800

    Update 274-h-index.js

--- 274-h-index.js
+++ 274-h-index.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 const hIndex = function(citations) {
+  const n = citations.length
+  const arr = Array(n + 1).fill(0)
+  for(let e of citations) {
+    if(e >= n) arr[n]++
+    else arr[e]++
+  }
+  for(let i = n, sum = 0; i >= 0; i--) {
+    sum += arr[i]
+    if(sum >= i) return i
+  }
+  return 0
+};
+
+// another
+
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+const hIndex = function(citations) {
   citations = citations.sort((a, b) => b - a)
 
   for (let i = 0, len = citations.length; i < len; i++) {
