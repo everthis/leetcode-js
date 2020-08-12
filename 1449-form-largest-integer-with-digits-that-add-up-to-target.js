1b1fed49feb50615166e94b2ea751fafbfcab6c9
Nothing <wwwaap@gmail.com>
2020-08-12 10:50:32 +0800

    Create 1449-form-largest-integer-with-digits-that-add-up-to-target.js

---
+++ 1449-form-largest-integer-with-digits-that-add-up-to-target.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} cost
+ * @param {number} target
+ * @return {string}
+ */
+const largestNumber = function(cost, target) {
+  const m = new Map()
+  const res = dfs(cost, 1, target, m)
+  return res.indexOf('0') !== -1 ? '0' : res
+};
+function dfs(cost, index, remain, m) {
+  if(remain === 0) return ''
+  if(remain < 0 || index === cost.length + 1) return '0'
+  const k = `${index}-${remain}`
+  if(m.has(k)) return m.get(k)
+  const take = '' + index + dfs(cost, 1, remain - cost[index - 1], m)
+  const skip = dfs(cost, index + 1, remain, m)
+  const res = getBigger(take, skip)
+  m.set(k, res)
+  return res
+}
+function getBigger(num1, num2) {
+  if(num1.indexOf('0') !== -1) return num2
+  if(num2.indexOf('0') !== -1) return num1
+  if(num1.length > num2.length) return num1
+  else if(num1.length < num2.length) return num2
+  else return num1 > num2 ? num1 : num2
+}
