736f66a25f5714621b801ca1f0ea63078bd7c77b
Nothing <wwwaap@gmail.com>
2020-08-14 15:15:39 +0800

    Update 409-longest-palindrome.js

--- 409-longest-palindrome.js
+++ 409-longest-palindrome.js
@@ -2,6 +2,29 @@
  * @param {string} s
  * @return {number}
  */
+const longestPalindrome = function (s) {
+  const set = new Set()
+  let counter = 0
+  for (let i = 0; i < s.length; i++) {
+    const currentChar = s[i]
+    if (set.has(currentChar)) {
+      counter++
+      set.delete(currentChar)
+    } else {
+      set.add(currentChar)
+    }
+  }
+  counter *= 2
+  if (set.size > 0) counter++
+  return counter
+}
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 const longestPalindrome = function(s) {
   const hash = {};
   let c;
