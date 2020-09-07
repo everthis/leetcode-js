e3bcd240b4d9682dd5975aefd258e244fc74f5fe
Nothing <wwwaap@gmail.com>
2020-09-07 10:41:48 +0800

    Update 1570-dot-product-of-two-sparse-vectors.js

--- 1570-dot-product-of-two-sparse-vectors.js
+++ 1570-dot-product-of-two-sparse-vectors.js
@@ -26,3 +26,42 @@ SparseVector.prototype.dotProduct = function(vec) {
 // let v1 = new SparseVector(nums1);
 // let v2 = new SparseVector(nums2);
 // let ans = v1.dotProduct(v2);
+
+// another
+
+class SparseVector {
+  /**
+   * @param {number[]} nums
+   * @return {SparseVector}
+   */
+  constructor(nums) {
+    // Space: O(n)
+    this.seen = new Map() // index -> value
+    for (let i = 0; i < nums.length; ++i) {
+      if (nums[i] !== 0) {
+        this.seen.set(i, nums[i])
+      }
+    }
+  }
+
+  /**
+   * Return the dotProduct of two sparse vectors
+   * @param {SparseVector} vec
+   * @return {number}
+   */
+  dotProduct(vec) {
+    // Time: O(n)
+    let sum = 0
+    for (const [i, val] of vec.seen) {
+      if (this.seen.has(i)) {
+        sum += val * this.seen.get(i)
+      }
+    }
+    return sum
+  }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// let v1 = new SparseVector(nums1);
+// let v2 = new SparseVector(nums2);
+// let ans = v1.dotProduct(v2);
