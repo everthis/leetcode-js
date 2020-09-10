5aa5218f20f351a2a4cfe4bd58567b67f1d4c9a3
Nothing <wwwaap@gmail.com>
2020-09-10 21:58:45 +0800

    Update 1490-clone-n-ary-tree.js

--- 1490-clone-n-ary-tree.js
+++ 1490-clone-n-ary-tree.js
@@ -18,3 +18,36 @@ const cloneTree = function(root) {
   }
   return node
 };
+
+// another
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.children = children === undefined ? [] : children;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+const cloneTree = function(root) {
+  if (root === null) return null
+  const Q = []
+  const rootCopy = new Node(root.val)
+  Q.push([root, rootCopy])
+  while (Q.length) {
+    const temp = Q.shift()
+    const node = temp[0]
+    const copy = temp[1]
+    node.children.forEach((child) => {
+      const copyChild = new Node(child.val)
+      copy.children.push(copyChild)
+      Q.push([child, copyChild])
+    })
+  }
+
+  return rootCopy
+};
