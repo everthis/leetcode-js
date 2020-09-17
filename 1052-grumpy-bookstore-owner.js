78faf3254cf3bc37136ba97e73692248fbfdb618
Nothing <wwwaap@gmail.com>
2020-09-17 20:45:23 +0800

    Update 1052-grumpy-bookstore-owner.js

--- 1052-grumpy-bookstore-owner.js
+++ 1052-grumpy-bookstore-owner.js
@@ -24,3 +24,28 @@ const maxSatisfied = function(customers, grumpy, X) {
 
   return totalSatisfiedCustomers + max
 }
+
+// another
+
+/**
+ * @param {number[]} customers
+ * @param {number[]} grumpy
+ * @param {number} X
+ * @return {number}
+ */
+const maxSatisfied = function (customers, grumpy, X) {
+  let satisfied = 0,
+    maxMakeSatisfied = 0
+  for (let i = 0, winOfMakeSatisfied = 0; i < grumpy.length; ++i) {
+    if (grumpy[i] === 0) {
+      satisfied += customers[i]
+    } else {
+      winOfMakeSatisfied += customers[i]
+    }
+    if (i >= X) {
+      winOfMakeSatisfied -= grumpy[i - X] * customers[i - X]
+    }
+    maxMakeSatisfied = Math.max(winOfMakeSatisfied, maxMakeSatisfied)
+  }
+  return satisfied + maxMakeSatisfied
+}
