64dd4efee5be920ec569cb8a2dbdc8ab4c77b32a
Nothing <wwwaap@gmail.com>
2020-09-19 11:35:46 +0800

    Update 992-subarrays-with-k-different-integers.js

--- 992-subarrays-with-k-different-integers.js
+++ 992-subarrays-with-k-different-integers.js
@@ -7,7 +7,7 @@ const subarraysWithKDistinct = function(A, K) {
   let res = 0
   let prefix = 0
   const m = new Array(A.length + 1).fill(0)
-  for (let i = 0, j = 0, cnt = 0; i < A.length; i++) {
+  for (let i = 0, j = 0, cnt = 0, len = A.length; i < len; i++) {
     if (m[A[i]]++ === 0) cnt++
     if (cnt > K) {
       m[A[j++]]--
