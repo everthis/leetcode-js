0b5af6903131fa4feb87d4f02b19546ce5c0f3da
Nothing <wwwaap@gmail.com>
2020-09-19 12:46:56 +0800

    Update 992-subarrays-with-k-different-integers.js

--- 992-subarrays-with-k-different-integers.js
+++ 992-subarrays-with-k-different-integers.js
@@ -22,3 +22,31 @@ const subarraysWithKDistinct = function(A, K) {
   }
   return res
 }
+
+// another
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+const subarraysWithKDistinct = function (A, K) {
+  const atMostK = (A, K) => {
+    let left = 0,
+      right = 0,
+      counter = 0,
+      count = [],
+      result = 0
+    while (right < A.length) {
+      let currentR = A[right++]
+      !count[currentR] ? ((count[currentR] = 1), counter++) : count[currentR]++
+      while (counter > K) {
+        let currentL = A[left++]
+        if (--count[currentL] == 0) counter--
+      }
+      result += right - left
+    }
+    return result
+  }
+  return atMostK(A, K) - atMostK(A, K - 1)
+}
