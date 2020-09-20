ad3a6500f50078289f5b0ac59f1c4b019982e7ef
Nothing <wwwaap@gmail.com>
2020-09-20 13:22:50 +0800

    Create 1593-split-a-string-into-the-max-number-of-unique-substrings.js

---
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maxUniqueSplit = function(s) {
+  return bt(s, '', 0, new Set())
+};
+
+function bt(str, cur, idx, useds) {
+  if(idx === str.length) return useds.size
+  cur += str[idx]
+  if(useds.has(cur)) return bt(str, cur, idx +1, useds)
+  else {
+    let ans = 0
+    useds.add(cur)
+    ans = Math.max(ans, bt(str, '', idx+1, useds))
+    useds.delete(cur)
+    ans = Math.max(ans, bt(str, cur, idx+1, useds))
+    return ans
+  }
+}
