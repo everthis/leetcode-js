a533f9c0c8261c3bb5cb091efddf53b3e6a547ec
Nothing <wwwaap@gmail.com>
2020-09-20 20:41:25 +0800

    Create 1595-minimum-cost-to-connect-two-groups-of-points.js

---
+++ 1595-minimum-cost-to-connect-two-groups-of-points.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} cost
+ * @return {number}
+ */
+const connectTwoGroups = function (cost) {
+  const min = Array(cost[0].length).fill(Infinity)
+  for (let j = 0; j < min.length; j++) {
+    for (let i = 0; i < cost.length; i++) {
+      min[j] = Math.min(min[j], cost[i][j])
+    }
+  }
+  const dp = Array.from({ length: 13 }, () => Array(4096).fill(0))
+  return dfs(cost, min, 0, 0, dp)
+}
+
+function dfs(cost, min, i, mask, dp) {
+  if (dp[i][mask]) return dp[i][mask] - 1
+  let res = i >= cost.length ? 0 : Infinity
+  if (i >= cost.length) {
+    for (let j = 0; j < cost[0].length; j++) {
+      if ((mask & (1 << j)) === 0) res += min[j]
+    }
+  } else {
+    for (let j = 0; j < cost[0].length; j++) {
+      res = Math.min(
+        res,
+        cost[i][j] + dfs(cost, min, i + 1, mask | (1 << j), dp)
+      )
+    }
+  }
+  dp[i][mask] = res + 1
+  return res
+}
