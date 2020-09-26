25aca9a054834b07df3c6dab1ae323524f06877f
Nothing <wwwaap@gmail.com>
2020-09-26 11:35:04 +0800

    Update 1190-reverse-substrings-between-each-pair-of-parentheses.js

--- 1190-reverse-substrings-between-each-pair-of-parentheses.js
+++ 1190-reverse-substrings-between-each-pair-of-parentheses.js
@@ -23,3 +23,33 @@ const reverseParentheses = function(s) {
   }
   return res[0]
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseParentheses = function(s) {
+  const n = s.length
+  const stack = []
+  const pair = []
+  for(let i = 0; i < n; i++) {
+    if(s[i] === '(') stack.push(i)
+    else if(s[i] === ')') {
+      const tmp = stack.pop()
+      pair[i] = tmp
+      pair[tmp] = i
+    }
+  }
+  let res = ''
+  for(let i = 0, d = 1; i < n; i += d) {
+    if(s[i] === '(' || s[i] ===')') {
+      i = pair[i]
+      d = -d
+    } else {
+      res += s[i]
+    }
+  }
+  return res
+}
