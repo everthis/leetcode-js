352b6b58e7927c2d10aad52e9a75675ee7782196
Nothing <wwwaap@gmail.com>
2020-09-29 08:58:09 +0800

    Update 55-jump-game.js

--- 55-jump-game.js
+++ 55-jump-game.js
@@ -11,3 +11,20 @@ const canJump = function(nums) {
   }
   return max >= nums.length - 1
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canJump = function(nums) {
+  let max = 0
+  const n = nums.length
+  for(let i = 0; i < n; i++) {
+    if(max < i) return false
+    max = Math.max(max, i + nums[i])
+    if(max >= n - 1) return true
+  }
+  return max >= n - 1
+};
