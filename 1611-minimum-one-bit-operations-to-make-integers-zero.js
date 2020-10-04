40e3371e13ec99ca2fa26401ba3ef8b92de80bed
Nothing <wwwaap@gmail.com>
2020-10-04 13:57:38 +0800

    Create 1611-minimum-one-bit-operations-to-make-integers-zero.js

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const minimumOneBitOperations = function (n) {
+  let sign = 1,
+    res = 0;
+  while (n) {
+    res += n ^ ((n - 1) * sign);
+    n &= n - 1;
+    sign = -sign;
+  }
+  return Math.abs(res);
+};
