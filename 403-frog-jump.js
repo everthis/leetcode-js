daf06828e74d2d94733b664590f338d3f3f20011
Nothing <wwwaap@gmail.com>
2020-10-04 09:08:37 +0800

    Update 403-frog-jump.js

--- 403-frog-jump.js
+++ 403-frog-jump.js
@@ -8,18 +8,18 @@ const canCross = function(stones) {
       return false
     }
   }
-  let count = new Set(stones)
-  let lastStone = stones[stones.length - 1]
-  let position = [0]
-  let jump = [0]
+  const count = new Set(stones)
+  const lastStone = stones[stones.length - 1]
+  const position = [0]
+  const jump = [0]
   while (position.length > 0) {
-    let nextPosition = position.pop()
-    let nextDistance = jump.pop()
+    const nextPosition = position.pop()
+    const nextDistance = jump.pop()
     for (let i = nextDistance - 1; i <= nextDistance + 1; i++) {
       if (i <= 0) {
         continue
       }
-      let nextStone = nextPosition + i
+      const nextStone = nextPosition + i
       if (nextStone == lastStone) {
         return true
       } else if (count.has(nextStone)) {
