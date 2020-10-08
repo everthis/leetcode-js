4d8ebbe6f1ac8d61ace35b8b4e8e5e189dabcde6
Nothing <wwwaap@gmail.com>
2020-10-08 12:53:58 +0800

    Update 1611-minimum-one-bit-operations-to-make-integers-zero.js

--- 1611-minimum-one-bit-operations-to-make-integers-zero.js
+++ 1611-minimum-one-bit-operations-to-make-integers-zero.js
@@ -27,3 +27,18 @@ const minimumOneBitOperations = function(n) {
   }
   return n;
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const minimumOneBitOperations = function(n) {
+  n ^= n >> 16
+  n ^= n >>  8
+  n ^= n >>  4
+  n ^= n >>  2
+  n ^= n >>  1
+  return n
+};
