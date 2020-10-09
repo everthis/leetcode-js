9b0e2bc795236355d2af439c0ea0e611b0e5dd6c
Nothing <wwwaap@gmail.com>
2020-10-09 19:53:13 +0800

    Update 1608-special-array-with-x-elements-greater-than-or-equal-x.js

--- 1608-special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -11,3 +11,20 @@ const specialArray = function (nums) {
     if(nums[i - 1] < i) return -1
     return i
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const specialArray = function(nums) {
+  nums.sort((a, b) => b - a)
+  let left = 0, right = nums.length
+  while(left <= right) {
+    const mid = left + ((right - left) >> 1)
+    if(mid < nums[mid]) left = mid + 1
+    else right = mid - 1
+  }
+  return left < nums.length && left === nums[left] ? -1 : left
+};
