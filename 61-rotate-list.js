94cd74b5038fb3c7b61e7b7861b13fd83cb37c6e
Nothing <wwwaap@gmail.com>
2020-10-15 08:53:07 +0800

    Update 61-rotate-list.js

--- 61-rotate-list.js
+++ 61-rotate-list.js
@@ -29,3 +29,37 @@ const rotateRight = function(head, k) {
     
     return dummy.next;
 };
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+const rotateRight = function(head, k) {
+  if(head == null) return null
+  let len = 1
+  let tmp = head
+  while(tmp.next) {
+    len++
+    tmp = tmp.next
+  }
+  k = k % len
+  if(k === 0) return head
+  let tail = head
+  for(let i = 1; i < len - k; i++) {
+    tail = tail.next
+  }
+  const newHead = tail.next
+  tmp.next = head
+  tail.next = null
+  return newHead
+};
