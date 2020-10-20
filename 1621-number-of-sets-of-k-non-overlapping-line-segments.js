5aff0ddd53897e925511962c362bae53167fdb3f
Nothing <wwwaap@gmail.com>
2020-10-20 14:55:18 +0800

    Update 1621-number-of-sets-of-k-non-overlapping-line-segments.js

--- 1621-number-of-sets-of-k-non-overlapping-line-segments.js
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments.js
@@ -13,3 +13,27 @@ const numberOfSets = function (n, k) {
   res = res % mod
   return res
 }
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const numberOfSets = function (n, k) {
+  const dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0))
+  const MOD = 10 ** 9 + 7
+  for (let i = 2; i <= n; i++) {
+    dp[i][i - 1] = 1
+    dp[i][1] = (i * (i - 1)) / 2
+  }
+  for (let i = 4; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+      let count =
+        dp[i - 1][j] - dp[i - 2][j] + dp[i - 1][j] + dp[i - 1][j - 1] + MOD
+      dp[i][j] = count % MOD
+    }
+  }
+  return dp[n][k]
+}
