20c8c4d40982c45ec65258b24d0c6a062015036e
Nothing <wwwaap@gmail.com>
2020-10-29 08:50:52 +0800

    Update 1547-minimum-cost-to-cut-a-stick.js

--- 1547-minimum-cost-to-cut-a-stick.js
+++ 1547-minimum-cost-to-cut-a-stick.js
@@ -21,3 +21,27 @@ const minCost = function(n, cuts) {
     return dp[i][j]
   }
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[]} cuts
+ * @return {number}
+ */
+const minCost = function (n, cuts) {
+  cuts.push(0, n)
+  cuts.sort((a, b) => a - b)
+  const N = cuts.length,
+    dp = Array.from({ length: N }, () => Array(N).fill(0))
+  for (let l = 2; l < N; l++) {
+    for (let i = 0; i + l < N; i++) {
+      const j = i + l
+      dp[i][j] = Infinity
+      for (let k = i + 1; k < j; k++) {
+        dp[i][j] = Math.min(dp[i][j], cuts[j] - cuts[i] + dp[i][k] + dp[k][j])
+      }
+    }
+  }
+  return dp[0][N - 1]
+}
