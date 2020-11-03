187fe9330b25dca555040ae477199e442823382d
Nothing <wwwaap@gmail.com>
2020-11-03 11:53:15 +0800

    Update 1639-number-of-ways-to-form-a-target-string-given-a-dictionary.js

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary.js
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary.js
@@ -4,6 +4,34 @@
  * @return {number}
  */
 const numWays = function (words, target) {
+  const m = words[0].length, len = words.length
+  const n = target.length, a = 'a'.charCodeAt(0)
+  const mod = 10 ** 9 + 7
+  const dp = Array(n).fill(0)
+  for(let i = 0; i < m; i++) {
+    const freq = Array(26).fill(0)
+    for(let j = 0; j < len; j++) {
+      freq[words[j].charCodeAt(i) - a]++
+    }
+    for(let j = Math.min(i, n - 1); j >= 0; j--) {
+      const code = target[j].charCodeAt(0) - a
+      if(freq[code] > 0) {
+        dp[j] += (j === 0 ? freq[code] : dp[j - 1] * freq[code])
+        dp[j] %= mod
+      }
+    }
+  }
+  return dp[n - 1]
+}
+
+// another
+
+/**
+ * @param {string[]} words
+ * @param {string} target
+ * @return {number}
+ */
+const numWays = function (words, target) {
   const m = words[0].length
   const n = target.length
   const memo = Array.from({ length: m }, () => Array(n))
