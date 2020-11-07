e2cb751934b7cb1e534b9eeaf940b7e7e307a510
Nothing <wwwaap@gmail.com>
2020-11-07 11:01:53 +0800

    Update 1537-get-the-maximum-score.js

--- 1537-get-the-maximum-score.js
+++ 1537-get-the-maximum-score.js
@@ -70,3 +70,39 @@ const maxSum = function(nums1, nums2) {
   }
   return Math.max(a, b) % mod;
 };
+
+// another
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const maxSum = function(nums1, nums2) {
+  const len1 = nums1.length, len2 = nums2.length
+  const mod = 10 ** 9 + 7
+  const map = new Map()
+  for(let i = 0; i < len1 - 1; i++) {
+    if(!map.has(nums1[i])) map.set(nums1[i], [])
+    map.get(nums1[i]).push(nums1[i + 1])
+  }
+  for(let j = 0; j < len2 - 1; j++) {
+    if(!map.has(nums2[j])) map.set(nums2[j], [])
+    map.get(nums2[j]).push(nums2[j + 1])
+  }
+  const memo = new Map()
+  return Math.max(greedy(nums1[0], map, memo), greedy(nums2[0], map, memo)) % mod
+};
+
+function greedy(cur, map, memo) {
+  if(memo.has(cur)) return memo.get(cur)
+  if(!map.has(cur)) return cur
+  let res = 0
+  for(let next of map.get(cur)) {
+    const tmp = greedy(next, map, memo)
+    if(tmp > res) res = tmp
+  }
+  res += cur
+  memo.set(cur, res)
+  return res
+}
