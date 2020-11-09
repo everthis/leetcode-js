a3d5d4f1a2481c1c475bdde7db9b79a22c61abe9
Nothing <wwwaap@gmail.com>
2020-11-09 09:53:01 +0800

    Update 518-coin-change-2.js

--- 518-coin-change-2.js
+++ 518-coin-change-2.js
@@ -4,17 +4,20 @@
  * @return {number}
  */
 function change(amount, coins) {
-    const dp = Array.from(new Array(coins.length+1), () => new Array(amount + 1).fill(0))
-    dp[0][0] = 1;
-    
-    for (let i = 1; i <= coins.length; i++) {
-        dp[i][0] = 1;
-        for (let j = 1; j <= amount; j++) {
-            dp[i][j] = dp[i-1][j] + (j >= coins[i-1] ? dp[i][j-coins[i-1]] : 0);
-        }
+  const dp = Array.from(new Array(coins.length + 1), () =>
+    new Array(amount + 1).fill(0)
+  )
+  dp[0][0] = 1
+  for (let i = 1; i <= coins.length; i++) {
+    dp[i][0] = 1
+    for (let j = 1; j <= amount; j++) {
+      dp[i][j] =
+        dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0)
     }
-    return dp[coins.length][amount];
+  }
+  return dp[coins.length][amount]
 }
+
 
 // another
 
@@ -27,9 +30,7 @@ const change = function (amount, coins) {
   const dp = Array(amount + 1).fill(0)
   dp[0] = 1
   for (let coin of coins) {
-    for (let i = coin; i < amount + 1; i++) {
-      dp[i] += dp[i - coin]
-    }
+    for (let i = coin; i < amount + 1; i++) dp[i] += dp[i - coin]
   }
   return dp[amount]
 }
