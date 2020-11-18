7f264b9195c632a5790cc642f3168c6cde1f405a
Nothing <wwwaap@gmail.com>
2020-11-18 21:05:22 +0800

    Create 1650-lowest-common-ancestor-of-a-binary-tree-iii.js

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii.js
@@ -0,0 +1,30 @@
+/**
+ * // Definition for a Node.
+ * function Node(val) {
+ *    this.val = val;
+ *    this.left = null;
+ *    this.right = null;
+ *    this.parent = null;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+const lowestCommonAncestor = function(p, q) {
+  const pa = [], qa = []
+  if(up(p, q, pa)) return q
+  if(up(q, p, qa)) return p
+  const set = new Set(pa)
+  for(let i = 0; i < qa.length; i++) {
+    if(set.has(qa[i])) return qa[i]
+  }
+  
+  function up(node, target, arr) {
+    if(node == null) return null
+    if(node === target) return target
+    arr.push(node)
+    return up(node.parent, target, arr)
+  }
+};
