14d50ac4c621ab10ec6cb78c2002515ea1eac8dd
Nothing <wwwaap@gmail.com>
2020-11-20 21:44:25 +0800

    Update 939-minimum-area-rectangle.js

--- 939-minimum-area-rectangle.js
+++ 939-minimum-area-rectangle.js
@@ -25,3 +25,26 @@ const minAreaRect = function(points) {
   }
   return res === Infinity ? 0 : res
 };
+
+// another
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+const minAreaRect = function (points) {
+  let ans = Infinity
+  const isPoint = {}
+  points.forEach(([x, y]) => (isPoint[x * 40000 + y] = true))
+  for (let idx1 = 0; idx1 < points.length - 1; idx1++) {
+    const [x1, y1] = points[idx1]
+    for (let idx2 = idx1 + 1; idx2 < points.length; idx2++) {
+      const [x2, y2] = points[idx2]
+      const area = Math.abs((x1 - x2) * (y1 - y2))
+      if (area === 0 || area >= ans) continue
+      if (isPoint[x1 * 40000 + y2] && isPoint[x2 * 40000 + y1]) ans = area
+    }
+  }
+  return ans !== Infinity ? ans : 0
+}
+
