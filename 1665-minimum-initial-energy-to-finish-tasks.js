54760c540246bbbef5d20314eeb0b931564ef2ac
Nothing <wwwaap@gmail.com>
2020-11-22 13:00:07 +0800

    Create 1665-minimum-initial-energy-to-finish-tasks.js

---
+++ 1665-minimum-initial-energy-to-finish-tasks.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} tasks
+ * @return {number}
+ */
+const minimumEffort = function (a) {
+  let low = 0,
+    high = 1e9
+  for (let x of a) low = Math.max(low, x[1])
+  a.sort((lhs, rhs) => (lhs[1] - lhs[0] > rhs[1] - rhs[0] ? -1 : 1))
+  let n = a.length
+  while (low != high) {
+    let mid = low + ((high - low) >> 1)
+    let found = false
+    let rem = mid
+    for (let i = 0; i < n; ++i) {
+      if (rem < a[i][1]) {
+        found = true
+        break
+      }
+      rem -= a[i][0]
+    }
+    if (found) {
+      low = mid + 1
+    } else {
+      high = mid
+    }
+  }
+  return high
+}
