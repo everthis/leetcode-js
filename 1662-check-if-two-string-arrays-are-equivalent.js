3742f8bec5c8319bc8bdaa952f17a0ebfcc24d78
Nothing <wwwaap@gmail.com>
2020-11-22 12:58:20 +0800

    Create 1662-check-if-two-string-arrays-are-equivalent.js

---
+++ 1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+const arrayStringsAreEqual = function(word1, word2) {
+  return word1.join('') === word2.join('')
+};
