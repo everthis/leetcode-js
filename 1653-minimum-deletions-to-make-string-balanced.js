128ef69aca768495c739c302454c566b24e7d09f
Nothing <wwwaap@gmail.com>
2020-11-24 22:05:46 +0800

    Update 1653-minimum-deletions-to-make-string-balanced.js

--- 1653-minimum-deletions-to-make-string-balanced.js
+++ 1653-minimum-deletions-to-make-string-balanced.js
@@ -4,6 +4,28 @@
  */
 const minimumDeletions = function(s) {
   const len = s.length
+  const dp = Array(len + 1).fill(0)
+  let bcount = 0
+  for(let i = 1; i <= len; i++) {
+    if(s[i - 1] === 'a') {
+      dp[i] = Math.min(dp[i - 1] + 1, bcount)
+    } else {
+      dp[i] = dp[i - 1]
+      bcount++
+    }
+  }
+  
+  return dp[len]
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minimumDeletions = function(s) {
+  const len = s.length
   const stack = []
   let res = 0
   for(let i = 0; i < len; i++) {
