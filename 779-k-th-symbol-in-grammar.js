236397badff524d8faaa81ae66925e6c4aa44433
Nothing <wwwaap@gmail.com>
2020-11-26 21:46:12 +0800

    Update 779-k-th-symbol-in-grammar.js

--- 779-k-th-symbol-in-grammar.js
+++ 779-k-th-symbol-in-grammar.js
@@ -8,3 +8,17 @@ const kthGrammar = function(N, K) {
 	if (K % 2 === 0) return (kthGrammar(N - 1, K / 2) === 0) ? 1 : 0;
 	else return (kthGrammar(N - 1, (K + 1) / 2) === 0) ? 0 : 1;
 };
+
+/**
+
+// think of the problem like this
+/*        0
+      /       \
+     0          1
+   /   \      /    \
+   0     1    1      0
+ / \     / \   / \   / \
+ 0  1   1   0  1  0  0  1
+*/
+
+*/
