7365ceb18f849120ddecaea4dc23cd14f13dd350
Nothing <wwwaap@gmail.com>
2020-11-26 21:37:21 +0800

    Create 779-k-th-symbol-in-grammar.js

---
+++ 779-k-th-symbol-in-grammar.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} N
+ * @param {number} K
+ * @return {number}
+ */
+const kthGrammar = function(N, K) {
+	if (N === 1) return 0;
+	if (K % 2 === 0) return (kthGrammar(N - 1, K / 2) === 0) ? 1 : 0;
+	else return (kthGrammar(N - 1, (K + 1) / 2) === 0) ? 0 : 1;
+};
