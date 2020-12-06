9a342b1e06e7aeb45436704e190ae22eadeb24fe
Nothing <wwwaap@gmail.com>
2020-12-06 17:46:00 +0800

    Update 659-split-array-into-consecutive-subsequences.js

--- 659-split-array-into-consecutive-subsequences.js
+++ 659-split-array-into-consecutive-subsequences.js
@@ -26,3 +26,32 @@ const isPossible = function(nums) {
 function getOrDefault(map, key) {
   return map.get(key) || 0
 }
+
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isPossible = function(nums) {
+  let prev = -Infinity, p1 = 0, p2 = 0, p3 = 0
+  let i = 0
+  const n = nums.length
+  while (i < n) {
+    let curr = nums[i], c1 = 0, c2 = 0, c3 = 0
+    let cnt = 0
+    while (i < n && nums[i] === curr) { cnt++; i++ }
+    if (curr !== prev+1) {
+      if (p1 > 0 || p2 > 0) { return false }
+      c1 = cnt; c2 = 0; c3 = 0
+    } else {
+      if (cnt < p1 + p2) { return false }
+      c2 = p1
+      c3 = p2 + Math.min(p3, cnt - p1 - p2)
+      c1 = Math.max(0, cnt - p1 - p2 - p3)
+    }
+    prev = curr; p1 = c1; p2 = c2; p3 = c3;
+  }
+  return p1 === 0 && p2 === 0
+};
