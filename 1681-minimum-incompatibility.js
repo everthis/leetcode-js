6fc5dadd70a47940b0af8cc4799fdc2b2487fd37
Nothing <wwwaap@gmail.com>
2020-12-08 21:00:17 +0800

    Update 1681-minimum-incompatibility.js

--- 1681-minimum-incompatibility.js
+++ 1681-minimum-incompatibility.js
@@ -3,6 +3,62 @@
  * @param {number} k
  * @return {number}
  */
+var minimumIncompatibility = function (nums, k) {
+  if (k === nums.length) {
+    return 0
+  }
+  const counts = Array(nums.length + 1).fill(0)
+  for (let num of nums) {
+    counts[num]++
+    if (counts[num] > k) {
+      return -1
+    }
+  }
+  const size = nums.length / k
+  let ans = Number.MAX_VALUE
+  const backtracking = (groupIdx, index, sum, lowIndex, curIndex) => {
+    if (index === size) {
+      sum += curIndex - lowIndex
+      if (sum > ans) {
+        return
+      }
+      if (groupIdx === k - 1) {
+        ans = sum
+        return
+      } else {
+        groupIdx++
+        index = 0
+      }
+    }
+    if (index === 0) {
+      for (let i = 0; i < counts.length; i++) {
+        if (counts[i]) {
+          counts[i]--
+          backtracking(groupIdx, index + 1, sum, i, i)
+          counts[i]++
+        }
+      }
+    } else {
+      for (let i = curIndex + 1; i < counts.length; i++) {
+        if (counts[i]) {
+          counts[i]--
+          backtracking(groupIdx, index + 1, sum, lowIndex, i)
+          counts[i]++
+        }
+      }
+    }
+  }
+  backtracking(0, 0, 0, 0, 0)
+  return ans
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
 const minimumIncompatibility = function (nums, k) {
   if (nums.length === k) return 0
   const maxInBucket = nums.length / k
