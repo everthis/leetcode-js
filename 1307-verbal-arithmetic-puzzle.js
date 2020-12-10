5b30e2bf2013c3b484ccf97889ca4ca893a34abe
Nothing <wwwaap@gmail.com>
2020-12-10 21:19:58 +0800

    Update 1307-verbal-arithmetic-puzzle.js

--- 1307-verbal-arithmetic-puzzle.js
+++ 1307-verbal-arithmetic-puzzle.js
@@ -4,54 +4,52 @@
  * @return {boolean}
  */
 const isSolvable = function (words, result) {
-  const firstChars = new Set()
-
-  // this will hold the key as the character and multiple as the value
-  const map = {}
-  for (let i = 0; i < result.length; i++) {
-    const char = result[i]
-    if (!i) firstChars.add(char)
-    if (!map.hasOwnProperty(char)) map[char] = 0
-    map[char] -= 10 ** (result.length - i - 1)
-  }
-  for (let j = 0; j < words.length; j++) {
-    const word = words[j]
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i]
-      if (!i) firstChars.add(char)
-      if (!map.hasOwnProperty(char)) map[char] = 0
-      map[char] += 10 ** (word.length - i - 1)
+  const _isSolvable = (wordIndex, charIndex, wordsSum, resultSum, num) => {
+    if (wordIndex >= words.length) {
+      return wordsSum === resultSum
     }
-  }
-
-  const positives = []
-  const negatives = []
-  Object.entries(map).forEach((entry) => {
-    if (entry[1] < 0) negatives.push(entry)
-    else positives.push(entry)
-  })
-
-  const numsUsed = new Set()
-  const backtrack = (val = 0) => {
-    // if we have used all the characters and the value is 0 the input is solvable
-    if (!positives.length && !negatives.length) return val === 0
-
-    // get the store that we are going to examine depending on the value
-    const store =
-      val > 0 || (val === 0 && negatives.length) ? negatives : positives
-    if (store.length === 0) return false
-    const entry = store.pop()
-    const [char, multiple] = entry
-
-    // try every possible value watching out for the edge case that it was a first character
-    for (let i = firstChars.has(char) ? 1 : 0; i < 10; i++) {
-      if (numsUsed.has(i)) continue
-      numsUsed.add(i)
-      if (backtrack(i * multiple + val)) return true
-      numsUsed.delete(i)
+    const wordLen = words[wordIndex].length
+    if (charIndex >= wordLen) {
+      if (wordIndex === words.length - 1) {
+        return _isSolvable(wordIndex + 1, 0, wordsSum, num, 0)
+      }
+      return _isSolvable(wordIndex + 1, 0, wordsSum + num, resultSum, 0)
     }
-    store.push(entry)
+    const char = words[wordIndex][charIndex]
+    if (map.get(char) !== undefined) {
+      if (map.get(char) === 0 && num === 0 && charIndex >= 1) {
+        return false
+      }
+      return _isSolvable(
+        wordIndex,
+        charIndex + 1,
+        wordsSum,
+        resultSum,
+        num * 10 + map.get(char)
+      )
+    }
+    for (let digit = 0; digit <= 9; digit++) {
+      if (digit === 0 && num === 0 && wordLen > 1) continue
+      if (map.get(digit) !== undefined) continue
+      map.set(digit, char)
+      map.set(char, digit)
+      if (
+        _isSolvable(
+          wordIndex,
+          charIndex + 1,
+          wordsSum,
+          resultSum,
+          num * 10 + digit
+        )
+      ) {
+        return true
+      }
+      map.set(digit, undefined)
+      map.set(char, undefined)
+    }
     return false
   }
-  return backtrack()
+  const map = new Map()
+  words = [...words, result]
+  return _isSolvable(0, 0, 0, 0, 0)
 }
