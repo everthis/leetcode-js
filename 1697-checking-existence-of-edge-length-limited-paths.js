e2743ca7c139a565c3a8d3b462ffbb9c819e6974
Nothing <wwwaap@gmail.com>
2020-12-22 21:13:08 +0800

    Update 1697-checking-existence-of-edge-length-limited-paths.js

--- 1697-checking-existence-of-edge-length-limited-paths.js
+++ 1697-checking-existence-of-edge-length-limited-paths.js
@@ -4,27 +4,29 @@
  * @param {number[][]} queries
  * @return {boolean[]}
  */
-const distanceLimitedPathsExist = function(n, edgeList, queries) {
-    edgeList.sort((a, b) => a[2] - b[2])
-    let q = queries.length;
-    const ans = Array(q).fill(false);
-    const order = Array(q).fill(0);
-    for (let i = 0; i < q; ++i) order[i] = i;
-    order.sort((i, j) => queries[i][2] - queries[j][2])
-    const uf = new UnionFind(n);
-    let idx = 0;
-    for (let i of order) {
-        let limit = queries[i][2];
-        while (idx < edgeList.length && edgeList[idx][2] < limit) {
-            let u = edgeList[idx][0], v = edgeList[idx][1];
-            uf.union(u, v);
-            idx++;
-        }
-        let u0 = queries[i][0], v0 = queries[i][1];
-        if (uf.find(u0) === uf.find(v0)) ans[i] = true;
+const distanceLimitedPathsExist = function (n, edgeList, queries) {
+  edgeList.sort((a, b) => a[2] - b[2])
+  const m = queries.length
+  const ans = Array(m).fill(false)
+  const order = Array(m).fill(0)
+  for (let i = 0; i < m; ++i) order[i] = i
+  order.sort((i, j) => queries[i][2] - queries[j][2])
+  const uf = new UnionFind(n)
+  let idx = 0
+  for (let i of order) {
+    const limit = queries[i][2]
+    while (idx < edgeList.length && edgeList[idx][2] < limit) {
+      const u = edgeList[idx][0],
+        v = edgeList[idx][1]
+      uf.union(u, v)
+      idx++
     }
-    return ans;
-};
+    const u0 = queries[i][0],
+      v0 = queries[i][1]
+    if (uf.find(u0) === uf.find(v0)) ans[i] = true
+  }
+  return ans
+}
 class UnionFind {
   constructor(n) {
     this.parents = Array(n)
@@ -33,7 +35,7 @@ class UnionFind {
     this.ranks = Array(n).fill(0)
   }
   root(x) {
-    while(x !== this.parents[x]) {
+    while (x !== this.parents[x]) {
       this.parents[x] = this.parents[this.parents[x]]
       x = this.parents[x]
     }
