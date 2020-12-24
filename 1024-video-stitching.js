452fcea2656a2aa7ef27815229f81e3711706ef5
Nothing <wwwaap@gmail.com>
2020-12-24 21:07:42 +0800

    Update 1024-video-stitching.js

--- 1024-video-stitching.js
+++ 1024-video-stitching.js
@@ -43,3 +43,24 @@ const videoStitching = function (clips, T) {
   return res
 }
 
+
+// another
+
+/**
+ * @param {number[][]} clips
+ * @param {number} T
+ * @return {number}
+ */
+const videoStitching = function (clips, T) {
+  const dp = Array(T + 1).fill( T + 1 )
+  dp[0] = 0
+  for(let i = 0; i <= T; i++) {
+    for(let c of clips) {
+      if(i >= c[0] && i <= c[1]) dp[i] = Math.min(dp[i], dp[c[0]] + 1)
+    }
+    if(dp[i] === T + 1) return -1
+  }
+  return dp[T]
+}
+
+
