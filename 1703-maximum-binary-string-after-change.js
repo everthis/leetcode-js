87d20e6ef15e111c6af6b5788bd0bff17abac6b6
Nothing <wwwaap@gmail.com>
2020-12-28 18:36:54 +0800

    Update 1703-maximum-binary-string-after-change.js

--- 1703-maximum-binary-string-after-change.js
+++ 1703-maximum-binary-string-after-change.js
@@ -1,30 +1,30 @@
 /**
- * @param {string} binary
- * @return {string}
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
  */
-const maximumBinaryString = function(binary) {
-   let n = binary.length;
-    const {max} = Math
-    let nums = [];
-    for (let i = 0; i < n; ++i) {
-      nums.push(+binary[i]);
-    }
-    for (let i = 0, j = 0; i < n - 1; ++i) {
-      if (nums[i] == 1) continue;
-      if (nums[i + 1] == 0) {
-        nums[i] = 1;
-        continue;
-      }
-      j = max(j, i + 1);
-      while (j < n && nums[j]) ++j;
-      if (j === n) break;
-      nums[j++] = 1;
-      nums[i + 1] = 0;
-      nums[i] = 1;
-    }
-    let res = "";
-    for (let i = 0; i < n; ++i) {
-      res += (nums[i] + '');
-    }
-    return res;
-};
+const minMoves = function (nums, k) {
+  if (k === 1) return 0
+  let n = 0
+  const pos = []
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i]) pos.push(i - n++)
+  }
+  const sums = []
+  sums[0] = pos[0]
+  for (let i = 1; i < n; ++i) sums[i] = pos[i] + sums[i - 1]
+  let res = Number.MAX_VALUE
+  let l = (k / 2) >> 0,
+    r = k - l - 1
+  for (let i = 0; i + k <= n; ++i) {
+    const m = i + ((k / 2) >>> 0)
+    const cur =
+      pos[m] * l -
+      (sums[m - 1] - sums[i] + pos[i]) -
+      pos[m] * r +
+      sums[i + k - 1] -
+      sums[m]
+    res = Math.min(cur, res)
+  }
+  return res
+}
