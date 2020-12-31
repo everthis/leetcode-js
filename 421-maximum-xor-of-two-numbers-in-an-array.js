882cb7c31bce2f7f5d7f218a16dc7da72ebd5229
Nothing <wwwaap@gmail.com>
2020-12-31 13:54:56 +0800

    Update 421-maximum-xor-of-two-numbers-in-an-array.js

--- 421-maximum-xor-of-two-numbers-in-an-array.js
+++ 421-maximum-xor-of-two-numbers-in-an-array.js
@@ -1,3 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaximumXOR = function(nums) {
+  let res = 0, mask = 0
+  for(let i = 31; i >= 0; i--) {
+    mask = mask | (1 << i)
+    const set = new Set()
+    for(let e of nums) set.add(e & mask)
+    const tmp = res | (1 << i)
+    for(let e of set) {
+      if(set.has(e ^ tmp)) {
+         res = tmp
+         break
+      }
+    }
+  }
+  return res
+};
+
+// another
+
 /*
  * @lc app=leetcode id=421 lang=javascript
  *
