25485ec551db06ec665f99eb574e3268a0562e2a
Nothing <wwwaap@gmail.com>
2021-01-02 17:02:25 +0800

    Update 435-non-overlapping-intervals.js

--- 435-non-overlapping-intervals.js
+++ 435-non-overlapping-intervals.js
@@ -1,4 +1,25 @@
 /**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const eraseOverlapIntervals = function(intervals) {
+  if(intervals == null || intervals.length === 0) return 0
+  intervals.sort((a, b) => a[1] - b[1])
+  let res = 1, end = intervals[0][1]
+  const len = intervals.length
+  for(let i = 1; i < len; i++) {
+    if(intervals[i][0] >= end) {
+      end = intervals[i][1]
+      res++
+    }
+  }
+  
+  return len - res
+};
+
+// another
+
+/**
  * Definition for an interval.
  * function Interval(start, end) {
  *     this.start = start;
