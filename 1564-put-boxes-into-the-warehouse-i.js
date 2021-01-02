3c7e464f33400b7ff4470473f16ba39b7bf81f57
Nothing <wwwaap@gmail.com>
2021-01-02 19:13:38 +0800

    Create 1564-put-boxes-into-the-warehouse-i.js

---
+++ 1564-put-boxes-into-the-warehouse-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} boxes
+ * @param {number[]} warehouse
+ * @return {number}
+ */
+const maxBoxesInWarehouse = function(boxes, warehouse) {
+  boxes.sort((a, b) => a - b)
+  const m = boxes.length, n = warehouse.length
+  let i = 0, j = 0
+  for(; i < m && j < n; i++) {
+    if(boxes[m - i - 1] <= warehouse[j]) {
+      j++
+    }
+  }
+  return j
+};
