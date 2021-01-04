c141a2b1b3d8e4c6914db78d38781cb93c4fe9ae
Nothing <wwwaap@gmail.com>
2021-01-04 20:36:25 +0800

    Update 1712-ways-to-split-array-into-three-subarrays.js

--- 1712-ways-to-split-array-into-three-subarrays.js
+++ 1712-ways-to-split-array-into-three-subarrays.js
@@ -5,17 +5,17 @@
 const waysToSplit = function (nums) {
   const N = nums.length
   let ret = 0
-  const presum = Array(N + 1).fill(0)
+  const presum = Array(N + 1).fill(0), MOD = 10 ** 9 + 7
   for (let i = 0; i < N; i++) presum[i + 1] = presum[i] + nums[i]
   let avg = (presum[N] / 3 + 1) >> 0
-  for (let l = 1, r = 2, rr = 2; l < N - 1; l++) {
+  for (let l = 1, m = 2, r = 2; l < N - 1; l++) {
     if (presum[l] > avg) break
-    while (r < N && presum[l] > presum[r] - presum[l]) r++
-    r = Math.max(r, l + 1)
-    if (r > rr) rr = r
-    while (rr < N && presum[N] - presum[rr] >= presum[rr] - presum[l]) rr++
-    ret += rr - r
-    if (ret >= 1000000007) ret -= 1000000007
+    while (m < N && presum[l] > presum[m] - presum[l]) m++
+    m = Math.max(m, l + 1)
+    if (m > r) r = m
+    while (r < N && presum[N] - presum[r] >= presum[r] - presum[l]) r++
+    ret += r - m
+    if (ret >= MOD) ret -= MOD
   }
   return ret
 }
