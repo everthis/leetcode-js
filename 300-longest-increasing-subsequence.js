8d74abef2b2d19aa0362f777db2afd0fbaa25626
Nothing <wwwaap@gmail.com>
2021-01-05 14:46:29 +0800

    Update 300-longest-increasing-subsequence.js

--- 300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence.js
@@ -3,6 +3,30 @@
  * @return {number}
  */
 const lengthOfLIS = function(nums) {
+  const stack = [], n = nums.length
+  for(let e of nums) {
+    if(stack.length === 0 || e > stack[stack.length - 1]) {
+      stack.push(e)
+      continue
+    }
+    let l = 0, r = stack.length - 1, mid
+    while(l < r) {
+      const mid = l + ((r - l) >> 1)
+      if(e > stack[mid]) l = mid + 1
+      else r = mid
+    }
+    stack[l] = e
+  }
+  return stack.length
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const lengthOfLIS = function(nums) {
     if (nums.length === 0) {
         return 0
     }
