676539c64396dd2a02b59c0ddd4ea99114ef5a71
Nothing <wwwaap@gmail.com>
2021-01-16 23:22:25 +0800

    Create 1693-daily-leads-and-partners.sql

---
+++ 1693-daily-leads-and-partners.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY 1, 2;
