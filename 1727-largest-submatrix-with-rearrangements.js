44325c83f9e1eccbafef259ba10dd4b2a9ff804a
Nothing <wwwaap@gmail.com>
2021-01-19 19:35:13 +0800

    Update 1727-largest-submatrix-with-rearrangements.js

--- 1727-largest-submatrix-with-rearrangements.js
+++ 1727-largest-submatrix-with-rearrangements.js
@@ -2,20 +2,21 @@
  * @param {number[][]} matrix
  * @return {number}
  */
-const largestSubmatrix = function (a) {
-  const n = a.length
-  const m = a[0].length
-  const count = Array(m).fill(0)
-  let result = 0
-  for (let i = 0; i < n; ++i) {
-    for (let j = 0; j < m; ++j) {
-      count[j] = a[i][j] ? count[j] + 1 : 0
+const largestSubmatrix = function(matrix) {
+    const n = matrix.length;
+    const m = matrix[0].length;
+    const cols = Array(m).fill(0)
+    let res = 0
+    for(let i = 0; i < n; i++) {
+      for(let j = 0; j < m; j++) {
+        cols[j] = matrix[i][j] === 1 ? cols[j] + 1 : 0
+      }
+      const tmp = cols.slice()
+      tmp.sort((a, b) => b - a)
+      for(let j = 0; j < m; j++) {
+        res = Math.max(res, (j + 1) * tmp[j])
+      }
     }
-    const b = count.slice()
-    b.sort((a, b) => b - a)
-    for (let j = 0; j < m; ++j) {
-      result = Math.max(result, (j + 1) * b[j])
-    }
-  }
-  return result
-}
+    return res
+};
+
