7b9639b4aa8977b90316aac0756bc230a9e388ea
Nothing <wwwaap@gmail.com>
2021-01-19 21:32:52 +0800

    Create 1623-all-valid-triplets-that-can-represent-a-country.sql

---
+++ 1623-all-valid-triplets-that-can-represent-a-country.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT sa.student_name AS member_a
+    , sb.student_name AS member_b
+    , sc.student_name AS member_c
+FROM schoola sa CROSS JOIN schoolb sb 
+    CROSS JOIN schoolc sc
+        WHERE sa.student_name != sb.student_name 
+            AND sa.student_name != sc.student_name
+            AND sb.student_name != sc.student_name
+            AND sa.student_id != sc.student_id
+            AND sb.student_id != sc.student_id
+            AND sa.student_id != sb.student_id;
