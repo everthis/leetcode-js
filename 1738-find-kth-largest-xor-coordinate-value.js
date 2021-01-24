dcf0a5ae482e23d8dc66bf54102c0828cee6875b
Nothing <wwwaap@gmail.com>
2021-01-24 17:36:43 +0800

    Update 1738-find-kth-largest-xor-coordinate-value.js

--- 1738-find-kth-largest-xor-coordinate-value.js
+++ 1738-find-kth-largest-xor-coordinate-value.js
@@ -27,4 +27,34 @@ var kthLargestValue = function(matrix, k) {
     return v[k - 1];
 };
 
+// another
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+const kthLargestValue = function(matrix, k) {
+    const tmp = []
+    const n = matrix.length, m = matrix[0].length
+    const dp = Array.from({ length: n }, () => Array(m).fill(0))
+    dp[0][0] = matrix[0][0]
+    tmp.push(dp[0][0])
+    for(let j = 1; j < m; j++) {
+      dp[0][j] = dp[0][j - 1] ^ matrix[0][j]
+      tmp.push(dp[0][j])
+    }
+    for(let i = 1; i < n; i++) {
+      dp[i][0] = dp[i - 1][0] ^ matrix[i][0]
+      tmp.push(dp[i][0])
+    }
+    for(let i = 1; i < n; i++) {
+        for(let j = 1; j < m; j++) {
+            dp[i][j] = dp[i][j - 1] ^ dp[i - 1][j] ^ matrix[i][j] ^ dp[i - 1][j - 1]
+            tmp.push(dp[i][j])
+        }
+    }
+    tmp.sort((a, b) => b - a)
+    return tmp[k - 1]
+};
 
