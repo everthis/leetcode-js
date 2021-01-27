86a93f86dd6c1929e55934cedc318ac65d956a6e
Nothing <wwwaap@gmail.com>
2021-01-27 21:29:10 +0800

    Update 1739-building-boxes.js

--- 1739-building-boxes.js
+++ 1739-building-boxes.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 const minimumBoxes = function(n) {
+  let i = 1, c = 1, s = 1
+  while(s < n) {
+    i += 1, c += i, s += c
+  }
+  while(s - i >= n) {
+    s -= i, c -= 1, i -= 1
+  }
+  return c
+};
+
+// another
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const minimumBoxes = function(n) {
   let sum = 1n, base = 1n, row = 1n;
   n = BigInt(n)
   while (sum < n) {
