c8fb6bf901115d53d808b49d5da497f7b40cf6b5
Nothing <wwwaap@gmail.com>
2021-01-30 22:38:52 +0800

    Update 448-find-all-numbers-disappeared-in-an-array.js

--- 448-find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array.js
@@ -15,3 +15,21 @@ const findDisappearedNumbers = function(nums) {
   }
   return arr;
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const findDisappearedNumbers = function(nums) {
+  for(let i = 0, len = nums.length; i < len; i++) {
+    const idx = Math.abs(nums[i]) - 1
+    nums[idx] = - Math.abs(nums[idx])
+  }
+  const res = []
+  nums.forEach((e, i) => {
+    if(e > 0) res.push(i + 1)
+  })
+  return res
+};
