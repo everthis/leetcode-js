2249138e661ef1636dee5ee0528745d5305a9f75
Nothing <wwwaap@gmail.com>
2021-02-02 20:14:51 +0800

    Update 1745-palindrome-partitioning-iv.js

--- 1745-palindrome-partitioning-iv.js
+++ 1745-palindrome-partitioning-iv.js
@@ -53,6 +53,31 @@ function manacher(s) {
  * @param {string} s
  * @return {boolean}
  */
+const checkPartitioning = function (s) {
+  const n = s.length
+  const dp = Array.from({ length: n }, () => Array(n).fill(false))
+  for(let i = n - 1; i >= 0; i--) {
+    for(let j = i; j < n; j++) {
+      if(s[i] === s[j]) {
+        dp[i][j] = i + 1 <= j - 1 ? dp[i + 1][j - 1] : true
+      } else dp[i][j] = false
+    }
+  }
+  for(let i = 1; i < n - 1; i++) {
+    for(let j = i; j < n - 1; j++) {
+      if(dp[0][i - 1] && dp[i][j] && dp[j + 1][n - 1]) return true
+    }
+  }
+  return false
+}
+
+
+// another
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 const checkPartitioning = function(s) {
   for(let i = 1, len = s.length; i < len - 1; i++) {
     for(let j = i + 1; j < len; j++) {
