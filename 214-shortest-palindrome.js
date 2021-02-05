323504b6c22c3441cfaf89903c0cb296d12f1c58
Nothing <wwwaap@gmail.com>
2021-02-05 19:35:55 +0800

    Update 214-shortest-palindrome.js

--- 214-shortest-palindrome.js
+++ 214-shortest-palindrome.js
@@ -11,3 +11,42 @@ const shortestPalindrome = function(s) {
   let suffix = s.substring(j);
   return suffix.split('').reverse().join('') + shortestPalindrome(s.substring(0, j)) + suffix;
 };
+
+// another
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const shortestPalindrome = function (s) {
+  const tmp = s + '#' + s.split('').reverse().join('')
+  const fail = getFail(tmp)
+  return (
+    s
+      .split('')
+      .slice(fail[fail.length - 1])
+      .reverse()
+      .join('') + s
+  )
+}
+
+function getFail(s) {
+  const n = s.length
+  const table = new Array(n).fill(0)
+  let index = 0
+  for (let i = 1; i < n; ) {
+    if (s.charAt(index) === s.charAt(i)) {
+      table[i] = ++index
+      i++
+    } else {
+      if (index > 0) {
+        index = table[index - 1]
+      } else {
+        index = 0
+        i++
+      }
+    }
+  }
+  return table
+}
+
