59fea935f1c8ffa84629ea90684bd3ee6033062b
Nothing <wwwaap@gmail.com>
2021-02-09 18:25:37 +0800

    Update 1749-maximum-absolute-sum-of-any-subarray.js

--- 1749-maximum-absolute-sum-of-any-subarray.js
+++ 1749-maximum-absolute-sum-of-any-subarray.js
@@ -2,31 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
-const maxAbsoluteSum = function(nums) {
-            let min = 0, max = 0;
-            let positiveSum = 0, negativeSum = 0;
-            for (let num of nums)
-            {
-                positiveSum += num;
-                if (positiveSum > max)
-                {
-                    max = positiveSum;
-                }
+const maxAbsoluteSum = function (nums) {
+  let min = Infinity,
+    max = -Infinity
+  let positiveSum = 0,
+    negativeSum = 0
+  for (let num of nums) {
+    positiveSum += num
+    if (positiveSum > max) {
+      max = positiveSum
+    }
 
-                if (positiveSum < 0)
-                {
-                    positiveSum = 0;
-                }
-                negativeSum += num;
-                if (negativeSum < min)
-                {
-                    min = negativeSum;
-                }
-                if (negativeSum > 0)
-                {
-                    negativeSum = 0;
-                }
-            }
+    if (positiveSum < 0) {
+      positiveSum = 0
+    }
+    negativeSum += num
+    if (negativeSum < min) {
+      min = negativeSum
+    }
+    if (negativeSum > 0) {
+      negativeSum = 0
+    }
+  }
 
-            return Math.max(Math.abs(min), max);
-};
+  return Math.max(Math.abs(min), max)
+}
