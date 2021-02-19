71f31959f7ecf4683e9eb168fe293f1dbb97777c
Nothing <wwwaap@gmail.com>
2021-02-19 14:38:49 +0800

    Create 1514-path-with-maximum-probability.js

---
+++ 1514-path-with-maximum-probability.js
@@ -0,0 +1,105 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} succProb
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ */
+var maxProbability = function (n, edges, succProb, start, end) {
+  const g = {}
+  for (let i = 0; i < edges.length; ++i) {
+    const a = edges[i][0],
+      b = edges[i][1]
+    if (g[a] == null) g[a] = []
+    if (g[b] == null) g[b] = []
+    g[a].push([b, i])
+    g[b].push([a, i])
+  }
+  const p = new Array(n).fill(0)
+  p[start] = 1
+  const pq = new PriorityQueue((a, b) => -p[a] < -p[b])
+  pq.push(start)
+  while (!pq.isEmpty()) {
+    const cur = pq.pop()
+    if (cur === end) {
+      return p[end]
+    }
+    for (let a of g[cur] || []) {
+      const neighbor = a[0],
+        index = a[1]
+      if (p[cur] * succProb[index] > p[neighbor]) {
+        p[neighbor] = p[cur] * succProb[index]
+        pq.push(neighbor)
+      }
+    }
+  }
+  return 0
+}
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
