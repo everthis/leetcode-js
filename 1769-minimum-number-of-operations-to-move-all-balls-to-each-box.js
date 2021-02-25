3980cf723d112f328d1a6a73a9cc202f69bf82d9
Nothing <wwwaap@gmail.com>
2021-02-25 17:39:28 +0800

    Update 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -3,6 +3,32 @@
  * @return {number[]}
  */
 const minOperations = function(boxes) {
+  const n = boxes.length
+  const res = Array(n).fill(0)
+  let cum = 0, sum = 0
+  for(let i = 0; i < n; i++) {
+    res[i] += sum
+    cum += +boxes[i]
+    sum += cum
+  }
+  cum = 0, sum = 0
+  for(let i = n - 1; i >= 0; i--) {
+    res[i] += sum
+    cum += +boxes[i]
+    sum += cum
+  }
+  return res
+};
+
+
+// another
+
+
+/**
+ * @param {string} boxes
+ * @return {number[]}
+ */
+const minOperations = function(boxes) {
   const res = []
   for(let i = 0, len = boxes.length; i < len; i++) {
     res[i] = helper(boxes, i)
