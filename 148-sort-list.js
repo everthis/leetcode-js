94d847ca5c0927f26c42e48f45784be6f70ba11c
Nothing <wwwaap@gmail.com>
2021-02-26 16:54:15 +0800

    Update 148-sort-list.js

--- 148-sort-list.js
+++ 148-sort-list.js
@@ -49,6 +49,38 @@ function merge(left, right) {
     return dummy.next;
 }
 
+
+// another
+
+    function sortList(head) {
+        quickSort(head, null);
+        return head;
+    }
+    
+    function quickSort( head,  tail){
+        if (head == tail) {
+            return;
+        }
+        let slow = head, fast = head.next;
+        let p = head.val;
+        while (fast != tail){
+            if (fast.val <= p){
+                slow = slow.next;
+                swap(slow, fast);
+            }
+            fast = fast.next;
+        }
+        swap(head, slow);
+        quickSort(head, slow);
+        quickSort(slow.next, tail);
+    }
+    
+    function swap( node1,  node2){
+         let tmp = node1.val;
+         node1.val = node2.val;
+         node2.val = tmp;
+    }
+
 // another
 
 /**
