3425d0e1467649bd41a6d80760ed638a3596a2ef
Nothing <wwwaap@gmail.com>
2021-02-26 19:41:04 +0800

    Update 3-longest-substring-without-repeating-characters.js

--- 3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters.js
@@ -3,6 +3,27 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = function(s) {
+  if(s.length < 2) return s.length
+  const hash = {}
+  let max = 0
+  for(let i = 0, j = -1, len = s.length; i < len; i++) {
+    const cur = s[i]
+    if(hash[cur] != null) j = Math.max(j, hash[cur])
+    
+    hash[cur] = i
+    max = Math.max(max, i - j)
+  }
+  
+  return max
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function(s) {
   // var p=0, q=0; //p: start of the sub, q: end of the queue
 
   //hashmap in js????? Array.indexOf
