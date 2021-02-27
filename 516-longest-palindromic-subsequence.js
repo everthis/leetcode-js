0d894d2bba240deb15c6ed1208b3449f9996da17
Nothing <wwwaap@gmail.com>
2021-02-27 20:24:40 +0800

    Update 516-longest-palindromic-subsequence.js

--- 516-longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence.js
@@ -1,3 +1,25 @@
+// 区间DP
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestPalindromeSubseq = function(s) {
+  const n = s.length
+  const dp = Array.from({ length: n }, () => Array(n).fill(0))
+  for(let i = 0; i < n; i++) dp[i][i] = 1
+  for(let len = 2; len <= n; len++) {
+    for(let i = 0; i + len - 1 < n; i++) {
+      const j = i + len - 1
+      if(s[i] === s[j]) dp[i][j] = 2 + dp[i + 1][j - 1]
+      else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
+    }
+  }
+  return dp[0][n - 1]
+};
+
+// another
+
 /**
  * @param {string} s
  * @return {number}
