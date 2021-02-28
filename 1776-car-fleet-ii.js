47569e5feac799e5c597c4ff90cabf6cea9459d1
Nothing <wwwaap@gmail.com>
2021-02-28 13:12:30 +0800

    Update 1776-car-fleet-ii.js

--- 1776-car-fleet-ii.js
+++ 1776-car-fleet-ii.js
@@ -2,6 +2,36 @@
  * @param {number[][]} cars
  * @return {number[]}
  */
+var getCollisionTimes = function(cars) {
+  const n = cars.length
+  const ans = Array(n).fill(0)
+  const stack = []
+  for(let i = n - 1; i >= 0; i--) {
+    while(stack.length) {
+      if(cars[stack[stack.length - 1]][1] >= cars[i][1]) stack.pop()
+      else {
+        if(ans[stack[stack.length - 1]] < 0) break
+        const d = ans[stack[stack.length - 1]] * (cars[i][1] - cars[stack[stack.length - 1]][1])
+        if(d > cars[stack[stack.length - 1]][0] - cars[i][0]) break
+        else stack.pop()
+      }
+    }
+    if(stack.length === 0) ans[i] = -1
+    else {
+      const t = (cars[stack[stack.length - 1]][0]-cars[i][0])/(cars[i][1]-cars[stack[stack.length - 1]][1])
+      ans[i] = t
+    }
+    stack.push(i)
+  }
+  return ans
+};
+
+// another
+
+/**
+ * @param {number[][]} cars
+ * @return {number[]}
+ */
 var getCollisionTimes = function (cars) {
   let n = cars.length,
     t = 0,
