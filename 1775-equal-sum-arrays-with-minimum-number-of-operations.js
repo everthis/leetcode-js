d2c79787144d707a4584e4fffc36656d49a42467
Nothing <wwwaap@gmail.com>
2021-03-03 11:44:06 +0800

    Update 1775-equal-sum-arrays-with-minimum-number-of-operations.js

--- 1775-equal-sum-arrays-with-minimum-number-of-operations.js
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations.js
@@ -6,6 +6,38 @@
 const minOperations = function(nums1, nums2) {
   const m = nums1.length, n = nums2.length
   if(m > n * 6 || n > m * 6) return -1
+  let sum1 = sum(nums1), sum2 = sum(nums2)
+  if(sum1 > sum2) return minOperations(nums2, nums1)
+
+  const arr = Array(6).fill(0)
+  nums1.forEach(e => arr[6 - e]++)
+  nums2.forEach(e => arr[e - 1]++)
+
+  let res = 0, i = 5
+  while(sum1 < sum2) {
+    while(arr[i] === 0) i--
+    sum1 += i
+    res++
+    arr[i]--
+  }
+
+  return res
+};
+
+function sum(arr) {
+  return arr.reduce((ac, e) => ac + e, 0)
+}
+
+// another
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const minOperations = function(nums1, nums2) {
+  const m = nums1.length, n = nums2.length
+  if(m > n * 6 || n > m * 6) return -1
   const sum1 = nums1.reduce((ac, e) => ac + e, 0)
   const sum2 = nums2.reduce((ac, e) => ac + e, 0)
   let largerArr, smallerArr
