72f19990721c195c79f376c36b4e50e8a94d6460
Nothing <wwwaap@gmail.com>
2021-03-05 13:49:57 +0800

    Update 1664-ways-to-make-a-fair-array.js

--- 1664-ways-to-make-a-fair-array.js
+++ 1664-ways-to-make-a-fair-array.js
@@ -2,6 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+const waysToMakeFair = function(nums) {
+  const n = nums.length, right = Array(2).fill(0), left = Array(2).fill(0)
+  let res = 0
+  for(let i = 0; i < n; i++) right[i % 2] += nums[i]
+  for(let i = 0; i < n; i++) {
+    right[i % 2] -= nums[i]
+    if(left[0] + right[1] === left[1] + right[0]) res++
+    left[i % 2] += nums[i]
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 const waysToMakeFair = function (nums) {
   const n = nums.length
   const preOddSum = new Array(n + 1).fill(0)
