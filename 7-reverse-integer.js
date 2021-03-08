db08b2428b245350007aa7a8c1e4b6cebfa7dd48
Nothing <wwwaap@gmail.com>
2021-03-08 11:27:28 +0800

    Update 7-reverse-integer.js

--- 7-reverse-integer.js
+++ 7-reverse-integer.js
@@ -2,6 +2,28 @@
  * @param {number} x
  * @return {number}
  */
+const reverse = function (x) {
+  let res = 0, tail, newResult
+  const low = -Math.pow(2, 31), high = Math.pow(2, 31)
+  while(x !== 0) {
+    tail = x % 10
+    newResult = res * 10 + tail
+    // if((newResult - tail) / 10 !== res) return 0
+    if(newResult < low || newResult >= high) return 0
+    res = newResult
+    x = ~~(x / 10)
+  }
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
 const reverse = function(num) {
   let negative = false;
   let result = 0;
