35b9564e97f705bf2ebedb77980f2a7b77f146c1
Nothing <wwwaap@gmail.com>
2021-03-09 22:09:13 +0800

    Update 1525-number-of-good-ways-to-split-a-string.js

--- 1525-number-of-good-ways-to-split-a-string.js
+++ 1525-number-of-good-ways-to-split-a-string.js
@@ -3,6 +3,39 @@
  * @return {number}
  */
 const numSplits = function(s) {
+  const n = s.length
+  const freq = new Map()
+  const prefix = Array(26).fill(0)
+  for(let i = 0; i < n; i++) {
+    if(freq.get(s[i]) == null) freq.set(s[i], 0)
+    freq.set(s[i], freq.get(s[i]) + 1)
+    prefix[i] = freq.size
+  }
+  freq.clear()
+  const suffix = Array(26).fill(0)
+  for(let i = n - 1; i >= 0 ;i--) {
+    if(freq.get(s[i]) == null) freq.set(s[i], 0)
+    freq.set(s[i], freq.get(s[i]) + 1)
+    suffix[i] = freq.size
+  }
+  // console.log(prefix, suffix)
+  let res = 0
+  for(let i = 1; i < n; i++) {
+    if(prefix[i - 1] === suffix[i]) res++
+  }
+  
+  return res
+};
+
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numSplits = function(s) {
   const arr = Array(26).fill(0)
   const a = 'a'.charCodeAt(0)
   for(let i = 0, len = s.length; i < len; i++) {
