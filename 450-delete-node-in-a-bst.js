600e1d9a7625c703b45cacb4fd8a534db881ec50
Nothing <wwwaap@gmail.com>
2021-03-10 14:29:54 +0800

    Update 450-delete-node-in-a-bst.js

--- 450-delete-node-in-a-bst.js
+++ 450-delete-node-in-a-bst.js
@@ -11,22 +11,23 @@
  * @return {TreeNode}
  */
 const deleteNode = function(root, key) {
-    if(root == null) return null
-    if (root.val > key) {
-        root.left = deleteNode(root.left, key)
-    } else if(root.val < key) {
-        root.right = deleteNode(root.right, key)
+  if(root == null) return null
+  if(key < root.val) {
+    root.left = deleteNode(root.left, key)
+  } else if(key > root.val) {
+    root.right = deleteNode(root.right, key)
+  } else {
+    if(root.left == null) {
+      return root.right
+    } else if(root.right == null) {
+      return root.left
     } else {
-        if (root.left == null) {
-            return root.right
-        }
-        if (root.right == null) {
-            return root.left
-        }
-        let rightSmallest = root.right
-        while(rightSmallest.left != null) rightSmallest = rightSmallest.left
-        rightSmallest.left = root.left
-        return root.right
+      let smallestRight = root.right
+      while(smallestRight.left !== null) smallestRight = smallestRight.left
+      smallestRight.left = root.left
+      return root.right
     }
-    return root
+  }
+  
+  return root
 };
