6d1982117c21f99244fe3f1b09bbdd988f22a202
Nothing <wwwaap@gmail.com>
2021-03-11 09:15:19 +0800

    Update 83-remove-duplicates-from-sorted-list.js

--- 83-remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list.js
@@ -20,3 +20,31 @@ const deleteDuplicates = function(head) {
   }
   return head;
 };
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = function(head) {
+  let prev = null, cur = head
+  while(cur) {
+    if(prev && prev.val === cur.val) {
+      prev.next = cur.next
+      cur = cur.next
+    } else {
+      prev = cur
+      cur = cur.next      
+    }
+  }
+  return head
+};
+
