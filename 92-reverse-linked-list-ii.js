8d3e6b8c9c3b509a1013eab4275f96fcc24c8ca6
Nothing <wwwaap@gmail.com>
2021-03-12 10:33:27 +0800

    Update 92-reverse-linked-list-ii.js

--- 92-reverse-linked-list-ii.js
+++ 92-reverse-linked-list-ii.js
@@ -83,3 +83,45 @@ const reverseBetween = function(head, m, n) {
     }
     return dummy.next;
 };
+
+
+// another
+
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+const reverseBetween = function(head, left, right) {
+  if(head == null) return head
+  if(left === right) return head
+  let cur = head, prev = null
+  let step = 1
+  while(step !== left) {
+    prev = cur
+    cur = cur.next
+    step++
+  }
+  let l = cur
+  while(step !== right) {
+    cur = cur.next
+    step++
+  }
+  let r = cur, next = cur.next
+  // reverse
+  
+  let start = l, p = null
+  while(start !== r) {
+    let n = start.next
+    start.next = p
+    p = start
+    start = n
+  }
+
+  r.next = p
+  l.next = next
+  if(prev) prev.next = r
+
+  return prev ? head : r
+};
