4c538f4ad6626be3fb6de6742b331d558cbe3a54
Nothing <wwwaap@gmail.com>
2021-03-12 16:17:34 +0800

    Update 162-find-peak-element.js

--- 162-find-peak-element.js
+++ 162-find-peak-element.js
@@ -3,6 +3,25 @@
  * @return {number}
  */
 const findPeakElement = function(nums) {
+  let low = 0;
+  let high = nums.length-1;
+
+  while(low < high) {
+    let mid1 = low + ((high - low) >> 1);
+    let mid2 = mid1 + 1;
+    if(nums[mid1] < nums[mid2]) low = mid2;
+    else high = mid1;
+  }
+  return low;
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findPeakElement = function(nums) {
   if(nums == null) return -1
   const len = nums.length
   if(len === 1) return 0
