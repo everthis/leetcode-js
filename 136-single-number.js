3a1fab0a953130082df7248de0eafb5466d5d092
Nothing <wwwaap@gmail.com>
2021-03-13 20:05:27 +0800

    Update 136-single-number.js

--- 136-single-number.js
+++ 136-single-number.js
@@ -7,3 +7,14 @@ const singleNumber = function(nums) {
   for(let i = 1; i< nums.length; i++) xor ^= nums[i]
   return xor
 };
+
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function(nums) {
+  return nums.reduce((ac, e) => ac ^ e, 0)
+};
