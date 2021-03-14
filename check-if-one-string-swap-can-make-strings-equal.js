2d4f56225a366946e4047cc37287fd25929466c3
Nothing <wwwaap@gmail.com>
2021-03-14 11:43:05 +0800

    Create check-if-one-string-swap-can-make-strings-equal.js

---
+++ check-if-one-string-swap-can-make-strings-equal.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+const areAlmostEqual = function(s1, s2) {
+  if (s1 === s2) return true
+  let arr = []
+  for(let i = 0, len = s1.length; i < len; i++) {
+    if(s1[i] !== s2[i]) arr.push(i)
+    
+    if(arr.length > 2) return false
+  }
+  
+  if(arr.length === 1) return false
+  const [i1, i2] = arr
+  if(s1[i2] === s2[i1]) return true
+  return false
+};
