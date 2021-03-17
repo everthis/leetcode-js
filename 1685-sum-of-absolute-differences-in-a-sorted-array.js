f1a8a88c35e7f69301887dbaa51dc9b2f2782919
Nothing <wwwaap@gmail.com>
2021-03-17 20:14:49 +0800

    Update 1685-sum-of-absolute-differences-in-a-sorted-array.js

--- 1685-sum-of-absolute-differences-in-a-sorted-array.js
+++ 1685-sum-of-absolute-differences-in-a-sorted-array.js
@@ -2,20 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var getSumAbsoluteDifferences = function(nums) {
-        let left = 0
-        let right = nums.length - 2
-        let begin = 0
-        for(let i = 1, len = nums.length; i < len; i++) {
-          begin += (nums[i] - nums[0])
-        }
-
-        const res = [begin]
-        for(let i = 1, len = nums.length; i < len; i++) {
-            res.push(res[i - 1] - (nums[i] - nums[i - 1]) * (right - left))
-            left += 1
-            right -= 1
-        }
-
-        return res    
+const getSumAbsoluteDifferences = function(nums) {
+  const res = [], n = nums.length
+  let first = 0
+  for(let i = 1; i < n; i++) {
+    first += nums[i] - nums[0]
+  }
+  res[0] = first
+  for(let i = 1; i < n; i++) {
+    res[i] = res[i - 1] + (nums[i] - nums[i - 1]) * i - (nums[i] - nums[i - 1]) * (n - i)
+  }
+  
+  return res
 };
