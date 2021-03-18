a5b2cee814fb7121fd335f30d67eeb67dcdab616
Nothing <wwwaap@gmail.com>
2021-03-18 19:49:25 +0800

    Create 1506-find-root-of-n-ary-tree.js

---
+++ 1506-find-root-of-n-ary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.children = children === undefined ? [] : children;
+ * };
+ */
+
+/**
+ * @param {Node[]} tree
+ * @return {Node}
+ */
+const findRoot = function(tree) {
+  let sum = 0
+  for(let n of tree) {
+    sum += n.val
+    for(let c of n.children) {
+      sum -= c.val
+    }
+  }
+  for(let n of tree) {
+    if(n.val === sum) return n
+  }
+  return null
+};
