5b157f4bcf38a90176c1174dbb2409976cc5b767
Nothing <wwwaap@gmail.com>
2021-03-18 20:10:01 +0800

    Update 1506-find-root-of-n-ary-tree.js

--- 1506-find-root-of-n-ary-tree.js
+++ 1506-find-root-of-n-ary-tree.js
@@ -23,3 +23,31 @@ const findRoot = function(tree) {
   }
   return null
 };
+
+// another
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.children = children === undefined ? [] : children;
+ * };
+ */
+
+/**
+ * @param {Node[]} tree
+ * @return {Node}
+ */
+const findRoot = function(tree) {
+  let sum = 0
+  for(let n of tree) {
+    sum ^= n.val
+    for(let c of n.children) {
+      sum ^= c.val
+    }
+  }
+  for(let n of tree) {
+    if(n.val === sum) return n
+  }
+  return null
+};
