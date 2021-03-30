be44778d1bab37d6ec1429fc1099c77b20c97053
Nothing <wwwaap@gmail.com>
2021-03-30 12:05:31 +0800

    Update 343-integer-break.js

--- 343-integer-break.js
+++ 343-integer-break.js
@@ -3,6 +3,23 @@
  * @return {number}
  */
 const integerBreak = function(n) {
+  const dp = Array(n + 1).fill(0)
+  dp[2] = 1
+  for(let i = 3; i <= n; i++) {
+    for(let j = 1; j < i; j++) {
+      dp[i] = Math.max(dp[i], j * Math.max(i - j, dp[i - j]))
+    }
+  }
+  return dp[n]
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const integerBreak = function(n) {
   if (n <= 2) return 1;
 
   const maxArr = [];
