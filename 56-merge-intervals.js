4e78b3dfe2c9a77323edd567768da884ebe109e1
Nothing <wwwaap@gmail.com>
2021-03-30 17:02:46 +0800

    Update 56-merge-intervals.js

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -1,4 +1,27 @@
 /**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+const merge = function(intervals) {
+  if(intervals == null || intervals.length === 0) return []
+  intervals.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0])
+  const res = [intervals[0]]
+  for(let i = 1, n = intervals.length; i < n; i++) {
+    const last = res[res.length - 1]
+    const lastEnd = last[1]
+    const [s, e] = intervals[i]
+    if(s > lastEnd) {
+      res.push(intervals[i])
+    } else {
+      last[1] = Math.max(last[1], e)
+    }
+  }
+  return res
+};
+
+// another
+
+/**
  * Definition for an interval.
  * function Interval(start, end) {
  *     this.start = start;
