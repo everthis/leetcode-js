c037c113f9b965d69069291f15388154db01f374
Nothing <wwwaap@gmail.com>
2021-03-30 10:24:33 +0800

    Update 343-integer-break.js

--- 343-integer-break.js
+++ 343-integer-break.js
@@ -50,3 +50,14 @@ const integerBreak = function(n) {
   }
   return rem === 0 ? Math.pow(3, num) : Math.pow(3, num) * rem
 };
+
+/**
+
+If an optimal product contains a factor f >= 4, 
+then you can replace it with factors 2 and f-2 without losing optimality,
+as 2*(f-2) = 2f-4 >= f. So you never need a factor greater than or equal to 4,
+meaning you only need factors 1, 2 and 3 (and 1 is of course wasteful and you'd only use it for n=2 and n=3, where it's needed).
+
+For the rest I agree, 3*3 is simply better than 2*2*2, so you'd never use 2 more than twice.
+
+*/
