2ee578c45fb6e2efa8c3770381988839c8d1e1bc
Nothing <wwwaap@gmail.com>
2021-03-30 17:49:32 +0800

    Update 1041-robot-bounded-in-circle.js

--- 1041-robot-bounded-in-circle.js
+++ 1041-robot-bounded-in-circle.js
@@ -12,3 +12,25 @@ const isRobotBounded = function(instructions) {
     }
   return x == 0 && y == 0 || i > 0;
 };
+
+// another
+
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
+const isRobotBounded = function(instructions) {
+  let x = 0, y = 0, i = 0
+  const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]] // U, R, D, L
+  for(let e of instructions) {
+    if(e === 'R') {
+      i = (i + 1) % 4
+    } else if(e === 'L') {
+      i = (i + 3) % 4
+    } else {
+      x += dirs[i][0]
+      y += dirs[i][1]
+    }
+  }
+  return x === 0 && y === 0 || i > 0
+};
