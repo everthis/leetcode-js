05a559ad65396138719e9795cf063b8fb92193f3
Nothing <wwwaap@gmail.com>
2021-03-30 10:10:34 +0800

    Update 343-integer-break.js

--- 343-integer-break.js
+++ 343-integer-break.js
@@ -32,3 +32,21 @@ const integerBreak = function(n) {
   }
   return maxArr[n];
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const integerBreak = function(n) {
+  if(n === 2) return 1
+  if(n === 3) return 2
+  let num = ~~(n / 3)
+  let rem = n % 3
+  if(rem === 1) {
+    rem += 3
+    num--
+  }
+  return rem === 0 ? Math.pow(3, num) : Math.pow(3, num) * rem
+};
