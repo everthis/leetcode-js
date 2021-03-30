1755a3c80f22965f4ae839e52e2961aa75821ec7
Nothing <wwwaap@gmail.com>
2021-03-30 13:39:51 +0800

    Update 191-number-of-1-bits.js

--- 191-number-of-1-bits.js
+++ 191-number-of-1-bits.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 const hammingWeight = function(n) {
-    let str = (n >>> 0).toString(2)
-    let count = 0
-    for(let i = 0; i < str.length; i++) {
-        if(str.charAt(i) === '1') {
-            count += 1
-        }
-    }
-    return count
+  const str = (n >>> 0).toString(2)
+  let res = 0
+  for(let c of str) {
+    if(c === '1') res++
+  }
+  return res
 };
