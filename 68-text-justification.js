e2a8908702156fc913aa32da22221daa01c00f47
Nothing <wwwaap@gmail.com>
2021-03-31 10:53:21 +0800

    Update 68-text-justification.js

--- 68-text-justification.js
+++ 68-text-justification.js
@@ -1,5 +1,42 @@
 /**
  * @param {string[]} words
+ * @param {number} maxWidth
+ * @return {string[]}
+ */
+const fullJustify = function(words, maxWidth) {
+  const res = []
+  let curRow = []
+  let numOfChars = 0
+  
+  for (let w of words) {
+    if (numOfChars + w.length + curRow.length > maxWidth) {
+      for(let i = 0; i < maxWidth - numOfChars; i++) {
+        if(curRow.length === 1) {
+          curRow[0] += ' '
+        } else {
+          curRow[i % (curRow.length - 1)] += ' '
+        }
+      }
+      res.push(curRow.join(''))
+      curRow = []
+      numOfChars = 0
+    }
+    curRow.push(w)
+    numOfChars += w.length
+  }
+
+  const numOfSpace = maxWidth - numOfChars - (curRow.length - 1)
+  let tail = ''
+  for(let i = 0; i < numOfSpace; i++) tail += ' '
+  res.push(curRow.join(' ') + tail)
+
+  return res
+};
+
+// another
+
+/**
+ * @param {string[]} words
  * @param {number} L
  * @returns {string[]}
  */
