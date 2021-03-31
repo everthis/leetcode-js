5fc8bd8e71fe05e1ae7dce0d6ab917204afce7b1
Nothing <wwwaap@gmail.com>
2021-03-31 14:41:47 +0800

    Update 91-decode-ways.js

--- 91-decode-ways.js
+++ 91-decode-ways.js
@@ -3,6 +3,30 @@
  * @return {number}
  */
 const numDecodings = function(s) {
+  if(s == null || s.length === 0) return 1
+  if(s[0] === '0') return 0
+  const set = new Set()
+  const n = s.length
+  for(let i = 1; i <= 26; i++) {
+    set.add(`${i}`)
+  }
+  const dp = Array(n + 1).fill(0)
+  dp[0] = dp[1] = 1
+  for(let i = 2; i <= n; i++) {
+    if(set.has(s[i - 2] + s[i - 1])) dp[i] += dp[i - 2]
+    if(set.has(s[i - 1])) dp[i] += dp[i - 1]
+  }
+  return dp[n]
+};
+
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numDecodings = function(s) {
   if (s.length === 0) return 0
   const N = s.length
   const dp = Array(N + 1).fill(0)
