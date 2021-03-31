de9d4d2dd51761de36991a6e3220bd574b483ef4
Nothing <wwwaap@gmail.com>
2021-03-31 15:46:48 +0800

    Update 450-delete-node-in-a-bst.js

--- 450-delete-node-in-a-bst.js
+++ 450-delete-node-in-a-bst.js
@@ -31,3 +31,45 @@ const deleteNode = function(root, key) {
   
   return root
 };
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+const deleteNode = function(root, key) {
+  if(root == null) return root
+
+  if(root.val < key) root.right = deleteNode(root.right, key)
+  else if(root.val > key) root.left = deleteNode(root.left, key)
+  else {
+    if(root.left == null && root.right === null) root = null
+    else if(root.left == null) root = root.right
+    else if(root.right == null) root = root.left
+    else {
+      const min = findMin(root.right)
+      root.val = min.val
+      root.right = deleteNode(root.right, root.val)
+    }
+  }
+
+  return root
+};
+
+function findMin(node) {
+  let cur = node
+  while(cur.left) {
+    cur = cur.left
+  }
+  return cur
+}
