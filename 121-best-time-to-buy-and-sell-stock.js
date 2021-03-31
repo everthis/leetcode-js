4a5426426d1d4112c53dafd150f04b8c006c28be
Nothing <wwwaap@gmail.com>
2021-03-31 11:27:27 +0800

    Update 121-best-time-to-buy-and-sell-stock.js

--- 121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock.js
@@ -21,12 +21,11 @@ const maxProfit = function(prices) {
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function (prices) {
-  let maxCur = 0,
-    maxSoFar = 0
-  for (let i = 1; i < prices.length; i++) {
-    maxCur = Math.max(0, (maxCur += prices[i] - prices[i - 1]))
-    maxSoFar = Math.max(maxCur, maxSoFar)
+const maxProfit = function(prices) {
+  let res = 0, maxCur = 0
+  for(let i = 1; i < prices.length; i++) {
+    maxCur = Math.max(0, maxCur + (prices[i] - prices[i - 1]))
+    res = Math.max(res, maxCur)
   }
-  return maxSoFar
-}
+  return res
+};
