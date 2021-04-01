b6a3ad61f14f418dc0d3279271ac49bfb87447d9
Nothing <wwwaap@gmail.com>
2021-04-01 13:56:26 +0800

    Update 829-consecutive-numbers-sum.js

--- 829-consecutive-numbers-sum.js
+++ 829-consecutive-numbers-sum.js
@@ -9,3 +9,21 @@ const consecutiveNumbersSum = function (N) {
   }
   return count
 }
+
+// another
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+const consecutiveNumbersSum = function(N) {
+  let res = 0
+  for(let i = 1; i <= N; i++) {
+    const diff = i * (i - 1) / 2
+    const nd = N - diff
+    if(nd <= 0) break
+    if(nd % i === 0) res++
+  }
+  
+  return res
+};
