aba904e3346c6d4102feb8a43b89996399459179
Nothing <wwwaap@gmail.com>
2021-04-06 21:10:20 +0800

    Update 699-falling-squares.js

--- 699-falling-squares.js
+++ 699-falling-squares.js
@@ -31,3 +31,58 @@ function getHeight(intervals, cur) {
   intervals.push(cur)
   return cur.height
 }
+
+// another
+
+/**
+ * @param {number[][]} positions
+ * @return {number[]}
+ */
+var fallingSquares = function (positions) {
+  let ranges = [{ left: 0, height: 0, right: 1e8 + 1e6 }], rtn = [], max = 0
+
+  outer:
+  for (let [left, length] of positions) {
+    let curHeight = 0, startI = -1, right = left + length, newRanges = []
+    for (let i = 0; i < ranges.length; i++) {
+      let range = ranges[i]
+      if (left < range.right && startI == -1) {
+        startI = i
+        // left part
+        if (left != range.left) {
+          newRanges.push({
+            left: range.left,
+            height: range.height,
+            right: left
+          })
+        }
+      }
+      if (startI != -1) {
+        curHeight = Math.max(curHeight, range.height)
+      }
+      if (right <= range.right) {
+        // added part
+        let newHeight = length + curHeight
+        newRanges.push({
+          left,
+          height: newHeight,
+          right,
+        })
+        // right part
+        if (right != range.right) {
+          newRanges.push({
+            left: right,
+            height: range.height,
+            right: range.right,
+          })
+        }
+        max = Math.max(newHeight, max)
+        rtn.push(max)
+        // replace
+        ranges.splice(startI, i - startI + 1, ...newRanges)
+        continue outer
+      }
+    }
+  }
+  return rtn
+};
