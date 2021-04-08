94f9d112bd49d22eb3daa6fb16e266bb638c31c1
Nothing <wwwaap@gmail.com>
2021-04-08 20:27:49 +0800

    Update 1818-minimum-absolute-sum-difference.js

--- 1818-minimum-absolute-sum-difference.js
+++ 1818-minimum-absolute-sum-difference.js
@@ -3,26 +3,37 @@
  * @param {number[]} nums2
  * @return {number}
  */
-const minAbsoluteSumDiff = function(nums1, nums2) {
-  let sum = 0, lgDiff = -Infinity, idx = -1
+const minAbsoluteSumDiff = function (A, B) {
   const mod = 10 ** 9 + 7
-  const n = nums1.length, { min, max, abs } = Math 
-  for(let i = 0; i < n; i++) {
-    let tmp = abs(nums1[i] - nums2[i])
-    if(tmp > lgDiff) lgDiff = tmp, idx = i
-    sum = (sum + tmp) % mod
+  const sA = [...A].sort((a, b) => a - b)
+  let res = 0
+  let gain = 0
+
+  for (let i = 0; i < A.length; i++) {
+    const delta = Math.abs(A[i] - B[i])
+    res += delta
+    // if delta <= gain, delta - newDelta is not possbile to be better than gain
+    if (delta <= gain) continue
+    // Find closest to B[i] in A
+    const idx = binaryS(sA, B[i])
+    // Double check l, l + 1, l - 1
+    const newDelta = Math.min(
+      Math.abs(sA[idx] - B[i]),
+      idx >= 1 ? Math.abs(sA[idx - 1] - B[i]) : Infinity,
+      idx + 1 < A.length ? Math.abs(sA[idx + 1] - B[i]) : Infinity
+    )
+    gain = Math.max(gain, delta - newDelta)
   }
-    
-  let res = sum, delta = lgDiff, t = Infinity
-  for(let i = 0; i < n; i++) {
-    if(i !== idx) {
-      const t1 = abs(nums1[i] - nums2[idx])
-      // const t2 = abs(nums2[i] - nums1[idx])
-      const tmp = min(t1, lgDiff)
-      if(tmp < t) t = tmp
-    }
+  return (res - gain) % mod
+}
+function binaryS(A, b) {
+  let [l, r] = [0, A.length - 1]
+  while (l < r) {
+    const mid = l + ((r - l) >> 1)
+    const midV = A[mid]
+    if (midV === b) return mid
+    if (midV < b) l = mid + 1
+    else r = mid - 1
   }
-  // console.log(sum, lgDiff, idx, t)
-  return (sum - lgDiff + t) % mod
-  
-};
+  return l
+}
