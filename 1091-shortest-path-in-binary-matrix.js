ccb9524d3736c0af446c8ef53515ae1e0b3d760d
Nothing <wwwaap@gmail.com>
2021-04-09 16:25:20 +0800

    Update 1091-shortest-path-in-binary-matrix.js

--- 1091-shortest-path-in-binary-matrix.js
+++ 1091-shortest-path-in-binary-matrix.js
@@ -3,32 +3,32 @@
  * @return {number}
  */
 const shortestPathBinaryMatrix = function(grid) {
-  let N = grid.length,
-    qu = [[0, 0]],
-    path = 1
+  if(grid == null || grid.length === 0 || grid[0][0] === 1) return -1 
+  let res = 1
+  const n = grid.length
   const dirs = [
-    [-1, 0],
     [1, 0],
-    [0, -1],
+    [-1, 0],
     [0, 1],
+    [0, -1],
     [-1, -1],
     [-1, 1],
+    [1, 1],
     [1, -1],
-    [1, 1]
   ]
-  while (qu.length !== 0) {
-    let cop = qu
-    qu = []
-    for (let [r, c] of cop) {
-      if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) continue
-      if (grid[r][c] === 1) continue
-      grid[r][c] = 1
-      if (r === N - 1 && c === N - 1) return path
-      for (let dir of dirs) {
-        qu.push([r + dir[0], c + dir[1]])
+  let q = [[0, 0]]
+  while(q.length) {
+    const tmp = q
+    q = []
+    for(let [x, y] of tmp) {
+      if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] !== 0) continue
+      if(x === n - 1 && y === n - 1) return res
+      grid[x][y] = 1
+      for(let [dx, dy] of dirs) {    
+        q.push([x + dx, y + dy])
       }
     }
-    path++
+    res++
   }
   return -1
-}
+};
