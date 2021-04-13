b80c50c72d0c534c6c04750e4331063aa52a38ab
Nothing <wwwaap@gmail.com>
2021-04-13 12:32:03 +0800

    Update 84-largest-rectangle-in-histogram.js

--- 84-largest-rectangle-in-histogram.js
+++ 84-largest-rectangle-in-histogram.js
@@ -58,3 +58,24 @@ const largestRectangleArea = function(heights) {
   }
   return max;
 };
+
+// another
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+const largestRectangleArea = function(heights) {
+  heights.push(0)
+  const st = [], n = heights.length
+  let res = 0
+  for(let i = 0; i <= n; i++) {
+    while(st.length && heights[st[st.length - 1]] >= heights[i]) {
+      const top = st.pop()
+      const pre = st.length ? st[st.length - 1] : -1
+      res = Math.max(res, heights[top] * (i - pre - 1))
+    }
+    st.push(i)
+  }
+  return res
+};
