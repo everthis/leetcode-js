6016beec42726321f64448a2f1c878e846bc9ecd
Nothing <wwwaap@gmail.com>
2021-04-13 14:02:20 +0800

    Update 1353-maximum-number-of-events-that-can-be-attended.js

--- 1353-maximum-number-of-events-that-can-be-attended.js
+++ 1353-maximum-number-of-events-that-can-be-attended.js
@@ -1,3 +1,103 @@
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
+/**
+ * @param {number[][]} events
+ * @return {number}
+ */
+function maxEvents(events) {
+  const pq = new PriorityQueue((a, b) => a < b)
+  
+  events.sort((a, b) => a[0] - b[0])
+  let i = 0, res = 0, d = 0, n = events.length
+
+  while(!pq.isEmpty() || i < n) {
+    if(pq.isEmpty()) {
+      d = events[i][0]
+    }
+    while(i < n && events[i][0] <= d) {
+      pq.push(events[i++][1])
+    }
+    pq.pop()
+    res++
+    d++
+    while(!pq.isEmpty() && pq.peek() < d) {
+      pq.pop()
+    }
+  }
+
+  return res
+}
+
+
+// another
+
+
+
 /**
  * @param {number[][]} events
  * @return {number}
