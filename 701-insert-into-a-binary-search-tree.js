1c1a5eb2a6679e0b80ab8de31248caa390247992
Nothing <wwwaap@gmail.com>
2021-04-14 09:36:20 +0800

    Update 701-insert-into-a-binary-search-tree.js

--- 701-insert-into-a-binary-search-tree.js
+++ 701-insert-into-a-binary-search-tree.js
@@ -44,3 +44,25 @@ const insertIntoBST = function(root, val) {
     }
     return root;
 }; 
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+const insertIntoBST = function(root, val) {
+  if(root == null) return new TreeNode(val)
+  if(val < root.val) root.left = insertIntoBST(root.left, val)
+  else if(val > root.val) root.right = insertIntoBST(root.right, val)
+  return root
+};
