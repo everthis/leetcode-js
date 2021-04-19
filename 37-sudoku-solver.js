ad69750f106d591ae255c87abe7f5b1a87993def
Nothing <wwwaap@gmail.com>
2021-04-19 12:19:45 +0800

    Update 37-sudoku-solver.js

--- 37-sudoku-solver.js
+++ 37-sudoku-solver.js
@@ -34,3 +34,46 @@ const solveSudoku = function(board) {
     return true
   }
 }
+
+
+// another
+
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+const solveSudoku = function(board) {
+  helper(board, 0 , 0)
+};
+
+function helper(board, row, col) {
+  for(let i = row, m = board.length; i < m; i++, col = 0) {
+    for(let j = col, n = board[0].length; j < n; j++) {
+      if(board[i][j] !== '.') continue
+      for(let k = 1; k <= 9; k++) {
+        const ch = `${k}`
+        const res = valid(board, i, j, ch)
+        if(res) {
+          board[i][j] = ch
+          if(helper(board, i, j + 1)) return true
+          else {
+            board[i][j] = '.'
+          }
+        }
+      }
+      return false
+    }
+  }
+  return true
+}
+
+
+function valid(board, row, col, ch) {
+  const blkRow = ~~(row / 3), blkCol = ~~(col / 3)
+  for(let i = 0; i < 9; i++) {
+    if(board[row][i] === ch) return false
+    if(board[i][col] === ch) return false
+    if(board[blkRow * 3 + Math.floor(i / 3)][blkCol * 3 + (i % 3)] === ch) return false
+  }
+  return true
+}
