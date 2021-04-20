5aeb5ef8a0caf49bfa8adbf5918467cc71eec881
Nothing <wwwaap@gmail.com>
2021-04-20 10:22:01 +0800

    Update 31-next-permutation.js

--- 31-next-permutation.js
+++ 31-next-permutation.js
@@ -32,3 +32,42 @@ function swap(arr, i, j) {
   arr[j] ^= arr[i];
   arr[i] ^= arr[j];
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const nextPermutation = function(nums) {
+  const n = nums.length
+  let start, end
+  for(let i = n - 2; i >= 0; i--) {
+    if(nums[i] < nums[i + 1]) {
+      start = i
+      break
+    }
+  }
+  if(start == null) {
+    reverse(nums, 0, n - 1)
+  } else {
+    for(let i = n - 1; i >= 0; i--) {
+      if(nums[i] > nums[start]) {
+        end = i
+        break
+      }
+    }
+    swap(nums, start, end)
+    reverse(nums, start + 1, n - 1)
+  }
+};
+function reverse(arr, start, end) {
+  while(start < end) {
+    swap(arr, start++, end--)
+  }
+}
+function swap(arr, i, j) {
+  const tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
