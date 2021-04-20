888fc68b3d934b7bc2857cc40c51448c2f0a2b85
Nothing <wwwaap@gmail.com>
2021-04-20 11:05:59 +0800

    Update 983-minimum-cost-for-tickets.js

--- 983-minimum-cost-for-tickets.js
+++ 983-minimum-cost-for-tickets.js
@@ -7,9 +7,9 @@ const mincostTickets = function(days, costs) {
   const last7 = [], last30 = []
   let res = 0
   for(let d of days) {
-    while(last7.length && last7[0][0] + 7 <= d) last7.pop()
-    while(last30.length && last30[0][0] + 30 <= d) last30.pop()
-    last7.push([d, res  + costs[1]])
+    while(last7.length && last7[0][0] + 7 <= d) last7.shift()
+    while(last30.length && last30[0][0] + 30 <= d) last30.shift()
+    last7.push([d, res + costs[1]])
     last30.push([d, res + costs[2]])
     res = Math.min(res + costs[0], last7[0][1], last30[0][1])
   }
