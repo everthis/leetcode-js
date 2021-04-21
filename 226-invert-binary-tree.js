7e5b79b857e3bd0bd653dca65e738fb93df02e68
Nothing <wwwaap@gmail.com>
2021-04-21 14:04:03 +0800

    Update 226-invert-binary-tree.js

--- 226-invert-binary-tree.js
+++ 226-invert-binary-tree.js
@@ -48,3 +48,25 @@ const invertTree = function (root) {
   }
   return root
 }
+
+// anoother
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function(root) {
+  if(root == null) return root
+  let tmp = root.left
+  root.left = invertTree(root.right)
+  root.right = invertTree(tmp)
+  return root
+};
