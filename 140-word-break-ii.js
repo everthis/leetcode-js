03ccae90835ca70185bc38be48bba0c686b695dd
Nothing <wwwaap@gmail.com>
2021-04-22 15:23:41 +0800

    Update 140-word-break-ii.js

--- 140-word-break-ii.js
+++ 140-word-break-ii.js
@@ -57,3 +57,35 @@ const wordBreak = function (s, wordDict) {
   const res = dfs(0)
   return res.filter((a) => a.join('') === s).map((a) => a.join(' '))
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+
+const wordBreak = (s, wordDict) => {
+  const set = new Set(wordDict)
+  return helper(s, 0, set)
+}
+
+function helper(s, idx, dict) {
+  if(idx === s.length) return []
+  const res = []
+  for(let i = idx; i < s.length; i++) {
+    const tmp = s.slice(idx, i + 1)
+    if(dict.has(tmp)) {
+      const arr = helper(s, i + 1, dict)
+      if(i + 1 >= s.length) {
+        res.push(tmp)
+      } else if(arr.length) {
+        for(let e of arr) {
+          res.push(tmp + ' ' + e)
+        }
+      }
+    }
+  }
+  return res
+}
