5b5c64ac452ad325ba6c1d3dd47b816c42c752ae
Nothing <wwwaap@gmail.com>
2021-04-22 12:19:49 +0800

    Update 139-word-break.js

--- 139-word-break.js
+++ 139-word-break.js
@@ -51,3 +51,28 @@ const wordBreak = function(s, wordDict) {
 
     return f[len];
 };
+
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = function(s, wordDict) {
+  const set = new Set(wordDict)
+  const dp = Array(s.length + 1).fill(false)
+  dp[0] = true
+  for(let i = 1; i <= s.length; i++) {
+    for(let j = 0; j < i; j++) {
+      if(dp[j] && set.has(s.slice(j, i))) {
+        dp[i] = true
+        break
+      }
+    }
+  }
+  
+  return dp[s.length]
+};
+
