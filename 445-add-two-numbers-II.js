9db2621bba8d5faf64980288d033be42b8f928f2
Nothing <wwwaap@gmail.com>
2021-04-23 22:10:44 +0800

    Update 445-add-two-numbers-II.js

--- 445-add-two-numbers-II.js
+++ 445-add-two-numbers-II.js
@@ -40,3 +40,55 @@ const addTwoNumbers = function(l1, l2) {
 
   return list.val === 0 ? list.next : list;
 };
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+  const s1 = [], s2 = []
+  let h1 = l1, h2 = l2
+  while(h1) {
+    s1.push(h1.val)
+    h1 = h1.next
+  }
+  while(h2) {
+    s2.push(h2.val)
+    h2 = h2.next
+  }
+  let inc = false
+  let tail = null
+  while(s1.length || s2.length) {
+    let tmp = 0
+    if(s1.length) tmp += s1.pop()
+    if(s2.length) tmp += s2.pop()
+    if(inc) tmp++
+    if(tmp > 9) {
+      inc = true
+    } else {
+      inc = false
+    }
+    tmp = tmp % 10
+    const cur = new ListNode(tmp)
+    if(tail) cur.next = tail
+    tail = cur
+  }
+  
+  if(inc) {
+    const head = new ListNode(1)
+    head.next = tail
+    return head
+  }
+  return tail
+  
+};
