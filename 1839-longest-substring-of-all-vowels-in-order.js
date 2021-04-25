6fd5ea7daed988999762984ec17e943a9a540f45
Nothing <wwwaap@gmail.com>
2021-04-25 14:44:58 +0800

    Update 1839-longest-substring-of-all-vowels-in-order.js

--- 1839-longest-substring-of-all-vowels-in-order.js
+++ 1839-longest-substring-of-all-vowels-in-order.js
@@ -3,6 +3,35 @@
  * @return {number}
  */
 function longestBeautifulSubstring(word) {
+  let res = 0, cur = 'a', cnt = 0
+  const set = new Set()
+  for(let ch of word) {
+    if(ch < cur) {
+      set.clear()
+      cnt = 0
+      cur = 'a'
+      if(ch === cur) {
+        cnt++
+        set.add(cur)
+      }
+    } else {
+      cnt++
+      set.add(ch)
+      cur = ch
+      if(set.size === 5) res = Math.max(res, cnt)
+    }
+  }
+  return res
+}
+
+
+// another
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+function longestBeautifulSubstring(word) {
   let result = 0,
     current = 0
   let currentVowel = "a"
