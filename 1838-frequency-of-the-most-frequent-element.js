0631ed7785981f5c685221cb3c3b641c0e2af08a
Nothing <wwwaap@gmail.com>
2021-04-25 14:01:54 +0800

    Update 1838-frequency-of-the-most-frequent-element.js

--- 1838-frequency-of-the-most-frequent-element.js
+++ 1838-frequency-of-the-most-frequent-element.js
@@ -3,17 +3,17 @@
  * @param {number} k
  * @return {number}
  */
-var maxFrequency = function(nums, k) {
-    let res = 1, i = 0, j;
-    let sum = 0;
-    nums.sort((a,  b) => a - b)
-    for (j = 0; j < nums.length; ++j) {
-        sum += nums[j];
-        while (sum + k < nums[j] * (j - i + 1)) {
-            sum -= nums[i];
-            i += 1;
-        }
-        res = Math.max(res, j - i + 1);
+const maxFrequency = function(nums, k) {
+  let res = 1, i = 0, j = 0, sum = 0
+  const n = nums.length
+  nums.sort((a, b) => a - b)
+  for(j = 0; j < n; j++) {
+    sum += nums[j]
+    while(sum + k < (j - i + 1)  * nums[j]) {
+      sum -= nums[i]
+      i++
     }
-    return res;
+    res = Math.max(res, j - i + 1)
+  } 
+  return res
 };
