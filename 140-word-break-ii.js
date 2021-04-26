95965ef936aeedad28893cc700d08dab6b9da773
Nothing <wwwaap@gmail.com>
2021-04-26 19:55:03 +0800

    Update 140-word-break-ii.js

--- 140-word-break-ii.js
+++ 140-word-break-ii.js
@@ -4,6 +4,35 @@
  * @return {string[]}
  */
 const wordBreak = function(s, wordDict) {
+  const set = new Set(wordDict)
+  return helper(s, 0, set)
+};
+
+function helper(str, idx, set) {
+  if(idx === str.length) return []
+  const res = []
+  for(let i = idx; i < str.length; i++) {
+    const tmp = str.slice(idx, i + 1)
+    if(set.has(tmp)) {
+      const arr = helper(str, i + 1, set)
+      if(i === str.length - 1) res.push(tmp)
+      for(let item of arr) {
+        res.push(`${tmp} ${item}`)
+      }
+    }
+  }
+  return res
+}
+
+// another
+
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+const wordBreak = function(s, wordDict) {
     return backTrack(s, wordDict, {})
 };
 
