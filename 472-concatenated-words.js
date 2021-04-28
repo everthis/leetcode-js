07718d5f8d065712fb87b8af78f9633d6df91e11
Nothing <wwwaap@gmail.com>
2021-04-28 22:24:46 +0800

    Update 472-concatenated-words.js

--- 472-concatenated-words.js
+++ 472-concatenated-words.js
@@ -4,6 +4,47 @@
  */
 
 const findAllConcatenatedWordsInADict = function (words) {
+  const pre = new Set()
+  words.sort((a, b) => a.length - b.length)
+  const res = []
+  for(let i = 0; i < words.length; i++) {
+    if(valid(words[i], pre)) {
+      res.push(words[i])
+    }
+    pre.add(words[i])
+  }
+
+  return res
+
+  function valid(str, set) {
+    if(set.size === 0) return false
+    const dp = Array(str.length + 1).fill(false)
+    dp[0] = true
+    for(let i = 1; i <= str.length; i++) {
+      for(let j = 0; j < i; j++) {
+        if(!dp[j]) continue
+        if(set.has(str.slice(j, i))) {
+          dp[i] = true
+          break
+        }
+      }
+    }
+    
+    return dp[str.length]
+  }
+}
+
+
+
+
+// another
+
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+
+const findAllConcatenatedWordsInADict = function (words) {
   const set = new Set(words)
   const res = []
   const map = new Map()
