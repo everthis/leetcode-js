83179dee578c83768a86f88feea94c987bf0c51f
Nothing <wwwaap@gmail.com>
2021-05-08 17:45:28 +0800

    Create 1325-delete-leaves-with-a-given-value.js

---
+++ 1325-delete-leaves-with-a-given-value.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+const removeLeafNodes = function(root, target) {
+  return dfs(root, target)
+  
+};
+
+function dfs(node, target) {
+  if(node == null) return node
+  if(node.left == null && node.right == null) {
+    if(node.val === target)  return null
+    else return node
+  }
+  node.right = dfs(node.right, target)
+  node.left = dfs(node.left, target)
+  if(node.right == null && node.left == null) return dfs(node, target)
+  return node
+}
