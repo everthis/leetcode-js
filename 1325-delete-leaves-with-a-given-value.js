a8118d1bb71501e64f88fe3ba57b0417b00b7a02
Nothing <wwwaap@gmail.com>
2021-05-08 20:18:11 +0800

    Update 1325-delete-leaves-with-a-given-value.js

--- 1325-delete-leaves-with-a-given-value.js
+++ 1325-delete-leaves-with-a-given-value.js
@@ -13,7 +13,6 @@
  */
 const removeLeafNodes = function(root, target) {
   return dfs(root, target)
-  
 };
 
 function dfs(node, target) {
@@ -27,3 +26,24 @@ function dfs(node, target) {
   if(node.right == null && node.left == null) return dfs(node, target)
   return node
 }
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+const removeLeafNodes = function(root, target) {
+  if(root.left)  root.left = removeLeafNodes(root.left, target)
+  if(root.right) root.right = removeLeafNodes(root.right, target)
+  return root.left == root.right && root.val === target ? null : root
+};
