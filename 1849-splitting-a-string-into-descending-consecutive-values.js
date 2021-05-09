23d7c3b237beeeb04fdff930d37a30bbad488dac
Nothing <wwwaap@gmail.com>
2021-05-09 21:03:45 +0800

    Update 1849-splitting-a-string-into-descending-consecutive-values.js

--- 1849-splitting-a-string-into-descending-consecutive-values.js
+++ 1849-splitting-a-string-into-descending-consecutive-values.js
@@ -3,6 +3,29 @@
  * @return {boolean}
  */
 const splitString = function(s) {
+  return helper(s, null)
+};
+
+function helper(str, previous) {
+  for(let i = 0, n = str.length; i < n; i++) {
+    const tmp = +(str.slice(0, i + 1))
+    if(previous == null) {
+      if(helper(str.slice(i + 1), tmp)) return true
+    } else if(previous - tmp === 1 && (i === n - 1 || helper(str.slice(i + 1), tmp))) return true
+  }
+  
+  return false
+}
+
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const splitString = function(s) {
   return dfs(s, 0, [Infinity])
 };
 
