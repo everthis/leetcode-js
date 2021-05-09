ea5025be569fda76a7d9b0d38f8cee96d050770b
Nothing <wwwaap@gmail.com>
2021-05-09 18:12:33 +0800

    Update 1854-maximum-population-year.js

--- 1854-maximum-population-year.js
+++ 1854-maximum-population-year.js
@@ -3,6 +3,36 @@
  * @return {number}
  */
 const maximumPopulation = function(logs) {
+  const n = logs.length
+  const arr = Array(101).fill(0)
+  const base = 1950
+  for(let log of logs) {
+    const [start, end] = log
+    arr[start - base]++
+    arr[end - base]--
+  }
+  
+  let res = 0, tmp = -Infinity
+  for(let i = 1; i < 101; i++) {
+    arr[i] += arr[i - 1]
+  }
+  for(let i = 0; i < 101; i++) {
+    if(arr[i] > tmp) {
+      res = i
+      tmp = arr[i]
+    }
+  }
+  return res + base
+};
+
+
+// another
+
+/**
+ * @param {number[][]} logs
+ * @return {number}
+ */
+const maximumPopulation = function(logs) {
   logs.sort((a, b) => {
     if(a[0] === b[0]) return a[1] - b[1]
     return a[0] - b[0]
