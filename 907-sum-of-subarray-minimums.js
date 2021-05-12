6550b7e2dccd2c5905fc77ff33885b16119a506a
Nothing <wwwaap@gmail.com>
2021-05-12 22:17:18 +0800

    Update 907-sum-of-subarray-minimums.js

--- 907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums.js
@@ -1,37 +1,33 @@
 /**
- * @param {number[]} A
+ * @param {number[]} arr
  * @return {number}
  */
-const sumSubarrayMins = function (A) {
-  const n = A.length
-  const s1 = []
-  const s2 = []
-  const left = new Array(n)
-  const right = new Array(n)
-
-  for (let i = 0; i < n; i++) {
-    let count = 1
-    while (s1.length && s1[s1.length - 1][0] > A[i]) {
-      count += s1.pop()[1]
+const sumSubarrayMins = function(arr) {
+  const n = arr.length, s1 = [], s2 = [], left = Array(n), right = Array(n)
+  for(let i = 0; i < n; i++) {
+    let cnt = 1
+    while(s1.length && s1[s1.length - 1][0] > arr[i]) {
+      cnt += s1.pop()[1]
     }
-    left[i] = count
-    s1.push([A[i], count])
+    left[i] = cnt
+    s1.push([arr[i], cnt])
   }
-
-  for (let i = n - 1; i >= 0; i--) {
-    let count = 1
-    // use ">=" to deal with duplicate elements
-    while (s2.length && s2[s2.length - 1][0] >= A[i]) {
-      count += s2.pop()[1]
+  
+  for(let i = n - 1; i >= 0; i--) {
+    let cnt = 1
+    while(s2.length && s2[s2.length - 1][0] >= arr[i]) {
+      cnt += s2.pop()[1]
     }
-    right[i] = count
-    s2.push([A[i], count])
+    right[i] = cnt
+    s2.push([arr[i], cnt])
   }
-
   let res = 0
   const mod = 1e9 + 7
-  for (let i = 0; i < n; i++) {
-    res = (res + left[i] * A[i] * right[i]) % mod
+  for(let i = 0; i < n; i++) {
+    // left[i] number of starting positions
+    // right[i] number of ending positions
+    res = (res + arr[i] * left[i] * right[i]) % mod
   }
+  
   return res
-}
+};
