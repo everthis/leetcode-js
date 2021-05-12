216822059bd2537b8d962c5630b96bd99a1d7e12
Nothing <wwwaap@gmail.com>
2021-05-12 20:37:24 +0800

    Update 907-sum-of-subarray-minimums.js

--- 907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums.js
@@ -2,35 +2,36 @@
  * @param {number[]} A
  * @return {number}
  */
-const sumSubarrayMins = function(A) {
-  let n = A.length;
-  let s1 = [];
-  let s2 = [];
-  let left = new Array(n);
-  let right = new Array(n);
+const sumSubarrayMins = function (A) {
+  const n = A.length
+  const s1 = []
+  const s2 = []
+  const left = new Array(n)
+  const right = new Array(n)
 
   for (let i = 0; i < n; i++) {
-    let count = 1;
+    let count = 1
     while (s1.length && s1[s1.length - 1][0] > A[i]) {
-      count += s1.pop()[1];
+      count += s1.pop()[1]
     }
-    left[i] = count;
-    s1.push([A[i], count]);
+    left[i] = count
+    s1.push([A[i], count])
   }
 
   for (let i = n - 1; i >= 0; i--) {
-    let count = 1;
+    let count = 1
+    // use ">=" to deal with duplicate elements
     while (s2.length && s2[s2.length - 1][0] >= A[i]) {
-      count += s2.pop()[1];
+      count += s2.pop()[1]
     }
-    right[i] = count;
-    s2.push([A[i], count]);
+    right[i] = count
+    s2.push([A[i], count])
   }
 
-  let res = 0;
-  let mod = 1e9 + 7;
+  let res = 0
+  const mod = 1e9 + 7
   for (let i = 0; i < n; i++) {
-    res = (res + left[i] * A[i] * right[i]) % mod;
+    res = (res + left[i] * A[i] * right[i]) % mod
   }
-  return res;
-};
+  return res
+}
