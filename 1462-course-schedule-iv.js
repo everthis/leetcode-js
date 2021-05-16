2f842ea60ca320ebfab3e614684381652d358a57
Nothing <wwwaap@gmail.com>
2021-05-16 19:55:12 +0800

    Create 1462-course-schedule-iv.js

---
+++ 1462-course-schedule-iv.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @param {number[][]} queries
+ * @return {boolean[]}
+ */
+const checkIfPrerequisite = function(numCourses, prerequisites, queries) {
+  const n = numCourses
+  const connected = Array.from({ length: n }, () => Array(n).fill(false))
+  for(let p of prerequisites) connected[p[0]][p[1]] = true
+  for(let k = 0; k < n; k++) {
+    for(let i = 0; i < n; i++) {
+      for(let j = 0; j < n; j++) {
+        connected[i][j] = connected[i][j] || (connected[i][k] && connected[k][j]);
+      }
+    }
+  }
+  const res = []
+  for(let q of queries) res.push(connected[q[0]][q[1]])
+  return res
+};
