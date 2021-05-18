9f2a4d917c07e457a9599c6286577dc5369cfbd8
Nothing <wwwaap@gmail.com>
2021-05-18 21:31:21 +0800

    Update 48-rotate-image.js

--- 48-rotate-image.js
+++ 48-rotate-image.js
@@ -21,3 +21,22 @@ const rotate = function(matrix) {
     }
   }
 }
+
+// another
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const rotate = function (matrix) {
+  matrix.reverse()
+  for (let i = 0; i < matrix.length; ++i) {
+    for (let j = i + 1; j < matrix[i].length; ++j) swap(matrix, i, j)
+  }
+}
+
+function swap(matrix, i, j) {
+  const tmp = matrix[j][i]
+  matrix[j][i] = matrix[i][j]
+  matrix[i][j] = tmp
+}
