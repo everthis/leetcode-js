c855c194bfa88f42bdded4a7e3839dcb51076eee
Nothing <wwwaap@gmail.com>
2021-05-19 22:09:46 +0800

    Update 1861-rotating-the-box.js

--- 1861-rotating-the-box.js
+++ 1861-rotating-the-box.js
@@ -6,6 +6,30 @@ const rotateTheBox = function(box) {
   const m = box.length, n = box[0].length
   const res = Array.from({ length: n }, () => Array(m).fill('.'))
   for(let i = 0; i < m; i++) {
+    for(let j = n - 1, pos = n - 1; j >= 0; j--) {
+      if(box[i][j] === '.') continue
+      if(box[i][j] === '#') {
+        res[pos][m - 1 - i] = '#'
+        pos--
+      } else {
+        res[j][m - 1 - i] = '*'
+        pos = j - 1
+      }
+    }
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {character[][]} box
+ * @return {character[][]}
+ */
+const rotateTheBox = function(box) {
+  const m = box.length, n = box[0].length
+  const res = Array.from({ length: n }, () => Array(m).fill('.'))
+  for(let i = 0; i < m; i++) {
     let j = n - 1
     let pos = j
     while(j >= 0) {
