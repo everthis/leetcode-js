e882167372ee999aa0f5d56342d8d5be963876f0
Nothing <wwwaap@gmail.com>
2021-05-21 22:41:32 +0800

    Update 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.js

--- 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.js
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.js
@@ -14,3 +14,28 @@ const rearrangeSticks = function(n, k) {
   }
   return g[n][k]
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const rearrangeSticks = function(n, k) {
+    const MOD = 1e9 + 7;
+    // first # can be smallest # in which case we recurse for (n - 1, k - 1) 
+    // or it can not be and smallest can be in any of n - 1 otehr positions for recursed(n - 1, k)
+    const dp = new Array(n + 1).fill().map( _ => new Array(k + 1).fill(0) );
+    for (let i = 1; i <= n; ++i) {
+        for (let j = 1; j <= k; ++j) {
+            if (j === i) {
+                dp[i][j] = 1;
+            } else if (j < i) {
+                dp[i][j] = (dp[i - 1][j - 1] + (i - 1) * dp[i - 1][j]) % MOD;
+            }
+        }
+    }
+    return dp[n][k] % MOD;
+    
+};
