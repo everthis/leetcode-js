db3653cb9d9d6c7e1e2f672bf4535df1389454c5
Nothing <wwwaap@gmail.com>
2021-05-21 14:00:11 +0800

    Update 470-implement-rand10-using-rand7.js

--- 470-implement-rand10-using-rand7.js
+++ 470-implement-rand10-using-rand7.js
@@ -10,3 +10,17 @@ const rand10 = function() {
   }
   return (result % 10) + 1
 }
+
+// another
+
+/**
+ * The rand7() API is already defined for you.
+ * var rand7 = function() {}
+ * @return {number} a random integer in the range 1 to 7
+ */
+const rand10 = function() {
+  let tmp = 40
+  while(tmp >= 40) tmp = 7 * (rand7() - 1) + (rand7() - 1)
+  
+  return tmp % 10 + 1
+};
