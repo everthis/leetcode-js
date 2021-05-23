31fcfc8a250c66890064cb86bc97aaf82b02ed52
Nothing <wwwaap@gmail.com>
2021-05-23 14:31:31 +0800

    Update 1871-jump-game-vii.js

--- 1871-jump-game-vii.js
+++ 1871-jump-game-vii.js
@@ -5,6 +5,34 @@
  * @return {boolean}
  */
 const canReach = function(s, minJump, maxJump) {
+  const n = s.length
+  const queue = [0]
+  let mx = 0
+  const { max, min } = Math
+  while(queue.length) {
+    const i = queue.shift()
+    for(let j = max(i + minJump, mx + 1); j < min(s.length, i + maxJump + 1); j++) {
+      if(s[j] === '0') {
+        if(j === n - 1) return true
+        queue.push(j)
+      }
+    }
+    mx = i + maxJump
+  }
+  
+  return false
+};
+
+
+// another
+
+/**
+ * @param {string} s
+ * @param {number} minJump
+ * @param {number} maxJump
+ * @return {boolean}
+ */
+const canReach = function(s, minJump, maxJump) {
   let n = s.length;
   const {max, min} = Math
   if (s[n - 1] != '0') return false;
