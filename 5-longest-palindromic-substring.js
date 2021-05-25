0e77faebddc32b773407f547bc1452736d9ded57
Nothing <wwwaap@gmail.com>
2021-05-25 21:59:12 +0800

    Update 5-longest-palindromic-substring.js

--- 5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring.js
@@ -70,3 +70,40 @@ function preProcess(s) {
   ret += "#$";
   return ret;
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = function(s) {
+  const n = s.length
+  let res = ''
+  for(let i = 0; i < n; i++) {
+    const first = chk(s, i, i, n)
+    if(first.length > res.length) res = first
+    const second = chk(s, i, i + 1, n)
+    if(second.length > res.length) res = second
+  }
+  return res
+};
+
+function chk(str, i, j, n) {
+  if(j >= n) return str[i]
+  let l = i, r = j
+  while(l >= 0 && r < n) {
+    if(str[l] === str[r]) {
+      l--
+      r++
+    } else {
+      return str.slice(l + 1, r)
+    }
+  }
+  if(l < 0) {
+    return str.slice(0, r)
+  } else {
+    return str.slice(l + 1, n)
+  }
+}
+
