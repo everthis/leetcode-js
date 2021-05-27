c5577654b39813bd9d72d2f6c5ab474d0ad96f1c
Nothing <wwwaap@gmail.com>
2021-05-27 21:37:49 +0800

    Update 995-minimum-number-of-k-consecutive-bit-flips.js

--- 995-minimum-number-of-k-consecutive-bit-flips.js
+++ 995-minimum-number-of-k-consecutive-bit-flips.js
@@ -16,3 +16,31 @@ const minKBitFlips = function(A, K) {
     }
     return res;
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const minKBitFlips = function(nums, k) {
+  const n = nums.length, q = []
+  let res = 0
+  for(let i = 0; i < n; i++) {
+    if(nums[i] === 0) {
+      if(q.length === 0 || q.length % 2 === 0) {
+        res++
+        q.push(i + k - 1)
+      }
+    } else {
+      if(q.length % 2 === 1) {
+        res++
+        q.push(i + k - 1)
+      }
+    }
+    if(q.length && i >= q[0]) q.shift()
+  }
+  return q.length ? -1 : res
+};
+
