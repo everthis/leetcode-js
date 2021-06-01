fda25f0e7ea9fe337add55934697b5259235f69b
Nothing <wwwaap@gmail.com>
2021-06-01 23:13:58 +0800

    Update 1674-minimum-moves-to-make-array-complementary.js

--- 1674-minimum-moves-to-make-array-complementary.js
+++ 1674-minimum-moves-to-make-array-complementary.js
@@ -24,3 +24,35 @@ const minMoves = function (nums, limit) {
   }
   return ans
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+const minMoves = function (nums, limit) {
+  const n = nums.length, { max, min } = Math
+  const delta = Array(2 * limit + 2).fill(0)
+  for(let i = 0; i < n / 2; i++) {
+    const a = nums[i], b = nums[n - 1 - i]
+    // [2, min(a, b) + 1)
+    delta[2] += 2
+    // [min(a, b) + 1, a + b)
+    delta[min(a, b) + 1] -= 1
+    delta[a + b]--
+    // [a + b + 1, max(a, b) + limit]
+    delta[a + b + 1] += 1
+    // (max(a, b) + limit, 2 * limit]
+    delta[max(a, b) + limit + 1] +=1
+  }
+  
+  let res = n, cur = 0
+  for(let i = 2; i <= limit * 2; i++) {
+    cur += delta[i]
+    res = min(cur, res)
+  }
+  
+  return res
+}
