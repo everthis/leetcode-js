96b9e3fe35942bc1293e5a4540043643ed105d14
Nothing <wwwaap@gmail.com>
2021-06-04 12:51:58 +0800

    Update 1878-get-biggest-three-rhombus-sums-in-a-grid.js

--- 1878-get-biggest-three-rhombus-sums-in-a-grid.js
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid.js
@@ -5,49 +5,25 @@
 const getBiggestThree = function (grid) {
   const rows = grid.length
   const cols = grid[0].length
-  const maxSide = Math.ceil(Math.min(rows, cols) / 2)
-  let max1 = 0
-  let max2 = 0
-  let max3 = 0
-  let sum, lastColIndex
-
-  const calcSums = function (row, col) {
-    for (let i = 0; i < maxSide; ++i) {
-      if (row + i < rows && row - i >= 0 && col + i * 2 < cols) {
-        sum = grid[row][col]
-        if (i > 0) {
-          for (let j = 1; j <= i; ++j) {
-            sum += grid[row + j][col + j] + grid[row - j][col + j]
-          }
-          lastColIndex = col + i * 2
-          for (let j = 1; j < i; ++j) {
-            sum +=
-              grid[row + j][lastColIndex - j] + grid[row - j][lastColIndex - j]
-          }
-          sum += grid[row][lastColIndex]
+  const res = []
+  for(let i = 0; i < rows; i++) {
+    for(let j = 0; j < cols; j++) {
+      for(let size = 0; i - size >= 0 && i + size < rows && j + size * 2 < cols; size++) {
+        let tmp = 0, r = i, c = j
+        do {tmp += grid[r++][c++]} while(r < rows && c < cols && r < i + size)
+        if(size > 0) {
+          do {tmp += grid[r--][c++]} while(c < cols && c < j + 2 * size)
+          do {tmp += grid[r--][c--]} while(r > 0 && r > i - size)
+          do {tmp += grid[r++][c--]} while(c > 0 && r < i)
         }
-
-        if (sum > max1) {
-          max3 = max2
-          max2 = max1
-          max1 = sum
-        } else if (sum > max2 && sum !== max1) {
-          max3 = max2
-          max2 = sum
-        } else if (sum > max3 && sum !== max1 && sum !== max2) {
-          max3 = sum
+        if(res.indexOf(tmp) === -1) res.push(tmp)
+        if(res.length > 3) {
+          res.sort((a, b) => b - a)
+          res.splice(3)
         }
-      } else {
-        break
       }
     }
   }
-
-  for (let i = 0; i < rows; ++i) {
-    for (let j = 0; j < cols; ++j) {
-      calcSums(i, j)
-    }
-  }
-
-  return [max1, max2, max3].filter((x) => x)
+  res.sort((a, b) => b - a)
+  return res
 }
