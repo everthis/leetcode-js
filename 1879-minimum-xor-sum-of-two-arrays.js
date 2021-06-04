65e65b3faee663915fd7ead195f37f1832bc1194
Nothing <wwwaap@gmail.com>
2021-06-04 21:45:50 +0800

    Update 1879-minimum-xor-sum-of-two-arrays.js

--- 1879-minimum-xor-sum-of-two-arrays.js
+++ 1879-minimum-xor-sum-of-two-arrays.js
@@ -19,3 +19,27 @@ function dfs(dp, a, b, i, mask) {
   }
   return dp[mask]
 }
+
+// another
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const minimumXORSum = function (nums1, nums2) {
+  const dp = Array(1 << nums2.length).fill(Infinity)
+  return dfs(0, 0)
+  
+  function dfs(i, mask) {
+    if(i >= nums2.length) return 0
+    if(dp[mask] != Infinity) return dp[mask]
+    for(let j = 0; j < nums2.length; j++) {
+      if((mask & (1 << j)) === 0) {
+        dp[mask] = Math.min(dp[mask], (nums1[i] ^ nums2[j]) + dfs(i + 1, mask + (1 << j)) )   
+      }
+    } 
+    return dp[mask]
+  }
+}
+
