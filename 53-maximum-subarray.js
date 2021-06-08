78b951ee408b3727cbf015d595133ae4b6c60c3f
Nothing <wwwaap@gmail.com>
2021-06-08 21:20:04 +0800

    Update 53-maximum-subarray.js

--- 53-maximum-subarray.js
+++ 53-maximum-subarray.js
@@ -11,3 +11,33 @@ const maxSubArray = function(nums) {
   }
   return maxSum;
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = function(nums) {
+  return helper(nums, 0, nums.length - 1)
+};
+
+function helper(arr, l, r) {
+  if(l > r) return -Infinity
+  const mid = l + ((r - l) >> 1)
+  let cur = 0, leftMax = 0, rightMax = 0
+  for(let i = mid - 1; i >= l; i--) {
+    cur += arr[i]
+    leftMax = Math.max(leftMax, cur)
+  }
+  cur = 0
+  for(let i = mid + 1; i <= r; i++) {
+    cur += arr[i]
+    rightMax = Math.max(rightMax, cur)
+  }
+  const res = arr[mid] + leftMax + rightMax
+  const leftRes = helper(arr, l, mid - 1)
+  const rightRes = helper(arr, mid + 1, r)
+  
+  return Math.max(res, leftRes, rightRes)
+}
