056e513ee3a0d2f26bd19581a9e3778e87a699eb
Nothing <wwwaap@gmail.com>
2021-06-09 21:21:56 +0800

    Update 25-reverse-nodes-in-k-group.js

--- 25-reverse-nodes-in-k-group.js
+++ 25-reverse-nodes-in-k-group.js
@@ -28,3 +28,88 @@ const reverseKGroup = function(head, k) {
   }
   return dmy.next
 }
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+const reverseKGroup = function (head, k) {
+  let ptr = head
+  let ktail = null
+
+  // Head of the final, moified linked list
+  let new_head = null
+
+  // Keep going until there are nodes in the list
+  while (ptr != null) {
+    let count = 0
+
+    // Start counting nodes from the head
+    ptr = head
+
+    // Find the head of the next k nodes
+    while (count < k && ptr != null) {
+      ptr = ptr.next
+      count += 1
+    }
+
+    // If we counted k nodes, reverse them
+    if (count == k) {
+      // Reverse k nodes and get the new head
+      let revHead = reverseLinkedList(head, k)
+
+      // new_head is the head of the final linked list
+      if (new_head == null) new_head = revHead
+
+      // ktail is the tail of the previous block of
+      // reversed k nodes
+      if (ktail != null) ktail.next = revHead
+
+      ktail = head
+      head = ptr
+    }
+  }
+
+  // attach the final, possibly un-reversed portion
+  if (ktail != null) ktail.next = head
+
+  return new_head == null ? head : new_head
+}
+
+function reverseLinkedList(head, k) {
+  // Reverse k nodes of the given linked list.
+  // This function assumes that the list contains
+  // atleast k nodes.
+  let new_head = null
+  let ptr = head
+
+  while (k > 0) {
+    // Keep track of the next node to process in the
+    // original list
+    let next_node = ptr.next
+
+    // Insert the node pointed to by "ptr"
+    // at the beginning of the reversed list
+    ptr.next = new_head
+    new_head = ptr
+
+    // Move on to the next node
+    ptr = next_node
+
+    // Decrement the count of nodes to be reversed by 1
+    k--
+  }
+
+  // Return the head of the reversed list
+  return new_head
+}
