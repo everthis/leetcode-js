b0404f6a4f75e912ca6df87d971be68c205cc542
Nothing <wwwaap@gmail.com>
2021-06-13 20:58:46 +0800

    Update 857-minimum-cost-to-hire-k-workers.js

--- 857-minimum-cost-to-hire-k-workers.js
+++ 857-minimum-cost-to-hire-k-workers.js
@@ -5,6 +5,39 @@
  * @return {number}
  */
 const mincostToHireWorkers = function(quality, wage, K) {
+    const workers = [], n = quality.length;
+    for (let i = 0; i < n; i++) {
+        workers[i] = { ratio: wage[i] / quality[i], quality: quality[i] }
+    }
+    workers.sort((a, b) => a.ratio - b.ratio);
+    const heap = new MaxPriorityQueue({ priority: x => x.quality });
+    let totalQuality = 0, res = Infinity;
+    while (workers.length) {
+        const curWorker = workers.shift();
+        totalQuality += curWorker.quality;
+        heap.enqueue(curWorker);
+        
+        if (heap.size() > K) {
+            totalQuality -= heap.dequeue().element.quality;
+        }
+        if (heap.size() === K) {
+            res = Math.min(res, curWorker.ratio * totalQuality)
+        }
+    }
+    return res;
+};
+
+
+
+// another
+
+/**
+ * @param {number[]} quality
+ * @param {number[]} wage
+ * @param {number} K
+ * @return {number}
+ */
+const mincostToHireWorkers = function(quality, wage, K) {
   const workers = Array.from({length: quality.length}, () => new Array(2));
   for (let i = 0; i < quality.length; ++i) workers[i] = [wage[i] / quality[i], quality[i]];
   workers.sort((a, b) => a[0] - b[0])
