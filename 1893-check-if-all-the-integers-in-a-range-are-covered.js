963053468fd30e41bb517a14bb5d35e305180f81
Nothing <wwwaap@gmail.com>
2021-06-14 22:26:58 +0800

    Update 1893-check-if-all-the-integers-in-a-range-are-covered.js

--- 1893-check-if-all-the-integers-in-a-range-are-covered.js
+++ 1893-check-if-all-the-integers-in-a-range-are-covered.js
@@ -5,6 +5,30 @@
  * @return {boolean}
  */
 const isCovered = function(ranges, left, right) {
+  const arr = Array(52).fill(0)
+  for(let [s, e] of ranges) {
+    arr[s]++
+    arr[e + 1]--
+  }
+  for(let i = 1; i < 52; i++) {
+    arr[i] += arr[i - 1]
+  }
+  for(let i = left; i <= right; i++) {
+    if(arr[i] === 0) return false
+  }
+  
+  return true
+};
+
+// another
+
+/**
+ * @param {number[][]} ranges
+ * @param {number} left
+ * @param {number} right
+ * @return {boolean}
+ */
+const isCovered = function(ranges, left, right) {
 	for(let i = left; i <= right; i++) {
 		let seen = false;
 		for(let j = 0; j < ranges.length && !seen; j++) 
