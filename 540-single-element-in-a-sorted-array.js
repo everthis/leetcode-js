e62044c4b6e1093333b0fd72f3c131691454761c
Nothing <wwwaap@gmail.com>
2021-06-22 19:57:43 +0800

    Update 540-single-element-in-a-sorted-array.js

--- 540-single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array.js
@@ -20,6 +20,24 @@ const singleNonDuplicate = function(nums) {
  * @return {number}
  */
 const singleNonDuplicate = function(nums) {
+  const n = nums.length
+  let left = 0, right = n - 1
+  while(left < right) {
+    const mid = ~~((left + right) / 2)
+    if((mid % 2 === 0 && nums[mid] === nums[mid + 1]) || (mid % 2 === 1 && nums[mid] === nums[mid - 1])) left = mid + 1
+    else right = mid
+  }
+  
+  return nums[left]
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNonDuplicate = function(nums) {
   if(nums.length === 0) return 0
   let res = nums[0]
   for(let i = 1, len = nums.length; i < len; i++) {
