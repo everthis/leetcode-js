75df5ade870512ddaf71750a2535fbc6988dac73
Nothing <wwwaap@gmail.com>
2021-06-23 20:10:29 +0800

    Update 1904-the-number-of-full-rounds-you-have-played.js

--- 1904-the-number-of-full-rounds-you-have-played.js
+++ 1904-the-number-of-full-rounds-you-have-played.js
@@ -4,8 +4,28 @@
  * @return {number}
  */
 const numberOfRounds = function(startTime, finishTime) {
-  let start = 60 * parseInt(startTime.slice(0, 2)) + parseInt(startTime.slice(3))
-  let finish = 60 * parseInt(finishTime.slice(0, 2)) + parseInt(finishTime.slice(3));
-  if (start > finish) finish += 60 * 24; // If `finishTime` is earlier than `startTime`, add 24 hours to `finishTime`.
-  return Math.floor(finish / 15) - Math.ceil(start / 15); // floor(finish / 15) - ceil(start / 15)
+  const { ceil, floor } = Math
+  const start = new Node(startTime), finish = new Node(finishTime)
+  if(finish.compare(start)) finish.hour += 24
+  let cnt = 0
+  if(start.hour === finish.hour) {
+    const r = floor(finish.minute / 15)
+    const l = ceil(start.minute / 15)
+    if(l >= r) return 0
+    return r - l
+  }
+  cnt += 4 - ceil(start.minute / 15) + floor(finish.minute / 15)
+  start.hour++
+  cnt += (finish.hour - start.hour) * 4
+  return cnt
 };
+
+class Node {
+  constructor(str) {
+    this.hour = +str.slice(0, 2)
+    this.minute = +str.slice(3)
+  }
+  compare(node) {
+    return this.hour === node.hour ? this.minute < node.minute : this.hour < node.hour
+  }
+}
