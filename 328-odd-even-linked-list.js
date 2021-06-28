41cd8bab244fb21d5185cd8ecabb5495f3c97347
Nothing <wwwaap@gmail.com>
2021-06-28 16:14:58 +0800

    Update 328-odd-even-linked-list.js

--- 328-odd-even-linked-list.js
+++ 328-odd-even-linked-list.js
@@ -23,3 +23,40 @@ const oddEvenList = function(head) {
   odd.next = evenHead;
   return head;
 };
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function oddEvenList(head) {
+  if(head == null) return head
+  const dummyOdd = new ListNode()
+  const dummyEven = new ListNode()
+  
+  dummyOdd.next = head
+  let odd = head, even = dummyEven
+  let idx = 2, cur = head.next
+  while(cur) {
+    if (idx % 2 === 1) {
+      odd.next = cur
+      odd = odd.next
+    } else {
+      even.next = cur
+      even = even.next
+    }
+    cur = cur.next
+    idx++
+  }
+  odd.next = dummyEven.next
+  even.next = null
+  return dummyOdd.next
+}
