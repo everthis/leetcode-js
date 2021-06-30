e18ae1d53d26652d8d68b0f760cce2965d15248f
Nothing <wwwaap@gmail.com>
2021-06-30 23:04:12 +0800

    Update 1911-maximum-alternating-subsequence-sum.js

--- 1911-maximum-alternating-subsequence-sum.js
+++ 1911-maximum-alternating-subsequence-sum.js
@@ -10,3 +10,17 @@ const maxAlternatingSum = function(nums) {
   }
   return even;
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxAlternatingSum = function(nums) {
+  let res = nums[0]
+  for(let i = 1; i < nums.length; i++) {
+    res += Math.max(nums[i] - nums[i - 1], 0)
+  }
+  return res
+};
