71f6c586f514180b8d40bb4a5f8b7ade1913cb3f
Nothing <wwwaap@gmail.com>
2021-07-02 19:10:20 +0800

    Update 1915-number-of-wonderful-substrings.js

--- 1915-number-of-wonderful-substrings.js
+++ 1915-number-of-wonderful-substrings.js
@@ -2,6 +2,30 @@
  * @param {string} word
  * @return {number}
  */
+const wonderfulSubstrings = (word) => {
+  let res = 0, count = Array(1024).fill(0);
+  let cur = 0;
+  count[0] = 1;
+  for (let i = 0; i < word.length; ++i) {
+    const num = word[i].charCodeAt() - 97;
+    cur ^= 1 << (num);
+    res += count[cur];
+    ++count[cur];
+    
+    for (let j = 0; j < 10; ++j) {
+      res += count[cur ^ (1 << j)];
+    }
+  }
+  
+  return res;
+};
+
+// another
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
 const asi = (c) => c.charCodeAt();
 const wonderfulSubstrings = (s) => {
     let res = 0;
