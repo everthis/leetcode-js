a149b67e1ab0dc53275964091c6f5d1745695ba7
Nothing <wwwaap@gmail.com>
2021-07-02 11:34:05 +0800

    Update tmp.js

--- tmp.js
+++ tmp.js
@@ -84,11 +84,58 @@ class Solution {
     }
 }
 
-/**
- * @param {number[][]} points
- * @param {number} k
- * @return {number}
- */
-var minDayskVariants = function(points, k) {
-    
-};
+class Solution:
+    def minDayskVariants(self, points: List[List[int]], k: int) -> int:
+        lo = 0
+        hi = int(1e9)
+        
+		# binary search check helper function
+        def check(day):
+            lines = collections.defaultdict(collections.Counter)
+            
+            # 2d sweep line
+            for x, y in points:
+                lbx, lby = (x, y - day) # left point
+                ubx, uby = (x - day, y) # bottom point
+                
+				# lbx + lby == ubx + uby == new x axis's open line
+                lines[lbx+lby][lby-lbx] += 1
+                lines[ubx+uby][uby-ubx+1] -= 1 # 
+                
+				# lbx + lby == ubx + uby == new x axis's close line
+                lbx, lby = (x + day, y) # right point
+                ubx, uby = (x, y + day) # upper point
+                lines[lbx+lby+1][lby-lbx] -= 1 
+                lines[ubx+uby+1][uby-ubx+1] += 1
+            
+            # hold a new ranges to sweep all lines from left to right on new x axis
+            ranges = collections.Counter()
+			
+			# for every critical points on new x axis (it's a diag on the original axis),
+			# add the sweep lines on new y axis
+            for diag in sorted(lines):
+                for num in sorted(lines[diag]):
+                    cnt = lines[diag][num]
+                    ranges[num] += cnt
+                
+				# for every critical points, check whether there is an area having 
+				# overlapping points >= k
+                cur = 0
+                for num in sorted(ranges):
+                    cnt = ranges[num] 
+                    cur += cnt
+
+                    if cur >= k:
+                        return True
+            
+            return False
+                
+		# binary search
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if check(mid):
+                hi = mid
+            else:
+                lo = mid + 1
+        
+        return lo
