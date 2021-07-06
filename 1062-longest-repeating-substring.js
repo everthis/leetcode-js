62ef8fb36232f64f370f57d168d03b51b075c44c
Nothing <wwwaap@gmail.com>
2021-07-06 19:59:00 +0800

    Update 1062-longest-repeating-substring.js

--- 1062-longest-repeating-substring.js
+++ 1062-longest-repeating-substring.js
@@ -3,6 +3,38 @@
  * @return {number}
  */
 const longestRepeatingSubstring = function(s) {
+  let left = 0;
+  let right = s.length - 1;
+  while(left < right) {
+    let pivot = Math.floor((left + right + 1) / 2);
+    if (hasRepeat(s, pivot)) {
+      left = pivot;
+    } else {
+      right = pivot - 1;
+    }
+  }
+  return left;
+};
+
+const hasRepeat = (s, l) => {
+  const strings = new Set();
+  for (let i = 0; i < s.length - l + 1; i++) {
+    const sub = s.substr(i, l);
+    if (strings.has(sub)) {
+      return true;
+    }
+    strings.add(sub);
+  }
+  return false;
+}
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestRepeatingSubstring = function(s) {
     const n = s.length;
     // dp[i][j] means # of repeated chars for substrings ending at i and j
     const dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
