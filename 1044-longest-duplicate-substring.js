b1155a8772f666932f0d0adf0cbe1320f80f8c7a
Nothing <wwwaap@gmail.com>
2021-07-07 20:58:53 +0800

    Update 1044-longest-duplicate-substring.js

--- 1044-longest-duplicate-substring.js
+++ 1044-longest-duplicate-substring.js
@@ -1,4 +1,37 @@
 /**
+ * @param {string} s
+ * @return {string}
+ */
+const longestDupSubstring = function(s) {
+  const n = s.length
+  let l = 0, r = n, res = ''
+  while(l < r) {
+    const mid = (l + r + 1) >> 1
+    const [chk, str] = valid(s, mid)
+    if(chk) {
+      l = mid
+      res = str
+    } else {
+      r = mid - 1
+    }
+  }
+  return res
+};
+
+function valid(s, len) {
+  const set = new Set()
+  for(let i = 0, n = s.length; i <= n - len; i++) {
+    const tmp = s.substr(i, len)
+    if(set.has(tmp)) return [true, tmp]
+    set.add(tmp)
+  }
+  
+  return [false, '']
+}
+
+// another
+
+/**
  * @param {string} S
  * @return {string}
  */
