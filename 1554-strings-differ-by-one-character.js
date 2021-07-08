434170a76041492af110b885a579b4c05ea1e399
Nothing <wwwaap@gmail.com>
2021-07-08 13:21:22 +0800

    Update 1554-strings-differ-by-one-character.js

--- 1554-strings-differ-by-one-character.js
+++ 1554-strings-differ-by-one-character.js
@@ -15,3 +15,37 @@
 
   return false  
 };
+
+// another
+
+/**
+ * @param {string[]} dict
+ * @return {boolean}
+ */
+const differByOne = function (dict) {
+  const M = dict.length,
+    N = dict[0].length
+  const hash = Array(M).fill(0),
+    ord = (c) => c.charCodeAt(0),
+    MOD = 1e13, seen = new Set()
+  // 1. generate each i-th rolling hash
+  for (let i = 0; i < M; ++i) {
+    let base = 1
+    for (let j = 0; j < N; ++j) {
+      hash[i] = (hash[i] + base * ord(dict[i][j])) % MOD
+      base = (123 * base) % MOD
+    }
+  }
+  // 2. remove each j-th char from each i-th rolling hash to 🔍 find a diff collision 💥
+  for (let i = 0; i < M; ++i) {
+    let base = 1
+    for (let j = 0; j < N; ++j) {
+      const diff = (hash[i] - base * ord(dict[i][j])) % MOD
+      if (seen.has(diff)) return true // 🎯 found a diff collision 💥
+      seen.add(diff)
+      base = (123 * base) % MOD
+    }
+  }
+  return false
+}
+
