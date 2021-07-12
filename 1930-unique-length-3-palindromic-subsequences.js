0076ffaddd7bdf657bebb06be929c086a64e66c3
Nothing <wwwaap@gmail.com>
2021-07-12 10:29:18 +0800

    Update 1930-unique-length-3-palindromic-subsequences.js

--- 1930-unique-length-3-palindromic-subsequences.js
+++ 1930-unique-length-3-palindromic-subsequences.js
@@ -2,6 +2,35 @@
  * @param {string} s
  * @return {number}
  */
+const countPalindromicSubsequence = function(s) {
+  const first = Array(26).fill(Infinity), last = Array(26).fill(0)
+  let res = 0
+  const n = s.length, a = 'a'.charCodeAt(0)
+  for(let i = 0; i < n; i++) {
+    const code = s[i].charCodeAt(0)
+    first[code - a] = Math.min(i, first[code - a])
+    last[code - a] = i
+  }
+
+  for(let i = 0; i < 26; i++) {
+    if(last[i] - 1 > first[i]) {
+      const tmp = s.slice(first[i] + 1, last[i])
+      const set = new Set()
+      for(let ch of tmp) set.add(ch)
+      res += set.size
+    }
+  }
+
+  return res
+};
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 const countPalindromicSubsequence = (s) => {
     let res = 0;
     for (let i = 0; i < 26; i++) {
