bc34f30424794e40a7e1596c94aee37a6f5c1201
Nothing <wwwaap@gmail.com>
2021-07-12 09:37:30 +0800

    Update 792-number-of-matching-subsequences.js

--- 792-number-of-matching-subsequences.js
+++ 792-number-of-matching-subsequences.js
@@ -1,4 +1,37 @@
 /**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+const numMatchingSubseq = function(s, words) {
+  const hash = {}
+  for(let w of words) {
+    if(hash[w[0]] == null) hash[w[0]] = []
+    const it = w[Symbol.iterator]()
+    hash[w[0]].push( it )
+    it.next()
+  }
+  let res = 0
+  for(let ch of s) {
+    const advance = hash[ch] || []
+    hash[ch] = []
+    for(let it of advance) {
+      const obj = it.next()
+      if(obj.done === false) {
+        if(hash[obj.value] == null) hash[obj.value] = []
+        hash[obj.value].push(it)
+      } else {
+        res++
+      }
+    }
+  }
+
+  return res
+};
+
+// another
+
+/**
  * @param {string} S
  * @param {string[]} words
  * @return {number}
@@ -20,3 +53,4 @@ const numMatchingSubseq = function(S, words) {
     }
     return res
   };
+
