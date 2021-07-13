e4c370f67b5167732252a665d58b650d48731b25
Nothing <wwwaap@gmail.com>
2021-07-13 11:35:36 +0800

    Update 524-longest-word-in-dictionary-through-deleting.js

--- 524-longest-word-in-dictionary-through-deleting.js
+++ 524-longest-word-in-dictionary-through-deleting.js
@@ -1,5 +1,33 @@
 /**
  * @param {string} s
+ * @param {string[]} dictionary
+ * @return {string}
+ */
+const findLongestWord = function(s, dictionary) {
+  dictionary.sort((a, b) => a.length === b.length ? cmp(a, b) : b.length - a.length)
+  let res = ''
+  for(let d of dictionary) {
+    let j = 0
+    for(let i = 0, n = s.length; i < n; i++) {
+      if(d[j] === s[i]) j++
+      if(j === d.length) return d
+    }
+  }
+  return ''
+  function cmp(s1, s2) {
+    for(let i = 0, n = s1.length; i < n; i++) {
+      if(s1[i] < s2[i]) return -1
+      else if(s1[i] > s2[i]) return 1
+    }
+    return 0
+  }
+};
+
+// another
+
+
+/**
+ * @param {string} s
  * @param {string[]} d
  * @return {string}
  */
