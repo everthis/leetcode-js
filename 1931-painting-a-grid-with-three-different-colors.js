020e1611e5fc883086e80af8e2611ea656179eb3
Nothing <wwwaap@gmail.com>
2021-07-16 17:05:13 +0800

    Update 1931-painting-a-grid-with-three-different-colors.js

--- 1931-painting-a-grid-with-three-different-colors.js
+++ 1931-painting-a-grid-with-three-different-colors.js
@@ -49,3 +49,54 @@ const colorTheGrid = function(m, n) {
   return dp(0, 0)
   
 };
+
+// another
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const colorTheGrid = function(m, n) {
+  const mod = 10 ** 9 + 7
+  const colors = [1, 2, 3]
+  const memoDp = {}, memoOpts = {}
+  function getColor(pos, preMask) {
+    return (preMask >> (pos * 2)) & 3
+  }
+  function setColor(pos, color, curMask) {
+    return curMask | (color << (pos * 2))
+  }
+  function dfs(pos, curMask, preMask, res) {
+    if(pos === m) {
+      res.push(curMask)
+      return
+    }
+    for(let c of colors) {
+      if(getColor(pos, preMask) !== c && (pos === 0 || getColor(pos - 1, curMask) !== c)) {
+        dfs(pos + 1, setColor(pos, c, curMask), preMask, res)
+      }
+    }
+  }
+  function curOpts(preMask) {
+    if (memoOpts[preMask]) return memoOpts[preMask]
+    const res = []
+    dfs(0, 0, preMask, res)
+    memoOpts[preMask] = res
+    return res
+  }
+  function dp(col, preMask) {
+    const k = `${col},${preMask}`
+    if(col === n) return 1
+    if(memoDp[k]) return memoDp[k]
+    let res = 0
+    const cur = curOpts(preMask)
+    for(let mask of cur) {
+      res = (res + dp(col + 1, mask)) % mod
+    }
+    memoDp[k] = res
+    return res
+  }
+
+  return dp(0, 0)
+};
