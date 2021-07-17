2255c916c2a089d0d37e1a22a757a7e017ab88b4
Nothing <wwwaap@gmail.com>
2021-07-17 11:33:00 +0800

    Update 1214-two-sum-bsts.js

--- 1214-two-sum-bsts.js
+++ 1214-two-sum-bsts.js
@@ -23,3 +23,46 @@ const twoSumBSTs = function(root1, root2, target) {
     return twoSumBSTs(root1.left, root2, target) || twoSumBSTs(root1, root2.left, target)
   }
 };
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @param {number} target
+ * @return {boolean}
+ */
+const twoSumBSTs = function(root1, root2, target) {
+  if(root1 == null || root2 == null) return false
+  const s1 = [], s2 = []
+  while(true) {
+    while(root1) {
+      s1.push(root1)
+      root1 = root1.left
+    }
+    while(root2) {
+      s2.push(root2)
+      root2 = root2.right
+    }
+    if(s1.length === 0 || s2.length === 0) return false
+    const t1 = s1[s1.length - 1], t2 = s2[s2.length - 1]
+    if(t1.val + t2.val === target) return true
+    if(t1.val + t2.val < target) {
+      root1 = t1.right
+      s1.pop()
+    } else {
+      root2 = t2.left
+      s2.pop()
+    }
+    
+  }
+  return false
+};
