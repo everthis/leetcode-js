a81ac651d12303773009f36997d7a8fa625541f6
Nothing <wwwaap@gmail.com>
2021-07-19 13:50:47 +0800

    Update 1937-maximum-number-of-points-with-cost.js

--- 1937-maximum-number-of-points-with-cost.js
+++ 1937-maximum-number-of-points-with-cost.js
@@ -3,6 +3,39 @@
  * @return {number}
  */
 const maxPoints = function(points) {
+  const m = points.length, n = points[0].length
+  let prev = points[0].slice()
+  for(let i = 1; i < m; i++) {
+    const left = []
+    left[0] = prev[0]
+    // left to right
+    for(let j = 1; j < n; j++) {
+      left[j] = Math.max(prev[j], left[j - 1] - 1)
+    }
+    const right = []
+    right[n - 1] = prev[n - 1]
+    // right to left
+    for(let j = n - 2; j >= 0; j--) {
+      right[j] = Math.max(prev[j], right[j + 1] - 1)
+    }
+
+    const cur = []
+    for(let j = 0; j < n; j++) {
+      cur[j] = Math.max(left[j], right[j]) + points[i][j]
+    }
+    prev = cur
+  }
+
+  return Math.max(...prev)
+};
+
+// another
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+const maxPoints = function(points) {
     let m = points.length, n = points[0].length;
     let result = 0;
     // dp
