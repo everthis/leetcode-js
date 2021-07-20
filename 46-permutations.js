a55f0fa13d51d4c02f10898b5cf979f79091ffb5
Nothing <wwwaap@gmail.com>
2021-07-20 08:31:27 +0800

    Update 46-permutations.js

--- 46-permutations.js
+++ 46-permutations.js
@@ -21,3 +21,28 @@ function backtrack(list, tempList, nums) {
     }
   }
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = function(nums) {
+  const res = []
+  bt(nums, 0, [], res)
+  return res
+};
+
+function bt(nums, idx, cur, res) {
+  if(idx === nums.length) {
+    res.push(cur.slice())
+    return
+  }
+  for(let i = 0; i < nums.length; i++) {
+    if(cur.indexOf(nums[i]) !== -1) continue
+    cur.push(nums[i])
+    bt(nums, idx + 1, cur, res)
+    cur.pop()
+  }
+}
