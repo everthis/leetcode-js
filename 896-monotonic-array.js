d2766b511d1344f5d298f7469fe72dc63e62ed4b
Nothing <wwwaap@gmail.com>
2021-07-22 16:08:45 +0800

    Update 896-monotonic-array.js

--- 896-monotonic-array.js
+++ 896-monotonic-array.js
@@ -20,3 +20,18 @@ function dec(nums) {
   }
   return true 
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isMonotonic = function(nums) {
+  let inc = true, dec =  true
+  for(let i = 1, n = nums.length; i < n; i++) {
+    inc &= nums[i] >= nums[i - 1]
+    dec &= nums[i] <= nums[i - 1]
+  }
+  return inc ||  dec
+};
