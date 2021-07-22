3c32c5726bd48cf4292ed22350f8b9d5e6768876
Nothing <wwwaap@gmail.com>
2021-07-22 16:03:09 +0800

    Create 896-monotonic-array.js

---
+++ 896-monotonic-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isMonotonic = function(nums) {
+  return inc(nums) || dec(nums)
+};
+
+function inc(nums) {
+  if(nums == null || nums.length <= 1) return true
+  for(let i = 1, n = nums.length; i < n; i++) {
+    if(nums[i] < nums[i - 1]) return false
+  }
+  return true 
+}
+function dec(nums) {
+  if(nums == null || nums.length <= 1) return true
+  for(let i = 1, n = nums.length; i < n; i++) {
+    if(nums[i] > nums[i - 1]) return false
+  }
+  return true 
+}
