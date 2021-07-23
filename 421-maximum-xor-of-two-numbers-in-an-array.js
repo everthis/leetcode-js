07bd61894a91e2f50066092a0d4b42751a17f5c2
Nothing <wwwaap@gmail.com>
2021-07-23 09:08:26 +0800

    Update 421-maximum-xor-of-two-numbers-in-an-array.js

--- 421-maximum-xor-of-two-numbers-in-an-array.js
+++ 421-maximum-xor-of-two-numbers-in-an-array.js
@@ -22,6 +22,20 @@ const findMaximumXOR = function(nums) {
 // another
 
 /*
+
+This algorithm's idea is:
+to iteratively determine what would be each bit of the final result from left to right.
+And it narrows down the candidate group iteration by iteration. e.g. assume input are a,b,c,d,...z, 26 integers in total.
+In first iteration, if you found that a, d, e, h, u differs on the MSB(most significant bit),
+so you are sure your final result's MSB is set. Now in second iteration,
+you try to see if among a, d, e, h, u there are at least two numbers make the 2nd MSB differs,
+if yes, then definitely, the 2nd MSB will be set in the final result.
+And maybe at this point the candidate group shinks from a,d,e,h,u to a, e, h.
+Implicitly, every iteration, you are narrowing down the candidate group,
+but you don't need to track how the group is shrinking, you only cares about the final result.
+
+*/
+/*
  * @lc app=leetcode id=421 lang=javascript
  *
  * [421] Maximum XOR of Two Numbers in an Array
