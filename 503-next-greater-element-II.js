f24cc0a298d5179c505be4643abc039c4929661b
Nothing <wwwaap@gmail.com>
2021-07-30 16:50:49 +0800

    Update 503-next-greater-element-II.js

--- 503-next-greater-element-II.js
+++ 503-next-greater-element-II.js
@@ -20,3 +20,23 @@ function single(idx, arr) {
   }
   return -1;
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ const nextGreaterElements = function(nums) {
+  const res = [], n = nums.length
+  const stack = []
+  for(let i = 2 * n - 1; i >= 0; i--) {
+    while(stack.length && nums[stack[stack.length - 1]] <= nums[i % n]) {
+      stack.pop()
+    }
+    res[i % n] = stack.length ? nums[stack[stack.length - 1]] : -1
+    stack.push(i % n)
+  }
+
+  return res
+};
