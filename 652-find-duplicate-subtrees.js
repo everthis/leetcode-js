0d2645e1f1da34f695704f914b4212f3187ef0ef
Nothing <wwwaap@gmail.com>
2021-08-02 20:49:02 +0800

    Update 652-find-duplicate-subtrees.js

--- 652-find-duplicate-subtrees.js
+++ 652-find-duplicate-subtrees.js
@@ -24,3 +24,35 @@ function pre(node, hash, res) {
   if(hash[str] === 2) res.push(node)
   return str
 }
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+const findDuplicateSubtrees = function(root) {
+  const serId = {}, cntId = {}, res = []
+  let id = 1
+  post(root)
+  return res
+  
+  function post(node) {
+    if(node == null) return 0
+    const curId = `${post(node.left)},${node.val},${post(node.right)}`
+    serId[curId] = serId[curId] || id
+    if(serId[curId] === id) id++
+    cntId[curId] = (cntId[curId] || 0) + 1
+    if(cntId[curId] === 2) res.push(node)
+    return serId[curId]
+  }
+};
+
