e5a95a458bab1d99b325bbea3d77a954d6d16df1
Nothing <wwwaap@gmail.com>
2021-08-04 10:53:48 +0800

    Update 1955-count-number-of-special-subsequences.js

--- 1955-count-number-of-special-subsequences.js
+++ 1955-count-number-of-special-subsequences.js
@@ -1,3 +1,25 @@
+/*
+ * @lc app=leetcode id=1955 lang=javascript
+ *
+ * [1955] Count Number of Special Subsequences
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const countSpecialSubsequences = function (nums) {
+  const dp = Array(3).fill(0),
+    mod = 10 ** 9 + 7
+  for (let e of nums) {
+    dp[e] = (((dp[e] + dp[e]) % mod) + (e > 0 ? dp[e - 1] : 1)) % mod
+  }
+  return dp[2]
+}
+
+// another
+
 /**
  * @param {number[]} nums
  * @return {number}
