849aeb1131f4190528cf9d69817806e7fb7b0d4b
Nothing <wwwaap@gmail.com>
2021-08-06 12:11:29 +0800

    Update 160-intersection-of-two-linked-lists.js

--- 160-intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists.js
@@ -12,6 +12,30 @@
  * @return {ListNode}
  */
 const getIntersectionNode = function(headA, headB) {
+  let a = headA, b = headB
+  while(a !== b) {
+    a = a == null ? headB : a.next
+    b = b == null ? headA : b.next
+  }
+  return a
+};
+
+// another
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = function(headA, headB) {
     let aend = null
     let bend = null
     let ahead = headA
