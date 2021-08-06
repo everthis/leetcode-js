3fbc63eb0543ebb63112ba57ee9a204750c31e45
Nothing <wwwaap@gmail.com>
2021-08-06 11:37:18 +0800

    Update 767-reorganize-string.js

--- 767-reorganize-string.js
+++ 767-reorganize-string.js
@@ -36,3 +36,41 @@ const reorganizeString = function(S) {
     }
     return parts.join('');
 };
+
+// another
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reorganizeString = function(s) {
+  const arr = Array(26).fill(0), a = 'a'.charCodeAt(0)
+  for(let ch of s) arr[ch.charCodeAt(0) - a]++
+  let max = 0, idx = -1
+  for(let i = 0; i < 26; i++) {
+    if(arr[i] > max) {
+      max = arr[i]
+      idx = i
+    }
+  }
+  const n = s.length
+  const res = Array(n)
+  if(max > (n + 1) / 2) return ''
+
+  let i = 0
+  while(arr[idx] > 0) {
+    res[i] = String.fromCharCode(a + idx)
+    i += 2
+    arr[idx]--
+  }
+  
+  for(let j = 0; j < 26; j++) {
+    while(arr[j]) {
+      if(i >= n) i = 1
+      res[i] = String.fromCharCode(a + j)
+      i += 2
+      arr[j]--
+    }
+  }
+  return res.join('')
+};
