742d3b19affb361fc798de1fc7899801ea6c7461
Nothing <wwwaap@gmail.com>
2021-08-07 11:32:44 +0800

    Update 1054-distant-barcodes.js

--- 1054-distant-barcodes.js
+++ 1054-distant-barcodes.js
@@ -20,3 +20,48 @@ const rearrangeBarcodes = function(barcodes) {
 
 	return barcodes;
 };
+
+// another
+
+/**
+ * @param {number[]} barcodes
+ * @return {number[]}
+ */
+const rearrangeBarcodes = function(barcodes) {
+  const hash = {}, n = barcodes.length
+  for(let e of barcodes) {
+    if(hash[e] == null) hash[e] = 0
+    hash[e]++
+  }
+  const res = Array(n)
+  let max = 0, idx = -1
+  for(let k in hash) {
+    if(hash[k] > max) {
+      max = hash[k]
+      idx = +k
+    }
+  }
+  let i = 0
+  // max freq first
+  while(max > 0) {
+    res[i] = idx
+    max--
+    i += 2
+  }
+  // the rest
+  const keys = Object.keys(hash).map(e => +e)
+  for(let j = 0, len = keys.length; j < len; j++) {
+    if(keys[j] !== idx) {
+      const k = keys[j]
+      let freq = hash[k]
+      while(freq > 0) {
+        if(i >= n) i = 1
+        res[i] = k
+        freq--
+        i += 2
+      }
+    }
+  }
+  
+  return res
+};
