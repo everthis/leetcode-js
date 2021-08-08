6849085e1543f3d112d675cb8feb0255b0a3b4ad
Nothing <wwwaap@gmail.com>
2021-08-08 16:44:35 +0800

    Create 1962-remove-stones-to-minimize-the-total.js

---
+++ 1962-remove-stones-to-minimize-the-total.js
@@ -0,0 +1,87 @@
+/**
+ * @param {number[]} piles
+ * @param {number} k
+ * @return {number}
+ */
+const minStoneSum = function(piles, k) {
+  const pq = new PriorityQueue((a, b) => a > b)
+  for(let e of piles) pq.push(e)
+  while(k > 0) {
+    const tmp = pq.pop()
+    const e = tmp - (~~(tmp / 2))
+    pq.push(e)
+    k--
+  }
+  let res = 0
+  while(!pq.isEmpty()) {
+    res += pq.pop()
+  }
+  return res
+};
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
