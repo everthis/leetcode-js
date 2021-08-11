381afd1e6476cfb8168762585c7797def77c0d0d
Nothing <wwwaap@gmail.com>
2021-08-11 21:35:06 +0800

    Update 1405-longest-happy-string.js

--- 1405-longest-happy-string.js
+++ 1405-longest-happy-string.js
@@ -5,6 +5,38 @@
  * @return {string}
  */
 const longestDiverseString = function (a, b, c) {
+  const arr = [['a', a], ['b', b], ['c', c]]
+  
+  let res = ''
+  while(true) {
+    arr.sort((a, b) => b[1] - a[1])
+    if(res.length >= 2 && arr[0][0] === res[res.length - 1] && arr[0][0] === res[res.length - 2]) {
+      if(arr[1][1] > 0) {
+        res += arr[1][0]
+        arr[1][1]--
+      } else break
+    } else {
+      if(arr[0][1] > 0) {
+        res += arr[0][0]
+        arr[0][1]--
+      } else break
+    }
+  }
+  
+  return res
+};
+
+
+// another
+
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {string}
+ */
+const longestDiverseString = function (a, b, c) {
   return generate(a, b, c, "a", "b", "c");
 };
 
