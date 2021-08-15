38581beaca6debc09aa9ab91188fc829bbdd55c4
Nothing <wwwaap@gmail.com>
2021-08-15 23:12:25 +0800

    Update 1249-minimum-remove-to-make-valid-parentheses.js

--- 1249-minimum-remove-to-make-valid-parentheses.js
+++ 1249-minimum-remove-to-make-valid-parentheses.js
@@ -18,3 +18,34 @@
   }
   return arr.join('')
 };
+
+// another
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const minRemoveToMakeValid = function(s) {
+  let cnt = 0
+  let res = s.split('')
+  // console.log(res)
+  for(let i = 0; i < res.length; ) {
+    const ch = res[i]
+    if(ch === '(') cnt++
+    if(ch === ')') cnt--
+    if(cnt < 0) {
+      // console.log(res, i)
+      res.splice(i, 1)
+      cnt++
+    } else i++
+  }
+  // console.log(res)
+  let idx = res.length - 1
+  while(cnt > 0) {
+    if(res[idx] === '(') {
+      res.splice(idx, 1)
+      cnt--
+    } else idx--
+  }
+  return res.join('')
+};
