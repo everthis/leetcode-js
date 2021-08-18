7b2e52e3a14cf516c1706ed0ee2da2c85913e3b8
Nothing <wwwaap@gmail.com>
2021-08-18 22:13:56 +0800

    Update 1959-minimum-total-space-wasted-with-k-resizing-operations.js

--- 1959-minimum-total-space-wasted-with-k-resizing-operations.js
+++ 1959-minimum-total-space-wasted-with-k-resizing-operations.js
@@ -4,21 +4,21 @@
  * @return {number}
  */
 const minSpaceWastedKResizing = function(nums, k) {
-  const n = nums.length, INF = 200 * 1e6;
-  const memo = Array.from({ length: 200 }, () => Array(200));
-  return dp(nums, 0, k);  
-  function dp(nums, i, k) {
-    if (i === n) return 0;
-    if (k === -1) return INF;
-    if (memo[i][k] != null) return memo[i][k];
-    let ans = INF, maxNum = nums[i], totalSum = 0;
-    for (let j = i; j < n; j++) {
-      maxNum = Math.max(maxNum, nums[j]);
-      totalSum += nums[j];
-      const wasted = maxNum * (j - i + 1) - totalSum;
-      ans = Math.min(ans, dp(nums, j + 1, k - 1) + wasted);
+  const n = nums.length, INF = 200 * 1e6
+  const memo = Array.from({length: n}, () => Array(k))
+  return dp(0, k)
+  
+  function dp(i, k) {
+    if(i === n) return 0
+    if(k < 0) return INF
+    if(memo[i][k] != null) return memo[i][k]
+    let res = INF, max = nums[i], sum = 0
+    for(let j = i; j < n; j++) {
+      max = Math.max(max, nums[j])
+      sum += nums[j]
+      const waste = max * (j - i + 1) - sum
+      res = Math.min(res, dp(j + 1, k - 1) + waste)
     }
-    return memo[i][k] = ans;
+    return memo[i][k] = res
   }
 };
-
