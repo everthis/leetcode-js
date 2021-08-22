85da8858a58c7b83b68bae0e6e14840cafff9191
Nothing <wwwaap@gmail.com>
2021-08-22 20:04:18 +0800

    Update 1964-find-the-longest-valid-obstacle-course-at-each-position.js

--- 1964-find-the-longest-valid-obstacle-course-at-each-position.js
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position.js
@@ -4,6 +4,50 @@
  */
 const longestObstacleCourseAtEachPosition = function(obstacles) {
   const n = obstacles.length
+  const stack = [], res = []
+  let len = 0
+  
+  for(let i = 0; i < n; i++) {
+    const cur = obstacles[i]
+    const idx = chk(cur)
+    if(idx === len) {
+      stack.push(cur)
+      len++
+      res.push(len)
+    }else {
+      stack[idx] = cur
+      res.push(idx + 1)
+    }
+  }
+
+  return res
+  
+  function chk(x) {
+    if(len && stack[len - 1] <= x) return len
+    let l = 0, r = len - 1
+    while(l < r) {
+      const mid = ~~((l + r) / 2)
+      if(stack[mid] <= x) {
+        l = mid + 1
+      } else {
+        r = mid
+      }
+    }
+    return l
+  }
+};
+
+
+
+
+// another
+
+/**
+ * @param {number[]} obstacles
+ * @return {number[]}
+ */
+const longestObstacleCourseAtEachPosition = function(obstacles) {
+  const n = obstacles.length
   const stack = [], res = Array(n).fill(0)
   let m = 0
   let j = 0;
