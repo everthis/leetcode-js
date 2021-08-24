0749af83dede671e42e0c541a2addaaa00748333
Nothing <wwwaap@gmail.com>
2021-08-24 22:10:19 +0800

    Update 1981-minimize-the-difference-between-target-and-chosen-elements.js

--- 1981-minimize-the-difference-between-target-and-chosen-elements.js
+++ 1981-minimize-the-difference-between-target-and-chosen-elements.js
@@ -19,3 +19,29 @@ const minimizeTheDifference = function(mat, target) {
   }
 };
 
+// another
+
+/**
+ * @param {number[][]} mat
+ * @param {number} target
+ * @return {number}
+ */
+const minimizeTheDifference = function(mat, target) {
+  const m = mat.length, n = mat[0].length
+  const memo = Array.from({ length: m }, () => Array())
+  return dfs(0, 0)
+  
+  function dfs(row, sum) {
+    if(row === m) return Math.abs(target - sum)
+    if(memo[row][sum] != null) return memo[row][sum]
+    let res = Number.MAX_SAFE_INTEGER
+    for(let i = 0; i < n; i++) {
+      res = Math.min(res, dfs(row + 1, sum + mat[row][i]))
+    }
+    
+    return memo[row][sum] = res
+  }
+};
+
+
+
