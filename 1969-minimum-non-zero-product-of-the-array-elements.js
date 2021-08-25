d963fbbb7d44874b9ceccf4bd833116cd0832772
Nothing <wwwaap@gmail.com>
2021-08-25 21:59:54 +0800

    Update 1969-minimum-non-zero-product-of-the-array-elements.js

--- 1969-minimum-non-zero-product-of-the-array-elements.js
+++ 1969-minimum-non-zero-product-of-the-array-elements.js
@@ -3,6 +3,30 @@
  * @return {number}
  */
 const minNonZeroProduct = function (p) {
+  const MOD = BigInt(10 ** 9 + 7), bp = BigInt(p)
+  const bpm = BigInt(1n << bp) - 1n, base = BigInt(1n << bp) - 2n, pow = BigInt(1n << (bp - 1n)) - 1n 
+  return Number((bpm % MOD) * fastPow(base, pow, MOD) % MOD)
+
+}
+
+function fastPow(base, power, mod) {
+  if(power === 0n) return 1n
+  base %= mod
+  let res = fastPow(base, power / 2n, mod)
+  res = (res * res) % mod
+  if(power & 1n) res = (res * base) % mod
+  return res
+}
+
+
+// another
+
+
+/**
+ * @param {number} p
+ * @return {number}
+ */
+const minNonZeroProduct = function (p) {
   const b = BigInt(p)
   const mod = BigInt(10 ** 9 + 7)
 
