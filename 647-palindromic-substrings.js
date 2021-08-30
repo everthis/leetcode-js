80552c5ff923485227b68480fe55ad7ac31b08a5
Nothing <wwwaap@gmail.com>
2021-08-30 09:27:30 +0800

    Update 647-palindromic-substrings.js

--- 647-palindromic-substrings.js
+++ 647-palindromic-substrings.js
@@ -30,3 +30,35 @@ const countSubstrings = function(s) {
 
 console.log(countSubstrings("abc"));
 console.log(countSubstrings("aaa"));
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = function(s) {
+  const arr = manachers(s)
+  return arr.map(e => ~~((e + 1) / 2)).reduce((ac, e) => ac + e, 0)
+};
+
+function manachers(s) {
+  const str = `@#${s.split('').join('#')}#$`
+  const arr = Array(str.length).fill(0)
+  
+  let center = right = 0
+  for(let i = 1, n = str.length; i < n - 1; i++) {
+    if(i < right) {
+      arr[i] = Math.min(right - i, arr[2 * center - i])
+    }
+    while(str[i + arr[i] + 1] === str[i - arr[i] - 1]) {
+      arr[i] += 1
+    }
+    if(i + arr[i] > right) {
+      center = i
+      right = i + arr[i]
+    }
+  }
+  
+  return arr
+}
