4ef906fb8f51682bf9df1bee0a1fd73b9909fa29
Nothing <wwwaap@gmail.com>
2021-09-02 19:57:26 +0800

    Update 1977-number-of-ways-to-separate-numbers.js

--- 1977-number-of-ways-to-separate-numbers.js
+++ 1977-number-of-ways-to-separate-numbers.js
@@ -1,21 +1,32 @@
-function  numberOfCombinations( num) {
-  let dp = Array(3501).fill(0), dpp=Array(3501).fill(0), pf=Array(3501).fill(0), n = num.length, mod = 1000000007;
+/**
+ * @param {string} num
+ * @return {number}
+ */
+function numberOfCombinations(num) {
+  let dpArr = Array(3501).fill(0),
+    dp = Array(3501).fill(0),
+    prefix = Array(3501).fill(0),
+    n = num.length,
+    mod = 1e9 + 7
   for (let l = 1; l <= n; ++l) {
-       dpp[0] = 1;
-      for (let i = n; i - l > 0; --i)
-          pf[i - 1] = num[i - 1 - l] == num[i - 1] ? pf[i] + 1 : 0;
-      for (let i = 0; i < n; ++i) {
-          dp[i + 1] = dpp[i + 1];
-          if (l <= i + 1 && num[i + 1 - l] != '0') {
-              if (i + 1 - 2 * l >= 0 && (pf[i + 1 - l] >= l || num[i + 1 - l + pf[i + 1 - l]] > num[i + 1 - 2 * l + pf[i + 1 - l]]))                    
-                  dp[i + 1] = (dp[i + 1] + dp[i + 1 - l]) % mod;
-              else
-                  dp[i + 1] = (dp[i + 1] + dpp[i + 1 - l]) % mod;
-          }
+    dp[0] = 1
+    for (let i = n; i - l > 0; --i)
+      prefix[i - 1] = num[i - 1 - l] === num[i - 1] ? prefix[i] + 1 : 0
+    for (let i = 0; i < n; ++i) {
+      dpArr[i + 1] = dp[i + 1]
+      if (l <= i + 1 && num[i + 1 - l] != '0') {
+        if (
+          i + 1 - 2 * l >= 0 &&
+          (prefix[i + 1 - l] >= l ||
+            num[i + 1 - l + prefix[i + 1 - l]] > num[i + 1 - 2 * l + prefix[i + 1 - l]])
+        )
+          dpArr[i + 1] = (dpArr[i + 1] + dpArr[i + 1 - l]) % mod
+        else dpArr[i + 1] = (dpArr[i + 1] + dp[i + 1 - l]) % mod
       }
-      let tmp = dpp
-      dpp = dp
-      dp = tmp
-  }    
-  return dpp[n];
+    }
+    const tmp = dp
+    dp = dpArr
+    dpArr = tmp
+  }
+  return dp[n]
 }
