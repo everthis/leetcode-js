894a06434d963031bb68d5fa43f4704cf3f5fc4d
Nothing <wwwaap@gmail.com>
2021-09-06 21:36:47 +0800

    Create 1576-replace-all-s-to-avoid-consecutive-repeating-characters.js

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const modifyString = function(s) {
+  const arr = s.split('')
+  for(let i = 0, n = s.length; i < n; i++) {
+    const cur = arr[i]
+    if(cur === '?') {
+      for(let j = 0, a = 'a'.charCodeAt(0); j < 26; j++) {
+        const ch = String.fromCharCode(a + j)
+        if(
+          n === 1 ||
+          (i === 0 && i < n - 1 && ch !== arr[i + 1]) ||
+          (i > 0 && ch !== arr[i - 1] && i < n - 1 && ch !== arr[i + 1]) ||
+          (i=== n -1 && i - 1 >= 0 && ch !== arr[i - 1])
+        ) {
+          
+          arr[i] = ch
+          break
+        }
+      }
+    }
+  }
+  
+  return arr.join('')
+};
