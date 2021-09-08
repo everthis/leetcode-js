cfa4ebc50e7e7755f5685fc7df89d2b05db7582a
Nothing <wwwaap@gmail.com>
2021-09-08 19:26:35 +0800

    Update 940-distinct-subsequences-ii.js

--- 940-distinct-subsequences-ii.js
+++ 940-distinct-subsequences-ii.js
@@ -1,4 +1,26 @@
 /**
+ * @param {string} s
+ * @return {number}
+ */
+const distinctSubseqII = function(s) {
+  const n = s.length,
+    dp = Array(26).fill(0),
+    a = 'a'.charCodeAt(0),
+    mod = 1e9 + 7
+  let res = 0
+  for(let ch of s) {
+    const idx = ch.charCodeAt(0) - a
+    let tmp = 0
+    for(let i = 0; i < 26; i++) tmp = (tmp + dp[i]) % mod
+    tmp = (tmp + 1) % mod
+    dp[idx] = tmp
+  }
+  return dp.reduce((ac, e) => (ac + e) % mod, 0)
+};
+
+// another
+
+/**
  * @param {string} S
  * @return {number}
  */
