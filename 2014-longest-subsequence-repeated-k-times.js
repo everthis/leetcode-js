5375fee8c10850c2de4bb19b5273b41413714668
Nothing <wwwaap@gmail.com>
2021-09-20 15:33:15 +0800

    Update 2014-longest-subsequence-repeated-k-times.js

--- 2014-longest-subsequence-repeated-k-times.js
+++ 2014-longest-subsequence-repeated-k-times.js
@@ -3,6 +3,56 @@
  * @param {number} k
  * @return {string}
  */
+const longestSubsequenceRepeatedK = function(s, k) {
+  const n = s.length, a = 'a'.charCodeAt(0)
+  
+  let res = ''
+  const q = ['']
+  
+  while(q.length) {
+    const size = q.length
+    for(let i = 0; i < size; i++) {
+      const cur = q.shift()
+      for(let j = 0; j < 26; j++) {
+        const next = cur + String.fromCharCode(a + j)
+        if(isSub(s, next, k)) {
+          res = next
+          q.push(next)
+        }
+      }
+      
+    }
+  }
+  
+  return res
+  
+  
+  function isSub(s, p, k) {
+    let repeated = 0
+    for(let i = 0, j = 0, n = s.length, m = p.length; i < n; i++) {
+      if(s[i] === p[j]) {
+        j++
+        if(j === m) {
+          repeated++
+          j = 0
+          if(repeated === k) {
+            return true
+          }
+        }
+      }
+    }
+    
+    return false
+  }
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
 var longestSubsequenceRepeatedK = function(s, k) {
     // Max length of the subsequence can be determined by the length of the string and k
     const maxLen = Math.floor(s.length / k);
