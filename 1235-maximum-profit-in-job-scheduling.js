822dff0c934595131d66c89bfb0d952b1d945770
Nothing <wwwaap@gmail.com>
2021-09-26 19:39:39 +0800

    Update 1235-maximum-profit-in-job-scheduling.js

--- 1235-maximum-profit-in-job-scheduling.js
+++ 1235-maximum-profit-in-job-scheduling.js
@@ -6,6 +6,43 @@
  */
 const jobScheduling = function (startTime, endTime, profit) {
   const n = startTime.length
+  const items = Array(n)
+  for(let i = 0;i < n; i++) items[i] = [startTime[i], endTime[i], profit[i]]
+  items.sort((a, b) => a[1] - b[1])
+  const dpEndTime = [0]
+  const dpProfit = [0]
+  for(const [s, e, p] of items) {
+    const prevIdx = binarySearch(dpEndTime, 0, dpEndTime.length - 1, s)
+    const curProfit = dpProfit[prevIdx] + p, maxProfit = dpProfit[dpProfit.length - 1]
+    if(curProfit > maxProfit) {
+      dpProfit.push(curProfit)
+      dpEndTime.push(e)
+    }
+  }
+  
+  return dpProfit[dpProfit.length - 1]
+}
+
+function binarySearch(arr, l, r, x) {
+  while (l < r) {
+    const mid = r - ((r - l) >> 1)
+    if (arr[mid] > x) r = mid - 1
+    else l = mid
+  }
+  return l
+}
+
+
+// another
+
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number[]} profit
+ * @return {number}
+ */
+const jobScheduling = function (startTime, endTime, profit) {
+  const n = startTime.length
   const items = Array.from({ length: startTime.length }, () => Array(3).fill(0))
   for (let i = 0; i < startTime.length; i++) {
     items[i] = [startTime[i], endTime[i], profit[i]]
