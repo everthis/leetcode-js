19b9b62dd2e26a6a19231635a617a0fa29776ada
Nothing <wwwaap@gmail.com>
2021-09-27 10:52:11 +0800

    Update 1751-maximum-number-of-events-that-can-be-attended-ii.js

--- 1751-maximum-number-of-events-that-can-be-attended-ii.js
+++ 1751-maximum-number-of-events-that-can-be-attended-ii.js
@@ -4,6 +4,34 @@
  * @return {number}
  */
 const maxValue = function (events, k) {
+  const n = events.length
+  const memo = Array.from({ length: n + 1 }, () => Array(k + 1).fill(-1))
+  events.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0])
+  return helper(memo, events, n, 0, k)
+}
+
+function helper(memo, events, n, i, k) {
+  if(i === n || k === 0) return 0
+  if(memo[i][k] !== -1) return memo[i][k]
+  let ni = i + 1
+  for(; ni < n; ni++) {
+    if(events[ni][0] > events[i][1]) break
+  }
+
+  return memo[i][k] = Math.max(
+    helper(memo, events, n, i + 1, k),
+    events[i][2] + helper(memo, events, n, ni, k - 1)
+  )
+}
+
+// another
+
+/**
+ * @param {number[][]} events
+ * @param {number} k
+ * @return {number}
+ */
+const maxValue = function (events, k) {
   // d[i][j] 表示以 events[i]结尾的取最多j个最大值
   // d[i][j-1],  Math.max( d[m][j-1] + v[i]) for m ending < start[i]
   events.sort((a, b) => a[1] - b[1])
