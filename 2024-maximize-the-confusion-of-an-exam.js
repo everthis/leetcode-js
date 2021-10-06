a35a0e0e49e5cc1e8f5aabb6108ff345b364370e
Nothing <wwwaap@gmail.com>
2021-10-06 13:59:15 +0800

    Update 2024-maximize-the-confusion-of-an-exam.js

--- 2024-maximize-the-confusion-of-an-exam.js
+++ 2024-maximize-the-confusion-of-an-exam.js
@@ -23,3 +23,28 @@ const maxConsecutiveAnswers = function(answerKey, k) {
   return Math.max(helper(answerKey, 0), helper(answerKey, 1))
 };
 
+// another
+
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+const maxConsecutiveAnswers = function(answerKey, k) {
+  let s = answerKey
+  const freq = Array(26).fill(0), n = s.length, A = 'A'.charCodeAt(0)
+  let res = 0, l = 0, r = 0, maxFreq = 0
+  while(r < n) {
+    maxFreq = Math.max(maxFreq, ++freq[s.charCodeAt(r) - A])
+    if(r - l + 1 - maxFreq > k) {
+      freq[s.charCodeAt(l) - A]--
+      l++
+    }
+    res = Math.max(res, r - l + 1)
+    r++
+  }
+  
+  return res
+};
+
+
