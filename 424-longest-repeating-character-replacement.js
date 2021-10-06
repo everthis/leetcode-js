69fb0020b050c8030dafe42ad856ddd3e5154484
Nothing <wwwaap@gmail.com>
2021-10-06 13:58:09 +0800

    Update 424-longest-repeating-character-replacement.js

--- 424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement.js
@@ -23,3 +23,27 @@ const characterReplacement = function(s, k) {
 
 console.log(characterReplacement("ABAB", 2));
 console.log(characterReplacement("AABABBA", 1));
+
+
+// another
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const characterReplacement = function(s, k) {
+  const freq = Array(26).fill(0), n = s.length, A = 'A'.charCodeAt(0)
+  let res = 0, l = 0, r = 0, maxFreq = 0
+  while(r < n) {
+    maxFreq = Math.max(maxFreq, ++freq[s.charCodeAt(r) - A])
+    if(r - l + 1 - maxFreq > k) {
+      freq[s.charCodeAt(l) - A]--
+      l++
+    }
+    res = Math.max(res, r - l + 1)
+    r++
+  }
+  
+  return res
+};
