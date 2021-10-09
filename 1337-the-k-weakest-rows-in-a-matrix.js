b9c069e1c09fd403281426d8d45b46be744aa7a9
Nothing <wwwaap@gmail.com>
2021-10-09 11:48:35 +0800

    Create 1337-the-k-weakest-rows-in-a-matrix.js

---
+++ 1337-the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,94 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+const kWeakestRows = function(mat, k) {
+  const pq = new PriorityQueue((a,  b) => a[0] === b[0] ? a[1] > b[1] : a[0] > b[0])
+  const res = [], m = mat.length
+  for(let i = 0; i < m; i++) {
+    pq.push([oneNum(mat[i]), i])
+    if(pq.size() > k) pq.pop()
+  }
+  while(k > 0) res[--k] = pq.pop()[1]
+  return res
+};
+
+function oneNum(arr) {
+  let l = 0, h = arr.length
+  while(l < h) {
+    const mid  = l + ((h - l) >> 1)
+    if(arr[mid] === 1) l = mid + 1
+    else h = mid
+  }
+  return l
+}
+
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
