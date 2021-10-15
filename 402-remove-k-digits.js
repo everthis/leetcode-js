4b5d92ba15eed0f11c1535e9c7a6c6127a8cb8ac
Nothing <wwwaap@gmail.com>
2021-10-15 14:28:06 +0800

    Update 402-remove-k-digits.js

--- 402-remove-k-digits.js
+++ 402-remove-k-digits.js
@@ -48,3 +48,28 @@ const removeKdigits = function(num, k) {
   while(stack[0] === '0') stack.shift()
   return stack.length ? stack.join('') : '0'
 };
+
+// another
+
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+const removeKdigits = function(num, k) {
+  const n = num.length, stack = []
+  for(let i = 0; i < n; i++) {
+    const ch = num[i]
+    while(stack.length && k > 0 && ch < stack[stack.length - 1]) {
+      stack.pop()
+      k--
+    }
+    stack.push(ch)
+  }
+  while(k > 0) {
+    stack.pop()
+    k--
+  }
+  while(stack[0] === '0') stack.shift()
+  return stack.length ? stack.join('') : '0'
+};
