8548592a9eb9e45f4558c5b10e35c83dd0574181
Nothing <wwwaap@gmail.com>
2021-10-21 14:21:59 +0800

    Update 1478-allocate-mailboxes.js

--- 1478-allocate-mailboxes.js
+++ 1478-allocate-mailboxes.js
@@ -24,3 +24,39 @@ const minDistance = function (A, K) {
   }
   return dp[n - 1]
 }
+
+// another
+
+/**
+ * @param {number[]} houses
+ * @param {number} k
+ * @return {number}
+ */
+function minDistance(houses, k) {
+  const n = houses.length, { abs, min } = Math, INF = Infinity
+  houses.sort((a, b) => a - b)
+  const costs = Array.from({ length: 100 }, () => Array(100).fill(0))
+  const memo = Array.from({ length: 100 }, () => Array(100).fill(null))
+
+  for(let i = 0; i < n; i++) {
+    for(let j = 0; j < n; j++) {
+      const mid = houses[~~((i + j) >> 1)]
+      for (let k = i; k <= j; k++) costs[i][j] += abs(mid - houses[k])
+    }
+  }
+  
+  return dp(k, 0)
+
+  function dp(k, i) {
+    if (k === 0 && i === n) return 0
+    if (k === 0 || i === n) return INF
+    if (memo[k][i] != null) return memo[k][i]
+    let res = INF
+    for (let j = i; j < n; j++) {
+      res = min(res, costs[i][j] + dp(k - 1, j + 1))
+    }
+
+    return memo[k][i] = res
+  }
+}
+
