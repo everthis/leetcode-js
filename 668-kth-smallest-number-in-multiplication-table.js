365dd7962c80e58aa16164fb75465cf9c3dc89ee
Nothing <wwwaap@gmail.com>
2021-10-29 14:36:57 +0800

    Update 668-kth-smallest-number-in-multiplication-table.js

--- 668-kth-smallest-number-in-multiplication-table.js
+++ 668-kth-smallest-number-in-multiplication-table.js
@@ -30,3 +30,33 @@ function count(m, n, target) {
   }
   return nSmaller;
 }
+
+// another
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const findKthNumber = function(m, n, k) {
+  let left = 1;
+  let right = m * n;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    const num = count(m, n, mid);
+    if (num < k) left = mid + 1;
+    else right = mid;
+  }
+  return left;
+};
+
+function count(m, n, target) {
+  let res = 0;
+  let j = n;
+  for (let i = 1; i <= m; i++) {
+    while (i * j > target) j--
+    res += j;
+  }
+  return res;
+}
