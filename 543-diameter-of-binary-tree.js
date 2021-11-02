222e3fa75622394cb7124aa5a23f6989cd7e5ca9
Nothing <wwwaap@gmail.com>
2021-11-02 09:33:23 +0800

    Update 543-diameter-of-binary-tree.js

--- 543-diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree.js
@@ -23,3 +23,30 @@ const diameterOfBinaryTree = function (root) {
   return longest - 1
 }
 
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const diameterOfBinaryTree = function(root) {
+  let res = 0
+  dfs(root)
+  return res
+
+  function dfs(node) {
+    if(node == null) return 0
+    const left = dfs(node.left), right = dfs(node.right)
+    res = Math.max(res, left + right)
+    return Math.max(left, right) + 1
+  }
+};
+
