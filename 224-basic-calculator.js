a6835c80e891d67c55f59c7a1f85ae4736556b17
Nothing <wwwaap@gmail.com>
2021-11-03 22:17:59 +0800

    Update 224-basic-calculator.js

--- 224-basic-calculator.js
+++ 224-basic-calculator.js
@@ -34,3 +34,46 @@ const calculate = function(s) {
   }
   return res + sign * num
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const calculate = function(s) {
+  s = s.split(' ').join('')
+  const n = s.length, stack = []
+  const isNum = ch => ch >= '0' && ch <= '9'
+  let num = 0, op = 1, res = 0
+  for(let i = 0; i < n; i++) {
+    const ch = s[i]
+    if(isNum(ch)) {
+      num = num * 10 + (+ch)
+    } else {
+      if(ch === '(') {
+        stack.push(res)
+        stack.push(op)
+        num = 0
+        op = 1
+        res = 0
+      } else if(ch === ')') {
+        res += num * op
+        res *= stack.pop()
+        res += stack.pop()
+        num = 0
+        op = 1
+      } else if(ch === '+') {
+        res += op * num
+        op = 1
+        num = 0
+      } else if(ch === '-') {
+        res += op * num
+        op = -1
+        num = 0
+      }
+    }
+  }
+  
+  return res + op * num
+};
