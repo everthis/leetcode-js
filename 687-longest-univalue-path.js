45fb60ae65a5e4ee7180ee5fea1c7bfff39ddacb
Nothing <wwwaap@gmail.com>
2021-11-03 10:11:18 +0800

    Update 687-longest-univalue-path.js

--- 687-longest-univalue-path.js
+++ 687-longest-univalue-path.js
@@ -1,3 +1,37 @@
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const longestUnivaluePath = function(root) {
+  let res = 0
+  dfs(root)
+  return res
+
+  function dfs(node) {
+    if(node == null) return 0
+    let left = dfs(node.left), right = dfs(node.right)
+    if(node.left && node.left.val === node.val) left++
+    else left = 0
+
+    if(node.right && node.right.val === node.val) right++
+    else right = 0
+
+    res = Math.max(res, left + right)
+    return Math.max(left, right)
+  }
+};
+
+// another
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
