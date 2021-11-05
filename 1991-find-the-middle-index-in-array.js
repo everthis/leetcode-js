9004782b244ed266ca464cc80d028a30251f075c
Nothing <wwwaap@gmail.com>
2021-11-05 16:14:47 +0800

    Update 1991-find-the-middle-index-in-array.js

--- 1991-find-the-middle-index-in-array.js
+++ 1991-find-the-middle-index-in-array.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 const findMiddleIndex = function(nums) {
-  const n = nums.length, leftSum = Array(n + 1).fill(0)
+  const n = nums.length
   const sum = nums.reduce((ac, e) => ac + e, 0) 
+  
+  let res, leftSum = 0
   for(let i = 0; i < n; i++) {
-    leftSum[i+1] = leftSum[i] + nums[i]
-  }
-
-  let res
-  for(let i = 0; i < n; i++) {
-    if(leftSum[i] === sum - leftSum[i] - nums[i]) {
+    if(leftSum === sum - leftSum - nums[i]) {
       res = i
       break
     }
+    leftSum += nums[i]
   }
 
   return res == null ? -1 : res
