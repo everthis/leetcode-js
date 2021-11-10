bee98e332bab4518eac55bfc8c9c84d7a5bb7e53
Nothing <wwwaap@gmail.com>
2021-11-10 16:27:46 +0800

    Update 2050-parallel-courses-iii.js

--- 2050-parallel-courses-iii.js
+++ 2050-parallel-courses-iii.js
@@ -6,6 +6,45 @@
  */
 const minimumTime = function(n, relations, time) {
   const graph = {}, dist = Array(n).fill(0), inDegree = Array(n).fill(0)
+  
+  for(let [pre, next] of relations) {
+    pre--, next--
+    if(graph[pre] == null) graph[pre] = []
+    graph[pre].push(next)
+    inDegree[next]++
+  }
+
+  const q = []
+  for(let i = 0; i < n; i++) {
+    if(inDegree[i] === 0) {
+      q.push(i)
+      dist[i] = time[i]
+    }
+  }
+
+  let res = 0
+  while(q.length) {
+    const cur = q.shift()
+    for(const next of (graph[cur] || [])) {
+      dist[next] = Math.max(dist[next], dist[cur] + time[next])
+      inDegree[next]--
+      if(inDegree[next] === 0) q.push(next)
+    }
+  }
+  
+  return Math.max(...dist)
+}
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[][]} relations
+ * @param {number[]} time
+ * @return {number}
+ */
+const minimumTime = function(n, relations, time) {
+  const graph = {}, dist = Array(n).fill(0), inDegree = Array(n).fill(0)
   for(let [from, to] of relations) {
     from--, to--
     if (graph[from] == null) graph[from] = []
