73dff679a61ab180deddf73a848529b52b64a11a
Nothing <wwwaap@gmail.com>
2021-11-15 10:18:44 +0800

    Update 524-longest-word-in-dictionary-through-deleting.js

--- 524-longest-word-in-dictionary-through-deleting.js
+++ 524-longest-word-in-dictionary-through-deleting.js
@@ -3,6 +3,29 @@
  * @param {string[]} dictionary
  * @return {string}
  */
+ const findLongestWord = function(s, dictionary) {
+  let res = ''
+  for (const word of dictionary) {
+    let j = 0
+    for (let i = 0, len = s.length; i < len; i++) {
+      if(word[j] === s[i]) j++
+      if(j === word.length) {
+        if(word.length > res.length) res = word
+        else if(word.length === res.length && word < res) res = word
+        break
+      }
+    }
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {string}
+ */
 const findLongestWord = function(s, dictionary) {
   let res = ''
   for(let d of dictionary) {
