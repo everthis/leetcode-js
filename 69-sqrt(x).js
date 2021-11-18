e0bba0818fc219be581de961bcaf8dc1a071ba08
Nothing <wwwaap@gmail.com>
2021-11-18 09:23:46 +0800

    Update 69-sqrt(x).js

--- 69-sqrt(x).js
+++ 69-sqrt(x).js
@@ -3,6 +3,25 @@
  * @return {number}
  */
 const mySqrt = function(x) {
+  let left = 0, right = x;
+  while (left < right) {
+    let mid = right - ((right - left) >> 1);
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else {
+      left = mid;
+    }
+  }
+  return left;
+};
+
+// another
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const mySqrt = function(x) {
   r = x;
   while (r * r > x) r = ((r + x / r) / 2) | 0;
   return r;
