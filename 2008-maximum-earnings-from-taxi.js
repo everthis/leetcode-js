73026c57c8fe232af275d7587a9965e71b42bce5
Nothing <wwwaap@gmail.com>
2021-11-19 09:38:44 +0800

    Update 2008-maximum-earnings-from-taxi.js

--- 2008-maximum-earnings-from-taxi.js
+++ 2008-maximum-earnings-from-taxi.js
@@ -19,3 +19,34 @@ const maxTaxiEarnings = function(n, rides) {
   }
   return dp[1];
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[][]} rides
+ * @return {number}
+ */
+const maxTaxiEarnings = function(n, rides) {
+  const size = rides.length
+  rides.sort((a, b) => a[1] - b[1])
+  const dp = [[0,0]]
+  for(const [s, e, t] of rides) {
+    const cur = bs(dp, s) + (e - s + t)
+    if(cur > dp[dp.length - 1][1]) {
+      dp.push([e, cur])
+    }
+  }
+  return dp[dp.length - 1][1]
+
+  function bs(arr, t) {
+    let l = 0, r = arr.length - 1
+    while(l < r) {
+      const mid = r - ((r - l) >> 1)
+      if(arr[mid][0] > t) r = mid - 1
+      else l = mid
+    }
+    // console.log(arr, t, l)
+    return arr[l][1]
+  }
+};
