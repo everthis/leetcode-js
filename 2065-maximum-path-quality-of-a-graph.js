6073326593ac506c815effe409254d0fe0177942
Nothing <wwwaap@gmail.com>
2021-11-23 10:46:07 +0800

    Update 2065-maximum-path-quality-of-a-graph.js

--- 2065-maximum-path-quality-of-a-graph.js
+++ 2065-maximum-path-quality-of-a-graph.js
@@ -12,6 +12,41 @@ const maximalPathQuality = function(values, edges, maxTime) {
     graph[u].push([v, t])
     graph[v].push([u, t])
   }
+  let res = 0, visited = new Array(n).fill(0)
+  visited[0] = 1
+  bt(0, values[0], 0)
+  return res
+
+  function bt(i, val, time) {
+    if(time > maxTime) return
+    if(i === 0) res = Math.max(res, val)
+    
+    for(const [next, nextTime] of (graph[i] || [])) {
+      const nextVal = visited[next] > 0 ? val : val + values[next]
+      visited[next]++
+      bt(next, nextVal, time + nextTime)
+      visited[next]--
+    }
+  }
+};
+
+// another
+
+
+/**
+ * @param {number[]} values
+ * @param {number[][]} edges
+ * @param {number} maxTime
+ * @return {number}
+ */
+const maximalPathQuality = function(values, edges, maxTime) {
+  const graph = {}, n = values.length
+  for(const [u, v, t] of edges) {
+    if(graph[u] == null) graph[u] = []
+    if(graph[v] == null) graph[v] = []
+    graph[u].push([v, t])
+    graph[v].push([u, t])
+  }
   let res = 0, visited = Array(n).fill(false)
   bt(0, 0, 0)
   return res
