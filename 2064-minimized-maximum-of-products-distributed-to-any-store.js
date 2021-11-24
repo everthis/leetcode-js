41402a32be7594023ca0cf89478e1b1cd47488c5
Nothing <wwwaap@gmail.com>
2021-11-24 18:54:37 +0800

    Update 2064-minimized-maximum-of-products-distributed-to-any-store.js

--- 2064-minimized-maximum-of-products-distributed-to-any-store.js
+++ 2064-minimized-maximum-of-products-distributed-to-any-store.js
@@ -22,3 +22,28 @@ var minimizedMaximum = function(n, quantities) {
       for (let x of quantities) cnt += Math.floor(x / max) + ((x % max) ? 1 : 0);
       return cnt;
   }
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[]} quantities
+ * @return {number}
+ */
+const minimizedMaximum = function(n, quantities) {
+  let MAX = 0;
+  for (let x of quantities) MAX = Math.max(x, MAX);
+  let l = 1, r = MAX;
+  while (l < r) {
+    let mid = Math.floor((l + r) / 2);
+    if (valid(quantities, mid, n)) l = mid + 1;
+    else r = mid;
+  }
+  return l; 
+};
+
+function valid(quantities,  max, n) {
+  let cnt = 0;
+  for (let x of quantities) cnt += Math.floor(x / max) + ((x % max) ? 1 : 0);
+  return cnt > n;
+}
