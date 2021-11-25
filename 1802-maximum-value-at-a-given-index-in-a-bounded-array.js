9497bf0343a4092c515979fb60dc67f7a21db95e
Nothing <wwwaap@gmail.com>
2021-11-25 17:11:09 +0800

    Update 1802-maximum-value-at-a-given-index-in-a-bounded-array.js

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array.js
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array.js
@@ -4,6 +4,36 @@
  * @param {number} maxSum
  * @return {number}
  */
+const maxValue = function(n, index, maxSum) {
+  const { floor, sqrt } = Math
+  maxSum -= n
+  if(index < Math.floor(n / 2)) index = n - 1 - index
+  let left = index // number of element to the left of the index
+  let right = n - 1 - index // number of element to the right of the index
+  // the triangle area for the left side if not hitting the boundary
+  let leftSum = floor((left * (left + 1)) / 2)
+  // the triangle area for the right side if not hitting the boundary
+  let rightSum = floor((right * (right + 1)) / 2)
+  // case: perfect pyramid
+  if (maxSum <= (rightSum * 2 + right + 1)) return floor(sqrt(maxSum) + 1)
+  // case: right side hits the boundary
+  if (maxSum <= (leftSum + rightSum + (left - right) * right + left + 1)) {
+    const b = 3 + 2 * right
+    return floor((-b + sqrt(b * b - 8 * (rightSum + 1 - right * right - maxSum))) / 2) + 1 + 1
+  }
+  // case: both sides hit boundaries
+  maxSum -= (leftSum + rightSum + (left - right) * right + left + 1)
+  return left + 1 + 1 + floor(maxSum / n)
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} index
+ * @param {number} maxSum
+ * @return {number}
+ */
 const maxValue = function (n, index, maxSum) {
   let ret = 0
   const { max } = Math
