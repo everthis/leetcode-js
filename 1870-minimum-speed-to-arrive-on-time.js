163b834d0906466dca183fbd0c24aa7c47cd4e60
Nothing <wwwaap@gmail.com>
2021-11-26 09:51:47 +0800

    Update 1870-minimum-speed-to-arrive-on-time.js

--- 1870-minimum-speed-to-arrive-on-time.js
+++ 1870-minimum-speed-to-arrive-on-time.js
@@ -4,6 +4,26 @@
  * @return {number}
  */
 const minSpeedOnTime = function(dist, hour) {
+  let n = dist.length, l = 1, r = 1e7 + 1
+  while(l < r) {
+    const mid = l + ((r - l) >> 1)
+    let time = 0
+    for(let i = 0; i < n - 1; i++) time += Math.ceil(dist[i] / mid)
+    time += dist[dist.length - 1] / mid
+    if(time > hour) l = mid + 1
+    else r = mid
+  }
+  return l > 1e7 ? -1 : l
+};
+
+// another
+
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+const minSpeedOnTime = function(dist, hour) {
   let l = 1, r = 1e7
   while(l <= r) {
     let mid = (l + r) >> 1
