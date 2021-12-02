dc2f9557e9a99b6c28166637370f71f5977ba122
Nothing <wwwaap@gmail.com>
2021-12-02 10:16:40 +0800

    Update 206-reverse-linked-list.js

--- 206-reverse-linked-list.js
+++ 206-reverse-linked-list.js
@@ -1,5 +1,32 @@
 /**
  * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function(head) {
+  if(head == null) return head
+  const pre = new ListNode(null, head)
+  let cur = head
+  while(cur.next) {
+    let tmp = pre.next
+    pre.next = cur.next
+    cur.next = cur.next.next
+    pre.next.next = tmp
+  }
+
+  return pre.next
+};
+
+// another
+
+/**
+ * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
  *     this.next = null;
