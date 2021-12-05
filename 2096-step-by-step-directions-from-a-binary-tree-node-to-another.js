cee772b02bf9ad516dadea834144e53232cc00d7
Nothing <wwwaap@gmail.com>
2021-12-05 18:59:45 +0800

    Create 2096-step-by-step-directions-from-a-binary-tree-node-to-another.js

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} startValue
+ * @param {number} destValue
+ * @return {string}
+ */
+var getDirections = function(root, startValue, destValue) {
+    let start = "";
+    let end = "";
+    
+    const traverse = (node, path) => {
+        if (node === null) return;
+        
+        if (node.val === startValue) start = path;
+        if (node.val === destValue) end = path;
+        
+        if (start !== "" && end !== "") return;
+        if (node.left !== null) traverse(node.left, path + "L");
+        if (node.right !== null) traverse(node.right, path + "R");
+    }
+    
+    traverse(root, "");
+    
+    let skip = 0;
+    while (start[skip] && start[skip] === end[skip]) ++skip;
+    
+    return "U".repeat(start.length - skip) + end.substring(skip);   
+};
