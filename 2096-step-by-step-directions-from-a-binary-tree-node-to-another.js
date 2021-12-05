75b4bc7c9b4a49f9b5c2ea0ba9d5f0525f48a97a
Nothing <wwwaap@gmail.com>
2021-12-05 20:34:15 +0800

    Update 2096-step-by-step-directions-from-a-binary-tree-node-to-another.js

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
@@ -12,25 +12,19 @@
  * @param {number} destValue
  * @return {string}
  */
-var getDirections = function(root, startValue, destValue) {
-    let start = "";
-    let end = "";
-    
-    const traverse = (node, path) => {
-        if (node === null) return;
-        
-        if (node.val === startValue) start = path;
-        if (node.val === destValue) end = path;
-        
-        if (start !== "" && end !== "") return;
-        if (node.left !== null) traverse(node.left, path + "L");
-        if (node.right !== null) traverse(node.right, path + "R");
-    }
-    
-    traverse(root, "");
-    
-    let skip = 0;
-    while (start[skip] && start[skip] === end[skip]) ++skip;
-    
-    return "U".repeat(start.length - skip) + end.substring(skip);   
-};
+const getDirections = function (root, startValue, destValue) {
+  let start = ''
+  let end = ''
+  const traverse = (node, path) => {
+    if (node === null) return
+    if (node.val === startValue) start = path
+    if (node.val === destValue) end = path
+    if (start !== '' && end !== '') return
+    if (node.left !== null) traverse(node.left, path + 'L')
+    if (node.right !== null) traverse(node.right, path + 'R')
+  }
+  traverse(root, '')
+  let skip = 0
+  while (start[skip] && start[skip] === end[skip]) skip++
+  return 'U'.repeat(start.length - skip) + end.slice(skip)
+}
