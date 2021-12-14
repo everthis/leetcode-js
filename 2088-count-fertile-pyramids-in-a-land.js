fbf647085254a8c7a705f0c6c003a2824d469bfb
Nothing <wwwaap@gmail.com>
2021-12-14 10:34:08 +0800

    Update 2088-count-fertile-pyramids-in-a-land.js

--- 2088-count-fertile-pyramids-in-a-land.js
+++ 2088-count-fertile-pyramids-in-a-land.js
@@ -2,35 +2,19 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const countPyramids = function(grid) {
-  const rev = clone(grid)
+ const countPyramids = function(grid) {
+  const rev = clone(grid).reverse()
   let res = count(grid)
-  rev.reverse()
   res += count(rev)
   return res
 
   function clone(grid) {
-    let res = []
-    for (const row of grid) {
-      res.push(row.slice())
-    }
-    return res
-  }
-  function reverse(grid) {
-    let l = 0, r = grid.length - 1
-    while(l < r) {
-      const tmp = grid[l]
-      grid[l] = grid[r]
-      grid[r] = tmp
-      l++
-      r--
-    }
+    return grid.map(e => e.slice())
   }
 
   function count(grid) {
     const m = grid.length, n = grid[0].length
     let res = 0
-
     for (let i = 1; i < m; i++) {
       for (let j = 1; j < n - 1; j++) {
         if (grid[i][j] && grid[i - 1][j]) {
@@ -42,7 +26,6 @@ const countPyramids = function(grid) {
         }
       }
     }
-
     return res
   }
 };
