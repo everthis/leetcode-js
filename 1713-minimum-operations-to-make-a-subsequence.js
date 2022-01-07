1664e4a1add4cd69f7b54a4004e07dacdde58c67
Nothing <wwwaap@gmail.com>
2022-01-07 09:50:56 +0800

    Update 1713-minimum-operations-to-make-a-subsequence.js

--- 1713-minimum-operations-to-make-a-subsequence.js
+++ 1713-minimum-operations-to-make-a-subsequence.js
@@ -3,6 +3,32 @@
  * @param {number[]} arr
  * @return {number}
  */
+const minOperations = function(target, arr) {
+  const hash = {}
+  for (let i = 0, n = target.length; i < n; i++) {
+    hash[target[i]] = i
+  }
+  const stk = []
+  for(let e of arr) {
+    if(hash[e] == null) continue
+    let l = 0, r = stk.length
+    while(l < r) {
+      const mid = l + (~~((r - l) / 2))
+      if(stk[mid] < hash[e]) l = mid + 1
+      else r = mid
+    }
+    stk[l] = hash[e]
+  }
+  return target.length - stk.length
+};
+
+// another
+
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {number}
+ */
 const minOperations = function (target, arr) {
   let length1 = target.length,
     length2 = arr.length
