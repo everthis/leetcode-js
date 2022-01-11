2a8c0177cea9eec674b222edeb52595c10cacd55
Nothing <wwwaap@gmail.com>
2022-01-11 09:34:13 +0800

    Update 1963-minimum-number-of-swaps-to-make-the-string-balanced.js

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced.js
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced.js
@@ -3,6 +3,25 @@
  * @return {number}
  */
 const minSwaps = function(s) {
+  const stk = []
+  for (let e of s) {
+    if(e === '[') stk.push(e)
+    else {
+      if(stk.length) {
+        stk.pop()
+      } else stk.push(e)
+    }
+  }
+  return Math.ceil(stk.length / 2)
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minSwaps = function(s) {
   const stack = []
   let num = 0
   for(let e of s) {
