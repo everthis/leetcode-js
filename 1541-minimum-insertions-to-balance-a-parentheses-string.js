903cc8d03e2b4db2a20b4e8dd0f175865083c325
Nothing <wwwaap@gmail.com>
2022-01-15 19:10:48 +0800

    Update 1541-minimum-insertions-to-balance-a-parentheses-string.js

--- 1541-minimum-insertions-to-balance-a-parentheses-string.js
+++ 1541-minimum-insertions-to-balance-a-parentheses-string.js
@@ -3,6 +3,37 @@
  * @return {number}
  */
 const minInsertions = function(s) {
+  let insert = 0, idx = 0, open = 0, len = s.length
+  while(idx < len) {
+    const ch = s[idx]
+    if(ch === '(') {
+      open++
+      idx++
+    } else {
+      if(open > 0) {
+        open--
+      } else {
+        insert++
+      }
+      if(idx < len - 1 && s[idx + 1] === ')') {
+        idx += 2
+      } else {
+        insert++
+        idx++
+      }
+    }
+  }
+  if(open) insert += open * 2
+  return insert
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minInsertions = function(s) {
     let res = 0, right = 0;
     for (let i = 0; i < s.length; ++i) {
         if (s.charAt(i) == '(') {
