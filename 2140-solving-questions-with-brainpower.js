1fb1cb2a59245efbb82183c198059131ba5dbc67
Nothing <wwwaap@gmail.com>
2022-01-16 17:21:13 +0800

    Create 2140-solving-questions-with-brainpower.js

---
+++ 2140-solving-questions-with-brainpower.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} questions
+ * @return {number}
+ */
+var mostPoints = function (questions) {
+  let n = questions.length
+  const temp = Array(n).fill(0)
+
+  temp[n - 1] = questions[n - 1][0]
+
+  for (let i = n - 2; i >= 0; i--) {
+    if (i + questions[i][1] + 1 <= n - 1)
+      temp[i] = Math.max(
+        temp[i + 1],
+        questions[i][0] + temp[Math.min(n - 1, i + questions[i][1] + 1)]
+      )
+    else temp[i] = Math.max(temp[i + 1], questions[i][0])
+  }
+  return temp[0]
+}
