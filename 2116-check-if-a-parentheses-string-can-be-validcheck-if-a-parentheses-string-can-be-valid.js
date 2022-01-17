910792ac99d0b4bcd40ccbf0f7a20241c5d4d759
Nothing <wwwaap@gmail.com>
2022-01-17 19:44:45 +0800

    Update 2116-check-if-a-parentheses-string-can-be-validcheck-if-a-parentheses-string-can-be-valid.js

--- 2116-check-if-a-parentheses-string-can-be-validcheck-if-a-parentheses-string-can-be-valid.js
+++ 2116-check-if-a-parentheses-string-can-be-validcheck-if-a-parentheses-string-can-be-valid.js
@@ -3,6 +3,31 @@
  * @param {string} locked
  * @return {boolean}
  */
+const canBeValid = function(s, locked) {
+  const n = s.length
+  if(n % 2 === 1) return false
+  let x = 0
+  for(let i = 0; i < n; i++) {
+    if(s[i] === '(' || locked[i] === '0') x++
+    else if(x > 0) x--
+    else return false
+  }
+  x = 0
+  for(let i = n - 1; i >= 0; i--) {
+    if(s[i] === ')' || locked[i] === '0') x++
+    else if(x > 0) x--
+    else return false
+  }
+  return true
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string} locked
+ * @return {boolean}
+ */
 const canBeValid = function (s, locked) {
   return s.length % 2 === 0 && chk(s, locked, '(') && chk(s, locked, ')')
 
