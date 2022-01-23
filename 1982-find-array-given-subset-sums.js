c36c73d2744b05c9a0c591adcdabca95316bd2ab
Nothing <wwwaap@gmail.com>
2022-01-23 20:14:25 +0800

    Update 1982-find-array-given-subset-sums.js

--- 1982-find-array-given-subset-sums.js
+++ 1982-find-array-given-subset-sums.js
@@ -89,3 +89,46 @@ function bs(arr, val) {
   return arr[l] === val
 }
 
+// another
+
+/**
+ * @param {number} n
+ * @param {number[]} sums
+ * @return {number[]}
+ */
+const recoverArray = function(n, sums) {
+  const res = []
+  sums.sort((a, b) => a - b)
+  
+  while(res.length < n) {
+    const m = sums.length, visited = Array(m).fill(false)
+    let a1 = [], a2 = [], delta = sums[1] - sums[0]
+    for(let i = 0, j = 1; i < m && j < m; i++, j++) {
+      while(i < m && visited[i]) i++
+      if(i === m) break
+      while(i >= j || sums[j] !== sums[i] + delta) j++
+      if(j === m) break
+      a1.push(sums[i])
+      a2.push(sums[j])
+      visited[i] = visited[j] = true
+    }
+    if(binarySearch(a1, 0)) {
+      sums = a1
+      res.push(delta)
+    } else {
+      sums = a2
+      res.push(-delta)
+    }
+  }
+  return res
+  
+  function binarySearch(arr, val) {
+    let l = 0, r = arr.length - 1
+    while(l < r) {
+      const mid = ~~((l + r) / 2)
+      if(arr[mid] < val) l = mid + 1
+      else r = mid
+    }
+    return arr[l] === val
+  }
+};
