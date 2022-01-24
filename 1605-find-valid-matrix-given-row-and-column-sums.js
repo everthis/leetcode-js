590f5ee5d699ee5226e82495d0ae2bda12fda4b4
Nothing <wwwaap@gmail.com>
2022-01-24 10:49:53 +0800

    Update 1605-find-valid-matrix-given-row-and-column-sums.js

--- 1605-find-valid-matrix-given-row-and-column-sums.js
+++ 1605-find-valid-matrix-given-row-and-column-sums.js
@@ -15,3 +15,23 @@ const restoreMatrix = function(rowSum, colSum) {
   }
   return res;
 };
+
+// another
+
+/**
+ * @param {number[]} rowSum
+ * @param {number[]} colSum
+ * @return {number[][]}
+ */
+const restoreMatrix = function(rowSum, colSum) {
+  const m = rowSum.length, n = colSum.length
+  const res = Array.from({ length: m }, () => Array(n).fill(0))
+  for(let i = 0; i < m; i++) {
+    for(let j = 0; j < n; j++) {
+      res[i][j] = Math.min(rowSum[i], colSum[j])
+      rowSum[i] -= res[i][j]
+      colSum[j] -= res[i][j]
+    }
+  }
+  return res
+};
