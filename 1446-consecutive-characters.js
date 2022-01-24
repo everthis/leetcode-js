db6919b54e1d00cf1ee55a810010a877e0dc3de3
Nothing <wwwaap@gmail.com>
2022-01-24 12:08:07 +0800

    Update 1446-consecutive-characters.js

--- 1446-consecutive-characters.js
+++ 1446-consecutive-characters.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 const maxPower = function(s) {
+  let res = 1, cnt = 1
+  for(let i = 1; i < s.length; i++) {
+    if(s[i] === s[i - 1]) {
+      if(++cnt > res) res = cnt
+    } else {
+      cnt = 1
+    }
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maxPower = function(s) {
   let prev = '', prevIdx = -1, res = -Infinity
   for(let i = 0; i < s.length; i++) {
     const cur = s[i]
