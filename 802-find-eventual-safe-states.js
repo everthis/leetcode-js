b3694be4df7fd5e2e3cdd768b3eaa558149f9ebc
Nothing <wwwaap@gmail.com>
2022-01-27 11:43:53 +0800

    Update 802-find-eventual-safe-states.js

--- 802-find-eventual-safe-states.js
+++ 802-find-eventual-safe-states.js
@@ -24,3 +24,30 @@ function dfs(graph, node, memo, visited) {
   memo[node] = hasCycle
   return hasCycle
 }
+
+// another
+
+/**
+ * @param {number[][]} graph
+ * @return {number[]}
+ */
+const eventualSafeNodes = function(graph) {
+  const res = []
+  if(graph == null || graph.length === 0) return res
+  const n = graph.length
+  const color = Array(n).fill(0)
+  for(let i = 0; i < n; i++) {
+    if(bt(graph, i, color)) res.push(i)
+  }
+  return res
+
+  function bt(graph, start, color) {
+    if(color[start] !== 0) return color[start] === 1
+    color[start] = 2
+    for(let next of graph[start]) {
+      if(!bt(graph, next, color)) return false
+    }
+    color[start] = 1
+    return true
+  }
+};
