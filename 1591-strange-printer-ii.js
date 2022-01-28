bd640742bf759bb97aee13d73652c5cdb7ec6488
Nothing <wwwaap@gmail.com>
2022-01-28 22:05:26 +0800

    Update 1591-strange-printer-ii.js

--- 1591-strange-printer-ii.js
+++ 1591-strange-printer-ii.js
@@ -49,3 +49,79 @@ const isPrintable = function (targetGrid) {
     return true
   }
 }
+
+// another
+
+/**
+ * @param {number[][]} targetGrid
+ * @return {boolean}
+ */
+const isPrintable = function (targetGrid) {
+  /*
+     1 -> 3
+     1 -> 4
+     1 -> 5
+     3 -> 4
+    */
+
+  const dependencies = {}
+
+  /*
+    3: [mini, maxi, minj, maxj]
+    */
+  const extents = {}
+
+  for (let i = 0; i < targetGrid.length; i++) {
+    for (let j = 0; j < targetGrid[i].length; j++) {
+      const n = targetGrid[i][j]
+      let inf = Infinity
+      extents[n] = extents[n] || {
+        n,
+        mini: inf,
+        minj: inf,
+        maxi: -inf,
+        maxj: -inf,
+      }
+      extents[n].mini = Math.min(i, extents[n].mini)
+      extents[n].minj = Math.min(j, extents[n].minj)
+      extents[n].maxi = Math.max(i, extents[n].maxi)
+      extents[n].maxj = Math.max(j, extents[n].maxj)
+    }
+  }
+
+  function canRemove(obj) {
+    for (let i = obj.mini; i <= obj.maxi; i++) {
+      for (let j = obj.minj; j <= obj.maxj; j++) {
+        const val = targetGrid[i][j]
+        if (val !== null && val !== obj.n) return false
+      }
+    }
+    return true
+  }
+
+  function remove(obj) {
+    for (let i = obj.mini; i <= obj.maxi; i++) {
+      for (let j = obj.minj; j <= obj.maxj; j++) {
+        targetGrid[i][j] = null
+      }
+    }
+    delete extents[obj.n]
+  }
+
+  while (Object.keys(extents).length > 0) {
+    let found = false
+    for (const n in extents) {
+      const obj = extents[n]
+      if (canRemove(obj)) {
+        remove(obj)
+        found = true
+        break
+      }
+    }
+    if (!found) {
+      return false
+    }
+  }
+  return true
+}
+
