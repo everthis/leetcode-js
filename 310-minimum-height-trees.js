a102910ac50ccaa8cbcfb473f01742ef299fbdcc
Nothing <wwwaap@gmail.com>
2022-01-29 10:59:54 +0800

    Update 310-minimum-height-trees.js

--- 310-minimum-height-trees.js
+++ 310-minimum-height-trees.js
@@ -3,6 +3,44 @@
  * @param {number[][]} edges
  * @return {number[]}
  */
+ const findMinHeightTrees = function(n, edges) {
+  if(n === 1) return [0]
+  const res = [], graph = {}
+  for(const [u, v] of edges) {
+    if(graph[u] == null) graph[u] = new Set()
+    if(graph[v] == null) graph[v] = new Set()
+    graph[u].add(v)
+    graph[v].add(u)
+  }
+
+  let leaves = []
+  Object.keys(graph).forEach(k => {
+    if(graph[k].size === 1) leaves.push(+k)
+  })
+  while(n > 2) {
+    const newLeaves = []
+    const size = leaves.length
+    for (let i = 0; i < size; i++) {
+      const cur = leaves.pop()
+      for (const next of graph[cur]) {
+        graph[next].delete(cur)
+        if(graph[next].size === 1) newLeaves.push(next)
+      }
+    }
+    n -= size
+    leaves = newLeaves
+  }
+  
+  return leaves
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
 const findMinHeightTrees = function(n, edges) {
   if (n === 1) {
     return [0];
