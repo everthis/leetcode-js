1f8cc21be613193c5822dbfb52c2085de861c4da
Nothing <wwwaap@gmail.com>
2022-02-02 00:19:38 +0800

    Update 2136-earliest-possible-day-of-full-bloom.js

--- 2136-earliest-possible-day-of-full-bloom.js
+++ 2136-earliest-possible-day-of-full-bloom.js
@@ -4,6 +4,31 @@
  * @return {number}
  */
 const earliestFullBloom = function(plantTime, growTime) {
+  const n = plantTime.length, arr = Array(n)
+  for(let i = 0; i < n; i++) {
+    arr.push([growTime[i], plantTime[i]])
+  }
+  arr.sort((a, b) => b[0] - a[0])
+  
+  let res = 0, cur = 0
+  for(let i = 0; i < n; i++) {
+    const e = arr[i]
+    res = Math.max(res, cur + e[0] + e[1])
+    cur += e[1]
+  }
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {number[]} plantTime
+ * @param {number[]} growTime
+ * @return {number}
+ */
+const earliestFullBloom = function(plantTime, growTime) {
   const sum = arr => arr.reduce((ac, e) => ac +e, 0)
   let l = 0, r = sum(plantTime) + sum(growTime)
   const n = plantTime.length
