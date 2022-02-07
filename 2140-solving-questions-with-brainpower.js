7745e06383b352bee689a8caebd4986f8e4d9757
Nothing <wwwaap@gmail.com>
2022-02-07 09:04:01 +0800

    Update 2140-solving-questions-with-brainpower.js

--- 2140-solving-questions-with-brainpower.js
+++ 2140-solving-questions-with-brainpower.js
@@ -2,6 +2,21 @@
  * @param {number[][]} questions
  * @return {number}
  */
+const mostPoints = function(questions) {
+  const n = questions.length, dp = Array(n + 1).fill(0)
+  for (let i = n - 1; i >= 0; i--) {
+    const [gain, p] = questions[i]
+    dp[i] = Math.max(dp[i + 1], (dp[p + i + 1] || 0) + gain)
+  }
+  return dp[0]
+};
+
+// another
+
+/**
+ * @param {number[][]} questions
+ * @return {number}
+ */
 const mostPoints = function (questions) {
   let n = questions.length
   const temp = Array(n).fill(0)
