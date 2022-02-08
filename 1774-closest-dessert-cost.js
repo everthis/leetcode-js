c413b748afe2243999c793f7615417401486a0bf
Nothing <wwwaap@gmail.com>
2022-02-08 13:51:18 +0800

    Update 1774-closest-dessert-cost.js

--- 1774-closest-dessert-cost.js
+++ 1774-closest-dessert-cost.js
@@ -5,6 +5,36 @@
  * @return {number}
  */
 const closestCost = function(baseCosts, toppingCosts, target) {
+  let res = baseCosts[0], n = baseCosts.length, m = toppingCosts.length
+  const { abs } = Math
+  for (let i = 0; i < n; i++) {
+    helper(0, baseCosts[i])
+  }
+  return res
+  function helper(i, cur) {
+    if(
+      abs(cur - target) < abs(res - target)
+      || (abs(cur - target) === abs(res - target) && cur < res)
+    ) {
+      res = cur
+    }
+    if(i === m || cur > target) return
+    helper(i + 1, cur)
+    helper(i + 1, cur + toppingCosts[i])
+    helper(i + 1, cur + toppingCosts[i] * 2)
+  }
+};
+
+// another
+
+
+/**
+ * @param {number[]} baseCosts
+ * @param {number[]} toppingCosts
+ * @param {number} target
+ * @return {number}
+ */
+const closestCost = function(baseCosts, toppingCosts, target) {
     let n = baseCosts.length, m = toppingCosts.length;
     const { abs } = Math
     const costs = new Set();
