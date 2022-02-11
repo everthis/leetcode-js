6dcb918f22258d822a1b5bc2d1ac9f9be9a0850e
Nothing <wwwaap@gmail.com>
2022-02-11 17:19:57 +0800

    Update 1601-maximum-number-of-achievable-transfer-requests.js

--- 1601-maximum-number-of-achievable-transfer-requests.js
+++ 1601-maximum-number-of-achievable-transfer-requests.js
@@ -1,3 +1,37 @@
+// O(n * 2 ^ r) 
+// r: number of requests
+/**
+ * @param {number} n
+ * @param {number[][]} requests
+ * @return {number}
+ */
+const maximumRequests = function(n, requests) {
+  const arr = Array(n).fill(0)
+  let res = 0
+  bt(requests, 0, arr, 0)
+  return res
+  function bt(r, idx, arr, num) {
+    if(idx === r.length) {
+      for(let i = 0; i < n; i++) {
+        if(arr[i] !== 0) return
+      }
+      res = Math.max(res, num)
+      return
+    }
+    const [from, to] = r[idx]
+    arr[from]++
+    arr[to]--
+    bt(r, idx + 1, arr, num + 1)
+    arr[from]--
+    arr[to]++
+    
+    bt(r, idx + 1, arr, num)
+  }
+};
+
+
+// another
+
 /**
  * @param {number} n
  * @param {number[][]} requests
