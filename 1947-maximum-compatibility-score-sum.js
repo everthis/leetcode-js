0436dd21684e3b77ba0ce01e16cee9e2f84b9e9d
Nothing <wwwaap@gmail.com>
2022-02-16 20:51:36 +0800

    Update 1947-maximum-compatibility-score-sum.js

--- 1947-maximum-compatibility-score-sum.js
+++ 1947-maximum-compatibility-score-sum.js
@@ -40,9 +40,8 @@ const maxCompatibilitySum = function(students, mentors) {
  */
 const maxCompatibilitySum = function(students, mentors) {
   const obj = { res: 0 }, hash = {}
-  for(let i = 0, n = students.length; i < n; i++) {
-    bt(students, mentors, 0, 0, obj, hash)
-  }
+  bt(students, mentors, 0, 0, obj, hash)
+
   return obj.res
 };
 
@@ -51,7 +50,6 @@ function bt(stu, men, i, score, obj, hash) {
   if(i === stu.length) {
     if(score > obj.res) {
       obj.res = score
-      // console.log(hash)
     }
     return
   }
