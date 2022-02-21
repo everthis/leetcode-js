d29635fe43e5dd092e5dd3755a6c2c0acd88b638
Nothing <wwwaap@gmail.com>
2022-02-21 13:36:55 +0800

    Update 1655-distribute-repeating-integers.js

--- 1655-distribute-repeating-integers.js
+++ 1655-distribute-repeating-integers.js
@@ -111,3 +111,59 @@ const canDistribute = function(nums, quantity) {
     return dp[idx][mask] = res
   }
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} quantity
+ * @return {boolean}
+ */
+const canDistribute = function (nums, quantity) {
+  const hash = {}
+  for(const e of nums) {
+    if(hash[e] == null) hash[e] = 0
+    hash[e]++
+  }
+  const cnts = Object.values(hash), m = quantity.length, n = cnts.length
+  const dp = Array.from({ length: n }, () => Array(1 << m).fill(null))
+  
+  return helper(0, 0)
+
+  function helper(idx, mask) {
+    // mask are already selected candidates
+    if(mask == (1 << m) - 1) {
+        return true;
+    }
+    if(idx == n) {
+        return false;
+    }
+    if(dp[idx][mask] != null) {
+        return dp[idx][mask];
+    }
+    let ans = helper(idx + 1, mask);
+    
+    for(let i = 1; i < (1 << m); ++i) {
+        // i are potential candidates in addition to already selected ones (from mask)
+        // if i == mask, we can skip as the candidate is selected already
+        // if mask != (mask & i) means that this candidate does not include selected ones e.g
+        // mask = 3 (i.e 2 elements 1,2 in binary) and i = 4 (the third element in binary as 4 does not include 1 & 2), there we skip
+        if(mask == i || mask != (mask & i)) continue;
+        let sum = 0;
+        for(let j = 0; j < m; ++j) {
+            // mask << ~j is just a fancy way to do: if(mask & (1 << j)) that i've learned from @Uwi and this way you don't have to use "(", ")"
+            // what it does is simply pushing the jth bit to the 2^31 bit which is negative
+            // thus if the jth bit is 1 then the value is less than zero and if its 0 then its greater or equal to zero
+            if(mask << ~j >= 0 && i << ~j < 0) {  // check that mask does not contain the new candidate and that the candidate is part of the potential candidate i
+                sum += quantity[j];
+            }
+        }
+        if(sum <= cnts[idx]) {
+            ans |= helper(idx + 1, i);
+        }
+        if(ans) break; // if ans is true, then a solution exists and no further computation is required
+    }
+    dp[idx][mask] = ans;
+    return ans;
+  }
+}
