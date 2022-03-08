7ea4451a50556738c3b1f6726990b9e36b6f7dd6
Nothing <wwwaap@gmail.com>
2022-03-08 15:51:34 +0800

    Update 2167-minimum-time-to-remove-all-cars-containing-illegal-goods.js

--- 2167-minimum-time-to-remove-all-cars-containing-illegal-goods.js
+++ 2167-minimum-time-to-remove-all-cars-containing-illegal-goods.js
@@ -27,6 +27,35 @@ const minimumTime = function(s) {
  * @param {string} s
  * @return {number}
  */
+const minimumTime = function(s) {
+  if(s.length === 1) return s === '1' ? 1 : 0
+  const n = s.length
+  const arr = []
+  for(let ch of s) {
+    arr.push(ch === '1' ? 1 : -1)
+  }
+  const score = minSum(arr)
+  return n + score
+
+  function minSum(ar) {
+    const dp = Array(n).fill(0)
+    dp[0] = ar[0]
+    let ans = dp[0]
+    for(let i = 1; i < n; i++) {
+      dp[i] = Math.min(ar[i], ar[i] + dp[i - 1])
+      ans = Math.min(0, ans, dp[i])
+    }
+    return ans
+  }
+};
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var minimumTime = function(s) {
 
   const { max, min } = Math
