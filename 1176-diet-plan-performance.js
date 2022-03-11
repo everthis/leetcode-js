fc823c28b47576fd0b4a2e380ec9ddd5f1a945fe
Nothing <wwwaap@gmail.com>
2022-03-11 15:17:46 +0800

    Update 1176-diet-plan-performance.js

--- 1176-diet-plan-performance.js
+++ 1176-diet-plan-performance.js
@@ -7,6 +7,31 @@
  */
 var dietPlanPerformance = function(calories, k, lower, upper) {
     let res = 0
+    for(let i = 0, n = calories.length, tmp = 0; i < n; i++) {
+      tmp += calories[i]
+      if(i >= k - 1) {
+        if(i >= k) {
+          tmp -= calories[i - k]
+        }
+        if(tmp < lower) res--
+        else if(tmp > upper) res++
+      }
+
+    }
+    return res
+};
+
+// another
+
+/**
+ * @param {number[]} calories
+ * @param {number} k
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number}
+ */
+var dietPlanPerformance = function(calories, k, lower, upper) {
+    let res = 0
     for(let i = 0, n = calories.length; i < n - k + 1; i++) {
       let tmp = 0
       for(let j = 0; j < k && i + j < n; j++) {
