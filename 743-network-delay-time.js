fd0f57aad0cde908c1fe2ebcfe39c3e3df41208d
Nothing <wwwaap@gmail.com>
2022-03-16 16:36:11 +0800

    Update 743-network-delay-time.js

--- 743-network-delay-time.js
+++ 743-network-delay-time.js
@@ -47,3 +47,104 @@ const networkDelayTime = function(times, N, K) {
     const res = Math.max(...distances);
     return res === Infinity ? -1 : res;
 };
+
+// another
+
+/**
+ * @param {number[][]} times
+ * @param {number} N
+ * @param {number} K
+ * @return {number}
+ */
+const networkDelayTime = function (times, N, K) {
+  const hash = {}
+  for(const [u, v, t] of times) {
+    if(hash[u] == null) hash[u] = {}
+    hash[u][v] = t
+  }
+  const pq = new PriorityQueue((a, b) => a[0] < b[0])
+  pq.push([0, K])
+  const visited = Array.from(N + 1)
+  let res = 0
+  while(!pq.isEmpty()) {
+    const [dist, cur] = pq.pop()
+    if(visited[cur]) continue
+    visited[cur] = true
+    res = dist
+    N--
+    if(hash[cur]) {
+      for(let next of Object.keys(hash[cur])) {
+        pq.push([dist + hash[cur][next], next])
+      }
+    }
+  }
+  return N === 0 ? res : -1
+}
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
