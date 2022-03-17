88be6ffb6f2ee43b090868fa3e6c1ffca0e50561
Nothing <wwwaap@gmail.com>
2022-03-17 10:28:10 +0800

    Update 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.js

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.js
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.js
@@ -2,6 +2,38 @@
  * @param {number[][]} grid
  * @return {number}
  */
+function minCost(grid) {
+  const m = grid.length, n = grid[0].length
+  const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]] // right, left, down, up
+  const dp = Array.from({ length: m }, () => Array(n).fill(Infinity))
+  let q = [[0, 0]]
+  dp[0][0] = 0
+  while(q.length) {
+    const tmp = []
+    for(let idx = q.length - 1; idx >= 0; idx--) {
+      const [r, c] = q[idx]
+      for(let i = 0; i < dirs.length; i++) {
+        const [dr, dc] = dirs[i]
+        const nr = r + dr, nc = c + dc
+        if(nr < 0 || nr >= m || nc < 0 || nc >= n) continue
+        if(dp[nr][nc] > dp[r][c] + (i === grid[r][c] - 1 ? 0 : 1)) {
+          dp[nr][nc] = dp[r][c] + (i === grid[r][c] - 1 ? 0 : 1)
+          tmp.push([nr, nc])
+        }
+      }
+    }
+    q = tmp
+  }
+
+  return dp[m - 1][n - 1]
+}
+
+// another
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
 const minCost = function (grid) {
   const n = grid.length
   const m = grid[0].length
