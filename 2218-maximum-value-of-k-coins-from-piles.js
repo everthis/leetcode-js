6b0a7f342a0efaf85524797d347ec45fda99d1bb
Nothing <wwwaap@gmail.com>
2022-03-30 11:55:28 +0800

    Update 2218-maximum-value-of-k-coins-from-piles.js

--- 2218-maximum-value-of-k-coins-from-piles.js
+++ 2218-maximum-value-of-k-coins-from-piles.js
@@ -32,7 +32,8 @@ const maxValueOfCoins = function(piles, k) {
   const memo = Array.from({ length: n + 1 }, () => Array(k + 1).fill(null))
   return helper(0, k)
 
-  // TC: O(n * m)
+  // TC: O(k * m)
+  // k: k
   // n: length of piles
   // m: sum(piles[i]), total elements of all piles
   function helper(i, k) {
