2927963c744ef0726c47f7209d2832d217959733
Nothing <wwwaap@gmail.com>
2022-04-12 22:19:32 +0800

    Update 2226-maximum-candies-allocated-to-k-children.js

--- 2226-maximum-candies-allocated-to-k-children.js
+++ 2226-maximum-candies-allocated-to-k-children.js
@@ -20,3 +20,23 @@ const maximumCandies = function(candies, k) {
   }
   return min;
 };
+
+// another
+
+/**
+ * @param {number[]} candies
+ * @param {number} k
+ * @return {number}
+ */
+const maximumCandies = function(candies, k) {
+  let max = candies.reduce((ac, e) => ac + e, 0)
+  let min = 0
+  while(min < max) {
+    const mid = max - Math.floor((max - min) /2)
+    let num = 0
+    for(let e of candies) num += ~~(e / mid)
+    if(num < k) max = mid - 1
+    else min = mid
+  }
+  return min
+};
