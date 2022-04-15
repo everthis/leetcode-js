26530bc1436d35a1abdd16c15e4a4c0d3573d63e
Nothing <wwwaap@gmail.com>
2022-04-15 16:00:03 +0800

    Update 1608-special-array-with-x-elements-greater-than-or-equal-x.js

--- 1608-special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -2,6 +2,32 @@
  * @param {number[]} nums
  * @return {number}
  */
+const specialArray = function(nums) {
+  let l = -1, r = 1001
+  while(l <= r) {
+    const mid = r - Math.floor((r - l) / 2)
+    const tmp = valid(mid)
+    if(tmp === mid) return mid
+    else if(tmp > mid) l = mid + 1
+    else r = mid - 1
+  }
+  return -1
+  
+  function valid(mid) {
+    let res = 0
+    for(let e of nums) {
+      if(e >= mid) res++
+    }
+    return res
+  }
+};
+
+// another 
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 const specialArray = function (nums) {
     nums.sort((a, b) => b - a)
     let i = 0
