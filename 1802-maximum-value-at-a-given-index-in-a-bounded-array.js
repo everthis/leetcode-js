be10a57c9a4c6f2815771f3b021c3c45dbe02daa
Nothing <wwwaap@gmail.com>
2022-04-23 22:02:40 +0800

    Update 1802-maximum-value-at-a-given-index-in-a-bounded-array.js

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array.js
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array.js
@@ -8,18 +8,18 @@ const maxValue = function (n, index, maxSum) {
   maxSum -= n;
   let left = 0, right = maxSum, mid;
   while (left < right) {
-      mid = Math.floor((left + right + 1) / 2);
-      if (v2(mid) <= maxSum)left = mid;
-      else right = mid - 1;
+    const mid = right - Math.floor((right - left) / 2);
+    if (valid(mid))left = mid;
+    else right = mid - 1;
   }
   return left + 1;
   
-  function v2(a) {
-    let b = Math.max(a - index, 0);
-    let res = (a + b) * (a - b + 1) / 2;
-    b = Math.max(a - ((n - 1) - index), 0);
-    res += (a + b) * (a - b + 1) / 2;
-    return res - a;
+  function valid(mid) {
+    let b = Math.max(mid - index, 0);
+    let res = (mid + b) * (mid - b + 1) / 2;
+    b = Math.max(mid - ((n - 1) - index), 0);
+    res += (mid + b) * (mid - b + 1) / 2;
+    return res - mid <= maxSum;
   }
 }
 
