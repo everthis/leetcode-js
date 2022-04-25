7b87f2fd38f913d5dab161cb1be61977f1e3a1f0
Nothing <wwwaap@gmail.com>
2022-04-25 11:33:22 +0800

    Update 1231-divide-chocolate.js

--- 1231-divide-chocolate.js
+++ 1231-divide-chocolate.js
@@ -21,3 +21,34 @@ const maximizeSweetness = function (sweetness, K) {
   }
   return left
 }
+
+// another
+
+/**
+ * @param {number[]} sweetness
+ * @param {number} K
+ * @return {number}
+ */
+const maximizeSweetness = function(sweetness, K) {
+  let l = 1, r = 10 ** 9
+  while(l < r) {
+    const mid = r - Math.floor((r - l) / 2)
+    if(valid(mid)) l = mid
+    else r = mid - 1
+  }
+  return l
+  
+  function valid(mid) {
+    let res = 0, cur = 0
+    const n = sweetness.length
+    for(let i = 0; i < n; i++) {
+      const e = sweetness[i]
+      cur += e
+      if(cur >= mid) {
+        res++
+        cur = 0
+      }
+    }
+    return res >= K + 1
+  }
+};
