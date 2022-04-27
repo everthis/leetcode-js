cdab9788e76ec950c84b61105d576245c14e5863
Nothing <wwwaap@gmail.com>
2022-04-27 22:42:17 +0800

    Update 1648-sell-diminishing-valued-colored-balls.js

--- 1648-sell-diminishing-valued-colored-balls.js
+++ 1648-sell-diminishing-valued-colored-balls.js
@@ -3,6 +3,44 @@
  * @param {number} orders
  * @return {number}
  */
+const maxProfit = function(inventory, orders) {
+  inventory = inventory.map(e => BigInt(e))
+  orders = BigInt(orders)
+  let l = 0n, r = BigInt(1e9 + 7)
+  while(l < r) {
+    const mid = l + (r - l) / 2n
+    if(valid(mid)) l = mid + 1n
+    else r = mid
+  }
+  
+  // console.log(l)
+  const mod = BigInt(1e9 + 7)
+  let t = l, res = 0n, cnt = 0n
+  for(const e of inventory) {
+    if(e <= t) continue
+    cnt += e - t
+    res = (res + (t + 1n + e) * (e - t) / 2n) % mod
+  }
+  
+  res = (res + (orders - cnt) * t) % mod
+  
+  return res
+  function valid(mid) {
+    let res = 0n
+    for(const e of inventory) {
+      if(e > mid) res += e - mid
+    }
+    return res > orders
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} inventory
+ * @param {number} orders
+ * @return {number}
+ */
 function maxProfit(inventory, orders) {
   inventory.sort((a, b) => a - b)
   inventory = inventory.map(e => BigInt(e))
