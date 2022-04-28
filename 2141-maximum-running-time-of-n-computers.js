55bde14e2fa2bb62832e809c731fbe572ce02f7c
Nothing <wwwaap@gmail.com>
2022-04-28 11:52:27 +0800

    Update 2141-maximum-running-time-of-n-computers.js

--- 2141-maximum-running-time-of-n-computers.js
+++ 2141-maximum-running-time-of-n-computers.js
@@ -3,6 +3,38 @@
  * @param {number[]} batteries
  * @return {number}
  */
+const maxRunTime = function(n, batteries) {
+  n = BigInt(n)
+  batteries = batteries.map(e => BigInt(e))
+  const sum = batteries.reduce((ac, e) => ac + e, 0n)
+  let l = 0n, r = sum / n
+  while(l < r) {
+    const mid = r - (r - l) / 2n
+    if(valid(mid)) l = mid
+    else r = mid - 1n
+  }
+  
+  return l
+  
+  function valid(mid) {
+    let curSum = 0n, target = mid * n
+    for(const e of batteries) {
+      curSum += e > mid ? mid : e
+      if(curSum >= target) return true
+    }
+    return false
+  }
+};
+
+
+// another
+
+
+/**
+ * @param {number} n
+ * @param {number[]} batteries
+ * @return {number}
+ */
 var maxRunTime = function (n, batteries) {
   batteries.sort((a, b) => a - b)
   const sum = batteries.reduce((ac, e) => ac + BigInt(e), 0n)
