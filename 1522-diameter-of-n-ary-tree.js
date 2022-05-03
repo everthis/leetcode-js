64529983792b5e4325bbeab34b12547639bfce9a
Nothing <wwwaap@gmail.com>
2022-05-03 20:06:47 +0800

    Update 1522-diameter-of-n-ary-tree.js

--- 1522-diameter-of-n-ary-tree.js
+++ 1522-diameter-of-n-ary-tree.js
@@ -14,22 +14,20 @@ var diameter = function(root) {
   let res = 0
   dfs(root)
   return res
+
   function dfs(node) {
     if(node == null) return 0
-    const arr = []
+    let max1 = 0, max2 = 0
     for(const child of node.children) {
       const tmp = dfs(child)
-      arr.push(tmp)
-    }
-    let max1 = 0, max2 = 0
-    for(const e of arr) {
-      if(e > max1) {
+      if(tmp > max1) {
         max2 = max1
-        max1 = e
-      } else if(e > max2) {
-        max2 = e
+        max1 = tmp
+      } else if(tmp > max2) {
+        max2 = tmp
       }
     }
+
     res = Math.max(res, max1 + max2)
     return max1 + 1
   }
