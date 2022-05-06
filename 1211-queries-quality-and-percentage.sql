4270cf9fd8f9bff63e6d923cb20ac5eced6c4205
Nothing <wwwaap@gmail.com>
2022-05-06 21:25:02 +0800

    Create 1211-queries-quality-and-percentage.sql

---
+++ 1211-queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+	query_name,
+	ROUND(AVG(rating / position), 2) AS quality,
+	ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage 
+FROM 
+	Queries
+GROUP BY 
+	query_name;
