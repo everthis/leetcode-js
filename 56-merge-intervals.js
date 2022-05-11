1f09f11541b83bcc04fbeca15927c8925df19411
Nothing <wwwaap@gmail.com>
2022-05-11 18:35:10 +0800

    Update 56-merge-intervals.js

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -3,6 +3,28 @@
  * @return {number[][]}
  */
 const merge = function(intervals) {
+  intervals.sort((a, b) => a[0] - b[0] || a[1] - b[1])
+  const res = [intervals[0]]
+  for(let i = 1, n = intervals.length; i < n; i++) {
+    const [s, e] = intervals[i]
+    const pre = res[res.length - 1]
+    if(s <= pre[1]) {
+      pre[1] = Math.max(pre[1], e)
+    } else {
+      res.push(intervals[i])
+    }
+  }
+  return res
+};
+
+// another
+
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+const merge = function(intervals) {
   if(intervals == null || intervals.length === 0) return []
   intervals.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0])
   const res = [intervals[0]]
