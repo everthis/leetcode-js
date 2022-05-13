45a5e1d9e20d7013d2fad7b88fc28b1c2175fde2
Nothing <wwwaap@gmail.com>
2022-05-13 19:44:27 +0800

    Update 253-meeting-rooms-ii.js

--- 253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii.js
@@ -89,3 +89,25 @@ const minMeetingRooms = function(intervals) {
   
   return res
 }
+
+// another
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const minMeetingRooms = function(intervals) {
+  const hash = {}
+  for(const [s, e] of intervals) {
+    hash[s] = (hash[s] || 0) + 1
+    hash[e] = (hash[e] || 0) - 1
+  }
+  let res = 0, cur = 0
+  const keys = Object.keys(hash).map(e => +e)
+  keys.sort((a, b) => a - b)
+  for(const k of keys) {
+    cur += hash[k]
+    res = Math.max(res, cur)
+  }
+  return res
+};
