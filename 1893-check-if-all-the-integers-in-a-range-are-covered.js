c5bf290c70787bcf734ebc7bc0b1d9a12cb1c3cc
Nothing <wwwaap@gmail.com>
2022-05-14 21:14:14 +0800

    Update 1893-check-if-all-the-integers-in-a-range-are-covered.js

--- 1893-check-if-all-the-integers-in-a-range-are-covered.js
+++ 1893-check-if-all-the-integers-in-a-range-are-covered.js
@@ -38,3 +38,28 @@ const isCovered = function(ranges, left, right) {
 	}
 	return true;
 };
+
+// another
+
+/**
+ * @param {number[][]} ranges
+ * @param {number} left
+ * @param {number} right
+ * @return {boolean}
+ */
+const isCovered = function(ranges, left, right) {
+  const arr = Array(52).fill(0)
+  for(let [s, e] of ranges) {
+    arr[s]++
+    arr[e + 1]--
+  }
+
+  let overlaps = 0
+  for(let i = 1; i <= right; i++) {
+    overlaps += arr[i];
+    if (i >= left && overlaps == 0) return false;
+  }
+  
+  return true
+};
+
