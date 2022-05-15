196758fa2ddf46621d364616f0d96e9e1589c08a
Nothing <wwwaap@gmail.com>
2022-05-15 21:59:05 +0800

    Update 1094-car-pooling.js

--- 1094-car-pooling.js
+++ 1094-car-pooling.js
@@ -4,6 +4,29 @@
  * @return {boolean}
  */
 const carPooling = function(trips, capacity) {
+  const arr = Array(1001).fill(0)
+  for(const [num, s, e] of trips) {
+    arr[s] += num
+    arr[e] -= num
+  }
+  for(let i = 1; i < 1001; i++) {
+    arr[i] += arr[i - 1]
+  }
+  
+  for(let e of arr) {
+    if(e > capacity) return false
+  }
+  return true
+};
+
+// another
+
+/**
+ * @param {number[][]} trips
+ * @param {number} capacity
+ * @return {boolean}
+ */
+const carPooling = function(trips, capacity) {
   let stops = new Array(1001).fill(0)
   for (let t of trips) {
     stops[t[1]] += t[0]
