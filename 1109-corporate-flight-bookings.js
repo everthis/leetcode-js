bca3f783e078b21520607797729577c440996600
Nothing <wwwaap@gmail.com>
2022-05-16 10:21:53 +0800

    Update 1109-corporate-flight-bookings.js

--- 1109-corporate-flight-bookings.js
+++ 1109-corporate-flight-bookings.js
@@ -4,6 +4,27 @@
  * @return {number[]}
  */
 const corpFlightBookings = function(bookings, n) {
+  const arr = Array(n + 2).fill(0)
+  for(const [s, e, num] of bookings) {
+    arr[s] += num
+    arr[e + 1] -= num
+  }
+  for(let i = 1; i < n + 2; i++) {
+    arr[i] += arr[i - 1]
+  }
+  arr.pop()
+  arr.shift()
+  return arr
+};
+
+// another
+
+/**
+ * @param {number[][]} bookings
+ * @param {number} n
+ * @return {number[]}
+ */
+const corpFlightBookings = function(bookings, n) {
   let res = new Array(n).fill(0)
   for (let v of bookings) {
     res[v[0] - 1] += v[2]
