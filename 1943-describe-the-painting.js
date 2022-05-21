8d89d6d87173f1db17573d3b080197aaeeccfb11
Nothing <wwwaap@gmail.com>
2022-05-21 10:51:08 +0800

    Update 1943-describe-the-painting.js

--- 1943-describe-the-painting.js
+++ 1943-describe-the-painting.js
@@ -22,3 +22,34 @@ const splitPainting = function(segments) {
   } 
   return res
 };
+
+// another
+
+/**
+ * @param {number[][]} segments
+ * @return {number[][]}
+ */
+const splitPainting = function(segments) {
+  const sum = {}
+  for(const [s, e, v] of segments) {
+    if(sum[s] == null) sum[s] = 0
+    if(sum[e] == null) sum[e] = 0
+    sum[s] += v
+    sum[e] -= v
+  }
+  const keys = Object.keys(sum).map(e => +e)
+  keys.sort((a, b) => a - b)
+  const res = []
+  let pre = 0, s = 0, n = keys.length
+  for(let i = 0; i < n; i++) {
+    const k = keys[i]
+    
+    if(s) {
+      res.push([pre, k, s])
+    }
+    s += sum[k]
+    pre = k
+  }
+  
+  return res
+};
