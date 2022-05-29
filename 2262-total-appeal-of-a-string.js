47cdc19372b5970635ad3b711aa4eac07418ccb8
Nothing <wwwaap@gmail.com>
2022-05-29 20:38:07 +0800

    Update 2262-total-appeal-of-a-string.js

--- 2262-total-appeal-of-a-string.js
+++ 2262-total-appeal-of-a-string.js
@@ -2,6 +2,28 @@
  * @param {string} s
  * @return {number}
  */
+var appealSum = function (s) {
+  const pos = Array(26).fill(-1)
+  let ans = 0
+  const n = s.length
+  const a = 'a'.charCodeAt(0)
+  for (let i = 0; i < n; i++) {
+    let tmp = n - i
+    if (pos[s.charCodeAt(i) - a] !== -1)
+      tmp += (i - pos[s.charCodeAt(i) - a] - 1) * (n - i)
+    else tmp += i * (n - i)
+    ans += tmp
+    pos[s.charCodeAt(i) - a] = i
+  }
+  return ans
+}
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var appealSum = function(s) {
   const cnt = Array(26).fill(-1);
   let ans = 0;
