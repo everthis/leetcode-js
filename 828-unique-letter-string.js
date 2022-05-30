2808f120bf47dad7eadcbac083b1256af96cf710
Nothing <wwwaap@gmail.com>
2022-05-30 20:58:34 +0800

    Update 828-unique-letter-string.js

--- 828-unique-letter-string.js
+++ 828-unique-letter-string.js
@@ -1,4 +1,28 @@
 /**
+ * @param {string} s
+ * @return {number}
+ */
+const uniqueLetterString = function(s) {
+  const n = s.length
+  const arr = Array.from({ length: 26 }, () => Array(2).fill(-1))
+  const A = 'A'.charCodeAt(0)
+  let res = 0
+  for(let i = 0; i < n; i++) {
+    const idx = s.charCodeAt(i) - A
+    res += (i - arr[idx][1]) * (arr[idx][1] - arr[idx][0])
+    arr[idx] = [arr[idx][1], i]
+  }
+  
+  for(let i = 0; i < 26; i++) {
+    res += (n - arr[i][1]) * (arr[i][1] - arr[i][0])
+  }
+  
+  return res
+};
+
+// another
+
+/**
  * @param {string} S
  * @return {number}
  */
