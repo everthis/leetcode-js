fce34a391d773b59cea3f1c8d5ad9bc7401163af
Nothing <wwwaap@gmail.com>
2022-05-31 22:09:33 +0800

    Update 907-sum-of-subarray-minimums.js

--- 907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums.js
@@ -1,3 +1,35 @@
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ const sumSubarrayMins = function (arr) {
+  const n = arr.length
+  const mod = 1e9 + 7, stk = []
+  const left = Array(n), right = Array(n)
+  for(let i = 0; i< n; i++) {
+    left[i] = i + 1
+    right[i] = n - i
+  }
+  let res = 0
+  for(let i = 0; i < n; i++) {
+    while(stk.length && arr[stk[stk.length - 1]] > arr[i]) {
+      const idx = stk.pop()
+      right[idx] = i - idx
+    }
+    if (stk.length) left[i] = i - stk[stk.length - 1]
+    stk.push(i)
+    
+  }
+  for(let i = 0; i < n; i++) {
+    res = (res + arr[i] * left[i] * right[i]) % mod
+  }
+  
+  return res
+}
+ 
+// another
+
 /**
  * @param {number[]} arr
  * @return {number}
