26c9daa1a599915276f90151ac9ada8de2cee967
Nothing <wwwaap@gmail.com>
2022-06-06 21:00:00 +0800

    Update 2276-count-integers-in-intervals.js

--- 2276-count-integers-in-intervals.js
+++ 2276-count-integers-in-intervals.js
@@ -1,3 +1,45 @@
+var CountIntervals = function() {
+  this.intervals = []
+  this.ans = 0
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {void}
+ */
+CountIntervals.prototype.add = function(left, right) {
+  let l = 0, r = this.intervals.length
+  while (l < r) {
+    const m = Math.floor((l + r) / 2)
+    if (this.intervals[m][1] >= left) {
+      r = m
+    } else {
+      l = m + 1
+    }
+  }
+  
+  let index = l
+  while (index < this.intervals.length && this.intervals[index][0] <= right) {
+    left = Math.min(left, this.intervals[index][0])
+    right = Math.max(right, this.intervals[index][1])
+    this.ans -= this.intervals[index][1] - this.intervals[index][0] + 1
+    index += 1
+  }
+  this.ans += right - left + 1
+  this.intervals.splice(l, index - l, [left, right])
+};
+
+
+/**
+ * @return {number}
+ */
+CountIntervals.prototype.count = function() {
+  return this.ans
+};
+
+// another
+
 function binarySearch(l, r, fn) {
   while (l <= r) {
     const m = Math.floor((l + r) / 2)
