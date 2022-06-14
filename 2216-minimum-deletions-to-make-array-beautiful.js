bf9013997876f726c02b582efeec6d390763efdb
Nothing <wwwaap@gmail.com>
2022-06-14 20:28:19 +0800

    Update 2216-minimum-deletions-to-make-array-beautiful.js

--- 2216-minimum-deletions-to-make-array-beautiful.js
+++ 2216-minimum-deletions-to-make-array-beautiful.js
@@ -3,6 +3,25 @@
  * @return {number}
  */
 const minDeletion = function(nums) {
+  let res = 0, n = nums.length
+
+  for(let i = 0; i < n; i += 2) {
+    while(i < n - 1 && nums[i] === nums[i + 1]) {
+      i++
+      res++
+    }
+  }
+  if((n - res) % 2 === 1) res++
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minDeletion = function(nums) {
   let res = 0, i = 0
   for(i = 0, n = nums.length; i < n - 1;) {
     if(nums[i] === nums[i + 1]) {
