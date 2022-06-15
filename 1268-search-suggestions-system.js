2c1121ca70907ddc7a69fee9151f257cecceb292
Nothing <wwwaap@gmail.com>
2022-06-15 22:56:53 +0800

    Update 1268-search-suggestions-system.js

--- 1268-search-suggestions-system.js
+++ 1268-search-suggestions-system.js
@@ -4,6 +4,26 @@
  * @return {string[][]}
  */
 const suggestedProducts = function(products, searchWord) {
+  products.sort()
+  let res = [], left = 0, right = products.length - 1
+  for (let i = 0; i < searchWord.length; i++) {
+    let c = searchWord.charAt(i), tmp = []
+    while (products[left]?.charAt(i) < c) left++
+    while (products[right]?.charAt(i) > c) right--
+    for (let j = 0; j < 3 && left + j <= right; j++) tmp.push(products[left+j])
+    res.push(tmp)
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {string[]} products
+ * @param {string} searchWord
+ * @return {string[][]}
+ */
+const suggestedProducts = function(products, searchWord) {
   const res = []
   for(let i = 0, n = searchWord.length; i < n; i++) {
     const tmp = [], pre = searchWord.slice(0, i + 1)
