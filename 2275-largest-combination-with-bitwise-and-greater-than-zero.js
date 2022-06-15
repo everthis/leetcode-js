df308a8e3037c03c2bea1b3e081128db71047de9
Nothing <wwwaap@gmail.com>
2022-06-15 17:34:51 +0800

    Update 2275-largest-combination-with-bitwise-and-greater-than-zero.js

--- 2275-largest-combination-with-bitwise-and-greater-than-zero.js
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero.js
@@ -13,3 +13,29 @@ const largestCombination = function(candidates) {
   }
   return res
 };
+
+// another
+
+/**
+ * @param {number[]} candidates
+ * @return {number}
+ */
+const largestCombination = function(candidates) {
+  const arr = Array(24).fill(0), len = 24
+  for(const e of candidates) {
+    const str = toBin(e)
+    for(let n = str.length, i = n - 1; i >= 0; i--) {
+      const cur = str[i]
+      if(cur === '1') {
+        arr[len - 1 - (n - 1 - i)]++
+      }
+    }
+  }
+  
+  return Math.max(...arr)
+  
+  function toBin(num) {
+    return (num >>> 0).toString(2)
+  }
+};
+
