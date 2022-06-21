c6c5b8f88abd8d86811e43d923c3dfd3f6ef6fa4
Nothing <wwwaap@gmail.com>
2022-06-21 22:23:46 +0800

    Update 926-flip-string-to-monotone-increasing.js

--- 926-flip-string-to-monotone-increasing.js
+++ 926-flip-string-to-monotone-increasing.js
@@ -4,6 +4,28 @@
  */
 const minFlipsMonoIncr = function(s) {
   const n = s.length
+  let res = 0, oneCnt = 0
+  for(const e of s) {
+    if(e === '1') oneCnt++
+    else {
+      const stayZero = oneCnt
+      const flipToOne = res + 1
+      res = Math.min(stayZero, flipToOne)
+    }
+  }
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minFlipsMonoIncr = function(s) {
+  const n = s.length
   const arr = Array(n).fill(0)
   let oneCnt = 0
   for(let i = 0; i < n; i++) {
