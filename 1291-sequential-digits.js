5279809e8e9d317d08151220041b74ba4e6555b9
Nothing <wwwaap@gmail.com>
2022-06-28 21:48:26 +0800

    Update 1291-sequential-digits.js

--- 1291-sequential-digits.js
+++ 1291-sequential-digits.js
@@ -4,6 +4,39 @@
  * @return {number[]}
  */
 const sequentialDigits = function(low, high) {
+  const res = []
+  
+  let q = []
+  for(let i = 1; i <= 9; i++) q.push(i)
+  
+  while(q.length) {
+    const tmp = []
+    const size = q.length
+    for(let i = 0; i < size; i++) {
+      const cur = q[i]
+      if(cur >= low && cur <= high) {
+        res.push(cur)
+      }
+      if(cur > high) break
+      const last = cur % 10
+      if(last === 9) continue
+      tmp.push(cur * 10 + last + 1)
+    }
+    
+    q = tmp
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+const sequentialDigits = function(low, high) {
    const set = new Set()
    let start = 0, end = 0
    for(let i = 10; i >= 0; i--) {
