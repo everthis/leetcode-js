3c33190c669e60ffce546227d7e169503fd76fc3
Nothing <wwwaap@gmail.com>
2022-06-30 22:06:23 +0800

    Update 1219-path-with-maximum-gold.js

--- 1219-path-with-maximum-gold.js
+++ 1219-path-with-maximum-gold.js
@@ -38,3 +38,46 @@ const getMaximumGold = function(grid) {
   }
   
 };
+
+// another
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var getMaximumGold = function (grid) {
+  const m = grid.length
+  const n = grid[0].length
+  let max = 0
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] != 0) {
+        const sum = backtrack(grid, i, j, m, n)
+        max = Math.max(sum, max)
+      }
+    }
+  }
+
+  return max
+}
+
+function backtrack(grid, row, col, m, n) {
+  if (outOfBound(row, col, m, n) || grid[row][col] === 0) return 0
+
+  let sum = grid[row][col]
+  grid[row][col] = 0 // mark as being visited already
+
+  const top = backtrack(grid, row - 1, col, m, n)
+  const right = backtrack(grid, row, col + 1, m, n)
+  const bot = backtrack(grid, row + 1, col, m, n)
+  const left = backtrack(grid, row, col - 1, m, n)
+
+  grid[row][col] = sum // backtrack to the original form
+
+  return sum + Math.max(top, right, bot, left)
+}
+
+function outOfBound(row, col, m, n) {
+  return row < 0 || col < 0 || row >= m || col >= n
+}
