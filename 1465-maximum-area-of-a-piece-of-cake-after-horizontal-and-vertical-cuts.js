d4f50c44513396c6a809c047ceae460a056c1171
Nothing <wwwaap@gmail.com>
2022-07-02 11:46:36 +0800

    Update 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
@@ -18,3 +18,27 @@ function getMax(limit, cuts) {
   }
   return max
 }
+
+// another
+
+/**
+ * @param {number} h
+ * @param {number} w
+ * @param {number[]} horizontalCuts
+ * @param {number[]} verticalCuts
+ * @return {number}
+ */
+const maxArea = function(h, w, horizontalCuts, verticalCuts) {
+  return (BigInt(maxGap(h, horizontalCuts)) * BigInt(maxGap(w, verticalCuts))) % BigInt(1e9 + 7)
+  function maxGap(limit, arr) {
+    let res = 0
+    arr.sort((a, b) => a - b)
+    for(let i = 0, n = arr.length; i < n; i++) {
+      let tmp = i === 0 ? arr[0] : arr[i] - arr[i - 1]
+      res = Math.max(res, tmp)
+    }
+    res = Math.max(res, limit - arr[arr.length - 1])
+    
+    return res
+  }
+};
