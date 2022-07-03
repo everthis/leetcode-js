c663f630a4cf324c565d6de6af53b566e8ccc907
Nothing <wwwaap@gmail.com>
2022-07-03 20:32:30 +0800

    Update 2327-number-of-people-aware-of-a-secret.js

--- 2327-number-of-people-aware-of-a-secret.js
+++ 2327-number-of-people-aware-of-a-secret.js
@@ -21,3 +21,29 @@ const peopleAwareOfSecret = function(n, delay, forget) {
     }
     return res;
 };
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} delay
+ * @param {number} forget
+ * @return {number}
+ */
+const peopleAwareOfSecret = function(n, delay, forget) {
+  const dp = Array(n + 1).fill(0)
+  dp[1] = 1
+  const mod = 1e9 + 7
+  const { min, max } = Math
+  let share = 0
+  for(let i = 2; i <= n; i++) {
+    share = (share + dp[max(i - delay, 0)] - dp[max(i - forget, 0)] + mod) % mod
+    dp[i] = share
+  }
+  let res = 0
+  for(let i = n - forget + 1; i <= n; i++) {
+    res = (res + dp[i]) % mod
+  }
+ 
+  return res
+};
