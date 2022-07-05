2aa5d711e392f656d6ff1b285096953e969b69d5
Nothing <wwwaap@gmail.com>
2022-07-05 11:12:14 +0800

    Update 1631-path-with-minimum-effort.js

--- 1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort.js
@@ -2,6 +2,103 @@
  * @param {number[][]} heights
  * @return {number}
  */
+const minimumEffortPath = function(heights) {
+  const m = heights.length, n = heights[0].length
+  const pq = new PriorityQueue()
+  const dist = Array.from({ length: m }, () => Array(n).fill(Infinity))
+  pq.push([0, 0, 0])
+  dist[0][0] = 0
+  const dirs = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+  while(!pq.isEmpty()) {
+    const [v, i, j] = pq.pop()
+    if(i === m - 1 && j === n - 1) return v
+    for(const [dx, dy] of dirs) {
+      const nx = i + dx, ny = j + dy
+      if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue
+      const diff = Math.max(v, Math.abs(heights[nx][ny] - heights[i][j]))
+      if(dist[nx][ny] > diff) {
+         dist[nx][ny] = diff
+         pq.push([diff, nx, ny])
+      }
+    }
+  }
+  return -1
+};
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a[0] < b[0]) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
+
+// another
+
+/**
+ * @param {number[][]} heights
+ * @return {number}
+ */
 const minimumEffortPath = function (heights) {
   const d = [0, 1, 0, -1, 0]
   let lo = 0,
