f238078838f68504c34fa38bb9652eb2f59a1163
Nothing <wwwaap@gmail.com>
2022-07-08 10:17:08 +0800

    Update 1186-maximum-subarray-sum-with-one-deletion.js

--- 1186-maximum-subarray-sum-with-one-deletion.js
+++ 1186-maximum-subarray-sum-with-one-deletion.js
@@ -2,17 +2,17 @@
  * @param {number[]} arr
  * @return {number}
  */
-const maximumSum = function(arr) {
+const maximumSum = function (arr) {
   const n = arr.length
-  let d1 = arr[0],
-    d2 = arr[0],
-    best = arr[0]
-  for (let i = 1; i < n; ++i) {
-    d2 = Math.max(d2 + arr[i], Math.max(d1, arr[i]))
-    d1 = Math.max(d1 + arr[i], arr[i])
-    best = Math.max(d2, best)
+  let oneDel = 0, noDel = arr[0], res = arr[0]
+
+  for(let i = 1; i < n; i++) {
+    oneDel = Math.max(noDel, oneDel + arr[i])
+    noDel = Math.max(arr[i], noDel + arr[i])
+    res = Math.max(res, oneDel, noDel)
   }
-  return best
+
+  return res
 }
 
 // another
