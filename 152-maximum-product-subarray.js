4cecce4a1ad159d79c1ca46c2398d72c5037216d
Nothing <wwwaap@gmail.com>
2022-07-09 21:06:26 +0800

    Update 152-maximum-product-subarray.js

--- 152-maximum-product-subarray.js
+++ 152-maximum-product-subarray.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 const maxProduct = function(nums) {
+  let min = nums[0], max = nums[0], res = nums[0]
+  for(let i = 1, n = nums.length; i < n; i++) {
+    const e = nums[i]
+    if(e < 0) [min, max] = [max, min]
+    min = Math.min(e, min * e)
+    max = Math.max(e, max * e)
+    res = Math.max(res, max)
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxProduct = function(nums) {
     let A = nums
     let r = A[0];
 
