636e57b91fc4e3cfdf0ddbd06956b3717a1b0153
Nothing <wwwaap@gmail.com>
2022-07-10 20:42:30 +0800

    Create 2272-substring-with-largest-variance.js

---
+++ 2272-substring-with-largest-variance.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const largestVariance = function (s) {
+  const se = new Set(s),
+    n = s.length
+  let res = 0
+  for (const x of se) {
+    // max
+    for (const y of se) {
+      // min
+      if (x != y) {
+        let pre = Array(n + 1).fill(0),
+          preX,
+          preY,
+          diff = 0
+        for (let i = 0; i < n; i++) {
+          if (s[i] == x) {
+            preX = i + 1
+            diff++
+          }
+          if (s[i] == y) {
+            preY = i + 1
+            diff--
+          }
+          pre[i + 1] = Math.min(pre[i], diff)
+          if (preX == undefined || preY == undefined) continue
+          res = Math.max(res, diff - pre[Math.min(preX, preY) - 1])
+        }
+      }
+    }
+  }
+  return res
+}
