341a30411b670dabb5aea485b23334dea4731e17
Nothing <wwwaap@gmail.com>
2022-07-12 11:41:32 +0800

    Update 2320-count-number-of-ways-to-place-houses.js

--- 2320-count-number-of-ways-to-place-houses.js
+++ 2320-count-number-of-ways-to-place-houses.js
@@ -2,6 +2,24 @@
  * @param {number} n
  * @return {number}
  */
+const countHousePlacements = function(n) {
+  const mod = BigInt(1e9 + 7)
+  let house = 1n, space = 1n, total = 2n
+  for(let i = 2; i <= n; i++) {
+    house = space
+    space = total
+    total = (house + space) % mod
+  }
+  
+  return total * total % mod
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
 var countHousePlacements = function(n) {
   const mod = 1e9 + 7
   let f0 = 1;
