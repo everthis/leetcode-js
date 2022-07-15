1ba426ba917aa86362c65f380b78850865351d67
Nothing <wwwaap@gmail.com>
2022-07-15 12:57:11 +0800

    Update 1388-pizza-with-3n-slices.js

--- 1388-pizza-with-3n-slices.js
+++ 1388-pizza-with-3n-slices.js
@@ -36,3 +36,32 @@ function maxSum(arr, n) {
   }
   return dp[m][n]
 }
+
+
+// another
+
+/**
+ * @param {number[]} slices
+ * @return {number}
+ */
+const maxSizeSlices = function (slices) {
+  const n = slices.length, m = ~~(n / 3)
+  const arr1 = slices.slice(1), arr2 = slices.slice(0, n - 1)
+  return Math.max(helper(arr1, m), helper(arr2, m))
+  function helper(arr, k) {
+    const len = arr.length
+    const dp = Array.from({ length: len + 1 }, () => Array(k + 1).fill(0))
+    for(let i = 1; i <= len; i++) {
+      for(let j = 1; j <= k; j++) {
+        if(i === 1) dp[i][j] = arr[i - 1]
+        else {
+          dp[i][j] = Math.max(
+            dp[i - 1][j],
+            dp[i - 2][j - 1] + arr[i - 1]
+          )
+        }
+      }
+    }
+    return dp[len][k]
+  }
+}
