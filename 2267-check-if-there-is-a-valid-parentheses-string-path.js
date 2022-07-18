7d4dc20ca63bb691125dac20b74175e9efffbbc4
Nothing <wwwaap@gmail.com>
2022-07-18 20:47:35 +0800

    Update 2267-check-if-there-is-a-valid-parentheses-string-path.js

--- 2267-check-if-there-is-a-valid-parentheses-string-path.js
+++ 2267-check-if-there-is-a-valid-parentheses-string-path.js
@@ -2,6 +2,43 @@
  * @param {character[][]} grid
  * @return {boolean}
  */
+const hasValidPath = function (grid) {
+  const m = grid.length
+  const n = grid[0].length
+  if (grid[0][0] != '(' || grid[m - 1][n - 1] != ')') return false
+  const dp = kdArr(-1, [m, n, ~~((m + n) / 2) + 1])
+
+
+  function dfs(i, j, left) {
+    if (i >= m || j >= n) return false
+    if (grid[i][j] === '(') left++
+    else left--
+    if (left < 0 || left > Math.floor((m + n) / 2)) return false
+    if (dp[i][j][left] != -1) return dp[i][j][left]
+    if (i == m - 1 && j == n - 1 && left == 0) return (dp[i][j][left] = true)
+    return (dp[i][j][left] = dfs(i, j + 1, left) || dfs(i + 1, j, left))
+  }
+  return dfs(0, 0, 0)
+  
+  function kdArr(defaultVal, arr) {
+    if(arr.length === 1) return Array(arr[0]).fill(defaultVal)
+    
+    const res = []
+    for(let i = 0, len = arr[0]; i < len; i++) {
+      res.push(kdArr(defaultVal, arr.slice(1)))
+    }
+    
+    return res
+  }
+}
+
+
+// another
+
+/**
+ * @param {character[][]} grid
+ * @return {boolean}
+ */
 var hasValidPath = function(grid) {
     if (grid[0][0] == ")") return false
     let m = grid.length, n = grid[0].length
