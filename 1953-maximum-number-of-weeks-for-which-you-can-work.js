a1d42132f16118d4d557037916798a558b500881
Nothing <wwwaap@gmail.com>
2022-07-23 20:10:52 +0800

    Update 1953-maximum-number-of-weeks-for-which-you-can-work.js

--- 1953-maximum-number-of-weeks-for-which-you-can-work.js
+++ 1953-maximum-number-of-weeks-for-which-you-can-work.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 const numberOfWeeks = function(milestones) {
+  let max = -Infinity
+  let res = 0, sum = 0
+  for(const e of milestones) {
+    max = Math.max(e, max)
+    sum += e
+  }
+  
+  return Math.min(sum, (sum - max) * 2 + 1)
+};
+
+
+// another
+
+/**
+ * @param {number[]} milestones
+ * @return {number}
+ */
+const numberOfWeeks = function(milestones) {
     let sum = 0;
     for (let i = 0; i < milestones.length; i++) {
         sum += milestones[i];
