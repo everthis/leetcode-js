40d45ad2a0eb2904b847fac5076516914bd5f57c
Nothing <wwwaap@gmail.com>
2022-07-24 23:09:10 +0800

    Update 2354-number-of-excellent-pairs.js

--- 2354-number-of-excellent-pairs.js
+++ 2354-number-of-excellent-pairs.js
@@ -4,32 +4,37 @@
  * @return {number}
  */
 const countExcellentPairs = function(nums, k) {
-    const map = new Map();
-    const set = new Set();
-    const l = nums.length;
-    let res = 0;
-    for (let num of nums) {
-        let temp = num.toString(2).split("1").length - 1;
-        if (!map.has(temp)) {
-            map.set(temp, new Set([num]));
-        } else {
-            map.get(temp).add(num);
-        }
+  const arr = [], set = new Set(nums)
+  for(const e of set) {
+    arr.push(setBits(e))
+  }
+
+  arr.sort((a, b) => a - b)
+  let res = 0
+  for(let i = 0, n = arr.length; i < n; i++) {
+    const idx = bs(arr, k - arr[i])
+    res += n - idx
+  }
+  return res
+  
+  
+  function bs(arr, target) {
+    let l = 0, r = arr.length
+    
+    while(l < r) {
+      const mid = (l + r) >> 1
+      if(arr[mid] < target) l = mid + 1
+      else r = mid
     }
     
-    for (let num of nums) {
-        let temp = num.toString(2).split("1").length - 1;
-        if(!set.has(num)) {
-            let gap = Math.max(0, k - temp)
-            for (let key of map.keys()) {
-                if (key >= gap) {
-                    res += map.get(key).size;
-                }
-            }  
-            set.add(num);
-        }else {
-            continue;
-        }
+    return l
+  }
+  function setBits(num) {
+    let res = 0
+    while(num) {
+      res += num % 2
+      num = num >> 1
     }
-    return res; 
+    return res
+  }
 };
