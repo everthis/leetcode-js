297ce135a015caa239f6bd4e873730613d7d5b38
Nothing <wwwaap@gmail.com>
2022-07-27 09:57:44 +0800

    Update 621-task-scheduler.js

--- 621-task-scheduler.js
+++ 621-task-scheduler.js
@@ -3,31 +3,31 @@
  * @param {number} n
  * @return {number}
  */
-const leastInterval = function(tasks, n) {
-    const counter = new Array(26).fill(0);
-    let max = 0;
-    let maxCount = 0;
-    const A = 'A'.charCodeAt(0)
-    for(let ch of tasks) {
-        const task = ch.charCodeAt(0)
-        counter[task - A]++;
-        if(max == counter[task - A]) {
-            maxCount++;
-        }
-        else if(max < counter[task - A]) {
-            max = counter[task - A];
-            maxCount = 1;
-        }
+const leastInterval = function (tasks, n) {
+  const len = tasks.length
+  const cnt = Array(26).fill(0)
+
+  const A = 'A'.charCodeAt(0)
+  let maxFreq = 0,
+    maxFreqCnt = 0
+  for (const ch of tasks) {
+    const idx = ch.charCodeAt(0) - A
+    cnt[idx]++
+    if (maxFreq === cnt[idx]) {
+      maxFreqCnt++
+    } else if (maxFreq < cnt[idx]) {
+      maxFreqCnt = 1
+      maxFreq = cnt[idx]
     }
+  }
 
-    const partCount = max - 1;
-    const partLength = n - (maxCount - 1);
-    const emptySlots = partCount * partLength;
-    const availableTasks = tasks.length - max * maxCount;
-    const idles = Math.max(0, emptySlots - availableTasks);
+  const slot = maxFreq - 1
+  const numOfPerSlot = n - (maxFreqCnt - 1)
+  const available = len - maxFreq * maxFreqCnt
+  const idles = Math.max(0, slot * numOfPerSlot - available)
+  return len + idles
+}
 
-    return tasks.length + idles;
-};
 
 // another
 
