e4acf9819314dcb994cd593b2ad633557e7acb18
Nothing <wwwaap@gmail.com>
2022-07-28 21:07:28 +0800

    Create 2128-remove-all-ones-with-row-and-column-flips.js

---
+++ 2128-remove-all-ones-with-row-and-column-flips.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+const removeOnes = function(grid) {
+  
+  const m = grid.length
+  const n = grid[0].length
+  const first = grid[0]
+  for(let i = 1; i < m; i++) {
+    if(!equal(first, grid[i]) && !equal(first, flip(grid[i]))) return false
+  }
+  
+  return true
+  
+  function flip(arr) {
+    const res = []
+    for(const e of arr) {
+      res.push(e === 1 ? 0 : 1)
+    }
+    return res
+  }
+  
+  function equal(a, b) {
+    const n = a.length
+    for(let i = 0; i < n; i++) {
+      if(a[i] !== b[i]) return false
+    }
+    
+    return true
+  }
+};
