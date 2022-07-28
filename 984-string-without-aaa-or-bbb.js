3744bb57a28c1bbd0571bfc021b1052ff228fc43
Nothing <wwwaap@gmail.com>
2022-07-28 10:49:01 +0800

    Update 984-string-without-aaa-or-bbb.js

--- 984-string-without-aaa-or-bbb.js
+++ 984-string-without-aaa-or-bbb.js
@@ -3,6 +3,46 @@
  * @param {number} b
  * @return {string}
  */
+const strWithout3a3b = function (a, b) {
+  let res = ''
+
+  while(a > 0 || b > 0) {
+    if(endsWith(res, 'aa')) {
+      res += 'b'
+      b--
+    } else if(endsWith(res, 'bb')) {
+      res += 'a'
+      a--
+    } else if(a >= b) {
+      res += 'a'
+      a--
+    } else {
+      res += 'b'
+      b--
+    }
+  }
+
+  return res
+
+  function endsWith(str, sub) {
+    let i = str.length - 1, j = sub.length - 1
+    for(; i >=0 && j >= 0;i--,j--) {
+      if(str[i] !== sub[j]) return false
+    }
+    if(j >= 0) return false
+
+    return true
+  }
+}
+
+// another
+
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {string}
+ */
 const strWithout3a3b = function(a, b) {
   let m = a, n = b, ch1 = 'a', ch2 = 'b'
   if(b > a) {
