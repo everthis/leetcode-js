652a2e08b036f4a800f546335277c86f54e4490a
Nothing <wwwaap@gmail.com>
2022-07-30 20:40:43 +0800

    Update 945-minimum-increment-to-make-array-unique.js

--- 945-minimum-increment-to-make-array-unique.js
+++ 945-minimum-increment-to-make-array-unique.js
@@ -18,9 +18,25 @@ const minIncrementForUnique = function(nums) {
   }
   
   return res
-    
-    
+ 
   function last(arr) {
     return arr[arr.length - 1]
   }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minIncrementForUnique = function(nums) {
+  let res = 0, nxt = 0
+  nums.sort((a, b) => a - b)
+  for(const e of nums) {
+    res += Math.max(0, nxt - e)
+    nxt = Math.max(nxt, e) + 1
+  }
+  
+  return res
 };
