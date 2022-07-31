a6ac1c14c47a2ab4b811de010fab022bb698baa4
Nothing <wwwaap@gmail.com>
2022-07-31 19:52:49 +0800

    Create 2333-minimum-sum-of-squared-difference.js

---
+++ 2333-minimum-sum-of-squared-difference.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k1
+ * @param {number} k2
+ * @return {number}
+ */
+const minSumSquareDiff = function (nums1, nums2, k1, k2) {
+  const n = nums1.length
+  const diff = Array(n).fill(0)
+  for (let i = 0; i < n; ++i) {
+    diff[i] = Math.abs(nums1[i] - nums2[i])
+  }
+  const M = Math.max(...diff)
+  const bucket = Array(M + 1).fill(0)
+  for (let i = 0; i < n; i++) {
+    bucket[diff[i]]++
+  }
+  let k = k1 + k2
+  for (let i = M; i > 0; i--) {
+    if (bucket[i] > 0) {
+      const minus = Math.min(bucket[i], k)
+      bucket[i] -= minus
+      bucket[i - 1] += minus
+      k -= minus
+    }
+  }
+  let res = 0
+  for (let i = M; i > 0; --i) {
+    res += bucket[i] * i * i
+  }
+  return res
+}
