725d1b1fa30dcdf66c5db726d93b08c58eace8bf
Nothing <wwwaap@gmail.com>
2022-08-15 19:53:19 +0800

    Update 357-count-numbers-with-unique-digits.js

--- 357-count-numbers-with-unique-digits.js
+++ 357-count-numbers-with-unique-digits.js
@@ -16,3 +16,27 @@ const countNumbersWithUniqueDigits = function(n) {
 
   return res;
 };
+
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const countNumbersWithUniqueDigits = function(n) {
+  const limit = 10 ** n
+  let res = 1
+  let m = 1
+  if(n === 0) return 1
+  while(10**m <= limit) {
+    res += 9 * helper(9, m - 1)
+    m++
+  }
+  
+  return res
+  
+  function helper(m, n) {
+    return n === 0 ? 1 : helper(m, n - 1) * (m - n + 1)
+  }
+};
