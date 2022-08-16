890aeb9523a082f5857b3f2f9b7ddad3d918c7a0
Nothing <wwwaap@gmail.com>
2022-08-16 16:36:49 +0800

    Update 2376-count-special-integers.js

--- 2376-count-special-integers.js
+++ 2376-count-special-integers.js
@@ -25,3 +25,36 @@ var countSpecialNumbers = function(n) {
 function A(m, n) {
   return n === 0 ? 1 : A(m, n - 1) * (m - n + 1);
 }
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const countSpecialNumbers = function (n) {
+  const s = '' + n
+  const dp = Array.from({ length: 11 }, () =>
+    Array.from({ length: 2 }, () => Array(1024).fill(-1))
+  )
+
+  return helper(0, 1, 0, s)
+  function helper(idx, tight = 1, mask = 0, digits) {
+    if (idx == digits.length) return mask !== 0 ? 1 : 0
+
+    if (dp[idx][tight][mask] != -1) return dp[idx][tight][mask]
+
+    let k = tight ? +digits[idx] : 9
+    let ans = 0
+
+    for (let i = 0; i <= k; i++) {
+      if (mask & (1 << i)) continue
+      let newMask = mask == 0 && i == 0 ? mask : mask | (1 << i)
+
+      let nextTight = tight && i == digits[idx] ? 1 : 0
+      ans += helper(idx + 1, nextTight, newMask, digits)
+    }
+
+    return (dp[idx][tight][mask] = ans)
+  }
+}
