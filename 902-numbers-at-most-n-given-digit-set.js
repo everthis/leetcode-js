f66ef849fd0d1c7777890bda3d582c39c036ae27
Nothing <wwwaap@gmail.com>
2022-08-20 18:37:09 +0800

    Update 902-numbers-at-most-n-given-digit-set.js

--- 902-numbers-at-most-n-given-digit-set.js
+++ 902-numbers-at-most-n-given-digit-set.js
@@ -1,21 +1,26 @@
 /**
- * @param {string[]} D
- * @param {number} N
+ * @param {string[]} digits
+ * @param {number} n
  * @return {number}
  */
-const atMostNGivenDigitSet = function (D, N) {
-  const NS = '' + N
-  const digit = NS.length,
-    dsize = D.length
-  let rtn = 0
-  for (let i = 1; i < digit; ++i) rtn += Math.pow(dsize, i)
-  for (let i = 0; i < digit; ++i) {
-    let hasSameNum = false
-    for (let d of D) {
-      if (d < NS[i]) rtn += Math.pow(dsize, digit - i - 1)
-      else if (d == NS[i]) hasSameNum = true
-    }
-    if (!hasSameNum) return rtn
+const atMostNGivenDigitSet = function(digits, n) {
+  const str = '' + n, { pow } = Math
+  const len = str.length, dsize = digits.length
+  let res = 0
+  
+  for(let i = 1; i < len; i++) {
+    res += pow(dsize, i)
   }
-  return rtn + 1
-}
+  
+  for(let i = 0; i < len; i++) {
+    let sameNum = false
+    for(const d of digits) {
+      if(+d < +str[i]) {
+        res += pow(dsize, len - i - 1)
+      } else if(+d === +str[i]) sameNum = true
+    }
+    if(sameNum === false) return res
+  }
+  
+  return res + 1
+};
