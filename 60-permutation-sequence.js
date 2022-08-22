815f2444913f904088cedbb1939ced2e252f8b9d
Nothing <wwwaap@gmail.com>
2022-08-22 15:59:31 +0800

    Update 60-permutation-sequence.js

--- 60-permutation-sequence.js
+++ 60-permutation-sequence.js
@@ -6,6 +6,42 @@
 const getPermutation = function(n, k) {
   const factorial = Array(n + 1).fill(0)
   factorial[0] = 1
+  for(let i = 1; i <= n; i++) {
+    factorial[i] = factorial[i - 1] * i
+  }
+  let res = ''
+  const visited = Array(n + 1).fill(0)
+  dfs(0)
+  return res
+    
+  function dfs(idx) {
+    if(idx === n) return
+    
+    const cnt = factorial[n - idx - 1]
+    for(let i = 1; i <= n; i++) {
+      if(visited[i]) continue
+      if(cnt < k) {
+          k -= cnt
+          continue
+      }
+      res += i
+      visited[i] = 1
+      dfs(idx + 1)
+      return
+    }
+  }
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+const getPermutation = function(n, k) {
+  const factorial = Array(n + 1).fill(0)
+  factorial[0] = 1
   for(let i = 1, pre = 1; i <= n; i++) {
     factorial[i] = pre * i
     pre = factorial[i]
