6e6292d49bea701a96a5e4a3119810489f71af71
Nothing <wwwaap@gmail.com>
2022-08-28 19:36:25 +0800

    Update 1723-find-minimum-time-to-finish-all-jobs.js

--- 1723-find-minimum-time-to-finish-all-jobs.js
+++ 1723-find-minimum-time-to-finish-all-jobs.js
@@ -3,6 +3,40 @@
  * @param {number} k
  * @return {number}
  */
+const minimumTimeRequired = function(jobs, k) {
+  const workers = Array(k).fill(0)
+  let res = Infinity
+  const n = jobs.length
+
+  dfs(0)
+  
+  return res
+
+  function dfs(idx) {
+    if(idx === n) {
+      res = Math.min(res, Math.max(...workers))
+      return
+    }
+    const visited = new Set()
+    const e = jobs[idx]
+    for(let i = 0; i < k; i++) {
+      if(visited.has(workers[i])) continue
+      if(workers[i] + e >= res) continue
+      visited.add(workers[i])
+      workers[i] += e
+      dfs(idx + 1)
+      workers[i] -= e
+    }
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} jobs
+ * @param {number} k
+ * @return {number}
+ */
 const minimumTimeRequired = function (jobs, k) {
   if (jobs.length <= k) {
     return Math.max(...jobs)
