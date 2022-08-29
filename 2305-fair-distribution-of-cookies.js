b1884a8989af43a909d501f86614d67e81ad6ae4
Nothing <wwwaap@gmail.com>
2022-08-29 18:21:38 +0800

    Create 2305-fair-distribution-of-cookies.js

---
+++ 2305-fair-distribution-of-cookies.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} cookies
+ * @param {number} k
+ * @return {number}
+ */
+const distributeCookies = function(cookies, k) {
+  const n = cookies.length
+  let res = Infinity
+  const arr = Array(n).fill(0)
+
+  bt(0)
+  return res
+  
+  function bt(idx) {
+    if(idx === n) {
+      res = Math.min(res, Math.max(...arr))
+      return
+    }
+    const cur = cookies[idx]
+    // const visited = new Set()
+    for(let i = 0; i < k; i++) {
+      const e = arr[i]
+      // if(visited.has(i)) continue
+      if(cur + e >= res) continue
+      
+      arr[i] += cur
+      bt(idx + 1)
+      arr[i] -= cur
+      if(arr[i] === 0) break
+    }
+  }
+};
