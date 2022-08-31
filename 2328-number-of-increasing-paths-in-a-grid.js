c2b5e4a367f24f29b718f35cb3179279590626f6
Nothing <wwwaap@gmail.com>
2022-08-31 21:09:58 +0800

    Update 2328-number-of-increasing-paths-in-a-grid.js

--- 2328-number-of-increasing-paths-in-a-grid.js
+++ 2328-number-of-increasing-paths-in-a-grid.js
@@ -2,6 +2,41 @@
  * @param {number[][]} grid
  * @return {number}
  */
+const countPaths = function (grid) {
+  const mod = 1e9 + 7
+  const m = grid.length, n = grid[0].length
+  let res = 0
+  const dirs = [[1,0], [-1,0], [0, 1], [0, -1]]
+  const memo = Array.from({ length: m }, () => Array(n).fill(0))
+  for(let i = 0; i <m ; i++) {
+    for(let j = 0; j < n; j++) {
+      res = (res + dfs(i, j)) % mod
+    }
+  } 
+  return res
+  
+  function dfs(i, j) {
+    let res = 1
+    if(memo[i][j] !== 0) return memo[i][j]
+    for(const [dx, dy] of dirs) {
+      const nx = i + dx, ny = j + dy
+      if(nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] > grid[i][j]) {
+        res = (res + dfs(nx, ny)) % mod
+      }
+    }
+    
+    memo[i][j] = res
+    
+    return res
+  }
+}
+
+// another
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
 var countPaths = function (grid) {
   const MOD = 1e9 + 7
   let res = 0
