346632cec07dfb2235d65b8d918574255d477215
Nothing <wwwaap@gmail.com>
2022-09-01 15:52:01 +0800

    Update 351-android-unlock-patterns.js

--- 351-android-unlock-patterns.js
+++ 351-android-unlock-patterns.js
@@ -38,3 +38,43 @@ function DFS(vis, skip, cur, remain) {
   vis[cur] = false
   return rst
 }
+
+// another
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const numberOfPatterns = function (m, n) {
+  const skip = Array.from({ length: 10 }, () => Array(10).fill(0))
+  let res = 0
+  skip[1][3] = skip[3][1] = 2
+  skip[1][7] = skip[7][1] = 4
+  skip[9][7] = skip[7][9] = 8
+  skip[9][3] = skip[3][9] = 6
+  skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5
+  const visited = new Set()
+  for(let i = m ;i <= n; i++) {
+    res += dfs(1, i - 1) * 4 // 1, 3, 7, 9
+    res += dfs(2, i - 1) * 4 // 2, 4, 6, 8
+    res += dfs(5, i - 1) // 5
+  }
+
+  return res
+
+  function dfs(cur, remain) {
+    if(remain === 0) return 1
+    let res = 0
+    visited.add(cur)
+    for(let i = 1; i <= 9; i++) {
+      if(!visited.has(i) && (skip[cur][i] === 0 || visited.has(skip[cur][i]))) {
+        res += dfs(i, remain - 1)
+      }
+    }
+    visited.delete(cur)
+
+    return res
+  }
+}
+
