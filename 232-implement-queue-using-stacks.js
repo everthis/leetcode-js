bbc37b184d691cfcd2fe39649edbb132c78c5d96
Nothing <wwwaap@gmail.com>
2022-09-01 11:31:55 +0800

    Update 232-implement-queue-using-stacks.js

--- 232-implement-queue-using-stacks.js
+++ 232-implement-queue-using-stacks.js
@@ -1,46 +1,46 @@
-/**
- * Initialize your data structure here.
- */
-const MyQueue = function() {
-    this.queue = []
+
+var MyQueue = function() {
+  this.input = []
+  this.output = []
 };
 
-/**
- * Push element x to the back of queue. 
+/** 
  * @param {number} x
  * @return {void}
  */
 MyQueue.prototype.push = function(x) {
-    this.queue.push(x)
+  this.input.push(x)
 };
 
 /**
- * Removes the element from in front of queue and returns that element.
  * @return {number}
  */
 MyQueue.prototype.pop = function() {
-    return this.queue.shift()
+  if(this.output.length === 0) {
+      while(this.input.length) {
+          this.output.push(this.input.pop())
+      }
+  }
+  return this.output.pop()
 };
 
 /**
- * Get the front element.
  * @return {number}
  */
 MyQueue.prototype.peek = function() {
-    return this.queue[0]
+  return this.output[this.output.length - 1] || this.input[0]
 };
 
 /**
- * Returns whether the queue is empty.
  * @return {boolean}
  */
 MyQueue.prototype.empty = function() {
-    return this.queue.length === 0
+  return this.input.length === 0 && this.output.length === 0
 };
 
 /** 
  * Your MyQueue object will be instantiated and called as such:
- * var obj = Object.create(MyQueue).createNew()
+ * var obj = new MyQueue()
  * obj.push(x)
  * var param_2 = obj.pop()
  * var param_3 = obj.peek()
