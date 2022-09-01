5e2f01145b33e4b9fce18b46704b8e3ab913237f
Nothing <wwwaap@gmail.com>
2022-09-01 22:40:32 +0800

    Update 861-score-after-flipping-matrix.js

--- 861-score-after-flipping-matrix.js
+++ 861-score-after-flipping-matrix.js
@@ -4,6 +4,27 @@
  */
 const matrixScore = function(grid) {
   const m = grid.length, n = grid[0].length
+  let res = 0
+  res += m * (1 << (n - 1))
+  for(let j = 1; j < n; j++) {
+    let same = 0
+    for(let i = 0; i < m; i++) {
+      if(grid[i][0] === grid[i][j]) same++
+    }
+    res += Math.max(same, m - same) * (1 << (n - 1 - j))
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const matrixScore = function(grid) {
+  const m = grid.length, n = grid[0].length
   for(let i = 0; i < m; i++) {
     if(grid[i][0] === 0) flipRow(i)
   }
