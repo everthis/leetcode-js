921d2d5e583bfcd59e29b8698a5888e5018430fd
Nothing <wwwaap@gmail.com>
2022-09-04 20:43:11 +0800

    Update 484-find-permutation.js

--- 484-find-permutation.js
+++ 484-find-permutation.js
@@ -3,6 +3,28 @@
  * @return {number[]}
  */
 const findPermutation = function(s) {
+    const n = s.length
+    const res = Array(n + 1)
+    res[n]  = n + 1
+    for (let i = 0, len = n; i < len;) {
+        let j = i;
+        while (j < len && s.charAt(j) === 'D') {
+            j++;
+        }
+        for (let k = j - i; k >= 0; k--, j--) {
+            res[i++] = j + 1;
+        }
+    }
+    return res;
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+const findPermutation = function(s) {
   const n = s.length
   const arr = Array.from({ length: n + 1 }, (el, idx) => idx + 1)
   for(let h = 0; h < n; h++) {
