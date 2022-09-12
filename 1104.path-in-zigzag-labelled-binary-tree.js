17d4382dee6cc51ad9521d15a9569c493076cbe4
Nothing <wwwaap@gmail.com>
2022-09-12 21:14:16 +0800

    Create 1104.path-in-zigzag-labelled-binary-tree.js

---
+++ 1104.path-in-zigzag-labelled-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} label
+ * @return {number[]}
+ */
+const pathInZigZagTree = function(label) {
+  const res = [], { log2, floor, ceil } = Math
+  
+  res.push(label)
+  
+  // check last row
+  const lev = ceil(log2(label + 1))
+  const reverse = lev % 2 === 0 ? true : false
+  // console.log(reverse, lev)
+  if(reverse) {
+    const idx = 2 ** lev - 1 - label
+    label = 2 ** (lev - 1) + idx
+  }
+  // console.log(label)
+  
+  while(label > 1) {
+    const level = floor(log2(label))
+    const parent = floor(label / 2)
+    const parentLevelNum = 2 ** (level - 1)
+    const parentReverse = level % 2 === 0 ? true : false
+    const parentStart = 2 ** (level - 1)
+    const parentEnd = 2 ** level - 1
+    // console.log(parentStart, parentEnd, parent)
+    const idx = parent - parentStart
+    res.push(parentReverse ? parentEnd - idx : parentStart + idx)
+    
+    label = parent
+  }
+  
+
+  res.reverse()
+  return res
+};
