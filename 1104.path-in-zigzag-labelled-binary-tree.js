d1eaffa781b26dca9ca90e9bd837a6e3bf9df8ea
Nothing <wwwaap@gmail.com>
2022-09-12 21:56:56 +0800

    Update 1104.path-in-zigzag-labelled-binary-tree.js

--- 1104.path-in-zigzag-labelled-binary-tree.js
+++ 1104.path-in-zigzag-labelled-binary-tree.js
@@ -4,6 +4,30 @@
  */
 const pathInZigZagTree = function(label) {
   const res = [], { log2, floor, ceil } = Math
+  const level = floor(log2(label))
+  let compl = 2 ** (level + 1) - 1 + 2 ** level - label
+  
+  while(label) {
+    res.push(label)
+    label = floor(label / 2)
+    compl = floor(compl / 2)
+    ;[label, compl] = [compl, label]
+  }
+  
+  res.reverse()
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {number} label
+ * @return {number[]}
+ */
+const pathInZigZagTree = function(label) {
+  const res = [], { log2, floor, ceil } = Math
   
   res.push(label)
   
