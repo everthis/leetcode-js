f25c5974db17d12b86a03ec8a2ee648933e6277f
Nothing <wwwaap@gmail.com>
2022-09-13 17:13:43 +0800

    Update 862-shortest-subarray-with-sum-at-least-k.js

--- 862-shortest-subarray-with-sum-at-least-k.js
+++ 862-shortest-subarray-with-sum-at-least-k.js
@@ -1,4 +1,34 @@
 /**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const shortestSubarray = function(nums, k) {
+  const q = [], n = nums.length
+  let res = Infinity, sum = 0
+  const prefix = []
+  for(let i = 0; i < n; i++) {
+    sum += nums[i]
+    prefix.push(sum)
+    if(sum >= k) res = Math.min(res, i + 1)
+  }
+  // console.log(prefix)
+  for(let i = 0; i < n; i++) {
+
+    while(q.length && prefix[i] - prefix[q[0]] >= k) {
+        res = Math.min(res, i - q[0])
+        q.shift()
+    }
+    while(q.length && prefix[i] <= prefix[q[q.length - 1]]) q.pop()
+    q.push(i)
+  }
+  
+  return res === Infinity ? -1 : res
+};
+
+// another
+
+/**
  * @param {number[]} A
  * @param {number} K
  * @return {number}
