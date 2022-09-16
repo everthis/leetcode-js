cf0ffd74e41377c5c34587768e6dc8722589d487
Nothing <wwwaap@gmail.com>
2022-09-16 22:09:41 +0800

    Update 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
@@ -3,6 +3,26 @@
  * @param {number} k
  * @return {string}
  */
+const getHappyString = function(n, k) {
+  const hash = {a: 'bc', b: 'ac', c: 'ab'}
+  const q = ['a', 'b', 'c']
+  while(q[0].length !== n) {
+    const pre = q.shift()
+    for(const ch of hash[pre[pre.length - 1]]) {
+      q.push(pre + ch)
+    }
+  }
+  
+  return q.length >= k ? q[k - 1] : ''
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
 var getHappyString = function(n, k) {
   const res = []
   dfs()
