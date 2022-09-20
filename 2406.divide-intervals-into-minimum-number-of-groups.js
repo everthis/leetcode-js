c78a5180ee78bff1eb0e9781f7e3c0d6fd1ef160
Nothing <wwwaap@gmail.com>
2022-09-20 11:18:56 +0800

    Update 2406.divide-intervals-into-minimum-number-of-groups.js

--- 2406.divide-intervals-into-minimum-number-of-groups.js
+++ 2406.divide-intervals-into-minimum-number-of-groups.js
@@ -18,3 +18,31 @@ var minGroups = function(intervals) {
   }
   return res
 };
+
+// another
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const minGroups = function(intervals) {
+  const hash = {}
+  for(const [s, e] of intervals) {
+    if(hash[s] == null) hash[s] = 0
+    if(hash[e + 1] == null) hash[e + 1] = 0
+    hash[s]++
+    hash[e + 1]--
+  }
+  const keys = Object.keys(hash)
+  keys.sort((a, b) => a - b)
+  const n = keys.length
+
+  const arr = Array(n).fill(0)
+  arr[0] = hash[keys[0]]
+  let res = arr[0]
+  for(let i = 1; i < n; i++) {
+     arr[i] = hash[keys[i]] + arr[i - 1]
+     res = Math.max(res, arr[i])
+  }
+  return res
+};
