61194de8d42ae8f91c883db258c343a583451ec9
Nothing <wwwaap@gmail.com>
2022-09-28 16:37:00 +0800

    Update 1492-the-kth-factor-of-n.js

--- 1492-the-kth-factor-of-n.js
+++ 1492-the-kth-factor-of-n.js
@@ -5,21 +5,20 @@
  */
 const kthFactor = function (n, k) {
   let d = 1
-  for (; d * d <= n; ++d) {
-    if (n % d == 0) {
-      if (--k == 0) {
-        return d
-      }
-    }
-  }
-  for (d = d - 1; d >= 1; d--) {
-    if (d * d == n) continue
-    if (n % d == 0) {
+  for (let i = 1; i * i < n; i++) {
+    if (n % i === 0) {
       k--
-      if (k == 0) {
-        return n / d
-      }
+      if (k === 0) return i
     }
   }
+
+  for (let i = ~~Math.sqrt(n); i >= 1; i--) {
+    if (n % ~~(n / i) === 0) {
+      k--
+      // console.log(n, i, n/i, n % (n / i))
+      if (k === 0) return n / i
+    }
+  }
+
   return -1
 }
