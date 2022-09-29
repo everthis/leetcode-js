d908f88637e6478e6c7bd71fe894a19128d59b1d
Nothing <wwwaap@gmail.com>
2022-09-29 12:01:33 +0800

    Update 322-coin-change.js

--- 322-coin-change.js
+++ 322-coin-change.js
@@ -13,3 +13,29 @@ const coinChange = function(coins, amount) {
   }
   return dp[amount] === amount + 1 ? -1 : dp[amount]
 }
+
+
+// another
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+const coinChange = function (coins, amount) {
+  const n = coins.length
+  const dp = Array.from({ length: n }, () =>
+    Array(amount + 1).fill(Infinity)
+  )
+
+  for (let i = 0; i < n; i++) {
+    dp[i][0] = 0
+    for (let j = 1; j <= amount; j++) {
+      if(i > 0) dp[i][j] = dp[i - 1][j]
+      if (j >= coins[i]) {
+        dp[i][j] = Math.min(dp[i][j], dp[i][j - coins[i]] + 1)
+      }
+    }
+  }
+  return dp[n - 1][amount] === Infinity ? -1 : dp[n - 1][amount]
+}
