b142dbe6e4f0294bb502d38ddf8dea99bf1ec780
Nothing <wwwaap@gmail.com>
2022-09-29 10:35:11 +0800

    Update 802-find-eventual-safe-states.js

--- 802-find-eventual-safe-states.js
+++ 802-find-eventual-safe-states.js
@@ -2,6 +2,39 @@
  * @param {number[][]} graph
  * @return {number[]}
  */
+const eventualSafeNodes = function (graph) {
+  const ing = {},
+    n = graph.length
+  const outDegree = Array(n).fill(0)
+  let q = []
+  for (let i = 0; i < n; i++) {
+    outDegree[i] = graph[i].length
+    if (outDegree[i] === 0) {
+      q.push(i)
+    }
+    for (const e of graph[i]) {
+      if (ing[e] == null) ing[e] = []
+      ing[e].push(i)
+    }
+  }
+
+  for (const term of q) {
+    for (const come of ing[term] || []) {
+      outDegree[come]--
+      if (outDegree[come] === 0) q.push(come)
+    }
+  }
+  q.sort((a, b) => a - b)
+  return q
+}
+
+// another
+
+
+/**
+ * @param {number[][]} graph
+ * @return {number[]}
+ */
 const eventualSafeNodes = function(graph) {
   const n = graph.length, memo = {}, visited = new Set(), res = []
   for(let i = 0; i < n; i++) {
