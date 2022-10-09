f94291d056cc32caa2ded282643e8025cd5f9ff6
Nothing <wwwaap@gmail.com>
2022-10-09 20:19:08 +0800

    Update 85-maximal-rectangle.js

--- 85-maximal-rectangle.js
+++ 85-maximal-rectangle.js
@@ -3,6 +3,50 @@
  * @return {number}
  */
 const maximalRectangle = function(matrix) {
+  const m = matrix.length, n = matrix[0].length
+  const left = Array(n).fill(0)
+  const right = Array(n).fill(n - 1)
+  const height = Array(n).fill(0)
+  
+  let res = 0
+  
+  for(let i = 0; i < m; i++) {
+    let l = 0, r = n - 1
+    for(let j = 0; j < n; j++) {
+      if(matrix[i][j] === '1') left[j] = Math.max(left[j], l)
+      else {
+        left[j] = 0
+        l = j + 1
+      }
+    }
+    
+    for(let j = n - 1; j >= 0; j--) {
+      if(matrix[i][j] === '1') right[j] = Math.min(right[j], r)
+      else {
+        right[j] = n - 1
+        r = j - 1
+      }
+    }
+
+    for(let j = 0; j < n; j++) {
+      height[j] = matrix[i][j] === '1' ? height[j] + 1 : 0
+      res = Math.max(res, (right[j] - left[j] + 1) * height[j])
+    }
+    
+    // console.log(left, right, height)
+  }
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+const maximalRectangle = function(matrix) {
     if(matrix.length === 0) return 0;
     const m = matrix.length; // rows
     const n = matrix[0].length; // cols
