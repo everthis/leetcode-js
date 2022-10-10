244b485a7a0a454c6ca69d78af1cc446da4bc7e0
Nothing <wwwaap@gmail.com>
2022-10-10 19:47:29 +0800

    Update 1944-number-of-visible-people-in-a-queue.js

--- 1944-number-of-visible-people-in-a-queue.js
+++ 1944-number-of-visible-people-in-a-queue.js
@@ -3,6 +3,32 @@
  * @return {number[]}
  */
 const canSeePersonsCount = function(heights) {
+  const res = []
+  if(heights.length === 0) return res
+  
+  const n = heights.length
+  const stk = []
+  for(let i = n - 1; i >= 0; i--) {
+    let del = 0
+    while(stk.length && heights[i] > heights[stk[stk.length - 1]]) {
+      stk.pop()
+      del++
+    }
+    res.push(del + (stk.length ? 1 : 0))
+    stk.push(i)
+  }
+  
+  return res.reverse()
+};
+
+// another
+
+
+/**
+ * @param {number[]} heights
+ * @return {number[]}
+ */
+const canSeePersonsCount = function(heights) {
     const ans = new Uint32Array(heights.length);
     
     const stack = [];
