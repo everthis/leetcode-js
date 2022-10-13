61ea90211483832982938b2af82a5028b91afbc2
Nothing <wwwaap@gmail.com>
2022-10-13 13:31:59 +0800

    Update 2434-using-a-robot-to-print-the-lexicographically-smallest-string.js

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string.js
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string.js
@@ -1,3 +1,44 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const robotWithString = function (s) {
+  const stk = []
+  const freq = Array(26).fill(0)
+  const a = 'a'.charCodeAt(0)
+  for (const ch of s) {
+    freq[ch.charCodeAt(0) - a]++
+  }
+
+  let res = ''
+
+  for (const ch of s) {
+    stk.push(ch)
+    freq[ch.charCodeAt(0) - a]--
+    while (stk.length && stk[stk.length - 1] <= helper(freq)) {
+      const e = stk.pop()
+      res += e
+    }
+  }
+
+  while (stk.length) {
+    res += stk.pop()
+  }
+
+  return res
+
+  function helper(arr) {
+    const a = 'a'.charCodeAt(0)
+    for (let i = 0; i < 26; i++) {
+      if (arr[i] !== 0) return String.fromCharCode(a + i)
+    }
+
+    return ''
+  }
+}
+
+
+// another
 
 const ord = (c) => c.charCodeAt();
 const char = (ascii) => String.fromCharCode(ascii);
