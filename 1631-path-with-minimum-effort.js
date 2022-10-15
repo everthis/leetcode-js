8a82391add15d8e13fc34e00538c47c44a299efc
Nothing <wwwaap@gmail.com>
2022-10-15 19:46:05 +0800

    Update 1631-path-with-minimum-effort.js

--- 1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort.js
@@ -4,6 +4,49 @@
  */
 const minimumEffortPath = function(heights) {
   const m = heights.length, n = heights[0].length
+  const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+  let l = 0, r = 1e6 - 1
+  
+  while(l < r) {
+    const mid = ~~((l + r) / 2)
+    if(valid(mid)) {
+      r = mid
+    } else {
+      l = mid + 1
+    }
+  }
+  
+  return l
+  
+  function valid(limit) {
+    const visited = Array.from({ length: m }, () => Array(n).fill(false))
+    return dfs(0, 0, limit, visited)
+  }
+  
+  function dfs(i, j, limit, visited) {
+    if(i === m - 1 && j === n - 1) return true
+    visited[i][j] = true
+    for(const [dx, dy] of dirs) {
+      const nx = i + dx, ny = j + dy
+      if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue
+      if(visited[nx][ny]) continue
+      if(Math.abs(heights[i][j] - heights[nx][ny]) <= limit && dfs(nx, ny, limit, visited)) return true
+    }
+    return false
+  }
+
+};
+
+// another
+
+
+
+/**
+ * @param {number[][]} heights
+ * @return {number}
+ */
+const minimumEffortPath = function(heights) {
+  const m = heights.length, n = heights[0].length
   const pq = new PriorityQueue()
   const dist = Array.from({ length: m }, () => Array(n).fill(Infinity))
   pq.push([0, 0, 0])
