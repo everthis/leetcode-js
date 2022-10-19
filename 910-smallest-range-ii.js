678025d4a926d688ce10658a70bed01a28e5e817
Nothing <wwwaap@gmail.com>
2022-10-19 15:36:33 +0800

    Update 910-smallest-range-ii.js

--- 910-smallest-range-ii.js
+++ 910-smallest-range-ii.js
@@ -6,8 +6,10 @@
 const smallestRangeII = function (nums, k) {
   let n = nums.length
   nums.sort((a, b) => a - b)
+  // all elements plus k or minus k
   let res = nums[n - 1] - nums[0]
 
+  // left side elements plus k, right side elements minus k
   let left = nums[0] + k, right = nums[n - 1] - k
   for(let i = 0; i < n - 1; i++) {
     const tmax = Math.max(right, nums[i] + k)
