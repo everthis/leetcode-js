33c9fd6f0763fd79a1bbe3c1239b14ed6b8ed9c8
Nothing <wwwaap@gmail.com>
2022-10-21 20:29:41 +0800

    Update 1627-graph-connectivity-with-threshold.js

--- 1627-graph-connectivity-with-threshold.js
+++ 1627-graph-connectivity-with-threshold.js
@@ -4,6 +4,56 @@
  * @param {number[][]} queries
  * @return {boolean[]}
  */
+const areConnected = function(n, threshold, queries) {
+  const arr = []
+  const uf = new UF(n)
+  setup(n, threshold, uf)
+  for(let i = 0, len = queries.length; i < len;i++) {
+    arr.push(uf.check(queries[i][0], queries[i][1]))
+  }
+  return arr
+};
+
+function setup(n, t, uf) {
+  for (let i = t + 1; i <= n; i++) {
+    let m = 1;
+    while (i * m <= n) {
+      uf.union(i, i * m);
+      m += 1;
+    }
+  }
+}
+
+
+class UF {
+  constructor(n) {
+    this.root = Array(n).fill(null).map((_, i) => i)
+  }
+  find(x) {
+    if (this.root[x] !== x) {
+      this.root[x] = this.find(this.root[x])
+    }
+    return this.root[x]
+  }
+  union(x, y) {
+    const xr = this.find(x)
+    const yr = this.find(y)
+    this.root[yr] = xr
+  }
+  check(x, y) {
+    return this.find(x) === this.find(y)
+  }
+}
+
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} threshold
+ * @param {number[][]} queries
+ * @return {boolean[]}
+ */
 const areConnected = function (n, threshold, queries) {
   const arr = []
   const uf = new UnionFind(n)
