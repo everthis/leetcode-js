c8a372b7dff763da2cec9cb23d9915bdd001c708
Nothing <wwwaap@gmail.com>
2022-10-26 10:41:04 +0800

    Update 2183-count-array-pairs-divisible-by-k.js

--- 2183-count-array-pairs-divisible-by-k.js
+++ 2183-count-array-pairs-divisible-by-k.js
@@ -3,6 +3,37 @@
  * @param {number} k
  * @return {number}
  */
+const countPairs = function (nums, k) {
+  const map = new Map()
+
+  let res = 0
+  for(const e of nums) {
+    const tmp = gcd(e, k)
+
+    for(const [key, v] of map) {
+      if(tmp * key % k === 0) {
+        res += v
+      }
+    }
+    if(map.get(tmp) == null) map.set(tmp, 0)
+    map.set(tmp, map.get(tmp) + 1)
+  }
+
+  return res
+
+  function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b)
+  }
+}
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
 const coutPairs = function(nums, k) {
   let res = 0;
   let cnt = Array(1e5 + 1).fill(0);
