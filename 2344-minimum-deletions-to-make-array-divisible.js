2c3836f9f8b6a2c27b9be29a80ebaa79823aa391
Nothing <wwwaap@gmail.com>
2022-10-27 10:44:06 +0800

    Update 2344-minimum-deletions-to-make-array-divisible.js

--- 2344-minimum-deletions-to-make-array-divisible.js
+++ 2344-minimum-deletions-to-make-array-divisible.js
@@ -3,6 +3,33 @@
  * @param {number[]} numsDivide
  * @return {number}
  */
+const minOperations = function (nums, numsDivide) {
+  nums.sort((a, b) => a - b)
+  let gcdVal = numsDivide[0]
+
+  for(let i = 1, n = numsDivide.length; i < n; i++) {
+    gcdVal = gcd(gcdVal, numsDivide[i])
+  }
+
+  for(let i = 0, n = nums.length; i < n && nums[i] <= gcdVal; i++) {
+    if(gcdVal % nums[i] === 0) return i
+  }
+
+  return -1
+
+
+  function gcd(a,b) {
+    return b === 0 ? a : gcd(b, a % b)
+  }
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} numsDivide
+ * @return {number}
+ */
 var minOperations = function(nums, numsDivide) {
   let div = numsDivide[0], min = Infinity
   for(let i = 1, n = numsDivide.length; i < n; i++) {
