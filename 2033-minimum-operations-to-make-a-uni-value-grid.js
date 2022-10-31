1443a2dd100e82a93beb76559d33c293df6c2b98
Nothing <wwwaap@gmail.com>
2022-10-31 16:58:52 +0800

    Update 2033-minimum-operations-to-make-a-uni-value-grid.js

--- 2033-minimum-operations-to-make-a-uni-value-grid.js
+++ 2033-minimum-operations-to-make-a-uni-value-grid.js
@@ -3,6 +3,34 @@
  * @param {number} x
  * @return {number}
  */
+const minOperations = function (grid, x) {
+  const arr = [],
+    m = grid.length,
+    n = grid[0].length
+  const len = m * n
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      arr.push(grid[i][j])
+    }
+  }
+  arr.sort((a, b) => a - b)
+  const mid = arr[~~(len / 2)], { abs } = Math
+  let res = 0
+  for(const e of arr) {
+    if(abs(mid - e) % x !== 0) return -1
+    res += abs(mid - e) / x
+  }
+
+  return res
+}
+
+// another
+
+/**
+ * @param {number[][]} grid
+ * @param {number} x
+ * @return {number}
+ */
 const minOperations = function(grid, x) {
   const arr = []
   const m = grid.length, n = grid[0].length
