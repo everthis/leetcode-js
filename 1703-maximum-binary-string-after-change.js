67c15beec0a51679723b68a4a35b931303d813f8
Nothing <wwwaap@gmail.com>
2022-11-01 17:05:25 +0800

    Update 1703-maximum-binary-string-after-change.js

--- 1703-maximum-binary-string-after-change.js
+++ 1703-maximum-binary-string-after-change.js
@@ -4,6 +4,42 @@
  * @return {number}
  */
 const minMoves = function (nums, k) {
+  const pos = [],
+    pre = []
+  const n = nums.length,
+    { min, floor: fl } = Math
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === 1) pos.push(i)
+  }
+  let res = Infinity
+
+  pre.push(0)
+  for (let i = 0, len = pos.length; i < len; i++) {
+    pre.push(pre[i] + pos[i])
+  }
+
+  for (let i = fl(k / 2), limit = pos.length - fl((k - 1) / 2); i <limit; i++) {
+    const lcnt = fl(k / 2),
+      rcnt = fl((k - 1) / 2)
+    let current =
+      lcnt * pos[i] - (pre[i] - pre[i - lcnt]) - (lcnt * (lcnt + 1)) / 2
+    current +=
+      pre[i + 1 + rcnt] - pre[i + 1] - rcnt * pos[i] - (rcnt * (rcnt + 1)) / 2
+
+    res = min(res, current)
+  }
+  return res
+}
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const minMoves = function (nums, k) {
   if (k === 1) return 0
   let n = 0
   const pos = []
