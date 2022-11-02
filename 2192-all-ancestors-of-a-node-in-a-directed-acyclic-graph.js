16a0f71c37ac6a876dc4c62c29468fc494b4b2d2
Nothing <wwwaap@gmail.com>
2022-11-02 21:06:29 +0800

    Update 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.js

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.js
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.js
@@ -4,6 +4,38 @@
  * @return {number[][]}
  */
 const getAncestors = function(n, edges) {
+  const res = Array.from({ length: n }, () => [])
+  const graph = {}
+  
+  for(const [u, v] of edges) {
+    if(graph[u] == null) graph[u] = []
+    graph[u].push(v)
+  }
+  
+  for(let i = 0; i < n; i++) {
+    dfs(i, i)
+  }
+  
+  return res
+  
+  function dfs(p, cur) {
+    for(const nxt of (graph[cur] || [])) {
+      if(res[nxt].length === 0 || res[nxt][res[nxt].length - 1] !== p) {
+        res[nxt].push(p)
+        dfs(p, nxt)
+      }
+    }
+  }
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[][]}
+ */
+const getAncestors = function(n, edges) {
   const res = Array.from({ length: n }, () => new Set())
   const inDegree = Array(n).fill(0)
   const graph = {}
