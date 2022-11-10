ebb64cb2c033065b51d856a1f843b61a2477b442
Nothing <wwwaap@gmail.com>
2022-11-10 15:23:26 +0800

    Update 1224-maximum-equal-frequency.js

--- 1224-maximum-equal-frequency.js
+++ 1224-maximum-equal-frequency.js
@@ -3,6 +3,44 @@
  * @return {number}
  */
 const maxEqualFreq = function (nums) {
+  const freqCnt = {}, cnt = {}, { max } = Math
+
+  let res = 0, maxF = 0, i = 0
+  for(const e of nums) {
+    if(cnt[e] == null) cnt[e] = 0
+    cnt[e]++
+
+    const f = cnt[e]
+
+    if(freqCnt[f - 1] == null) freqCnt[f - 1] = 0
+    if(freqCnt[f] == null) freqCnt[f] = 0
+    
+    if(freqCnt[f - 1] > 0) freqCnt[f - 1]--
+    freqCnt[f]++
+
+    maxF = max(maxF, f)
+
+    if(
+      maxF === 1 ||
+      maxF * freqCnt[maxF] === i ||
+      (maxF - 1) * (freqCnt[maxF - 1] + 1) === i
+    ) {
+      res = i + 1
+    }
+
+    i++
+  }
+
+  return res
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxEqualFreq = function (nums) {
   const cnt = {},
     freq = {}
   let maxF = 0,
