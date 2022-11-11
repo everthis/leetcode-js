631ba8ed86aef8bf0de07e3600df28be51b23c2f
Nothing <wwwaap@gmail.com>
2022-11-11 22:48:30 +0800

    Update 2380-time-needed-to-rearrange-a-binary-string.js

--- 2380-time-needed-to-rearrange-a-binary-string.js
+++ 2380-time-needed-to-rearrange-a-binary-string.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {number}
  */
+const secondsToRemoveOccurrences = function(s) {
+  let zeros = 0
+  const n = s.length, { max } = Math
+  let res = 0
+  
+  for(let i = 0; i < n; i++) {
+    if(s[i] === '0') zeros++
+    if(s[i] === '1' && zeros > 0) {
+      res = max(res + 1, zeros)
+    }
+  }
+  return res 
+};
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var secondsToRemoveOccurrences = function(s) {
   const n = s.length
     let zeros = 0, seconds = 0;
