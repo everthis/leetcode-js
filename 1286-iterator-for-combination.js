2074d6bf6c5d950331ab89576e4e3159e3303cd4
Nothing <wwwaap@gmail.com>
2022-11-16 20:00:10 +0800

    Update 1286-iterator-for-combination.js

--- 1286-iterator-for-combination.js
+++ 1286-iterator-for-combination.js
@@ -92,3 +92,57 @@ CombinationIterator.prototype.hasNext = function() {
  * var param_2 = obj.hasNext()
  */
 
+// another
+
+/**
+ * @param {string} characters
+ * @param {number} combinationLength
+ */
+const CombinationIterator = function(characters, combinationLength) {
+  const res = [], len = combinationLength, str = characters, n = str.length
+  helper()
+
+  // console.log(res)
+  this.arr = res
+  this.idx = 0
+  
+  function helper() {
+     const limit = 1 << n
+     for(let i = limit - 1; i > 0; i--) {
+       let tmp = i, ts = '', idx = n - 1
+       while(tmp) {
+         if(tmp & 1) {
+           ts = str[idx] + ts
+         }
+         idx--
+         tmp = (tmp >> 1)
+       }
+       if(ts.length === len) res.push(ts)
+     }
+  }
+};
+
+/**
+ * @return {string}
+ */
+CombinationIterator.prototype.next = function() {
+  if(this.hasNext()) {
+      return this.arr[this.idx++]
+  }
+};
+
+/**
+ * @return {boolean}
+ */
+CombinationIterator.prototype.hasNext = function() {
+  return this.arr[this.idx] != null
+};
+
+/** 
+ * Your CombinationIterator object will be instantiated and called as such:
+ * var obj = new CombinationIterator(characters, combinationLength)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+
+
