4362867ed44ad5f3a9be8852513626765545af61
Nothing <wwwaap@gmail.com>
2022-12-03 22:36:43 +0800

    Update 974-subarray-sums-divisible-by-k.js

--- 974-subarray-sums-divisible-by-k.js
+++ 974-subarray-sums-divisible-by-k.js
@@ -14,3 +14,22 @@ const subarraysDivByK = function (nums, k) {
   }
   return res
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const subarraysDivByK = function(nums, k) {
+  const memo = {0: 1}
+  let sum = 0, res = 0
+  for(const e of nums) {
+    sum += e
+    const remain = (k - (sum % k)) % k
+    res += memo[remain] ?? 0
+    memo[remain] = (memo[remain] ?? 0) + 1 
+  }
+  return res
+};
