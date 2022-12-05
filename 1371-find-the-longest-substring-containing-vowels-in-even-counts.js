039362f154331d1e77eeb7eb56e68e7e42326984
Nothing <wwwaap@gmail.com>
2022-12-05 22:09:57 +0800

    Update 1371-find-the-longest-substring-containing-vowels-in-even-counts.js

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts.js
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts.js
@@ -31,6 +31,35 @@ const findTheLongestSubstring = function(s) {
  */
 const findTheLongestSubstring = function(s) {
   const n = s.length
+  let res = 0, mask = 0
+  const map = new Map([[0, -1]])
+  
+  for(let i = 0; i < n; i++) {
+    const ch = s[i]
+    const idx = 'aeiou'.indexOf(ch)
+    if(idx !== -1) {
+      mask ^= (1 << idx)
+      if(map.has(mask)) {
+        res = Math.max(res, i - map.get(mask))
+      } else {
+        map.set(mask, i)
+      }
+    } else {
+      res = Math.max(res, i - map.get(mask))
+    }
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const findTheLongestSubstring = function(s) {
+  const n = s.length
   const ch2num = ch => {
     const idx = 'aeiou'.indexOf(ch)
     return idx === -1 ? 0 : (1 << idx)
