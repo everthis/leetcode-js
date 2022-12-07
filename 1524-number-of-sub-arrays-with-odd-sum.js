f5e490c8536ee55fd1b5e67599f92305d76ca56a
Nothing <wwwaap@gmail.com>
2022-12-07 23:04:58 +0800

    Update 1524-number-of-sub-arrays-with-odd-sum.js

--- 1524-number-of-sub-arrays-with-odd-sum.js
+++ 1524-number-of-sub-arrays-with-odd-sum.js
@@ -4,6 +4,32 @@
  */
 const numOfSubarrays = function(arr) {
   const n = arr.length, mod = 1e9 + 7
+  let sum = 0, res = 0, oddCnt = 0, evenCnt = 0
+  
+  for(let i = 0; i < n; i++) {
+    const cur = arr[i]
+    sum += cur
+    if(sum % 2 === 1) {
+      res++
+      res += evenCnt
+      oddCnt++
+    } else {
+      res += oddCnt
+      evenCnt++
+    }
+  }
+  
+  return res % mod
+};
+
+// another
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const numOfSubarrays = function(arr) {
+  const n = arr.length, mod = 1e9 + 7
 
   let oc = 0, ec = 1
   let sum = 0
