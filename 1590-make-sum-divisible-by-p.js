627fef5e864c24b13c370352456cc2fbd0a4e5d8
Nothing <wwwaap@gmail.com>
2022-12-09 22:55:21 +0800

    Update 1590-make-sum-divisible-by-p.js

--- 1590-make-sum-divisible-by-p.js
+++ 1590-make-sum-divisible-by-p.js
@@ -4,6 +4,33 @@
  * @return {number}
  */
 const minSubarray = function(nums, p) {
+  const remain = nums.reduce((ac, e) => ac + e, 0) % p
+  const n = nums.length, hash = {0: -1}
+  let res = n
+  if(remain === 0) return 0
+  for(let i = 0, sum = 0; i < n; i++) {
+    const cur = nums[i]
+    sum += cur
+    const target = (sum % p - remain + p) % p
+    if(hash[target] != null) {
+      res = Math.min(res, i - hash[target])
+    }
+
+    hash[sum % p] = i
+  }
+//   console.log(hash)
+  
+  return res === n ? -1 : res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} p
+ * @return {number}
+ */
+const minSubarray = function(nums, p) {
   const diff = nums.reduce((a, b) => a + b, 0) % p;
   let res = diff === 0 ? 0 : nums.length;
   
