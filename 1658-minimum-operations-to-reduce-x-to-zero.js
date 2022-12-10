ada1e7906e923f5eedbbed422f0ab90e6b9acaa2
Nothing <wwwaap@gmail.com>
2022-12-10 14:19:44 +0800

    Update 1658-minimum-operations-to-reduce-x-to-zero.js

--- 1658-minimum-operations-to-reduce-x-to-zero.js
+++ 1658-minimum-operations-to-reduce-x-to-zero.js
@@ -3,6 +3,31 @@
  * @param {number} x
  * @return {number}
  */
+const minOperations = function(nums, x) {
+  const sum = nums.reduce((ac, e) => ac + e, 0)
+  const subArrSum = sum - x
+  if(subArrSum === 0) return nums.length
+  const n = nums.length, hash = {0: -1}
+  let ac = 0, res = -1
+  for(let i = 0; i < n; i++) {
+    const cur = nums[i]
+    ac += cur
+    if(hash[ac - subArrSum] != null) {
+      res = Math.max(res, i - hash[ac - subArrSum])
+    }
+    hash[ac] = i
+  }
+ 
+  return res === -1 ? -1 : n - res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
 const minOperations = function (nums, x) {
   let l = 0,
     r = nums.length - 1;
