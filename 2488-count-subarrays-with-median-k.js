18ad49bb95a64894cc95143fb4c241fe2d92fed3
Nothing <wwwaap@gmail.com>
2022-12-13 22:59:55 +0800

    Update 2488-count-subarrays-with-median-k.js

--- 2488-count-subarrays-with-median-k.js
+++ 2488-count-subarrays-with-median-k.js
@@ -6,21 +6,22 @@
 const countSubarrays = (a, k) => permutationArrayWithMedianK(a, k);
 
 const permutationArrayWithMedianK = (a, k) => {
-    let m = new Map([[0, 1]]), find = false, balance = 0, res = 0;
-    for (const x of a) {
-        if (x < k) {
-            balance--;
-        } else if (x > k) {
-            balance++;
-        } else {
-            find = true;
-        }
-        if (find) {
-            // balance - 1, subarray length is even, has one more right
-            res += (m.get(balance) || 0) + (m.get(balance - 1) || 0);
-        } else {
-            m.set(balance, m.get(balance) + 1 || 1);
-        }
+  const m = new Map([[0, 1]])
+  let find = false, balance = 0, res = 0;
+  for (const x of a) {
+    if (x < k) {
+      balance--;
+    } else if (x > k) {
+      balance++;
+    } else {
+      find = true;
     }
-    return res;
+    if (find) {
+      // balance - 1, subarray length is even, has one more right
+      res += (m.get(balance) || 0) + (m.get(balance - 1) || 0);
+    } else {
+      m.set(balance, (m.get(balance) || 0) + 1);
+    }
+  }
+  return res;
 };
