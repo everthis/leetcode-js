818ca90ed17b2b2024822307d97271ab8f0373c0
Nothing <wwwaap@gmail.com>
2022-12-19 15:44:58 +0800

    Update 2430-maximum-deletions-on-a-string.js

--- 2430-maximum-deletions-on-a-string.js
+++ 2430-maximum-deletions-on-a-string.js
@@ -3,6 +3,30 @@
  * @return {number}
  */
 const deleteString = function (s) {
+  const dp = Array(4000).fill(0)
+  const n = s.length
+  return helper(0)
+  
+  function helper(i) {
+    if(dp[i] === 0) {
+      dp[i] = 1
+      for(let len = 1; dp[i] <= n - i - len; len++) {
+        if(s.slice(i, i + len) === s.slice(i + len, i + 2 * len)) {
+          dp[i] = Math.max(dp[i], 1 + helper(i + len))          
+        }
+      }
+    }
+    return dp[i]
+  }
+}
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const deleteString = function (s) {
   const dp = Array(4000).fill(0), lps = Array(4000).fill(0)
   for (let k = s.length - 1; k >= 0; --k) {
     dp[k] = 1; 
