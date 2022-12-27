7db705c3245b637cd56c38e69499f2ae35357b14
Nothing <wwwaap@gmail.com>
2022-12-27 19:50:50 +0800

    Update 494-target-sum.js

--- 494-target-sum.js
+++ 494-target-sum.js
@@ -1,5 +1,38 @@
 /**
  * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const findTargetSumWays = function(nums, target) {
+    const sum = nums.reduce((a, b) => a+b);
+    
+    if(Math.abs(target) > sum) {
+        return 0;
+    }
+
+    if((target + sum) % 2) {
+        return 0;
+    }
+
+    const halfSum = (target + sum) / 2;
+
+    let dp = new Array(halfSum+1).fill(0);
+    dp[0] = 1;
+
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = halfSum; j >= nums[i]; j--) {
+            dp[j] += dp[j - nums[i]];
+        }
+    }
+
+    return dp[halfSum];
+};
+
+
+// another
+
+/**
+ * @param {number[]} nums
  * @param {number} S
  * @return {number}
  */
