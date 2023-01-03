78c9926874381f8e01b018b003edf09e6bbe87fa
Nothing <wwwaap@gmail.com>
2023-01-03 17:20:33 +0800

    Update 2354-number-of-excellent-pairs.js

--- 2354-number-of-excellent-pairs.js
+++ 2354-number-of-excellent-pairs.js
@@ -4,6 +4,38 @@
  * @return {number}
  */
 const countExcellentPairs = function(nums, k) {
+  const cnt = Array(31).fill(0), set = new Set(nums)
+  for(const e of set) {
+    cnt[setBits(e)]++
+  }
+  let res = 0
+  
+  for(let i = 1; i < 31; i++) {
+    for(let j = 1; j < 31; j++) {
+      if(i + j >= k) res += cnt[i] * cnt[j]
+    }
+  }
+
+  return res
+  
+  function setBits(num) {
+    let res = 0
+    while(num) {
+      res += num % 2
+      num = num >> 1
+    }
+    return res
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countExcellentPairs = function(nums, k) {
   const arr = [], set = new Set(nums)
   for(const e of set) {
     arr.push(setBits(e))
