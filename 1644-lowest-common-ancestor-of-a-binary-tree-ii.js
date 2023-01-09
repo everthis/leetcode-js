9dad5df0b0dde568355c6cdcb7ffaadea0b2dd2b
Nothing <wwwaap@gmail.com>
2023-01-09 20:08:47 +0800

    Update 1644-lowest-common-ancestor-of-a-binary-tree-ii.js

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii.js
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii.js
@@ -33,3 +33,41 @@ const lowestCommonAncestor = function (root, p, q) {
   dfs(root)
   return cn
 }
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = function (root, p, q) {
+  let hasP = false, hasQ = false
+  const res = LCA(root, p, q)
+  return hasP && hasQ ? res : null
+
+  function LCA(root, p, q) {
+    if(root == null) return root
+    const left = LCA(root.left, p, q)
+    const right = LCA(root.right, p, q)
+    if(root === p) {
+      hasP = true
+      return root
+    }
+    if(root === q) {
+      hasQ = true
+      return root
+    }
+    if(left && right) return root
+    return left || right
+  }
+}
+
