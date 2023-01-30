561f40017c2b3dc0884b397355c96b7da022fa12
Nothing <wwwaap@gmail.com>
2023-01-30 19:34:20 +0800

    Update 1734-decode-xored-permutation.js

--- 1734-decode-xored-permutation.js
+++ 1734-decode-xored-permutation.js
@@ -3,6 +3,27 @@
  * @return {number[]}
  */
 const decode = function(encoded) {
+  const n = encoded.length + 1
+  let xor = 0
+  for(let i = 1; i <= n; i++) xor ^= i
+  for(let i = 1; i < n - 1; i += 2) xor ^= encoded[i]
+  const res = [xor]
+  let pre = xor
+  for(let i = 0; i < n - 1; i++) {
+    res.push(encoded[i] ^ pre)
+    pre = res[res.length - 1]
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} encoded
+ * @return {number[]}
+ */
+const decode = function(encoded) {
   let a = 0
   const n = encoded.length + 1
   for(let i = 0; i <= n; i++) {
