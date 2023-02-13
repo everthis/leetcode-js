d5baec98df6e2651107aa482d7f7bbaa70630243
Nothing <wwwaap@gmail.com>
2023-02-13 19:02:11 +0800

    Update 630-course-schedule-iii.js

--- 630-course-schedule-iii.js
+++ 630-course-schedule-iii.js
@@ -3,6 +3,29 @@
  * @return {number}
  */
 const scheduleCourse = function (courses) {
+  const compare = (a, b) => a[0] === b[0] ? 0 : (a[0] > b[0] ? -1 : 1)
+  const queue = new PriorityQueue({ compare })
+  courses.sort((a, b) => a[1] - b[1])
+  let time = 0
+  for(let e of courses) {
+    time += e[0]
+    queue.enqueue(e)
+    if(time > e[1]) {
+      const tmp = queue.dequeue()
+      time -= tmp[0]
+    }
+  }
+  return queue.size()
+}
+
+// another
+
+
+/**
+ * @param {number[][]} courses
+ * @return {number}
+ */
+const scheduleCourse = function (courses) {
   const queue = new MaxPriorityQueue({
     priority: e => e[0]
   })
