ced9e1f552261cdc94e90824438829a61bb0b4ff
Nothing <wwwaap@gmail.com>
2023-02-19 21:05:33 +0800

    Update 1653-minimum-deletions-to-make-string-balanced.js

--- 1653-minimum-deletions-to-make-string-balanced.js
+++ 1653-minimum-deletions-to-make-string-balanced.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 const minimumDeletions = function(s) {
+  let res = 0, b = 0
+  for(const e of s) {
+    if(e === 'b') b++
+    else if(b > 0) {
+      res++
+      b--
+    }
+  }
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const minimumDeletions = function(s) {
   let res = 0
   let cnt = 0
   for(let c of s) {
