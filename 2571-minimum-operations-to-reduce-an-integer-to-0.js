be5dd9294dbbc5783780a9cb442ca1fd95c2e040
Nothing <wwwaap@gmail.com>
2023-03-17 20:49:12 +0800

    Update 2571-minimum-operations-to-reduce-an-integer-to-0.js

--- 2571-minimum-operations-to-reduce-an-integer-to-0.js
+++ 2571-minimum-operations-to-reduce-an-integer-to-0.js
@@ -8,6 +8,38 @@ function bitCnt(s) {
   }
   return res
 }
+function cnt(num) {
+  return bitCnt(dec2bin(num))
+}
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const minOperations = function(n) {
+  let res = 0
+  for(let i = 0; i < 14; i++) {
+    if(cnt(n + (1 << i)) < cnt(n)) {
+      res++
+      n += (1 << i)
+    }
+  }
+  
+  return res + cnt(n)
+};
+
+// another
+
+
+function dec2bin(dec) {
+  return (dec >>> 0).toString(2);
+}
+function bitCnt(s) {
+  let res = 0
+  for(const e of s) {
+    if(e === '1') res++
+  }
+  return res
+}
 /**
  * @param {number} n
  * @return {number}
