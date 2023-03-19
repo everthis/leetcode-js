120dc0a5abd751da622192471cf006d2b5969423
Nothing <wwwaap@gmail.com>
2023-03-19 21:54:50 +0800

    Update 2576-find-the-maximum-number-of-marked-indices.js

--- 2576-find-the-maximum-number-of-marked-indices.js
+++ 2576-find-the-maximum-number-of-marked-indices.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 const maxNumOfMarkedIndices = function(nums) {
+  let res = 0
+  const n =  nums.length
+  nums.sort((a, b) => a - b)
+  for(let i = 0, j = n - Math.floor(n / 2); j < n; j++) {
+    if(nums[i] * 2 <= nums[j]) {
+      res += 2
+      i++
+    }
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxNumOfMarkedIndices = function(nums) {
     let i = 0, n = nums.length;
     nums.sort((a, b) => a - b)
     for (let j = n - (~~(n / 2)); j < n; ++j) {
