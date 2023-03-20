093323a64e9a98f775494a21bf2896291895ecc4
Nothing <wwwaap@gmail.com>
2023-03-20 19:05:21 +0800

    Update 307-range-sum-query-mutable.js

--- 307-range-sum-query-mutable.js
+++ 307-range-sum-query-mutable.js
@@ -1,3 +1,68 @@
+const lowBit = (x) => x & -x
+class FenwickTree {
+  constructor(n) {
+    if (n < 1) return
+    this.sum = Array(n + 1).fill(0)
+  }
+  update(i, delta) {
+    if (i < 1) return
+    while (i < this.sum.length) {
+      this.sum[i] += delta
+      i += lowBit(i)
+    }
+  }
+  query(i) {
+    if (i < 1) return 0
+    let sum = 0
+    while (i > 0) {
+      sum += this.sum[i]
+      i -= lowBit(i)
+    }
+    return sum
+  }
+}
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+  this.nums = nums
+  const n = nums.length
+  this.bit = new FenwickTree(n)
+  for(let i = 1; i <= n; i++) {
+    this.bit.update(i, nums[i - 1])
+  }
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(index, val) {
+    const delta = val - this.nums[index]
+    this.nums[index] = val
+    this.bit.update(index + 1, delta)
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    return this.bit.query(right + 1) - this.bit.query(left)
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * obj.update(index,val)
+ * var param_2 = obj.sumRange(left,right)
+ */
+
+// another
+
 /**
  * @param {number[]} nums
  */
