aea984e125f0b5138514d1065448d392325a1e71
Nothing <wwwaap@gmail.com>
2023-03-21 09:43:28 +0800

    Update 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.js

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.js
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.js
@@ -3,6 +3,22 @@
  * @return {number}
  */
 const minNumberOperations = function(target) {
+  let res = target[0]
+  
+  for(let i = 1; i < target.length; i++) {
+    res += Math.max(0, target[i] - target[i - 1])
+  }
+  
+  return res
+}
+
+// another
+
+/**
+ * @param {number[]} target
+ * @return {number}
+ */
+const minNumberOperations = function(target) {
   let totalOperations = target[0];
   for (let i = 1; i < target.length; ++i) {
     if (target[i] > target[i-1]) {
