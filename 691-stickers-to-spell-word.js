b875116529435b9f1ecfc542adf818d3ec124510
Nothing <wwwaap@gmail.com>
2023-03-30 15:25:10 +0800

    Update 691-stickers-to-spell-word.js

--- 691-stickers-to-spell-word.js
+++ 691-stickers-to-spell-word.js
@@ -3,6 +3,40 @@
  * @param {string} target
  * @return {number}
  */
+const minStickers = function (stickers, target) {
+  const n = stickers.length
+  const m = target.length
+  const limit = 1 << m
+  const dp = Array(limit).fill(Infinity)
+  dp[0] = 0
+  for (let i = 0; i < limit; i++) {
+    if (dp[i] === Infinity) continue
+    for (const sticker of stickers) {
+      let stat = i
+      for (const ch of sticker) {
+        for (let j = 0; j < m; j++) {
+          if (target[j] === ch && ((stat >> j) & 1) === 0) {
+            stat |= (1 << j)
+            break
+          }
+        }
+      }
+      dp[stat] = Math.min(dp[stat], dp[i] + 1)
+    }
+  }
+
+  return dp[limit - 1] === Infinity ? -1 : dp[limit - 1]
+}
+
+
+// another
+
+
+/**
+ * @param {string[]} stickers
+ * @param {string} target
+ * @return {number}
+ */
 const minStickers = function(stickers, target) {
   const isEqual = (arr1, arr2) => {
     for (let i = 0; i < arr1.length; ++i) if (arr1[i] !== arr2[i]) return false
