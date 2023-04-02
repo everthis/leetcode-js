a2be85f8aeffb79bf0872791c1cb1c7f0be27386
Nothing <wwwaap@gmail.com>
2023-04-02 20:35:40 +0800

    Create 2611-mice-and-cheese.js

---
+++ 2611-mice-and-cheese.js
@@ -0,0 +1,100 @@
+/**
+ * @param {number[]} reward1
+ * @param {number[]} reward2
+ * @param {number} k
+ * @return {number}
+ */
+const miceAndCheese = function(reward1, reward2, k) {
+  const pq = new PQ((a, b) => a[2] > b[2])
+  
+  const n = reward1.length
+  
+  for(let i = 0; i < n; i++) {
+    const tmp = [reward1[i], reward2[i], reward1[i] - reward2[i]]
+    pq.push(tmp)
+  }
+  
+  let res = 0
+  while(k) {
+    const [v1, v2, delta] = pq.pop()
+    res += v1
+    
+    k--
+  }
+  
+  while(!pq.isEmpty()) {
+    const [v1, v2] = pq.pop()
+    res += v2
+  }
+  
+  
+  return res
+};
+
+class PQ {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
