296904d087e85612c1ec9e464e680a1bd9833f5f
Nothing <wwwaap@gmail.com>
2023-04-02 21:30:46 +0800

    Update 503-next-greater-element-II.js

--- 503-next-greater-element-II.js
+++ 503-next-greater-element-II.js
@@ -3,6 +3,31 @@
  * @return {number[]}
  */
 const nextGreaterElements = function(nums) {
+  const arr = []
+  const n = nums.length
+  const res = Array(n).fill(-1)
+  nums.push(...nums)
+  const stk = []
+  for(let i = 0; i < 2 * n; i++) {
+    const e = nums[i]
+    while(stk.length && nums[stk.at(-1)] < e) {
+      const idx = stk.pop()
+      res[idx] = e
+    }
+    if(i < n) stk.push(i)
+  }
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const nextGreaterElements = function(nums) {
   const res = [];
   for (let i = 0; i < nums.length; i++) {
     res.push(single(i, nums));
