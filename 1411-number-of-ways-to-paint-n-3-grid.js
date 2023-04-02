b64ad8bcd8b48dade8765266f079efa38a1657ae
Nothing <wwwaap@gmail.com>
2023-04-02 15:14:40 +0800

    Update 1411-number-of-ways-to-paint-n-3-grid.js

--- 1411-number-of-ways-to-paint-n-3-grid.js
+++ 1411-number-of-ways-to-paint-n-3-grid.js
@@ -2,6 +2,27 @@
  * @param {number} n
  * @return {number}
  */
+const numOfWays = function(n) {
+  const mod = 1e9 + 7
+  let colors3 = 6, colors2 = 6
+  
+  for(let i = 1; i < n; i++) {
+    const colors3Tmp = colors3
+    colors3 = (2 * colors3 + 2 * colors2) % mod
+    colors2 = (2 * colors3Tmp + 3 * colors2) % mod
+  }
+  
+  
+  return (colors2 + colors3) % mod
+};
+
+// another
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
 const numOfWays = function (n) {
   let a121 = 6,
     a123 = 6,
