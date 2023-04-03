4fedf133b1f1a240c67262c33995bfdee0c4f1c8
Nothing <wwwaap@gmail.com>
2023-04-03 14:17:45 +0800

    Update 1434-number-of-ways-to-wear-different-hats-to-each-other.js

--- 1434-number-of-ways-to-wear-different-hats-to-each-other.js
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other.js
@@ -2,6 +2,49 @@
  * @param {number[][]} hats
  * @return {number}
  */
+const numberWays = function(hats) {
+  const map = new Map()
+  const n = hats.length
+  for(let i = 0; i < n; i++) {
+    for(const h of hats[i]) {
+      if(!map.has(h)) map.set(h, [])
+      map.get(h).push(i)
+    }
+  }
+  const mod = 1e9 + 7
+  const allMask = (1 << n) - 1
+  const dp = Array.from({ length: 41 }, () => Array(1024))
+  
+  return dfs(1, 0)
+  
+  function dfs(hat, mask) {
+    if(mask === allMask) return 1
+    if(hat > 40) return 0
+    if(dp[hat][mask] != null) return dp[hat][mask]
+    
+    let res = 0
+    
+    // not using this `hat`
+    res += dfs(hat + 1, mask)
+    for(const p of (map.get(hat) || [])) {
+      if(((mask >> p) & 1) === 0) {
+        res += dfs(hat + 1, mask | (1 << p))
+        res = res % mod
+      }
+    }
+    dp[hat][mask] = res
+    return res
+  }
+  
+};
+
+// another
+
+
+/**
+ * @param {number[][]} hats
+ * @return {number}
+ */
 const numberWays = function (hats) {
   const pplThatCanWearHats = new Array(40 + 1).fill(null).map(() => [])
   for (let i = 0; i < hats.length; i++) {
