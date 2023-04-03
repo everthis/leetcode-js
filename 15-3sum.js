b0e6b74f7c658bd3de34c7a77182662972cd2e02
Nothing <wwwaap@gmail.com>
2023-04-03 10:00:07 +0800

    Update 15-3sum.js

--- 15-3sum.js
+++ 15-3sum.js
@@ -2,6 +2,40 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+const threeSum = function(nums) {
+  const res = [], n = nums.length
+  nums.sort((a, b) => a - b)
+  
+  for(let i = 0; i < n; i++) {
+    const target = -nums[i]
+    let l = i + 1, r = n - 1
+    while(l < r) {
+      const sum = nums[l] + nums[r]
+      if(sum > target) r--
+      else if(sum < target) l++
+      else {
+        const e = [nums[i], nums[l], nums[r]]
+        res.push(e)
+        while(l + 1 < r && nums[l + 1] === nums[l]) l++
+        while(r - 1 > l && nums[r - 1] === nums[r]) r--
+        l++
+        r--
+      }
+    }
+    while(i + 1 < n && nums[i] === nums[i + 1]) i++
+  }
+  
+  
+  return res
+};
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 const threeSum = function (nums) {
   nums.sort((a, b) => a - b)
   const res = []
