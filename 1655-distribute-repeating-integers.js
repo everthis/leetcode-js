fd314ca2b6ff20cb7d46e27ab9398870add26e26
Nothing <wwwaap@gmail.com>
2023-04-04 15:02:50 +0800

    Update 1655-distribute-repeating-integers.js

--- 1655-distribute-repeating-integers.js
+++ 1655-distribute-repeating-integers.js
@@ -3,6 +3,56 @@
  * @param {number[]} quantity
  * @return {boolean}
  */
+const canDistribute = function(nums, quantity) {
+  const freq = new Map()
+  for(const e of nums) {
+    freq.set(e, (freq.get(e) || 0) + 1)
+  }
+  const cntArr = [...freq.values()]
+  const dp = Array.from({ length: 51 }, () => Array(1024).fill(false))
+  const n = cntArr.length, m = quantity.length
+  for(let i = 0; i < n; i++) {
+    dp[i][0] = true
+  }
+  cntArr.unshift(0)
+  const allMask = (1 << m) - 1
+  
+  for(let i = 1; i <= n; i++) {
+    for(let mask = 1; mask <= allMask; mask++) {
+      if(dp[i - 1][mask]) {
+        dp[i][mask] = true
+        continue
+      }
+      for(let subset = mask; subset > 0; subset = (subset - 1) & mask) {
+        if(dp[i - 1][mask - subset] === false) continue
+        if(canSatisfySubset(cntArr[i], subset)) {
+          dp[i][mask] = true
+          break
+        }
+      }
+    }
+  }
+  
+  return dp[n][allMask]
+  
+  function canSatisfySubset(cnt, subset) {
+    let sum = 0
+    for (let i = 0; i < 10; i++) {
+      if(subset & (1 << i)) {
+        sum += quantity[i]
+      }
+    }
+    return cnt >= sum
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} quantity
+ * @return {boolean}
+ */
 const canDistribute = function (nums, quantity) {
   const mp = {}
   for (let x of nums) {
