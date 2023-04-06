89ea11344b6ab6baafde8f975abf673ad40e065d
Nothing <wwwaap@gmail.com>
2023-04-06 20:47:14 +0800

    Update 1681-minimum-incompatibility.js

--- 1681-minimum-incompatibility.js
+++ 1681-minimum-incompatibility.js
@@ -3,6 +3,52 @@
  * @param {number} k
  * @return {number}
  */
+const minimumIncompatibility = function(nums, k) {
+  const n = nums.length
+  const size = n / k
+  const mod = 1e9 + 7
+  if(size === 1) return 0
+  const limit = 1 << n
+  const dp = Array.from({ length: limit }, () => Array(16).fill(Infinity))
+  for(let i = 0; i < n; i++) dp[1 << i][i] = 0
+  
+  for(let mask = 0; mask < limit; mask++) {
+    for(let i = 0; i < n; i++) {
+      if((mask & (1 << i)) === 0) continue
+      for(let j = 0; j < n; j++) {
+        if((mask & (1 << j))) continue
+        const newMask = mask | (1 << j)
+        if(bitCnt(mask) % size === 0) {
+          dp[newMask][j] = Math.min(dp[newMask][j], dp[mask][i])
+        } else if(nums[j] > nums[i]) {
+          dp[newMask][j] = Math.min(dp[newMask][j], dp[mask][i] + nums[j] - nums[i])
+        }
+      }
+    }
+  }
+
+  const candidate = Math.min(...dp.at(-1))
+  
+  return candidate === Infinity ? -1 : candidate
+    
+  function bitCnt(num) {
+    let res = 0
+    while(num) {
+      if(num & 1) res++
+      num = num >> 1
+    }
+    
+    return res
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
 var minimumIncompatibility = function (nums, k) {
   if (k === nums.length) {
     return 0
