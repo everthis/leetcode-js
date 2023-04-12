24572632c52eadba03a8e229534aed644a1cd2dc
Nothing <wwwaap@gmail.com>
2023-04-12 11:04:38 +0800

    Update 2002-maximum-product-of-the-length-of-two-palindromic-subsequences.js

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences.js
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences.js
@@ -27,3 +27,47 @@ function palindromic( i, s) {
   }
   return len;
 }
+
+// another
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maxProduct = function(s) {
+  const s1 = [], s2 = [], n = s.length
+  let res = 0
+  dfs(0)
+  return res
+  
+  function dfs(idx) {
+    if(idx === n) {
+      if(isPalindromic(s1) && isPalindromic(s2)) {
+        res = Math.max(res, s1.length * s2.length)
+      }
+      return
+    }
+    const ch = s[idx]
+    s1.push(ch)
+    dfs(idx + 1)
+    s1.pop()
+    
+    s2.push(ch)
+    dfs(idx + 1)
+    s2.pop()
+    
+    dfs(idx + 1)
+  }
+  function isPalindromic(arr) {
+    let l = 0, r = arr.length - 1
+    while(l < r) {
+      if(arr[l] === arr[r]) {
+        l++
+        r--
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+};
