245f37713471aebc67b50e92c26c5e4c635aa176
Nothing <wwwaap@gmail.com>
2023-04-17 15:34:48 +0800

    Update 1986-minimum-number-of-work-sessions-to-finish-the-tasks.js

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks.js
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks.js
@@ -5,6 +5,39 @@
  */
 const minSessions = function(tasks, sessionTime) {
   const n = tasks.length
+  const limit = 1 << n
+  
+  const dp = Array(limit).fill(Infinity)
+  dp[0] = 0
+  for(let mask = 1; mask < limit; mask++) {
+    for(let sub = mask; sub; sub = (sub - 1) & mask) {
+      if(valid(sub)) {
+        dp[mask] = Math.min(dp[mask], dp[mask - sub] + 1)        
+      }
+    }
+  }
+  
+  return dp[limit - 1]
+  
+  function valid(sub) {
+    let sum = 0
+    for(let i = 0; i < 14; i++) {
+      if((sub >> i) & 1) sum += tasks[i]
+    }
+    
+    return sum <= sessionTime
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} tasks
+ * @param {number} sessionTime
+ * @return {number}
+ */
+const minSessions = function(tasks, sessionTime) {
+  const n = tasks.length
   const dp = Array.from({ length: 1 << 14 }, () => Array(16).fill(-1))
   return fn(0, 0)
   
