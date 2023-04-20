e0f3a8e56388caf5c621c032296315c77ad73f40
Nothing <wwwaap@gmail.com>
2023-04-20 10:03:21 +0800

    Update 1947-maximum-compatibility-score-sum.js

--- 1947-maximum-compatibility-score-sum.js
+++ 1947-maximum-compatibility-score-sum.js
@@ -4,6 +4,53 @@
  * @return {number}
  */
 const maxCompatibilitySum = function(students, mentors) {
+  const m = students.length, n = students[0].length
+  const limit = 1 << m
+  const dp = Array(limit).fill(0)
+  for(let mask = 1; mask < limit; mask++) {
+    for(let i = 0; i < m; i++) {
+      if(mask & (1 << i)) {
+        dp[mask] = Math.max(
+          dp[mask],
+          dp[mask ^ (1 << i)] + calc(bitCnt(mask) - 1, i)
+        )
+      }
+    }
+  }
+  
+  
+  return dp[limit - 1]
+    
+  function bitCnt(num) {
+    let res = 0
+    while(num) {
+      num = num & (num - 1)
+      res++
+    }
+    
+    return res
+  }
+  // student, mentor
+  function calc(i, j) {
+    let res = 0
+    for(let k = 0; k < n; k++) {
+      if(students[i][k] === mentors[j][k]) {
+        res++
+      }
+    }
+    
+    return res
+  }
+};
+
+// another
+
+/**
+ * @param {number[][]} students
+ * @param {number[][]} mentors
+ * @return {number}
+ */
+const maxCompatibilitySum = function(students, mentors) {
   const n = students.length, dp = Array(1 << n).fill(-Infinity)
   const m = students[0].length
   return dfs(0, 0)
