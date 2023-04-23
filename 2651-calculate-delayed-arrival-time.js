e98dc390addbdc90aada4bb1613d26bbbaddc44c
Nothing <wwwaap@gmail.com>
2023-04-23 12:42:42 +0800

    Create 2651-calculate-delayed-arrival-time.js

---
+++ 2651-calculate-delayed-arrival-time.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} arrivalTime
+ * @param {number} delayedTime
+ * @return {number}
+ */
+var findDelayedArrivalTime = function(arrivalTime, delayedTime) {
+  return (arrivalTime + delayedTime) % 24
+};
