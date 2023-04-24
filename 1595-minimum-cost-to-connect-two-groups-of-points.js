98fe4174fbfc3f33b1bd13c700bb414910e3b391
Nothing <wwwaap@gmail.com>
2023-04-24 09:55:30 +0800

    Update 1595-minimum-cost-to-connect-two-groups-of-points.js

--- 1595-minimum-cost-to-connect-two-groups-of-points.js
+++ 1595-minimum-cost-to-connect-two-groups-of-points.js
@@ -2,6 +2,53 @@
  * @param {number[][]} cost
  * @return {number}
  */
+const connectTwoGroups = function(cost) {
+  const m = cost.length, n = cost[0].length, { min } = Math
+  const limit = 1 << n
+  const dp = Array.from({ length: m + 1 }, () => Array(limit).fill(Infinity))
+  const minCost = Array.from({ length: m + 1 }, () => Array(limit).fill(Infinity))
+  
+  for(let i = 0; i < m; i++) {
+    for(let mask = 0; mask < limit; mask++) {
+      let sum = 0
+      for(let j = 0; j < n; j++) {
+        if((mask >> j) & 1) {
+          sum += cost[i][j]
+        }
+      }
+      
+      minCost[i][mask] = sum
+    }
+  }
+  
+  dp[0][0] = 0
+  for(let i = 1; i <= m; i++) {
+    for(let mask = 0; mask < limit; mask++) {
+      for(let sub = mask; sub; sub = (sub - 1) & mask) {
+        dp[i][mask] = min(
+          dp[i][mask],
+          dp[i - 1][mask - sub] + minCost[i - 1][sub]
+        )
+      }
+      let tmp = Infinity
+      for(let j = 0; j < n; j++) {
+        tmp = min(tmp, cost[i - 1][j])
+      }
+      
+      dp[i][mask] = min(dp[i][mask], dp[i - 1][mask] + tmp)
+    }
+  }
+  // console.log(dp)
+  return dp[m][limit - 1]
+};
+
+// another
+
+
+/**
+ * @param {number[][]} cost
+ * @return {number}
+ */
 const connectTwoGroups = function (cost) {
   const min = Array(cost[0].length).fill(Infinity)
   for (let j = 0; j < min.length; j++) {
