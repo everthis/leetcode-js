5fe28f890405db45c51eab0da7b1319418d54bfa
Nothing <wwwaap@gmail.com>
2023-04-27 14:29:18 +0800

    Create 2630-memoize-ii.js

---
+++ 2630-memoize-ii.js
@@ -0,0 +1,36 @@
+const RES = Symbol("result");
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const globalCache = new Map();
+
+    return (...params) => {
+        let currentCache = globalCache;
+        for(const param of params) {
+            if (!currentCache.has(param)) {
+                currentCache.set(param, new Map());
+            }
+            currentCache = currentCache.get(param);
+        }
+
+        if (currentCache.has(RES)) return currentCache.get(RES);
+
+        const result = fn(...params);
+
+        currentCache.set(RES, result);
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
