e2561e49aa6d958f4d3779631c7914c7e2c1b568
Nothing <wwwaap@gmail.com>
2023-04-28 11:09:07 +0800

    Update 2584-split-the-array-to-make-coprime-products.js

--- 2584-split-the-array-to-make-coprime-products.js
+++ 2584-split-the-array-to-make-coprime-products.js
@@ -2,20 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findValidSplit = function(nums) {
-  let n = nums.length, right = {};
+const findValidSplit = function(nums) {
+  const n = nums.length, right = {};
   for (let i = 0; i < n; i++) {
-    let primeFactorsCount = getPrimeFactors(nums[i]);
+    const primeFactorsCount = getPrimeFactors(nums[i]);
     for (let prime in primeFactorsCount) {
-      let count = primeFactorsCount[prime];
+      const count = primeFactorsCount[prime];
       right[prime] = (right[prime] || 0) + count;
     }
   }
-  let left = {}, common = new Set();
+  const left = {}, common = new Set();
   for (let i = 0; i <= n - 2; i++) {
-    let primesFactorsCount = getPrimeFactors(nums[i]);
-    for (let prime in primesFactorsCount) {
-      let count = primesFactorsCount[prime];
+    const primesFactorsCount = getPrimeFactors(nums[i]);
+    for (const prime in primesFactorsCount) {
+      const count = primesFactorsCount[prime];
       left[prime] = (left[prime] || 0) + count;
       right[prime] -= count;
       if (right[prime] > 0) common.add(prime);
@@ -27,7 +27,7 @@ var findValidSplit = function(nums) {
 };
 
 function getPrimeFactors(n) {
-  let counts = {};
+  const counts = {};
   for (let x = 2; (x * x) <= n; x++) {
     while (n % x === 0) {
       counts[x] = (counts[x] || 0) + 1;
