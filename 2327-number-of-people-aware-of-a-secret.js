541ab0a408d5f09606057d337a2915e717e0d340
Nothing <wwwaap@gmail.com>
2023-04-29 20:48:44 +0800

    Update 2327-number-of-people-aware-of-a-secret.js

--- 2327-number-of-people-aware-of-a-secret.js
+++ 2327-number-of-people-aware-of-a-secret.js
@@ -5,6 +5,35 @@
  * @return {number}
  */
 const peopleAwareOfSecret = function(n, delay, forget) {
+  const dp = Array(n + 1).fill(0)
+  const mod = 1e9 + 7
+  
+  dp[1] = 1
+  for(let i = 1; i <= n; i++) {
+    for(let j = i + delay; j < i + forget; j++) {
+      if(j > n) break
+      dp[j] += dp[i]
+      dp[j] %= mod
+    }
+  }
+
+  let res = 0
+  for(let i = n - forget + 1; i <= n; i++) {
+    res = (dp[i] + res) % mod
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} delay
+ * @param {number} forget
+ * @return {number}
+ */
+const peopleAwareOfSecret = function(n, delay, forget) {
     let cnt = new Array(n+1).fill(0);
     cnt[1] = 1;
     let i = 1;
