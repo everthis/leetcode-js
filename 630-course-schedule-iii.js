c98b4ac4e51265322747826c9bb8a0f24e49a447
Nothing <wwwaap@gmail.com>
2023-05-04 13:11:09 +0800

    Update 630-course-schedule-iii.js

--- 630-course-schedule-iii.js
+++ 630-course-schedule-iii.js
@@ -20,6 +20,99 @@ const scheduleCourse = function (courses) {
 
 // another
 
+/**
+ * @param {number[][]} courses
+ * @return {number}
+ */
+const scheduleCourse = function(courses) {
+  const pq = new PQ((a, b) => a[0] === b[0] ? a[1] < b[1] : a[0] > b[0])
+  const n = courses.length
+  courses.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1])
+  
+  let time = 0
+  for(const e of courses) {
+    const [dur, end] = e
+    time += dur
+    pq.push(e)
+    if(time > end) {
+      const tmp = pq.pop()
+      time -= tmp[0]
+    }
+  }
+  
+  return pq.size()
+};
+
+class PQ {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
+
+// another
+
 
 /**
  * @param {number[][]} courses
