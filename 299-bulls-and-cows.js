a5869f3447b937a1b42549d8763b91d8071bb134
Nothing <wwwaap@gmail.com>
2023-05-05 13:00:27 +0800

    Update 299-bulls-and-cows.js

--- 299-bulls-and-cows.js
+++ 299-bulls-and-cows.js
@@ -4,6 +4,31 @@
  * @return {string}
  */
 const getHint = function(secret, guess) {
+  let x = 0, y = 0
+  const arr = Array(10).fill(0)
+  for(let i = 0; i < guess.length; i++) {
+    const ch = guess[i], e = secret[i]
+    if(secret[i] === ch) {
+      x++
+    } else {
+      if(arr[+ch] < 0) y++
+      if(arr[+e] > 0) y++
+      arr[+ch]++
+      arr[+e]--      
+    }
+  }
+
+  return `${x}A${y}B`
+};
+
+// another
+
+/**
+ * @param {string} secret
+ * @param {string} guess
+ * @return {string}
+ */
+const getHint = function(secret, guess) {
   let bulls = 0
   let cows = 0
   const h = {}
