7f25f44e3348339677d7e9ca3226b1ba7f2b24fb
Nothing <wwwaap@gmail.com>
2023-05-11 11:40:36 +0800

    Update 2653-sliding-subarray-beauty.js

--- 2653-sliding-subarray-beauty.js
+++ 2653-sliding-subarray-beauty.js
@@ -5,6 +5,48 @@
  * @return {number[]}
  */
 const getSubarrayBeauty = function(nums, k, x) {
+  const arr = Array(101).fill(0)
+  const res = [], n = nums.length, delta = 50
+  for(let i = 0; i < n; i++) {
+    const cur = nums[i]
+    const idx = cur + delta
+    arr[idx]++
+    if(i < k - 1) continue
+    else if(i === k - 1) res.push(helper())
+    else {
+      const prev = nums[i - k]
+      arr[prev + delta]--
+      res.push(helper())
+    }
+  }
+  
+  return res
+  
+  function helper() {
+    let res = 0, neg = 0
+    // -50 ---> 0
+    // -1  ---> 49
+    for(let i = 0, len = arr.length; i < len; i++) {
+      if(i < delta && arr[i] > 0) neg += arr[i]
+      if(neg >= x) {
+        res = i - delta
+        break
+      }
+    }
+    
+    return res
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+const getSubarrayBeauty = function(nums, k, x) {
   let counter = Array(50).fill(0), ans = new Array(nums.length - k + 1).fill(0);
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] < 0) counter[nums[i] + 50]++;
