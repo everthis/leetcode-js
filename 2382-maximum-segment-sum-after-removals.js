daa82ab353215a3ee24daa11de740004cde1f690
Nothing <wwwaap@gmail.com>
2023-05-15 14:15:05 +0800

    Update 2382-maximum-segment-sum-after-removals.js

--- 2382-maximum-segment-sum-after-removals.js
+++ 2382-maximum-segment-sum-after-removals.js
@@ -4,26 +4,27 @@
  * @return {number[]}
  */
 const maximumSegmentSum = function(nums, removeQueries) {
-  const INF = Infinity, n = nums.length
-  const res = Array(n).fill(0) 
-  const ds = Array(n).fill(INF);
-  for (let i = n - 1; i > 0; --i) {
-    const j = removeQueries[i];
-    ds[j] = -nums[j];
-    if (j > 0 && ds[j - 1] !== INF) merge(j, j - 1, ds);
-    if (j < n - 1 && ds[j + 1] !== INF) merge(j, j + 1, ds);
-    res[i - 1] = Math.max(res[i], -ds[find(j, ds)]);
+  removeQueries.reverse()
+  const rev = removeQueries
+  const hash = {}
+  const res = []
+  let cur = 0
+  for(const idx of rev) {
+    hash[idx] = [nums[idx], 1]
+    const [lv, ll] = (hash[idx - 1] || [0, 0])
+    const [rv, rl] = (hash[idx + 1] || [0, 0])
+    
+    const val = nums[idx] + lv + rv
+    hash[idx + rl] = [val, ll + rl + 1]
+    hash[idx - ll] = [val, ll + rl + 1]
+    
+    cur = Math.max(cur, val)
+    res.push(cur)
   }
-  return res;
+  res.pop()
+  res.reverse()
+  res.push(0)
+  
+  return res
 };
 
-function find(i, ds) {
-  return ds[i] < 0 ? i : ds[i] = find(ds[i], ds);
-}
-
-function merge(s1, s2, ds) {
-  const p1 = find(s1, ds);
-  const p2 = find(s2, ds);
-  ds[p2] += ds[p1];
-  ds[p1] = p2;
-}
