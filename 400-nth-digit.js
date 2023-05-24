a2fe75adf7d8bc1dddf6c47f1e2d1446c68b16d5
Nothing <wwwaap@gmail.com>
2023-05-24 21:02:07 +0800

    Update 400-nth-digit.js

--- 400-nth-digit.js
+++ 400-nth-digit.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 const findNthDigit = function(n) {
-    let start = 1
-    let len = 1
-    let base = 9
-    while(n > len * base) {
-      n = n - len * base
-      len++
-      start *= 10
-      base *= 10
-    }
-    let target = start + (n - 1) / len
-    let reminder = (n - 1) % len
-    return (''+target).charAt(reminder)
+  let start = 1
+  let len = 1
+  let base = 9
+  while(n > len * base) {
+    n = n - len * base
+    len++
+    start *= 10
+    base *= 10
+  }
+  let target = start + Math.floor((n - 1) / len)
+  let reminder = (n - 1) % len
+  return (''+target).charAt(reminder)
 };
