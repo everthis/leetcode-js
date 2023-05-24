8b0eeba2b7b47f788cdfd57090cc275f58386ca8
Nothing <wwwaap@gmail.com>
2023-05-24 13:57:38 +0800

    Update 386-lexicographical-numbers.js

--- 386-lexicographical-numbers.js
+++ 386-lexicographical-numbers.js
@@ -3,6 +3,31 @@
  * @return {number[]}
  */
 const lexicalOrder = function(n) {
+  const res = []
+  for(let i = 1; i < 10; i++) {
+    dfs(i)
+  }
+  
+  return res
+  
+  function dfs(num) {
+    if(num > n) return
+    res.push(num)
+    for(let i = 0; i < 10; i++) {
+      const tmp = num * 10 + i
+      if(tmp > n) return
+      dfs(tmp)
+    }
+  }
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const lexicalOrder = function(n) {
   const result = []
   for (let i = 1; i < 10; i++) {
     dfs(i)
