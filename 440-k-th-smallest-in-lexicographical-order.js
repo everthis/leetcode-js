09b9a7978cd294ffc97f4973f8bd06392975d5ea
Nothing <wwwaap@gmail.com>
2023-05-25 15:27:11 +0800

    Update 440-k-th-smallest-in-lexicographical-order.js

--- 440-k-th-smallest-in-lexicographical-order.js
+++ 440-k-th-smallest-in-lexicographical-order.js
@@ -3,29 +3,28 @@
  * @param {number} k
  * @return {number}
  */
-const findKthNumber = function(n, k) {
+const findKthNumber = function (n, k) {
   let curr = 1
   k = k - 1
   while (k > 0) {
     let steps = calSteps(n, curr, curr + 1)
     if (steps <= k) {
-      curr++
+      curr += 1
       k -= steps
     } else {
       curr *= 10
-      k--
+      k -= 1
     }
   }
   return curr
-}
 
-//use long in case of overflow
-function calSteps(n, n1, n2) {
-  let steps = 0
-  while (n1 <= n) {
-    steps += Math.min(n + 1, n2) - n1
-    n1 *= 10
-    n2 *= 10
+  function calSteps(n, n1, n2) {
+    let steps = 0
+    while (n1 <= n) {
+      steps += Math.min(n + 1, n2) - n1
+      n1 *= 10
+      n2 *= 10
+    }
+    return steps
   }
-  return steps
 }
