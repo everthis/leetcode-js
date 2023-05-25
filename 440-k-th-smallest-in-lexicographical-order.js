22b3aabc2ef02893db51b66ef05f0b6ff48a787e
Nothing <wwwaap@gmail.com>
2023-05-25 19:51:53 +0800

    Update 440-k-th-smallest-in-lexicographical-order.js

--- 440-k-th-smallest-in-lexicographical-order.js
+++ 440-k-th-smallest-in-lexicographical-order.js
@@ -4,6 +4,41 @@
  * @return {number}
  */
 const findKthNumber = function (n, k) {
+  let cur = 1
+  k = k - 1
+  while(k > 0) {
+    const num = calc(cur)
+    if(num <= k) {
+      cur++
+      k -= num
+    } else {
+      k--
+      cur *= 10
+    }
+  }
+  return cur
+  
+  function calc(cur) {
+    let total = 0
+    let nxt = cur + 1
+    while(cur <= n) {
+      total += Math.min(n - cur + 1, nxt - cur)
+      nxt *= 10
+      cur *= 10
+    }
+    
+    return total
+  }
+}
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const findKthNumber = function (n, k) {
   let curr = 1
   k = k - 1
   while (k > 0) {
