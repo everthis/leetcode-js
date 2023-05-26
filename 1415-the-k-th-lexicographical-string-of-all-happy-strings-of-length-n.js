15d22cd7f6d4e30181475f8506a1931de2471b4d
Nothing <wwwaap@gmail.com>
2023-05-26 11:58:06 +0800

    Update 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
@@ -7,6 +7,30 @@ const getHappyString = function(n, k) {
   const hash = {a: 'bc', b: 'ac', c: 'ab'}
   const q = ['a', 'b', 'c']
   while(q[0].length !== n) {
+    const e = q.shift()
+    const last = e.charAt(e.length - 1)
+    for(const ch of hash[last]) {
+      q.push(e + ch)
+    }
+  }
+  if(q.length >= k && q[k - 1].length === n) {
+     return q[k - 1]
+  }
+     
+  return ''
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+const getHappyString = function(n, k) {
+  const hash = {a: 'bc', b: 'ac', c: 'ab'}
+  const q = ['a', 'b', 'c']
+  while(q[0].length !== n) {
     const pre = q.shift()
     for(const ch of hash[pre[pre.length - 1]]) {
       q.push(pre + ch)
