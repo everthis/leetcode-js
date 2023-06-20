8cbe5cbe7fac6ddd69706a0bd22ceef04086d61f
Nothing <wwwaap@gmail.com>
2023-06-20 12:46:31 +0800

    Update 1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 const numSubseq = function(nums, target) {
-  const n = nums.length, pow = Array(n), mod = 1e9 + 7
-  pow[0] = 1
+  const n = nums.length, mod = 1e9 + 7
+  const pows = Array(n).fill(1)
   for(let i = 1; i < n; i++) {
-    pow[i] = (pow[i - 1] * 2) % mod
+    pows[i] = pows[i - 1] * 2 % mod
   }
+  let res = 0, l = 0, r = n - 1
   nums.sort((a, b) => a - b)
-  let l = 0, r = n - 1, res = 0
   while(l <= r) {
     if(nums[l] + nums[r] > target) r--
     else {
-      res = (res + pow[r - l]) % mod
+      res = (res + pows[r - l]) % mod
       l++
     }
   }
