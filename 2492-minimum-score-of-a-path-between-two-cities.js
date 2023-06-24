2277886292b9d639d0a7b7fe85d90b6a94b652b8
Nothing <wwwaap@gmail.com>
2023-06-24 17:14:57 +0800

    Update 2492-minimum-score-of-a-path-between-two-cities.js

--- 2492-minimum-score-of-a-path-between-two-cities.js
+++ 2492-minimum-score-of-a-path-between-two-cities.js
@@ -1,3 +1,36 @@
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+const minScore = function(n, roads) {
+  const g = {}, visited = Array(n + 1).fill(0)
+  let res = Infinity
+  for(const [u, v, d] of roads) {
+    if(g[u] == null) g[u] = []
+    if(g[v] == null) g[v] = []
+    
+    g[u].push([v, d])
+    g[v].push([u, d])
+  }
+  
+  dfs(1)
+  
+  return res
+  
+  function dfs(node) {
+    visited[node] = 1
+    for(const [nxt, dis] of (g[node] || [])) {
+      res = Math.min(res, dis)
+      if(visited[nxt] === 0) {
+        dfs(nxt)
+      }
+    }
+  }
+};
+
+// another
+
 class UnionFind {
     constructor() {
       this.sizes = new Map()
