95aa94fc8f4bda4fffd589431199887f85c02275
Nothing <wwwaap@gmail.com>
2023-06-29 13:38:56 +0800

    Update 459-repeated-substring-pattern.js

--- 459-repeated-substring-pattern.js
+++ 459-repeated-substring-pattern.js
@@ -3,6 +3,38 @@
  * @return {boolean}
  */
 const repeatedSubstringPattern = function(s) {
+  
+  const len = s.length
+  const table = build(s)
+  return table[len] && (table[len] % (len - table[len]) === 0)
+  
+  function build(str) {
+    const n = str.length
+    const table = Array(n + 1).fill(0)
+    let i = 1, j = 0
+    table[0] = -1
+    while(i < n) {
+      if(str[i] === str[j]) {
+        i++
+        j++
+        table[i] = j
+      } else {
+        if(j > 0) j = table[j]
+        else i++
+      }
+    }
+    
+    return table
+  }
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const repeatedSubstringPattern = function(s) {
     const len = s.length
     let tmp = ''
     for(let i = 1; i <= len; i++) {
