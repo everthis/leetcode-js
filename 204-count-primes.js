faae9739592ef82abe8768c6f53f9fea5966bc6d
Nothing <wwwaap@gmail.com>
2023-07-01 15:43:42 +0800

    Update 204-count-primes.js

--- 204-count-primes.js
+++ 204-count-primes.js
@@ -2,6 +2,31 @@
  * @param {number} n
  * @return {number}
  */
+const countPrimes = function(n) {
+  const arr = Array(n).fill(0)
+  
+  for(let i = 2; i * i < n; i++) {
+    if(arr[i] !== 0) continue
+    let j = i * i
+    while(j < n) {
+      arr[j] = 1
+      j += i
+    }
+  }
+  
+  let res = 0
+  for(let i = 2; i < n; i++) {
+    if(arr[i] === 0) res++
+  }
+  return res
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
 const countPrimes = function (n) {
   const memo = Array(n).fill(false)
   let res = 0
