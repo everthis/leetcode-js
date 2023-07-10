20b791bf8b9d2925d3745d7f4f629d7390f62eff
Nothing <wwwaap@gmail.com>
2023-07-10 23:06:31 +0800

    Update 233-number-of-digit-one.js

--- 233-number-of-digit-one.js
+++ 233-number-of-digit-one.js
@@ -3,6 +3,34 @@
  * @return {number}
  */
 const countDigitOne = function(n) {
+  let res = 0
+  const str = `${n}`
+  const len = str.length, { pow } = Math
+  
+  for(let i = 1; i <= len; i++) {
+    const pre = ~~(n / pow(10, i))
+    const remain = n % (pow(10, i - 1))
+    const post = pow(10, i - 1)
+    res += pre * post
+    const e = +(str[len - i])
+    if(e > 1) {
+      res += pow(10, i - 1)
+    } else if(e === 1) {
+      res += remain + 1
+    }
+  }
+  
+  return res
+};
+
+
+// another
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const countDigitOne = function(n) {
     return countNum(1, n + 1)
 };
 
