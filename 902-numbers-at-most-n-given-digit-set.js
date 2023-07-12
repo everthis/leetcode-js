95401de9a0c69abff59a395393f4e025828191c2
Nothing <wwwaap@gmail.com>
2023-07-12 15:02:30 +0800

    Update 902-numbers-at-most-n-given-digit-set.js

--- 902-numbers-at-most-n-given-digit-set.js
+++ 902-numbers-at-most-n-given-digit-set.js
@@ -4,6 +4,41 @@
  * @return {number}
  */
 const atMostNGivenDigitSet = function(digits, n) {
+  let res = 0
+  const str = `${n}`, len = str.length
+  const { pow } = Math, base = digits.length
+  for(let i = 1; i < len; i++) {
+    res += pow(base, i)
+  }
+  
+  dfs(0)
+  
+  return res
+  
+  function dfs(pos) {
+    if(pos === len) {
+      res++
+      return
+    }
+    for(const ch of digits) {
+      if(str[pos] > ch) {
+        res += pow(base, len - 1 - pos)
+      } else if(str[pos] === ch) {
+        dfs(pos + 1)
+      }
+    }
+  }
+};
+
+// another
+
+
+/**
+ * @param {string[]} digits
+ * @param {number} n
+ * @return {number}
+ */
+const atMostNGivenDigitSet = function(digits, n) {
   const str = '' + n, { pow } = Math
   const len = str.length, dsize = digits.length
   let res = 0
