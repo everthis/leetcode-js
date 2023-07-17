ca489ca0a5c9118afe99ee0f051aba55873f266b
Nothing <wwwaap@gmail.com>
2023-07-17 12:13:53 +0800

    Update 2537-count-the-number-of-good-subarrays.js

--- 2537-count-the-number-of-good-subarrays.js
+++ 2537-count-the-number-of-good-subarrays.js
@@ -4,6 +4,45 @@
  * @return {number}
  */
 const countGood = function(nums, k) {
+  let res = 0, total = 0
+  const cnt = {}, n = nums.length
+
+  for(let i = 0, j = 0; i < n; i++) {
+    
+    while(j < n && total < k) {
+      total += diff(nums[j], 1)
+      cnt[nums[j]] = (cnt[nums[j]] || 0) + 1
+      j++
+    }
+    
+    if(total >= k) {
+      res += n - j + 1
+    }
+    total += diff(nums[i], -1)
+    cnt[nums[i]]--
+  }
+  
+  return res
+  
+  function diff(num, delta) {
+    const pre = cnt[num] || 0
+    const old = pre * (pre - 1) / 2
+    const post = pre + delta
+    const cur = post * (post - 1) / 2
+    
+    return cur - old
+  }
+};
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countGood = function(nums, k) {
   let res = 0;
   const count = new Map()
   for(let i = 0, j = 0; j < nums.length; ++j){
