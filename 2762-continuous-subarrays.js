badd49c2eb1d1d81ba7d05b806a23d963c9e6a6d
Nothing <wwwaap@gmail.com>
2023-07-20 21:25:44 +0800

    Update 2762-continuous-subarrays.js

--- 2762-continuous-subarrays.js
+++ 2762-continuous-subarrays.js
@@ -6,6 +6,37 @@ const continuousSubarrays = function(nums) {
   let res = 0
   let l = 0
   let r = 0
+  const mq1 = [], mq2 = []
+  for(let r = 0; r < nums.length; r++) {
+    const e = nums[r]
+    while(mq1.length && nums[mq1.at(-1)] < e) mq1.pop()
+    mq1.push(r)
+    while(mq2.length && nums[mq2.at(-1)] > e) mq2.pop()
+    mq2.push(r)
+    
+    while(mq1.length && mq2.length && Math.abs(nums[mq1[0]] - nums[mq2[0]]) > 2) {
+      if(mq1.length && mq1[0] <= l) mq1.shift()
+      if(mq2.length && mq2[0] <= l) mq2.shift()
+      l++
+    }
+    
+    res += r - l + 1
+  }
+  return res
+};
+
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const continuousSubarrays = function(nums) {
+  let res = 0
+  let l = 0
+  let r = 0
   const mset = new MultiSet()
   for(let r = 0; r < nums.length; r++) {
     mset.add(nums[r])
