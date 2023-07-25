9ace2333e420ce94fb3d4b10ba3ee29430f6b586
Nothing <wwwaap@gmail.com>
2023-07-25 12:06:26 +0800

    Update 735-asteroid-collision.js

--- 735-asteroid-collision.js
+++ 735-asteroid-collision.js
@@ -3,6 +3,28 @@
  * @return {number[]}
  */
 const asteroidCollision = function(asteroids) {
+  const stk = [], n = asteroids.length, {abs} = Math
+  for(const e of asteroids) {
+    while(stk.length && stk.at(-1) > 0 && e < 0 && -e > stk.at(-1)) {
+      stk.pop()
+    }
+    if(stk.length && stk.at(-1) > 0 && e < 0 && -e === stk.at(-1)) {
+      stk.pop()
+    }else if(stk.length && stk.at(-1) > 0 && e < 0 && -e < stk.at(-1)) {
+      
+    }else stk.push(e)
+  }
+  return stk
+};
+
+// another
+
+
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+const asteroidCollision = function(asteroids) {
     const positive = []
     const res = []
     for(let i = 0; i < asteroids.length; i++) {
