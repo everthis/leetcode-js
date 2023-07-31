0feb7800bd11d00d5a531c6c63ae4af54fa98363
Nothing <wwwaap@gmail.com>
2023-07-31 19:28:41 +0800

    Update 2547-minimum-cost-to-split-an-array.js

--- 2547-minimum-cost-to-split-an-array.js
+++ 2547-minimum-cost-to-split-an-array.js
@@ -4,6 +4,33 @@
  * @return {number}
  */
 const minCost = function (nums, k) {
+  const n = nums.length
+  const dp = Array(n + 1).fill(Infinity)
+  for(let i = 0; i < n; i++) {
+    let score = 0, hash = {}
+    for(let j = i; j >= 0; j--) {
+      const e = nums[j]
+      if(hash[e] == null) hash[e] = 0
+      hash[e]++
+      if(hash[e] === 2) score += 2
+      else if(hash[e] > 2) score++
+      
+      if(j > 0) dp[i] = Math.min(dp[i], dp[j - 1] + score + k)
+      else dp[i] = Math.min(dp[i], score + k)
+    }
+  }
+  
+  return dp[n - 1]
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const minCost = function (nums, k) {
   const n = nums.length,
     max = Math.max(...nums),
     dp = Array(n + 1).fill(Number.MAX_SAFE_INTEGER)
