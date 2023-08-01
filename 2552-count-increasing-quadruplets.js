b6173e71cf3be95597865dcf5a338801efb5c486
Nothing <wwwaap@gmail.com>
2023-08-01 18:58:34 +0800

    Create 2552-count-increasing-quadruplets.js

---
+++ 2552-count-increasing-quadruplets.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const countQuadruplets = function(nums) {
+  const B = new Array(nums.length + 1).fill(0);
+  let quadruplets = 0;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    let countSmaller = 0;
+
+    for (let j = 0; j < i; j += 1) {
+      if (nums[j] < nums[i]) {
+        countSmaller += 1;
+        quadruplets += B[nums[j]];
+      } else {
+	    // countSmaller is all the As nums[j] is the B, nums[i] is C
+		  // so nums[j] is apart of countSmaller A-B-C relationships with nums[i]
+        B[nums[j]] += countSmaller;
+      }
+    }
+  }
+
+  return quadruplets
+};
