12e56cc5182226fdc5e5d846e8b932d8a479eb63
Nothing <wwwaap@gmail.com>
2023-08-07 15:57:52 +0800

    Update 956-tallest-billboard.js

--- 956-tallest-billboard.js
+++ 956-tallest-billboard.js
@@ -3,6 +3,31 @@
  * @return {number}
  */
 const tallestBillboard = function(rods) {
+  const sum = rods.reduce((ac, e) => ac + e, 0)
+  const dp = Array(sum + 1).fill(-1), { abs, max, min } = Math
+  dp[0] = 0
+  for(const e of rods) {
+    const bak = dp.slice()
+    for(let delta = 0; delta <= sum; delta++) {
+      if(bak[delta] < 0) continue
+      if(delta + e <= sum) dp[delta + e] = max(dp[delta + e], bak[delta])
+      dp[abs(delta - e)] = max(dp[abs(delta - e)], bak[delta] + min(e, delta))
+    }
+  }
+  
+  
+  return dp[0]
+};
+
+
+// another
+
+
+/**
+ * @param {number[]} rods
+ * @return {number}
+ */
+const tallestBillboard = function(rods) {
   const dp = {0 : 0}
   for(let el of rods) {
     for(let [diff, y] of Object.entries(dp)) {
