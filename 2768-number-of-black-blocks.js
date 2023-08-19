8365b8fdd11a286dca2c54b1951c1cf13e0f3027
Nothing <wwwaap@gmail.com>
2023-08-19 20:53:23 +0800

    Update 2768-number-of-black-blocks.js

--- 2768-number-of-black-blocks.js
+++ 2768-number-of-black-blocks.js
@@ -4,6 +4,56 @@
  * @param {number[][]} coordinates
  * @return {number[]}
  */
+const countBlackBlocks = function(m, n, coordinates) {
+   const block = {}
+   
+   for(const [i, j] of coordinates) {
+     helper(i, j)
+   }
+
+   const arr = Array(5).fill(0)
+   for(const k in block) {
+    if(block.hasOwnProperty(k)) {
+      arr[block[k]]++
+    }
+   }
+   arr[0] = (m - 1) * (n - 1) - arr.reduce((ac, e) => ac + e, 0) 
+   return arr
+
+   function helper(i, j) {
+
+     if(i < m - 1 && j < n - 1) {
+       update(i, j)
+       if(i > 0) update(i - 1, j)
+       if(j > 0) update(i, j - 1)
+       if(i > 0 && j > 0) update(i - 1, j - 1)         
+     } else if(i === m - 1 && j === n - 1) {
+       update(i - 1, j - 1)
+     } else if(i === m - 1) {
+       update(i - 1, j)
+       if(j > 0) update(i - 1, j - 1)
+     } else if(j === n - 1) {
+       update(i, j - 1)
+       if(i > 0) update(i - 1, j - 1)
+     }    
+     
+   }
+  
+   function update(i, j, delta = 1) {
+     const key = `${i},${j}`
+     if(block[key] == null) block[key] = 0
+     block[key]++
+   }
+};
+
+// another
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} coordinates
+ * @return {number[]}
+ */
 const countBlackBlocks = function (m, n, coordinates) {
   let arr = new Array(5).fill(0)
   arr[0] = (m - 1) * (n - 1)
