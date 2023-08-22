ca16016e025063995f41c936a645f280392e6af4
Nothing <wwwaap@gmail.com>
2023-08-22 19:01:31 +0800

    Update 2615-sum-of-distances.js

--- 2615-sum-of-distances.js
+++ 2615-sum-of-distances.js
@@ -3,6 +3,44 @@
  * @return {number[]}
  */
 const distance = function(nums) {
+   const res = []
+   const hash = {}, n = nums.length
+   for(let i = 0; i < n; i++) {
+     const e = nums[i]
+     if(hash[e] == null) hash[e] = []
+     hash[e].push(i)
+   }
+  
+   for(const [_, arr] of Object.entries(hash)) {
+     helper(arr)
+   }
+   
+   return res
+
+  function helper(arr) {
+    let sum = 0
+    const len = arr.length
+    for(let i = 1; i < len; i++) {
+      sum += arr[i] - arr[0]
+    }
+    const first = arr[0]
+    res[first] = sum
+    for(let i = 1; i < len; i++) {
+      const preIdx = arr[i - 1]
+      const pre = res[preIdx], diff = arr[i] - arr[i - 1]
+      const val = pre + i * diff - diff * (len - i)
+      res[arr[i]] = val
+    }
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const distance = function(nums) {
   const n = nums.length
   const res = Array(n).fill(0)
   const hash = {}
