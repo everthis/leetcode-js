824dbae17e9e48e4ee688fc09081fe17a518dc8a
Nothing <wwwaap@gmail.com>
2023-09-05 20:31:09 +0800

    Update 2673-make-costs-of-paths-equal-in-a-binary-tree.js

--- 2673-make-costs-of-paths-equal-in-a-binary-tree.js
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree.js
@@ -3,6 +3,27 @@
  * @param {number[]} cost
  * @return {number}
  */
+const minIncrements = function(n, cost) {
+  let res = 0
+  dfs(1)
+  return res
+  
+  function dfs(node) {
+    if(node > n) return 0
+    const l = dfs(2 * node)
+    const r = dfs(2 * node + 1)
+    res += Math.abs(l - r)
+    return Math.max(l, r) + cost[node - 1]
+  }
+};
+
+// another
+
+/**
+ * @param {number} n
+ * @param {number[]} cost
+ * @return {number}
+ */
 var minIncrements = function(n, cost) {
   let ans = 0;
   const {abs, max} = Math
