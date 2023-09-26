83370d88f05325b6c10011ca09082facaec8f91c
Nothing <wwwaap@gmail.com>
2023-09-26 00:13:24 -0500

    Update 2003-smallest-missing-genetic-value-in-each-subtree.js

--- 2003-smallest-missing-genetic-value-in-each-subtree.js
+++ 2003-smallest-missing-genetic-value-in-each-subtree.js
@@ -3,6 +3,64 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+function smallestMissingValueSubtree(parent, nums) {
+  const graph = {},
+    n = parent.length
+  const res = Array(n).fill(1)
+  const visited = new Set()
+  let miss = 1
+  for (let i = 0; i < n; i++) {
+    if (graph[parent[i]] == null) graph[parent[i]] = []
+    graph[parent[i]].push(i)
+  }
+  let idx = -1
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === 1) {
+      idx = i
+      break
+    }
+  }
+
+  if (idx === -1) return res
+  let cur = idx,
+    pre = -1
+  while (cur !== -1) {
+    const chidlren = graph[cur]
+    if (chidlren) {
+      for (const child of chidlren) {
+        if (child === pre) continue
+        dfs(child)
+      }
+    }
+    visited.add(nums[cur])
+    while (visited.has(miss)) {
+      miss++
+    }
+    // console.log(cur, miss, visited)
+    res[cur] = miss
+    pre = cur
+    cur = parent[cur]
+  }
+
+  return res
+
+  function dfs(node) {
+    visited.add(nums[node])
+    const chidlren = graph[node]
+    if (chidlren) {
+      for (const child of chidlren) dfs(child)
+    }
+  }
+}
+
+// another
+
+
+/**
+ * @param {number[]} parents
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 const smallestMissingValueSubtree = function(parents, nums) {
       let n = parents.length;
       const ans = new Array(n).fill(0);
