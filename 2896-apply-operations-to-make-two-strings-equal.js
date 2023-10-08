efa671fbb0c391bb249feda6c8057c949cec45d0
Nothing <wwwaap@gmail.com>
2023-10-08 01:49:00 -0500

    Update 2896-apply-operations-to-make-two-strings-equal.js

--- 2896-apply-operations-to-make-two-strings-equal.js
+++ 2896-apply-operations-to-make-two-strings-equal.js
@@ -4,6 +4,41 @@
  * @param {number} x
  * @return {number}
  */
+var minOperations = function(s1, s2, x) {
+    const n = s1.length;
+    const idxs = [];
+    for (let i = 0; i < n; i++) {
+        if (s1[i] !== s2[i]) {
+            idxs.push(i);
+        }
+    }
+    const k = idxs.length;
+    if (k % 2) {
+        return -1;
+    }
+    const dp = new Array(k + 1).fill(Infinity);
+    dp[0] = 0;
+    for (let i = 0; i < k; i++) {
+        if (i % 2 === 0) {
+            dp[i + 1] = dp[i];
+        } else {
+            dp[i + 1] = dp[i] + x;
+        }
+        if (i) {
+            dp[i + 1] = Math.min(dp[i + 1], dp[i - 1] + idxs[i] - idxs[i - 1]);
+        }
+    }
+    return dp[k];
+};
+
+// another
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {number} x
+ * @return {number}
+ */
 const minOperations = function(s1, s2, x) {
     const diffs = [];
     for (let i = 0; i < s1.length; i++) {
