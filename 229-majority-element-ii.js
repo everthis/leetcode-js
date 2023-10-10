97f5d05cacdcfa03e328dd2453a9b0e49edb829a
Nothing <wwwaap@gmail.com>
2023-10-10 22:17:35 +0800

    Update 229-majority-element-ii.js

--- 229-majority-element-ii.js
+++ 229-majority-element-ii.js
@@ -3,6 +3,45 @@
  * @return {number[]}
  */
 const majorityElement = function(nums) {
+  let candidate1 = 0, candidate2 = 0, cnt1 = 0, cnt2 = 0
+  for(const e of nums) {
+    if(e === candidate1) {
+      cnt1++
+    } else if(e === candidate2) {
+      cnt2++
+    } else if(cnt1 === 0) {
+      candidate1 = e
+      cnt1++
+    } else if(cnt2 === 0) {
+      candidate2 = e
+      cnt2++
+    } else {
+      cnt1--
+      cnt2--
+    }
+  }
+  
+  const n = nums.length
+  let c1 = 0, c2 = 0
+  for(const e of nums) {
+    if(e === candidate1) c1++
+    if(e === candidate2) c2++
+  }
+  const k = Math.floor(n / 3)
+  const res = []
+  if(c1 > k) res.push(candidate1)
+  if(c2 > k) res.push(candidate2)
+  if(res[0] === res[1]) res.pop()
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const majorityElement = function(nums) {
   const res = []
   const hash = {}
   const len = nums.length
