80dd26077ce5ad4d2490209a20fea88f8ab79706
Nothing <wwwaap@gmail.com>
2023-10-12 22:45:14 -0500

    Update 1915-number-of-wonderful-substrings.js

--- 1915-number-of-wonderful-substrings.js
+++ 1915-number-of-wonderful-substrings.js
@@ -2,6 +2,33 @@
  * @param {string} word
  * @return {number}
  */
+const wonderfulSubstrings = function (word) {
+  const n = word.length,
+    a = 'a'.charCodeAt(0)
+  const map = new Map()
+  map.set(0, 1)
+  let res = 0
+  for (let i = 0, mask = 0; i < n; i++) {
+    const idx = word[i].charCodeAt(0) - a
+    mask ^= 1 << idx
+    res += (map.get(mask) || 0) 
+    for (let j = 0; j < 10; j++) {
+      const tmp = mask ^ (1 << j)
+      res += map.get(tmp) || 0
+    }
+
+    map.set(mask, (map.get(mask) || 0) + 1)
+  }
+  return res
+}
+
+// another
+
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
 const wonderfulSubstrings = (word) => {
   let res = 0, count = Array(1024).fill(0);
   let cur = 0;
