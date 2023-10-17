3a68a2f65921d16bb66329fd3b7f7a8e27c06a1d
Nothing <wwwaap@gmail.com>
2023-10-16 21:37:43 -0500

    Create 2845-count-of-interesting-subarrays.js

---
+++ 2845-count-of-interesting-subarrays.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} modulo
+ * @param {number} k
+ * @return {number}
+ */
+const countInterestingSubarrays = function (nums, modulo, k) {
+  const n = nums.length
+  const map = new Map()
+  let res = 0
+  map.set(0, 1)
+  for (let i = 0, cnt = 0; i < n; i++) {
+    const cur = nums[i]
+    if (cur % modulo === k) {
+      cnt++
+    }
+    const key = cnt % modulo
+    if(key >= k) res += map.get(key - k) || 0
+    else res += map.get(modulo - (k - key)) || 0
+
+    map.set(key, (map.get(key) || 0) + 1)
+  }
+
+  return res
+}
