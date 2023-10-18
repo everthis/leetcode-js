9cf4604195c638d0eada49ce82098ac525952c0d
Nothing <wwwaap@gmail.com>
2023-10-18 00:53:56 -0500

    Update 2875-minimum-size-subarray-in-infinite-array.js

--- 2875-minimum-size-subarray-in-infinite-array.js
+++ 2875-minimum-size-subarray-in-infinite-array.js
@@ -3,6 +3,33 @@
  * @param {number} target
  * @return {number}
  */
+const minSizeSubarray = function (nums, target) {
+  const sum = nums.reduce((ac, e) => ac + e, 0)
+  const remain = target % sum
+  if(remain === 0) return target / sum * nums.length
+  const arr = [...nums, ...nums]
+  const map = new Map()
+  const n = arr.length
+  let r = Infinity
+  for(let i = 0, cur = 0; i < n; i++) {
+    const e = arr[i]
+    cur += e
+    // const tmp = cur % target
+    if(map.has(cur - remain)) r = Math.min(r, i - map.get(cur-remain))
+    map.set(cur, i)
+  }
+  if(r === Infinity) return -1
+  return r + Math.floor(target / sum) * nums.length
+}
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var minSizeSubarray = function(nums, target) {
     let sum = 0, su = 0;
     for (const a of nums) sum += a;
