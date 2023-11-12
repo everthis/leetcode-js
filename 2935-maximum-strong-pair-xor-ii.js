220cc1bce3f3ca79f0235462efb1c7884881822d
Nothing <wwwaap@gmail.com>
2023-11-12 14:14:00 +0800

    Update 2935-maximum-strong-pair-xor-ii.js

--- 2935-maximum-strong-pair-xor-ii.js
+++ 2935-maximum-strong-pair-xor-ii.js
@@ -1,3 +1,96 @@
+class Node {
+  constructor() {
+    this.links = [null, null]
+    this.frequency = 0
+  }
+
+  containsKey(bit) {
+    return this.links[bit] !== null
+  }
+
+  get(bit) {
+    return this.links[bit]
+  }
+
+  put(bit, node) {
+    this.links[bit] = node
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node()
+  }
+
+  insert(num) {
+    let node = this.root
+    for (let i = 31; i >= 0; i--) {
+      const bit = (num >> i) & 1
+      if (!node.containsKey(bit)) {
+        node.put(bit, new Node())
+      }
+      node = node.get(bit)
+      node.frequency++
+    }
+  }
+
+  getMax(num) {
+    let node = this.root
+    let res = 0
+    for (let i = 31; i >= 0; i--) {
+      const bit = (num >> i) & 1
+      if (node.containsKey(1 - bit) && node.get(1 - bit).frequency > 0) {
+        res = res | (1 << i)
+        node = node.get(1 - bit)
+      } else {
+        if (node.containsKey(bit) && node.get(bit).frequency > 0) {
+          node = node.get(bit)
+        } else {
+          return 0
+        }
+      }
+    }
+    return res
+  }
+
+  deleteKey(num) {
+    let node = this.root
+    for (let i = 31; i >= 0; i--) {
+      const bit = (num >> i) & 1
+      if (node.containsKey(bit)) {
+        node = node.get(bit)
+        node.frequency--
+      } else {
+        break
+      }
+    }
+  }
+}
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maximumStrongPairXor(nums) {
+  const n = nums.length
+  nums.sort((a, b) => a - b)
+  let maxi = 0
+  let j = 0
+  const t = new Trie()
+  t.insert(nums[0])
+  for (let i = 1; i < n; i++) {
+    while (j < i && nums[i] - nums[j] > Math.min(nums[i], nums[j])) {
+      t.deleteKey(nums[j])
+      j++
+    }
+    maxi = Math.max(maxi, t.getMax(nums[i]))
+    t.insert(nums[i])
+  }
+  return maxi
+}
+
+// another
+
+
 const maximumStrongPairXor= (nums) => {
   const A = nums
   let res = 0;
