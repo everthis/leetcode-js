06283d6218a2733cc7bf8d4d479b75319b58da81
Nothing <wwwaap@gmail.com>
2023-11-13 15:16:11 +0800

    Update 2313-minimum-flips-in-binary-tree-to-get-result.js

--- 2313-minimum-flips-in-binary-tree-to-get-result.js
+++ 2313-minimum-flips-in-binary-tree-to-get-result.js
@@ -11,6 +11,68 @@
  * @param {boolean} result
  * @return {number}
  */
+const minimumFlips = function(root, result) {
+  const dp = new Map()
+  return dfs(root, result)
+  
+  function dfs(node, expected) {
+    if(node.val === 0 || node.val === 1) {
+      return (!!node.val) === !!expected ? 0 : 1
+    }
+    if(dp.has(node) && dp.get(node).has(expected)) {
+      return dp.get(node).get(expected)
+    }
+    let res = Infinity
+    
+    if(node.val === 2) {
+      if(expected) {
+        res = Math.min(dfs(node.left, 1), dfs(node.right, 1))
+      } else {
+        res = dfs(node.left, 0) + dfs(node.right, 0)
+      }
+    } else if(node.val === 3) {
+      if(expected) {
+        res = dfs(node.left, 1) + dfs(node.right, 1)
+      } else {
+        res = Math.min(dfs(node.left, 0), dfs(node.right, 0))
+      }
+    } else if(node.val === 4) {
+      if(expected) {
+        res = Math.min(dfs(node.left, 0) + dfs(node.right, 1), dfs(node.left, 1) + dfs(node.right, 0))
+      } else {
+        res = Math.min(dfs(node.left, 1) + dfs(node.right, 1), dfs(node.left, 0) + dfs(node.right, 0))
+      }
+      
+    } else if(node.val === 5) {
+      const child = node.left || node.right
+      if(expected) {
+        res = dfs(child, 0)
+      } else {
+        res = dfs(child, 1)
+      }
+    }
+    
+    if(!dp.has(node)) dp.set(node, new Map())
+    dp.get(node).set(expected, res)
+    return res
+  }
+};
+
+// another
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {boolean} result
+ * @return {number}
+ */
 const minimumFlips = function (root, result) {
   const [FALSE, TRUE, OR, AND, XOR, NOT] = [0, 1, 2, 3, 4, 5]
   const costs = new Map()
