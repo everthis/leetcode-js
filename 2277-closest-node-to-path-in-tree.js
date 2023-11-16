2c60bd14698e0bfd6980831b53af13942a89df92
Nothing <wwwaap@gmail.com>
2023-11-16 14:13:42 +0800

    Update 2277-closest-node-to-path-in-tree.js

--- 2277-closest-node-to-path-in-tree.js
+++ 2277-closest-node-to-path-in-tree.js
@@ -4,6 +4,57 @@
  * @param {number[][]} query
  * @return {number[]}
  */
+const closestNode = function(n, edges, query) {
+  const g = new Map()
+  for(const [p, q] of edges) {
+    if(!g.has(p)) g.set(p, new Set())
+    if(!g.has(q)) g.set(q, new Set())
+    g.get(p).add(q)
+    g.get(q).add(p)
+  }
+  const dist = Array.from({ length: n }, () => Array(n).fill(null))
+  for(let i = 0; i < n; i++) dfs(i, i, 0)
+  const res = []
+  // console.log(dist)
+  for(const [s, e, t] of query) {
+    let tmp = Infinity, cur = s, el
+    while(true) {
+      if(dist[cur][t] < tmp) {
+          tmp = dist[cur][t]
+          el = cur
+      }
+      if(cur === e) break
+      for(const nxt of (g.get(cur) || [])) {
+        if(dist[cur][e] === dist[nxt][e] + 1) {
+          cur = nxt
+          break
+        }
+      }
+    }
+    res.push(el)
+  }
+    
+  return res
+  
+  function dfs(root, node, d) {
+    dist[root][node] = d
+    for(const nxt of (g.get(node) || [])) {
+      if(nxt !== root && dist[root][nxt] == null) {
+          dfs(root, nxt, d + 1)
+      }
+    }
+  }
+};
+
+// another
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[][]} query
+ * @return {number[]}
+ */
 const closestNode = function (n, edges, query) {
   const graph = {}
   for (const [a, b] of edges) {
