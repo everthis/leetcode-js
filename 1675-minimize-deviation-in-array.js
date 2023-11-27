31587c5e9d9924e53ae782a4d3e49c007c51e89c
Nothing <wwwaap@gmail.com>
2023-11-27 12:45:25 +0800

    Update 1675-minimize-deviation-in-array.js

--- 1675-minimize-deviation-in-array.js
+++ 1675-minimize-deviation-in-array.js
@@ -1,3 +1,97 @@
+class PQ {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minimumDeviation = function(nums) {
+  const pq = new PQ((a, b) => a > b)
+  let min = Infinity
+  for(const e of nums) {
+      const tmp = e % 2 === 1 ?  e * 2: e
+      pq.push(tmp)
+      min = Math.min(min, tmp)
+  }
+  let res = Infinity
+  while(pq.peek() % 2 === 0) {
+     const e = pq.pop()
+     res = Math.min(res, e - min)
+     min = Math.min(min, e / 2)
+     pq.push(e / 2)
+  }
+  if(pq.size()) {
+      res = Math.min(res, pq.peek() - min)
+  }
+  return res
+};
+
+// another
+
 /**
  * @param {number[]} nums
  * @return {number}
