3eca4642129713961bb27b1962b37018c4b88139
Nothing <wwwaap@gmail.com>
2023-12-03 20:55:07 +0800

    Update 330-patching-array.js

--- 330-patching-array.js
+++ 330-patching-array.js
@@ -4,6 +4,29 @@
  * @return {number}
  */
 const minPatches = function(nums, n) {
+  const len = nums.length
+  let miss = 1, res = 0, i = 0
+  while(miss <= n) {
+    if(i < len && nums[i] <= miss) {
+      miss += nums[i]
+      i++
+    } else {
+      res++
+      miss += miss
+    }
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+const minPatches = function(nums, n) {
   let answer = 0
   for (let i = 0, next = 1; next <= n; ) {
     if (i >= nums.length || nums[i] > next) {
