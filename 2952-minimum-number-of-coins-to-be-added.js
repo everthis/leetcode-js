097b9094310330f4a5e1f507957a9888f93af153
Nothing <wwwaap@gmail.com>
2023-12-03 20:26:33 +0800

    Update 2952-minimum-number-of-coins-to-be-added.js

--- 2952-minimum-number-of-coins-to-be-added.js
+++ 2952-minimum-number-of-coins-to-be-added.js
@@ -5,6 +5,33 @@
  */
 var minimumAddedCoins = function (coins, target) {
   coins.sort((a, b) => a - b)
+  let current_max = 0;
+  let additions = 0;
+  let index = 0;
+
+  while (current_max < target) {
+      if (index < coins.length && coins[index] <= current_max + 1) {
+          current_max += coins[index];
+          index++;
+      } else {
+          current_max += current_max + 1;
+          additions++;
+      }
+  }
+
+  return additions;
+}
+
+
+// another
+
+/**
+ * @param {number[]} coins
+ * @param {number} target
+ * @return {number}
+ */
+var minimumAddedCoins = function (coins, target) {
+  coins.sort((a, b) => a - b)
   let currin = 1
   let res = 0
 
