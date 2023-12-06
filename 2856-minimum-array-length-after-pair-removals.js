d2705d261687f489358b61c87e44060aecc4df68
Nothing <wwwaap@gmail.com>
2023-12-06 12:40:29 +0800

    Update 2856-minimum-array-length-after-pair-removals.js

--- 2856-minimum-array-length-after-pair-removals.js
+++ 2856-minimum-array-length-after-pair-removals.js
@@ -3,6 +3,27 @@
  * @return {number}
  */
 const minLengthAfterRemovals = function(nums) {
+  const n = nums.length, rightStart = Math.floor((n + 1) / 2)
+  let i = 0, j = rightStart
+  let num = 0
+  while(i < rightStart && j < n) {
+    if(nums[i] < nums[j]) {
+      num += 2
+      i++
+      j++
+    } else j++
+  }
+
+  return n - num
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const minLengthAfterRemovals = function(nums) {
   const map = new Map(), n = nums.length
   for(const e of nums) {
     if(!map.has(e)) map.set(e, 0)
