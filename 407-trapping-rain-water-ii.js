cf27a137173719d67a9d1a636adf96c268aaeb6f
Nothing <wwwaap@gmail.com>
2023-12-25 11:00:48 +0800

    Update 407-trapping-rain-water-ii.js

--- 407-trapping-rain-water-ii.js
+++ 407-trapping-rain-water-ii.js
@@ -1,3 +1,107 @@
+class PQ {
+  constructor(comparator = (a, b) => a > b) {
+    this.heap = []
+    this.top = 0
+    this.comparator = comparator
+  }
+  size() {
+    return this.heap.length
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    return this.heap[this.top]
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this.heap.push(value)
+      this.siftUp()
+    })
+    return this.size()
+  }
+  pop() {
+    const poppedValue = this.peek()
+    const bottom = this.size() - 1
+    if (bottom > this.top) {
+      this.swap(this.top, bottom)
+    }
+    this.heap.pop()
+    this.siftDown()
+    return poppedValue
+  }
+  replace(value) {
+    const replacedValue = this.peek()
+    this.heap[this.top] = value
+    this.siftDown()
+    return replacedValue
+  }
+
+  parent = (i) => ((i + 1) >>> 1) - 1
+  left = (i) => (i << 1) + 1
+  right = (i) => (i + 1) << 1
+  greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+  swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+  siftUp = () => {
+    let node = this.size() - 1
+    while (node > this.top && this.greater(node, this.parent(node))) {
+      this.swap(node, this.parent(node))
+      node = this.parent(node)
+    }
+  }
+  siftDown = () => {
+    let node = this.top
+    while (
+      (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+      (this.right(node) < this.size() && this.greater(this.right(node), node))
+    ) {
+      let maxChild =
+        this.right(node) < this.size() &&
+        this.greater(this.right(node), this.left(node))
+          ? this.right(node)
+          : this.left(node)
+      this.swap(node, maxChild)
+      node = maxChild
+    }
+  }
+}
+/**
+ * @param {number[][]} heightMap
+ * @return {number}
+ */
+const trapRainWater = function(heightMap) {
+  const pq = new PQ((a, b) => a[1] < b[1])
+  const m = heightMap.length, n = heightMap[0].length
+  const visited = Array.from({ length: m }, () => Array(n).fill(false))
+  for(let i = 0; i < m; i++) {
+    visited[i][0] = visited[i][n - 1] = true
+    pq.push([[i, 0], heightMap[i][0]])
+    pq.push([[i, n - 1], heightMap[i][n - 1]])
+  }
+  for(let j = 0; j < n; j++) {
+    visited[0][j] = visited[m - 1][j] = true
+    pq.push([[0, j], heightMap[0][j]])
+    pq.push([[m - 1, j], heightMap[m - 1][j]])
+  }
+  const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+  let res = 0
+  while(!pq.isEmpty()) {
+    const [coord, h] = pq.pop()
+    const [r, c] = coord
+    for(const [dr, dc] of dirs) {
+      const nr = r + dr, nc = c + dc
+      if(nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+        visited[nr][nc] = true
+        if (h > heightMap[nr][nc]) res += h - heightMap[nr][nc]
+        pq.push([[nr, nc], Math.max(heightMap[nr][nc], h)])
+      }
+    }
+  }/*  */
+  return res
+};
+
+// another
+
 /**
  * @param {number[][]} heightMap
  * @return {number}
