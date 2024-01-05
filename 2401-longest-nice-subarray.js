3d7932c94efa7254ba85144ba0b337b8daa3d180
Nothing <wwwaap@gmail.com>
2024-01-05 12:52:59 +0800

    Update 2401-longest-nice-subarray.js

--- 2401-longest-nice-subarray.js
+++ 2401-longest-nice-subarray.js
@@ -3,6 +3,27 @@
  * @return {number}
  */
 const longestNiceSubarray = function (nums) {
+  const n = nums.length
+  let i = 0, mask = 0, res = 0
+  for(let j = 0; j < n; j++) {
+    const e = nums[j]
+    while((mask & e) !== 0) {
+      mask ^= nums[i]
+      i++
+    }
+    mask |= e
+    res = Math.max(res, j - i + 1)
+  }
+  return res
+}
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestNiceSubarray = function (nums) {
   let res = 1, i = 0, j = 0, mask = 0
   const n = nums.length
   for(i = 0; i < n; i++) {
