4cfe2550d381efe8c41501f6d224b6234191cad0
Nothing <wwwaap@gmail.com>
2024-01-10 11:53:37 +0800

    Update 2516-take-k-of-each-character-from-left-and-right.js

--- 2516-take-k-of-each-character-from-left-and-right.js
+++ 2516-take-k-of-each-character-from-left-and-right.js
@@ -4,6 +4,48 @@
  * @return {number}
  */
 const takeCharacters = function(s, k) {
+   const n = s.length
+   const cnt = Array(3).fill(0)
+   const a = 'a'.charCodeAt(0)
+   for(const ch of s) {
+     cnt[ch.charCodeAt(0) - a]++
+   }
+   const target = Array(3).fill(0)
+   for(let i = 0; i < 3; i++) {
+     target[i] = cnt[i] - k
+   }
+   for(let e of target) {
+     if(e < 0) return -1
+   }
+   const arr = Array(3).fill(0)
+   let res = 0
+   let i = 0
+   for(let j = 0; j < n; j++) {
+     const idx = s[j].charCodeAt(0) - a
+     arr[idx]++
+     while(!valid()) {
+       const ii = s[i].charCodeAt(0) - a
+       arr[ii]--
+       i++
+     }
+     res = Math.max(res, j - i + 1)
+   }
+
+   return n - res
+
+   function valid() {
+     return arr.every((e, i) => e <= target[i])
+   }
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+const takeCharacters = function(s, k) {
   const cnt = {a: 0, b: 0, c: 0}
   const n = s.length
   for(const ch of s) {
