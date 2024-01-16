338cd2ca135327306b940081b3d4e3d2f7e952d0
Nothing <wwwaap@gmail.com>
2024-01-16 19:18:55 +0800

    Update 330-patching-array.js

--- 330-patching-array.js
+++ 330-patching-array.js
@@ -4,6 +4,28 @@
  * @return {number}
  */
 const minPatches = function(nums, n) {
+  let sum = 1, res = 0, i = 0
+  while(sum <= n) {
+    if(i < nums.length && nums[i] <= sum) {
+      sum += nums[i]
+      i++
+    } else {
+      res++
+      sum *= 2
+    }
+  }
+
+  return res 
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+const minPatches = function(nums, n) {
   const len = nums.length
   let miss = 1, res = 0, i = 0
   while(miss <= n) {
