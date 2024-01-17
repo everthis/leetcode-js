62a6e737a706754c0591deaccc74c017923c433f
Nothing <wwwaap@gmail.com>
2024-01-17 12:58:17 +0800

    Update 1798-maximum-number-of-consecutive-values-you-can-make.js

--- 1798-maximum-number-of-consecutive-values-you-can-make.js
+++ 1798-maximum-number-of-consecutive-values-you-can-make.js
@@ -11,3 +11,26 @@ const getMaximumConsecutive = function(coins) {
   }
   return res; 
 };
+
+// another
+
+/**
+ * @param {number[]} coins
+ * @return {number}
+ */
+const getMaximumConsecutive = function(coins) {
+  coins.sort((a, b) => a - b)
+  let sum = 1, res = 1, i = 0
+  while(true) {
+    const e = coins[i]
+    if(i >= coins.length) break
+    if(e <= sum) {
+      sum += e
+      i++
+    } else {
+      break
+    }
+  }
+
+  return sum
+};
