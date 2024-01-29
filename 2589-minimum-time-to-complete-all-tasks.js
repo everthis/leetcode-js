6fbb9a2697cdccecdd668e51e88a49dc66d43a00
Nothing <wwwaap@gmail.com>
2024-01-29 19:57:21 +0800

    Update 2589-minimum-time-to-complete-all-tasks.js

--- 2589-minimum-time-to-complete-all-tasks.js
+++ 2589-minimum-time-to-complete-all-tasks.js
@@ -3,6 +3,41 @@
  * @return {number}
  */
 const findMinimumTime = function(tasks) {
+  tasks.sort((a, b) => a[1] - b[1])
+  const {max} = Math
+  let maxEnd = -1
+  for(const [s,e,d] of tasks) maxEnd = max(maxEnd, e)
+  const arr = Array(maxEnd + 1).fill(0)
+  const n = tasks.length
+
+  for(let i = 0; i < n; i++) {
+    let [s, e, d] = tasks[i]
+    let overlap = 0
+    for(let j = e; j >= s; j--) {
+      if(arr[j]) overlap++
+    }
+    if(overlap >= d) continue
+    let diff = d - overlap
+    for(let j = e; j >= s; j--) {
+      if(arr[j] === 0) {
+        diff--
+        arr[j] = 1
+      } else continue
+      if(diff === 0) break
+    }
+
+  }
+
+  return arr.reduce((ac, e) => ac + e, 0)
+};
+
+// another
+
+/**
+ * @param {number[][]} tasks
+ * @return {number}
+ */
+const findMinimumTime = function(tasks) {
   let maxEnd = -Infinity
   for(const [s,e,d] of tasks) {
     maxEnd = Math.max(maxEnd, e)
