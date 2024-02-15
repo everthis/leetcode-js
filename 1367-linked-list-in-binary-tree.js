f16a5720a6ee8c1866ca37b1bcd2c5fc30048250
Nothing <wwwaap@gmail.com>
2024-02-15 16:44:14 +0800

    Update 1367-linked-list-in-binary-tree.js

--- 1367-linked-list-in-binary-tree.js
+++ 1367-linked-list-in-binary-tree.js
@@ -19,6 +19,60 @@
  * @return {boolean}
  */
 const isSubPath = function(head, root) {
+  return dfs(root)
+  
+  function dfs(node) {
+    if(node == null) return false
+    if(head.val === node.val) {
+      let cur = head
+      let q = [node]
+      while(q.length) {
+        const v = cur.val
+        const tmp = []
+        let mark = false
+        for(const e of q) {
+          if(e.val === v) {
+            mark = true
+            if(e.left) tmp.push(e.left)
+            if(e.right) tmp.push(e.right)
+          }
+        }
+        if(cur && !mark) break
+        cur = cur.next
+        if(cur == null) return true
+        q = tmp
+      }
+    }
+    return dfs(node.left) || dfs(node.right)
+  }
+  
+};
+
+
+// another
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSubPath = function(head, root) {
   const res = { found: false }
   traverse(root, head, res)
   return res.found
