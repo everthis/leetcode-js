2adc62ba1d11a7e1c1febb12e08f1f1c5c08449c
Nothing <wwwaap@gmail.com>
2024-02-16 14:36:06 +0800

    Update 1764-form-array-by-concatenating-subarrays-of-another-array.js

--- 1764-form-array-by-concatenating-subarrays-of-another-array.js
+++ 1764-form-array-by-concatenating-subarrays-of-another-array.js
@@ -4,6 +4,62 @@
  * @return {boolean}
  */
 const canChoose = function (groups, nums) {
+  const dp = new Array(1000).fill(0)
+  const lsps = preprocess(groups)
+  let cur = 0
+  for (let i = 0; i < groups.length; i++) {
+    if (cur >= nums.length) return false
+    cur = find(nums, cur, groups[i], lsps[i])
+    if (cur === -1) return false
+    cur += groups[i].length
+  }
+  return true
+  function find(nums, cur, p, lsp) {
+    const n = nums.length
+    dp[cur] = p[0] === nums[cur] ? 1 : 0
+    if (lsp.length === 1 && dp[cur] === 1) {
+      return cur
+    }
+    for (let i = cur + 1; i < n; i++) {
+      let j = dp[i - 1]
+      while (j > 0 && p[j] !== nums[i]) {
+        j = lsp[j - 1]
+      }
+      dp[i] = j + (p[j] === nums[i])
+      if (dp[i] === p.length) {
+        return i - p.length + 1
+      }
+    }
+    return -1
+  }
+
+  function preprocess(groups) {
+    const rets = []
+    for (let g of groups) {
+      const n = g.length
+      const dp = new Array(n)
+      dp[0] = 0
+      for (let i = 1; i < n; i++) {
+        let j = dp[i - 1]
+        while (j > 0 && g[j] !== g[i]) {
+          j = dp[j - 1]
+        }
+        dp[i] = j + (g[j] === g[i] ? 1 : 0)
+      }
+      rets.push(dp)
+    }
+    return rets
+  }
+}
+
+// another
+
+/**
+ * @param {number[][]} groups
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canChoose = function (groups, nums) {
   const m = nums.length
   let index = 0
   for (let group of groups) {
