ab956d281c390762fe7687a513801061bc2fc40a
Nothing <wwwaap@gmail.com>
2024-02-19 18:00:33 +0800

    Update 3008-find-beautiful-indices-in-the-given-array-ii.js

--- 3008-find-beautiful-indices-in-the-given-array-ii.js
+++ 3008-find-beautiful-indices-in-the-given-array-ii.js
@@ -6,6 +6,88 @@
  * @return {number[]}
  */
 var beautifulIndices = function (s, a, b, k) {
+  const is = kmp(s, a)
+  if (is.length === 0) return []
+  
+  const js = kmp(s, b)
+  if (js.length === 0) return []
+
+  const answer = []
+  let p = 0
+  let q = 0
+
+  while (p < is.length && q < js.length) {
+    const distance = Math.abs(is[p] - js[q])
+
+    if (distance <= k) {
+      answer.push(is[p])
+      p++
+    } else if (is[p] < js[q]) {
+      p++
+    } else {
+      q++
+    }
+  }
+
+  return answer
+}
+
+function kmp(str1, str2) {
+  const pattern = buildPattern(str2)
+
+  const answer = []
+  let i = 0
+  let j = 0
+
+  while (i < str1.length) {
+    if (str1[i] === str2[j]) {
+      i++
+      j++
+
+      if (j === str2.length) {
+        answer.push(i - str2.length)
+        j = pattern[j - 1] + 1
+      }
+    } else if (j > 0) {
+      j = pattern[j - 1] + 1
+    } else {
+      i++
+    }
+  }
+
+  return answer
+}
+
+function buildPattern(str) {
+  const pattern = new Array(str.length).fill(-1)
+  let i = 1
+  let j = 0
+
+  while (i < str.length) {
+    if (str[i] === str[j]) {
+      pattern[i] = j
+      i++
+      j++
+    } else if (j > 0) {
+      j = pattern[j - 1] + 1
+    } else {
+      i++
+    }
+  }
+
+  return pattern
+}
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string} a
+ * @param {string} b
+ * @param {number} k
+ * @return {number[]}
+ */
+var beautifulIndices = function (s, a, b, k) {
   let res = []
   let v1 = []
   let v2 = []
