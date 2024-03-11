6bbcdbf2109d87150a36e3ffc62828e577bd703a
Nothing <wwwaap@gmail.com>
2024-03-11 12:34:04 +0800

    Update 3074-apple-redistribution-into-boxes.js

--- 3074-apple-redistribution-into-boxes.js
+++ 3074-apple-redistribution-into-boxes.js
@@ -4,15 +4,15 @@
  * @return {number}
  */
 var minimumBoxes = function(apple, capacity) {
-let total_apple = apple.reduce((a, b) => a + b, 0)
-capacity.sort((a, b) => b - a)
-let ans = 0
-let cur = 0
-for (let c of capacity) {
-  cur += c
-  ans += 1
-  if (cur >= total_apple) {
-    return ans
+  const sum = apple.reduce((ac, e) => ac+ e, 0)
+  capacity.sort((a, b) => b - a)
+  let res = 0
+  let remain = sum, i = 0
+  while(remain > 0) {
+      res++
+      remain -= capacity[i]
+      i++
   }
-}
+  
+  return res
 };
