d380d4c03402c6ef32fee859a20717d537061bb9
Nothing <wwwaap@gmail.com>
2024-03-19 11:11:55 +0800

    Update 11-container-with-most-water.js

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -12,3 +12,27 @@ const maxArea = function(height) {
   }
   return res
 };
+
+// another
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function(height) {
+  const n = height.length, {min,max} = Math
+  let i = 0, j = n - 1, leftMax = height[i], rightMax = height[j]
+  let res = 0
+  while(i < j) {
+    res = max(res, (j - i) * min(leftMax, rightMax))
+    if(leftMax <= rightMax) {
+      i++
+      leftMax = max(leftMax, height[i])
+    } else {
+      j--
+      rightMax = max(rightMax, height[j])
+    }
+  }
+  
+  return res
+};
