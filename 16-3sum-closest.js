1f9f70fdc9194a8f3c472d061d8ccde0c426b4d1
Nothing <wwwaap@gmail.com>
2024-03-19 14:02:11 +0800

    Update 16-3sum-closest.js

--- 16-3sum-closest.js
+++ 16-3sum-closest.js
@@ -4,6 +4,36 @@
  * @return {number}
  */
 const threeSumClosest = function(nums, target) {
+  let n = nums.length, {abs, min, max} = Math
+  let res = nums[0] + nums[1] + nums[2]
+  nums.sort((a, b) => a - b)
+  for(let i = 0; i < n - 2; i++) {
+    let e = nums[i]
+    let l = i + 1, r = n - 1
+    if(i > 0 && nums[i] === nums[i - 1]) continue
+    while(l < r) {
+        const tmp = e + nums[l] + nums[r]
+        if(abs(tmp - target) < abs(res - target)) {
+           res = tmp
+        }
+        if(tmp > target) r--
+        else if (tmp < target) l++
+        else return tmp
+    }
+  }
+  
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const threeSumClosest = function(nums, target) {
   const nums = nums.sort((a, b) => a - b);
   let result;
   let lo;
