a3a343cb39b561ff7c247073bed4a3373e75f830
Nothing <wwwaap@gmail.com>
2024-04-08 13:36:57 +0800

    Update 2448-minimum-cost-to-make-array-equal.js

--- 2448-minimum-cost-to-make-array-equal.js
+++ 2448-minimum-cost-to-make-array-equal.js
@@ -29,3 +29,44 @@ const minCost = function (nums, cost) {
     return res
   }
 }
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} cost
+ * @return {number}
+ */
+const minCost = function(nums, cost) {
+  const n = nums.length
+  const {min, max, abs, floor} = Math
+  let l = Infinity, r = -Infinity
+  
+  for(const e of nums) {
+    l = min(e, l)
+    r = max(e, r)
+  }
+  let res = calcCost(l)
+  while(l < r) {
+    const mid = floor((l + r) / 2)
+    const v1 = calcCost(mid)
+    const v2 = calcCost(mid + 1)
+    res = min(res, v1, v2)
+    if(v1 < v2) {
+      r = mid
+    } else {
+      l = mid + 1
+    }
+  }
+  
+  return res
+  
+   
+  function calcCost(x) {
+    let res = 0
+    for(let i = 0; i < n; i++) {
+      res += abs(nums[i] - x) * cost[i]
+    }
+    return res
+  }
+};
