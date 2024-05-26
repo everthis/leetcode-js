e5d46707d4865330dd47121bb76ef39b533e1f42
Nothing <wwwaap@gmail.com>
2024-05-26 14:35:59 +0800

    Create 3162-find-the-number-of-good-pairs-i.js

---
+++ 3162-find-the-number-of-good-pairs-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfPairs = function (nums1, nums2, k) {
+  let res = 0
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] % (nums2[j] * k) === 0) {
+        res++
+      }
+    }
+  }
+  return res
+}
