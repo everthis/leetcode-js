a451c7d9de94c3db9cdcf9ecbfe7cedbb10c2d86
Nothing <wwwaap@gmail.com>
2024-05-27 12:14:38 +0800

    Update 992-subarrays-with-k-different-integers.js

--- 992-subarrays-with-k-different-integers.js
+++ 992-subarrays-with-k-different-integers.js
@@ -5,6 +5,36 @@
  */
 const subarraysWithKDistinct = function(nums, k) {
   return mostK(k) - mostK(k - 1)
+  
+  function mostK(k) {
+    const map = new Map(), n = nums.length
+    let i = 0, j = 0, res = 0
+    for(; j < n; j++) {
+      const e = nums[j]
+      map.set(e, (map.get(e) || 0) + 1)
+      while(map.size > k) {
+        const tmp = nums[i]
+        map.set(tmp, map.get(tmp) - 1)
+        if(map.get(tmp) === 0) map.delete(tmp)
+        i++
+      }
+      res += j - i + 1
+    }
+    
+    
+    return res
+  }
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const subarraysWithKDistinct = function(nums, k) {
+  return mostK(k) - mostK(k - 1)
   function mostK(limit) {
     const map = new Map()
     let i = 0, j = 0, res = 0
