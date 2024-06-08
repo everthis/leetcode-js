0803af6edb6f8b2a5967f26c211ea957ad27aed0
Nothing <wwwaap@gmail.com>
2024-06-08 18:36:04 +0800

    Update 337-house-robber-iii.js

--- 337-house-robber-iii.js
+++ 337-house-robber-iii.js
@@ -1,5 +1,31 @@
 /**
  * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const rob = function(root) {
+  if(root == null) return 0
+  return Math.max(...dfs(root))
+  
+  function dfs(node) {
+    if(node == null) return [0, 0]
+    const l = dfs(node.left), r = dfs(node.right)
+    return [node.val + l[1] + r[1], Math.max(...l) + Math.max(...r)]
+  }
+};
+
+// another
+
+
+/**
+ * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
  *     this.left = this.right = null;
