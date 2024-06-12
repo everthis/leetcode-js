54699c69f6926668f1976a11b8abf68e81575921
Nothing <wwwaap@gmail.com>
2024-06-12 13:07:17 +0800

    Update 3165-maximum-sum-of-subsequence-with-non-adjacent-elements.js

--- 3165-maximum-sum-of-subsequence-with-non-adjacent-elements.js
+++ 3165-maximum-sum-of-subsequence-with-non-adjacent-elements.js
@@ -1,3 +1,96 @@
+const M = 1e9 + 7
+/**
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var maximumSumSubsequence = function (nums, queries) {
+  const n = nums.length
+  const root = new SegTreeNode(0, n - 1, nums)
+  let res = 0
+  for (const q of queries) {
+    root.updateRange(q[0], q[1])
+    res += Math.max(root.info00, root.info01, root.info10, root.info11)
+    res %= M
+  }
+  return res
+}
+
+class SegTreeNode {
+  constructor(a, b, vals) {
+    this.left = null
+    this.right = null
+    this.start = a
+    this.end = b
+    if (a === b) {
+      this.info11 = vals[a]
+      this.info01 = -1e18
+      this.info10 = -1e18
+      this.info00 = 0
+      return
+    }
+    const mid = Math.floor((a + b) / 2)
+    this.left = new SegTreeNode(a, mid, vals)
+    this.right = new SegTreeNode(mid + 1, b, vals)
+    this.info11 = Math.max(
+      this.left.info10 + this.right.info01,
+      this.left.info11 + this.right.info01,
+      this.left.info10 + this.right.info11,
+    )
+    this.info00 = Math.max(
+      this.left.info00 + this.right.info00,
+      this.left.info01 + this.right.info00,
+      this.left.info00 + this.right.info10,
+    )
+    this.info10 = Math.max(
+      this.left.info10 + this.right.info00,
+      this.left.info10 + this.right.info10,
+      this.left.info11 + this.right.info00,
+    )
+    this.info01 = Math.max(
+      this.left.info00 + this.right.info01,
+      this.left.info01 + this.right.info01,
+      this.left.info00 + this.right.info11,
+    )
+  }
+
+  updateRange(a, val) {
+    if (a < this.start || a > this.end) {
+      return
+    }
+    if (this.start === this.end) {
+      this.info00 = 0
+      this.info11 = val
+      return
+    }
+    this.left.updateRange(a, val)
+    this.right.updateRange(a, val)
+    this.info11 = Math.max(
+      this.left.info10 + this.right.info01,
+      this.left.info11 + this.right.info01,
+      this.left.info10 + this.right.info11,
+    )
+    this.info00 = Math.max(
+      this.left.info00 + this.right.info00,
+      this.left.info01 + this.right.info00,
+      this.left.info00 + this.right.info10,
+    )
+    this.info10 = Math.max(
+      this.left.info10 + this.right.info00,
+      this.left.info10 + this.right.info10,
+      this.left.info11 + this.right.info00,
+    )
+    this.info01 = Math.max(
+      this.left.info00 + this.right.info01,
+      this.left.info01 + this.right.info01,
+      this.left.info00 + this.right.info11,
+    )
+  }
+}
+
+// another
+
+
 /**
  * @param {number[]} nums
  * @param {number[][]} queries
