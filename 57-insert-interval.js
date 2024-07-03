ffde7b2501073aed618953c56c2fbaa5cee1ae77
Nothing <wwwaap@gmail.com>
2024-07-03 12:04:40 +0800

    Update 57-insert-interval.js

--- 57-insert-interval.js
+++ 57-insert-interval.js
@@ -4,6 +4,33 @@
  * @return {number[][]}
  */
 const insert = function(intervals, newInterval) {
+  const res = [], n = intervals.length
+  let i = 0
+  while(i < n && intervals[i][1] < newInterval[0]) {
+    res.push(intervals[i])
+    i++
+  }
+  while(i < n && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0])
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1])
+    i++
+  }
+  res.push(newInterval)
+  while(i < n) {
+    res.push(intervals[i])
+    i++
+  }
+
+  return res
+};
+
+// another
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+const insert = function(intervals, newInterval) {
   let i = 0
   while (i < intervals.length && intervals[i][1] < newInterval[0]) i++
   while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
