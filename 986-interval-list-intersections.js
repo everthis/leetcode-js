b67f5fe68b04d26c5dbb37e6fd1dc307c6707de1
Nothing <wwwaap@gmail.com>
2024-07-04 12:00:02 +0800

    Update 986-interval-list-intersections.js

--- 986-interval-list-intersections.js
+++ 986-interval-list-intersections.js
@@ -21,3 +21,29 @@ const intervalIntersection = function (A, B) {
   }
   return intersection
 }
+
+// another
+
+/**
+ * @param {number[][]} firstList
+ * @param {number[][]} secondList
+ * @return {number[][]}
+ */
+const intervalIntersection = function(firstList, secondList) {
+    const res = [];
+    let i = 0;
+    let j = 0;
+    while (i < firstList.length && j < secondList.length) {
+        const [start1, end1] = firstList[i];
+        const [start2, end2] = secondList[j];
+        if (start1 <= end2 && start2 <= end1) {
+            res.push([Math.max(start1, start2), Math.min(end1, end2)]);
+        }
+        if (end1 < end2) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    return res;
+};
