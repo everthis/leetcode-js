28fed09b4d29d4b5f5a21e1bb67a9cc6c0f3b9bf
Nothing <wwwaap@gmail.com>
2024-07-05 17:11:29 +0800

    Update 1589-maximum-sum-obtained-of-any-permutation.js

--- 1589-maximum-sum-obtained-of-any-permutation.js
+++ 1589-maximum-sum-obtained-of-any-permutation.js
@@ -3,6 +3,36 @@
  * @param {number[][]} requests
  * @return {number}
  */
+const maxSumRangeQuery = function(nums, requests) {
+  nums.sort((a, b) => b - a)
+  const n = nums.length
+  const arr = Array(n).fill(0)
+  for(const [s, e] of requests) {
+    arr[s] += 1
+    if(e + 1 < n) arr[e + 1] -= 1
+  }
+  for(let i = 1; i < n; i++) {
+    arr[i] += arr[i - 1]
+  }
+  arr.sort((a, b) => b - a)
+  
+  let res = 0
+  const mod = 1e9 + 7
+  
+  for(let i = 0; i < n; i++) {
+    res = (res + nums[i] * arr[i]) % mod
+  }
+  
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number[][]} requests
+ * @return {number}
+ */
 const maxSumRangeQuery = function (nums, requests) {
   let res = 0
   const mod = 10 ** 9 + 7,
