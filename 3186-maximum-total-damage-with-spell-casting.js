c4119a5b20dd4c082f1be71a75114f13e72a22e1
Nothing <wwwaap@gmail.com>
2024-07-19 11:10:20 +0800

    Update 3186-maximum-total-damage-with-spell-casting.js

--- 3186-maximum-total-damage-with-spell-casting.js
+++ 3186-maximum-total-damage-with-spell-casting.js
@@ -2,6 +2,44 @@
  * @param {number[]} power
  * @return {number}
  */
+const maximumTotalDamage = function(power) {
+    const freq = new Map()
+    for (const p of power) {
+        freq.set(p, (freq.get(p) || 0) + 1)
+    }
+    const sorted = Array.from(freq.keys()).sort((a, b) => a - b)
+    const n = sorted.length
+    const dp = Array(n+1).fill(0)
+    dp[1] = sorted[0] * freq.get(sorted[0])
+    for(let i = 2; i <= n; i++) {
+        const val = sorted[i-1]
+        const cur = val * freq.get(val)
+        let j = i - 2
+        
+        if(j >= 0 && sorted[j] + 1 === val) {
+            j--
+        }
+        if(j >= 0 && sorted[j] + 2 === val) {
+            j--
+        }
+        
+        if(j >= 0) {
+            dp[i] = Math.max(dp[i-1], dp[j+1] + cur)
+        } else {
+            dp[i] = Math.max(dp[i-1], cur)
+        }
+        
+    }
+
+    return dp[n]
+};
+
+// another
+
+/**
+ * @param {number[]} power
+ * @return {number}
+ */
 var maximumTotalDamage = function (power) {
   power.sort((a, b) => a - b)
   const freqMap = new Map()
