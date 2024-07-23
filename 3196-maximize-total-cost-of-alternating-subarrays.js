a5c884e63afc0370fe19748e365aba9136c267bc
Nothing <wwwaap@gmail.com>
2024-07-23 11:25:09 +0800

    Update 3196-maximize-total-cost-of-alternating-subarrays.js

--- 3196-maximize-total-cost-of-alternating-subarrays.js
+++ 3196-maximize-total-cost-of-alternating-subarrays.js
@@ -2,6 +2,28 @@
  * @param {number[]} nums
  * @return {number}
  */
+const maximumTotalCost = function(nums) {
+  const n = nums.length;
+  const dp = Array.from({ length: n + 1 }, () => Array(2).fill(0));
+  dp[1][0] = nums[0]
+  dp[1][1] = nums[0]
+    for (let i = 2; i <= n; i++) {
+        const e = nums[i - 1]
+        dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]) + e
+        dp[i][1] = dp[i - 1][0] - e
+    }
+
+
+  return Math.max(dp[n][0], dp[n][1])
+};
+
+// another
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var maximumTotalCost = function (nums) {
   const n = nums.length
   const cache = new Map()
