c161a43a60c5a59b3f8a24421c04569059b556a4
Nothing <wwwaap@gmail.com>
2024-07-25 10:32:53 +0800

    Create 3177-find-the-maximum-length-of-a-good-subsequence-ii.js

---
+++ 3177-find-the-maximum-length-of-a-good-subsequence-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const maximumLength = function(nums, k) {
+  const n = nums.length;
+    const res = Array(k + 1).fill(0);
+    const dp = Array.from({ length: k + 1 }, () => new Map());
+    for (const a of nums) {
+        for (let i = k; i >= 0; i--) {
+            const v = dp[i].get(a) || 0;
+            const vv = Math.max(v + 1, i > 0 ? res[i - 1] + 1 : 0)
+            dp[i].set(a, vv);
+            res[i] = Math.max(res[i], vv);
+        }
+    }
+    return res[k]  
+};
