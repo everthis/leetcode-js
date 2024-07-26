c1466600c3d127416530111f1cfd4a5c41dcd73f
Nothing <wwwaap@gmail.com>
2024-07-26 13:02:10 +0800

    Update 629-k-inverse-pairs-array.js

--- 629-k-inverse-pairs-array.js
+++ 629-k-inverse-pairs-array.js
@@ -4,6 +4,30 @@
  * @return {number}
  */
 const kInversePairs = function(n, k) {
+  const mod = 1e9 + 7
+  const dp = Array.from({ length: n + 1 }, () => Array(k + 1).fill(0))
+  for(let i = 0; i <= n; i++) dp[i][0] = 1
+  for(let i = 2; i <= n; i++) {
+    for(let j = 1; j <= k; j++) {
+        if(j >= i) dp[i][j] = (dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - i]) % mod
+        else dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % mod
+
+        if(dp[i][j] < 0) dp[i][j] += mod
+    }
+  }
+
+  return dp[n][k]
+};
+
+// another
+
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const kInversePairs = function(n, k) {
   const dp = Array.from({ length: n + 1 }, () => new Array(k + 1).fill(0))
   for (let i = 1; i < n + 1; i++) {
     dp[i][0] = 1
