f4d3a0f89e1204d3724dce074b3a5b21fc120789
Nothing <wwwaap@gmail.com>
2024-08-08 15:25:55 +0800

    Update 662-maximum-width-of-binary-tree.js

--- 662-maximum-width-of-binary-tree.js
+++ 662-maximum-width-of-binary-tree.js
@@ -1,5 +1,39 @@
 /**
  * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const widthOfBinaryTree = function (root) {
+  let max = 1n
+  const stack = []
+  const bi = BigInt
+  const width = (root, level, pos) => {
+    if (root == null) return
+    if (level >= stack.length) stack.push(pos)
+    else {
+      // console.log(stack)
+      const tmp = pos - stack[level] + 1n
+      if(tmp > max) max = tmp
+    }
+    width(root.left, level + 1, 2n * pos)
+    width(root.right, level + 1, 2n * pos + 1n)
+  }
+  width(root, 0, 1n)
+  return Number(max)
+}
+
+
+// another
+
+/**
+ * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
  *     this.left = this.right = null;
