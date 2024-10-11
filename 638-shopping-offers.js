67267993fb0611557e4e77dadde106fc6627b1ac
Nothing <wwwaap@gmail.com>
2024-10-11 16:52:52 +0800

    Update 638-shopping-offers.js

--- 638-shopping-offers.js
+++ 638-shopping-offers.js
@@ -5,6 +5,49 @@
  * @return {number}
  */
 const shoppingOffers = function (price, special, needs) {
+  return helper(price, special, needs, 0);
+}
+
+
+function helper(price, special, needs, pos) {
+    let local_min = directPurchase(price, needs);
+    for (let i = pos; i < special.length; i++) {
+        let offer = special[i];
+        let temp = [];
+        for (let j = 0; j < needs.length; j++) {
+            if (needs[j] < offer[j]) { // check if the current offer is valid
+                temp = null;
+                break;
+            }
+            temp.push(needs[j] - offer[j]);
+        }
+
+        if (temp !== null) { // use the current offer and try next
+            local_min = Math.min(local_min, offer[offer.length - 1] + helper(price, special, temp, i));
+        }
+    }
+
+    return local_min;
+}
+
+function directPurchase(price, needs) {
+    let total = 0;
+    for (let i = 0; i < needs.length; i++) {
+        total += price[i] * needs[i];
+    }
+
+    return total;
+}
+
+// another
+
+/**
+ * @param {number[]} price
+ * @param {number[][]} special
+ * @param {number[]} needs
+ * @return {number}
+ */
+const shoppingOffers = function (price, special, needs) {
   const directBuy = function (price, needs) {
     let res = 0
     for (let i = 0; i < price.length; i++) {
