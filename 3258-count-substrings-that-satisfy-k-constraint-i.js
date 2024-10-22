0aeb8d9074da769c836d8aef5e6805902d12f3c4
Nothing <wwwaap@gmail.com>
2024-10-22 13:30:16 +0800

    Update 3258-count-substrings-that-satisfy-k-constraint-i.js

--- 3258-count-substrings-that-satisfy-k-constraint-i.js
+++ 3258-count-substrings-that-satisfy-k-constraint-i.js
@@ -3,6 +3,36 @@
  * @param {number} k
  * @return {number}
  */
+const countKConstraintSubstrings = function (s, k) {
+  const n = s.length
+  let res = 0,
+    l = 0,
+    r = 0,
+    zero = 0,
+    one = 0
+  while (r < n) {
+    if (s[r] === "0") zero++
+    else one++
+    while (zero > k && one > k) {
+      if (s[l] === "0") zero--
+      else one--
+      l++
+    }
+    res += r - l + 1
+    r++
+  }
+
+  return res
+}
+
+// another
+
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
 var countKConstraintSubstrings = function(s, k) {
   let res = 0
   const n = s.length
