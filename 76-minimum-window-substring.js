8d0ac4060478fd39a2be9c84dfb71166e4e9a30d
Nothing <wwwaap@gmail.com>
2024-10-24 15:46:36 +0800

    Update 76-minimum-window-substring.js

--- 76-minimum-window-substring.js
+++ 76-minimum-window-substring.js
@@ -3,6 +3,44 @@
  * @param {string} t
  * @return {string}
  */
+var minWindow = function(s, t) {
+  const a = 'a'.charCodeAt(0)
+  const A = 'A'.charCodeAt(0)
+  const arr = new Array(52).fill(0)
+  const n = s.length
+  const idx = (ch) => {
+    const code = ch.charCodeAt(0)
+    return code - (code >= a ? a : A) + (code >= a ? 26 : 0)
+  }
+  for (const ch of t) {
+    arr[idx(ch)]++
+  }
+  let l = 0, r = 0
+  let res = ''
+  while(r < n) {
+    const i = idx(s[r])
+    arr[i]--
+    while(l < r && arr[idx(s[l])] < 0) {
+        arr[idx(s[l])]++
+        l++
+    }
+    const tmp = s.slice(l, r + 1)
+    if(arr.every(x => x <= 0) && (res === '' || tmp.length < res.length)) {
+      res = tmp
+    }
+    r++
+  }
+
+  return res
+};
+
+// another
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
 const minWindow = function(s, t) {
   const map = {}
   for (const c of t) {
