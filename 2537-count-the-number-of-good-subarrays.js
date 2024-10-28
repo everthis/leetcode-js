7ad535dc1228124484f03737278743e003427a07
Nothing <wwwaap@gmail.com>
2024-10-28 13:19:43 +0800

    Update 2537-count-the-number-of-good-subarrays.js

--- 2537-count-the-number-of-good-subarrays.js
+++ 2537-count-the-number-of-good-subarrays.js
@@ -81,3 +81,43 @@ const countGood = function(nums, k) {
   }
   return res;
 };
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const countGood = function(nums, k) {
+  let res = 0
+  let total = 0
+  let i = 0, j = 0
+  const n = nums.length
+  const cnt = {}
+  while(i < n) {
+    while(j < n && total < k) {
+      total += calc(cnt, nums[j], 1)
+      cnt[nums[j]] = (cnt[nums[j]] || 0) + 1
+      j++
+    }
+    if(total >= k) {
+        res += n - j + 1
+    }
+    total += calc(cnt, nums[i], -1)
+    cnt[nums[i]]--
+
+    i++
+  }
+  
+  return res
+
+  function calc(cnt, num, delta) {
+    const tmp = cnt[num] || 0
+    let res = tmp * (tmp - 1) / 2
+    const tmp1 = tmp + delta
+    const tmp2 = tmp1 * (tmp1 - 1) / 2
+    res = tmp2 - res
+    return res
+  }
+};
