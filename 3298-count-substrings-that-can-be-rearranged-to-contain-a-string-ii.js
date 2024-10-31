3ad902aa8a1a1f1a1feb13596c47342e50983095
Nothing <wwwaap@gmail.com>
2024-10-31 15:39:25 +0800

    Update 3298-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.js

--- 3298-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.js
+++ 3298-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.js
@@ -1,3 +1,46 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const validSubstringCount = function(word1, word2) {
+  const n = word1.length;
+  const target = Array(26).fill(0);
+  const a = 'a'.charCodeAt(0);
+  for(const ch of word2) {
+    target[ch.charCodeAt(0) - a]++;
+  }
+  const targetCnt = target.reduce((acc, cur) => acc += cur > 0 ? 1 : 0, 0);
+  const cur = Array(26).fill(0);
+
+  let j = 0
+  let cnt = 0
+  let res = 0
+  for(let i = 0; i < n; i++) {
+    const e = word1[i].charCodeAt(0) - a
+    while(j < n && cnt < targetCnt) {
+        const idx= word1[j].charCodeAt(0) - a;
+        cur[idx]++;
+        if(cur[idx] === target[idx]) {
+          cnt++;
+        }
+        j++
+    }
+    if(cnt === targetCnt) {
+      res += (n - 1) - (j - 1) + 1
+    }
+    cur[e]--;
+    if(cur[e] === target[e] - 1) {
+      cnt--;
+    }
+  }
+  return res
+};
+
+// another
+
+
+
 function validSubstringCount(w1, w2) {
     const cnt = {};
     for (const ch of w2) {
