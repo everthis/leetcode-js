dc59c61e56ef5ed96f215256157a20d10b551818
Nothing <wwwaap@gmail.com>
2024-11-06 13:29:01 +0800

    Update 3076-shortest-uncommon-substring-in-an-array.js

--- 3076-shortest-uncommon-substring-in-an-array.js
+++ 3076-shortest-uncommon-substring-in-an-array.js
@@ -3,6 +3,70 @@
  * @return {string[]}
  */
 const shortestSubstrings = function (arr) {
+  const root = new Node()
+  const res = []
+  for (const s of arr) {
+    for (let i = 0; i < s.length; ++i) {
+      add(root, s, i)
+    }
+  }
+  for (const s of arr) {
+    const tmpRoot = new Node()
+    let resStr = s + s
+    for (let i = 0; i < s.length; ++i) {
+      add(tmpRoot, s, i)
+    }
+
+    for (let i = 0; i < s.length; ++i) {
+      const t = check(root, tmpRoot, s, i)
+      if (
+        t.length < resStr.length ||
+        (t.length === resStr.length && t < resStr)
+      ) {
+        resStr = t
+      }
+    }
+    res.push(resStr.length <= s.length ? resStr : "")
+  }
+
+  return res
+  function add(node, str, start) {
+    let ptr = node
+    for (let i = start; i < str.length; ++i) {
+      const c = str.charCodeAt(i) - "a".charCodeAt(0)
+      if (ptr.child[c] === null) ptr.child[c] = new Node()
+      ptr = ptr.child[c]
+      ptr.count++
+    }
+  }
+  function check(root, node, str, start) {
+    let ptr1 = root
+    let ptr2 = node
+    let res = ""
+    for (let i = start; i < str.length; ++i) {
+      const chIdx = str.charCodeAt(i) - "a".charCodeAt(0)
+      res += str[i]
+      ptr1 = ptr1.child[chIdx]
+      ptr2 = ptr2.child[chIdx]
+      if (ptr1.count === ptr2.count) return res
+    }
+    return str + str
+  }
+}
+class Node {
+  constructor() {
+    this.child = new Array(26).fill(null)
+    this.count = 0
+  }
+}
+
+// another
+
+/**
+ * @param {string[]} arr
+ * @return {string[]}
+ */
+const shortestSubstrings = function (arr) {
   const ans = []
   const head = new Node()
   for (const s of arr) {
