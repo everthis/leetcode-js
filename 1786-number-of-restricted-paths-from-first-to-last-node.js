e47fab6385cc88f165bb9e703c44eed4e3d74e4b
Nothing <wwwaap@gmail.com>
2024-11-13 13:48:01 +0800

    Update 1786-number-of-restricted-paths-from-first-to-last-node.js

--- 1786-number-of-restricted-paths-from-first-to-last-node.js
+++ 1786-number-of-restricted-paths-from-first-to-last-node.js
@@ -4,6 +4,120 @@
  * @return {number}
  */
 const countRestrictedPaths = function(n, edges) {
+  const g = {}
+    for (let [u, v, w] of edges) {
+        g[u] = g[u] || []
+        g[u].push([v, w])
+        g[v] = g[v] || []
+        g[v].push([u, w])
+    }
+    const dist = Array(n + 1).fill(Infinity)
+    dist[n] = 0
+    const pq = new PQ((a, b) => a[0] < b[0])
+    pq.push([0, n])
+    while(!pq.isEmpty()) {
+        const [d, u] = pq.pop()
+        if (d !== dist[u]) continue
+        for (let [v, w] of (g[u] || [])) {
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w
+                pq.push([dist[v], v])
+            }
+        }
+    }
+    const mod = 1e9 + 7
+    const memo = Array(n + 1).fill(null)
+    const dfs = (src) => {
+        if (memo[src] !== null) return memo[src]
+        if (src === n) return 1
+        let res = 0
+        for (let [v, w] of (g[src] || [])) {
+            if (dist[src] > dist[v]) {
+                res = (res + dfs(v)) % mod
+            }
+        }
+        return memo[src] = res
+    }
+    return dfs(1)
+};
+
+class PQ {
+    constructor(comparator = (a, b) => a > b) {
+      this.heap = []
+      this.top = 0
+      this.comparator = comparator
+    }
+    size() {
+      return this.heap.length
+    }
+    isEmpty() {
+      return this.size() === 0
+    }
+    peek() {
+      return this.heap[this.top]
+    }
+    push(...values) {
+      values.forEach((value) => {
+        this.heap.push(value)
+        this.siftUp()
+      })
+      return this.size()
+    }
+    pop() {
+      const poppedValue = this.peek()
+      const bottom = this.size() - 1
+      if (bottom > this.top) {
+        this.swap(this.top, bottom)
+      }
+      this.heap.pop()
+      this.siftDown()
+      return poppedValue
+    }
+    replace(value) {
+      const replacedValue = this.peek()
+      this.heap[this.top] = value
+      this.siftDown()
+      return replacedValue
+    }
+  
+    parent = (i) => ((i + 1) >>> 1) - 1
+    left = (i) => (i << 1) + 1
+    right = (i) => (i + 1) << 1
+    greater = (i, j) => this.comparator(this.heap[i], this.heap[j])
+    swap = (i, j) => ([this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]])
+    siftUp = () => {
+      let node = this.size() - 1
+      while (node > this.top && this.greater(node, this.parent(node))) {
+        this.swap(node, this.parent(node))
+        node = this.parent(node)
+      }
+    }
+    siftDown = () => {
+      let node = this.top
+      while (
+        (this.left(node) < this.size() && this.greater(this.left(node), node)) ||
+        (this.right(node) < this.size() && this.greater(this.right(node), node))
+      ) {
+        let maxChild =
+          this.right(node) < this.size() &&
+          this.greater(this.right(node), this.left(node))
+            ? this.right(node)
+            : this.left(node)
+        this.swap(node, maxChild)
+        node = maxChild
+      }
+    }
+  }
+
+// another
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+const countRestrictedPaths = function(n, edges) {
   const adj = {}
   const MOD = 10 ** 9 + 7
   for(let edge of edges) {
