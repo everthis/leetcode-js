23b830fb26070709cc9143ad6368f1fb436f22f8
Nothing <wwwaap@gmail.com>
2024-12-10 14:11:44 +0800

    Update 2360-longest-cycle-in-a-graph.js

--- 2360-longest-cycle-in-a-graph.js
+++ 2360-longest-cycle-in-a-graph.js
@@ -3,6 +3,55 @@
  * @return {number}
  */
 const longestCycle = function(edges) {
+  const n = edges.length;
+  const visited = new Array(n).fill(false);
+  const ind = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    if (edges[i] !== -1) {
+      ind[edges[i]]++;
+    }
+  }
+  let q = []
+  for(let i = 0; i < n; i++) {
+    if (ind[i] === 0) {
+      q.push(i);
+    }
+  }
+  while(q.length) {
+    const node = q.pop()
+    visited[node] = true;
+    const nxt = edges[node];
+    if(nxt !== -1) {
+      ind[nxt]--;
+      if (ind[nxt] === 0) {
+        q.push(nxt);
+      }
+    }
+  }
+  let res = -1
+  for(let i = 0; i < n; i++) {
+    if (!visited[i]) {
+        let cnt = 0
+        let cur = i
+        while (!visited[cur]) {
+            visited[cur] = true
+            cur = edges[cur]
+            cnt++
+        }
+        res = Math.max(res, cnt)
+    }
+  }
+
+  return res
+};
+
+// another
+
+/**
+ * @param {number[]} edges
+ * @return {number}
+ */
+const longestCycle = function(edges) {
   const n = edges.length, colors = Array(n).fill(0), dis = Array(n).fill(0)
   let res = -1
   
