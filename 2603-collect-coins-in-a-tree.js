d09846527d50f6d4b818cfba27be8379f97340a2
Nothing <wwwaap@gmail.com>
2024-12-13 16:31:07 +0800

    Update 2603-collect-coins-in-a-tree.js

--- 2603-collect-coins-in-a-tree.js
+++ 2603-collect-coins-in-a-tree.js
@@ -1,3 +1,69 @@
+/**
+ * @param {number[]} coins
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var collectTheCoins = function(coins, edges) {
+  let n = coins.length;
+  let next = Array.from({ length: n }, () => new Set());
+
+  let degree = new Array(n).fill(0);
+  for (let edge of edges) {
+    let a = edge[0],
+      b = edge[1];
+    next[a].add(b);
+    next[b].add(a);
+    degree[a]++;
+    degree[b]++;
+  }
+
+  let deleted = new Array(n).fill(0);
+  let q = [];
+  for (let i = 0; i < n; i++) {
+    if (degree[i] === 1 && coins[i] === 0) q.push(i);
+  }
+  while (q.length > 0) {
+    let len = q.length;
+    while (len--) {
+      let cur = q.shift();
+      deleted[cur] = 1;
+      for (let nxt of next[cur]) {
+        degree[nxt]--;
+        next[nxt].delete(cur);
+        if (degree[nxt] === 1 && coins[nxt] === 0) q.push(nxt);
+      }
+    }
+  }
+
+  let depth = new Array(n).fill(-1);
+  for (let i = 0; i < n; i++) {
+    if (degree[i] === 1 && deleted[i] === 0) {
+      q.push(i);
+      depth[i] = 1;
+    }
+  }
+  while (q.length > 0) {
+    let len = q.length;
+    while (len--) {
+      let cur = q.shift();
+      for (let nxt of next[cur]) {
+        degree[nxt]--;
+        next[nxt].delete(cur);
+        depth[nxt] = Math.max(depth[nxt], depth[cur] + 1);
+        if (degree[nxt] === 1) q.push(nxt);
+      }
+    }
+  }
+
+  let ret = 0;
+  for (let i = 0; i < n; i++) ret += depth[i] >= 3;
+
+  if (ret >= 1) return (ret - 1) * 2;
+  else return 0;
+};
+
+// another
+
 let a, cum, res, v, sum, g;
 const initializeGraph = (n) => { let g = []; for (let i = 0; i < n; i++) { g.push([]); } return g; };
 const packUG = (g, edges) => { for (const [u, v] of edges) { g[u].push(v); g[v].push(u); } };
