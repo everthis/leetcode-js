9817c607e37c748a246734f48930ce2c92473a24
Nothing <wwwaap@gmail.com>
2024-12-23 11:19:26 +0800

    Update 1442-count-triplets-that-can-form-two-arrays-of-equal-xor.js

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor.js
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor.js
@@ -1,4 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countTriplets = function(arr) {
+  const n = arr.length
+  const prefix = new Array(n + 1).fill(0)
+  for(let i = 0; i < n; i++) {
+    prefix[i + 1] = prefix[i] ^ arr[i]
+  }
+  let res = 0, xor = 0
+  const hash = {0: [-1]}
+  for(let i = 0; i < n; i++) {
+    const e = arr[i]
+    xor ^= e
+    if(hash[xor] === undefined) {
+      hash[xor] = []
+    } else {
+        for(const prev of hash[xor]) {
+            res += Math.max(0, i - prev - 1)
+        }
+    }
+    hash[xor].push(i)
+  }
 
+  return res
+};
+
+// another
 /**
  * @param {number[]} arr
  * @return {number}
