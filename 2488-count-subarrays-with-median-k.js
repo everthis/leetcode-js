eef52030e28da09aeb627efd03f65af9f0158844
Nothing <wwwaap@gmail.com>
2024-12-31 13:04:45 +0800

    Update 2488-count-subarrays-with-median-k.js

--- 2488-count-subarrays-with-median-k.js
+++ 2488-count-subarrays-with-median-k.js
@@ -25,3 +25,39 @@ const permutationArrayWithMedianK = (a, k) => {
   }
   return res;
 };
+
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countSubarrays = function(nums, k) {
+  const n = nums.length;
+  for(let i = 0; i < n; i++) {
+    nums[i] = nums[i] < k ? -1 : nums[i] === k ? 0 : 1
+  }
+  const evenSum = {}
+  const oddSum = {}
+  evenSum[0] = 1
+    let sum = 0
+    let res = 0
+    for(let i = 0; i < n; i++) {
+        sum += nums[i]
+        if(i % 2 === 0) {
+            if(evenSum[sum] != null) res += evenSum[sum]
+            if(oddSum[sum - 1] != null) res += oddSum[sum - 1]
+            if(oddSum[sum] == null) oddSum[sum] = 0
+            oddSum[sum]++
+        } else {
+            if(oddSum[sum] != null) res += oddSum[sum]
+            if(evenSum[sum - 1] != null) res += evenSum[sum - 1]
+            if(evenSum[sum] == null) evenSum[sum] = 0
+            evenSum[sum]++
+        }
+    }
+ // console.log(evenSum, oddSum, nums)
+  return res
+};
