ea84c7f9af48324493f97028fdcb0c433e54b2e9
Nothing <wwwaap@gmail.com>
2025-01-15 10:36:41 +0800

    Update 646-maximum-length-of-pair-chain.js

--- 646-maximum-length-of-pair-chain.js
+++ 646-maximum-length-of-pair-chain.js
@@ -2,6 +2,26 @@
  * @param {number[][]} pairs
  * @return {number}
  */
+var findLongestChain = function(pairs) {
+    pairs.sort((a, b) => a[1] - b[1]);
+    if(pairs.length === 0) return 0
+    let res = 1
+    let end = pairs[0][1]
+    for(let i = 1; i < pairs.length; i++) {
+        if(pairs[i][0] > end) {
+            res++
+            end = pairs[i][1]
+        }
+    }
+    return res
+};
+
+// another
+
+/**
+ * @param {number[][]} pairs
+ * @return {number}
+ */
 const findLongestChain = function(pairs) {
   let res = 0
   const n = pairs.length
