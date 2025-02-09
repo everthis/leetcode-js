391a9eb8aa41fccda5593423d018de2f8bc823fe
Nothing <wwwaap@gmail.com>
2025-02-09 03:54:08 -0800

    Update 3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences.js

--- 3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences.js
+++ 3428-maximum-and-minimum-sums-of-at-most-size-k-subsequences.js
@@ -4,6 +4,46 @@
  * @return {number}
  */
 var minMaxSums = function(nums, k) {
+  const big = BigInt
+  const mod = big(1e9 + 7)
+  const n = nums.length
+  const comb = Array.from({ length: n + 1 }, () => Array(k +  1).fill(0n))
+  
+  comb[0][0] = 1n
+  for(let i = 1; i <= n; i++) {
+    comb[i][0] = 1n
+    for(let j = 1; j <= k; j++) {
+        comb[i][j] = comb[i - 1][j - 1] + comb[i - 1][j]
+        comb[i][j] %= mod
+    }
+  }
+  
+  nums.sort((a, b) => a - b)
+
+  let res = 0n
+
+  for(let i = 0; i < n; i++) {
+    const e = big(nums[i])
+    for(let j = 0; j <= Math.min(i, k - 1); j++) {
+        res += (comb[i][j] * e) % mod
+    }
+    for(let j = 0; j < k; j++) {
+        res += (comb[n - 1 - i][j] * e) % mod
+    }
+  }
+
+
+  return Number(res % mod) 
+};
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minMaxSums = function(nums, k) {
   nums.sort((a, b) => a - b)
   let res = 0n
   const MOD = 1e9 + 7
