f4d938d4034f566e95de1b4b7a05a2cf2301030d
Nothing <wwwaap@gmail.com>
2025-02-14 03:39:13 -0800

    Update 3350-adjacent-increasing-subarrays-detection-ii.js

--- 3350-adjacent-increasing-subarrays-detection-ii.js
+++ 3350-adjacent-increasing-subarrays-detection-ii.js
@@ -4,6 +4,30 @@
  */
 var maxIncreasingSubarrays = function (nums) {
   const n = nums.length
+  let up = 1, preMaxUp = 0, res = 0
+
+  for(let i = 1; i < n; i++) {
+    if(nums[i] > nums[i - 1]) up++
+    else {
+        preMaxUp = up
+        up = 1
+    }
+    res = Math.max(res, Math.max(~~(up / 2), Math.min(preMaxUp, up)))
+  }
+
+
+  return res
+}
+
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxIncreasingSubarrays = function (nums) {
+  const n = nums.length
 
   const increasingRun = new Array(n).fill(1)
   for (let i = n - 2; i >= 0; --i) {
