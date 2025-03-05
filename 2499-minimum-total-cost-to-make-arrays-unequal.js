ff2d09a0a5a7bad3613d3ef8f156e83d9d7cfc0e
Nothing <wwwaap@gmail.com>
2025-03-05 04:14:12 -0800

    Update 2499-minimum-total-cost-to-make-arrays-unequal.js

--- 2499-minimum-total-cost-to-make-arrays-unequal.js
+++ 2499-minimum-total-cost-to-make-arrays-unequal.js
@@ -4,31 +4,32 @@
  * @return {number}
  */
 var minimumTotalCost = function(nums1, nums2) {
-    const n = nums1.length;
-    let ans = 0;
-    const freq = new Map();
-    let maxFrequency = 0, maxFrequencyValue = 0, toSwap = 0;
-
-    for (let i = 0; i < n; i++) {
-        if (nums1[i] === nums2[i]) {
-            freq.set(nums1[i], (freq.get(nums1[i]) || 0) + 1);
-            if (freq.get(nums1[i]) > maxFrequency) {
-                maxFrequencyValue = nums1[i];
-            }
-            maxFrequency = Math.max(maxFrequency, freq.get(nums1[i]));
-            toSwap++;
-            ans += i;
+  const n = nums1.length, {floor, max} = Math
+  let res = 0
+  const freq = new Map()
+  let maxFreq = 0, maxFreqVal = 0, toSwap = 0
+  for(let i = 0; i < n; i++) {
+    if(nums1[i] === nums2[i]) {
+        const e = nums1[i]
+        freq.set(e, (freq.get(e) || 0) + 1)
+        res += i
+        const f = freq.get(e)
+        toSwap++
+        if(f > maxFreq) {
+            maxFreqVal = e
         }
+        maxFreq = max(maxFreq, f)
     }
+  }
 
-    for (let i = 0; i < n; i++) {
-        if (maxFrequency > toSwap / 2 && nums1[i] !== nums2[i] && nums1[i] !== maxFrequencyValue && nums2[i] !== maxFrequencyValue) {
-            ans += i;
-            toSwap++;
-        }
+  for(let i = 0; i < n; i++) {
+    if(maxFreq > floor(toSwap / 2) && nums1[i] !== nums2[i] && nums1[i] !== maxFreqVal && nums2[i] !== maxFreqVal) {
+        toSwap++
+        res += i
     }
+  }
 
-    if (maxFrequency > toSwap / 2) return -1;
+  if(maxFreq > floor(toSwap / 2)) return -1
 
-    return ans;
+  return res
 };
