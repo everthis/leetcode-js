bf461b1db3bf3c99b4c078662e459e33e11445f8
Nothing <wwwaap@gmail.com>
2025-03-14 04:58:51 -0700

    Update 2745-construct-the-longest-new-string.js

--- 2745-construct-the-longest-new-string.js
+++ 2745-construct-the-longest-new-string.js
@@ -4,6 +4,30 @@
  * @param {number} z
  * @return {number}
  */
+var longestString = function(x, y, z) {
+  // x + y
+  // y + x
+  // y + z
+  // z + z
+  let res = 0
+  const {min} = Math
+  const minXY = min(x, y)
+  if(x === y) res = 2 * x + 2 * y + 2 * z
+  else {
+    res = (minXY * 2 + (minXY + 1) * 2 + 2 * z)
+  }
+
+  return res
+};
+
+// another
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
 const longestString = function(x, y, z) {
    const base = Math.min(x, y)
    let extra = 0
