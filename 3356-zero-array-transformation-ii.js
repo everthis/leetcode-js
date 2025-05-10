51a9c8e39dc80f13a48405e24172663df3847353
Nothing <wwwaap@gmail.com>
2025-05-10 06:49:28 -0700

    Update 3356-zero-array-transformation-ii.js

--- 3356-zero-array-transformation-ii.js
+++ 3356-zero-array-transformation-ii.js
@@ -4,6 +4,43 @@
  * @return {number}
  */
 var minZeroArray = function (nums, queries) {
+  const n = nums.length
+  let sum = 0,
+    k = 0
+  const differenceArray = new Array(n + 1).fill(0)
+
+  for (let index = 0; index < n; index++) {
+    // Iterate through queries while current index of nums cannot equal zero
+    while (sum + differenceArray[index] < nums[index]) {
+      k++
+
+      // Zero array isn't formed after all queries are processed
+      if (k > queries.length) {
+        return -1
+      }
+      const [left, right, val] = queries[k - 1]
+
+      // Process start and end of range
+      if (right >= index) {
+        differenceArray[Math.max(left, index)] += val
+        differenceArray[right + 1] -= val
+      }
+    }
+    // Update prefix sum at current index
+    sum += differenceArray[index]
+  }
+  return k
+}
+
+
+// another
+
+/**
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var minZeroArray = function (nums, queries) {
   if (nums.every((num) => num === 0)) return 0
   const n = nums.length
   const delta = new Array(n + 1).fill(0)
@@ -35,3 +72,4 @@ var minZeroArray = function (nums, queries) {
   }
   return -1
 }
+
