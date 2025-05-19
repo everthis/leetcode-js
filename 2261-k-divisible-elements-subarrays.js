e44d5385a8f1b5e837c34f6f2a18d1f24a81119f
Nothing <wwwaap@gmail.com>
2025-05-19 03:50:19 -0700

    Update 2261-k-divisible-elements-subarrays.js

--- 2261-k-divisible-elements-subarrays.js
+++ 2261-k-divisible-elements-subarrays.js
@@ -5,20 +5,19 @@
  * @return {number}
  */
 const countDistinct = function(nums, k, p) {
-    let ans = 0;
-    const se = new Set();
-    let n = nums.length;
+    const se = new Set()
+    let n = nums.length
     for (let i = 0; i < n; i++) {
-        let tmp = "";
-        let cnt = 0;
+        let tmp = ""
+        let cnt = 0
         for (let j = i; j < n; j++) {
             if (nums[j] % p == 0)
-                cnt++;
+                cnt++
             if (cnt <= k) {
-                tmp = tmp + (nums[j]) + "-";
-                se.add(tmp);
-            } else break;
+                tmp = tmp + (nums[j]) + "-"
+                se.add(tmp)
+            } else break
         }
     }
-    return se.size;
+    return se.size
 };
