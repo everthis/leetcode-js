6a0882fd8304d3c57d08907c63ec2dffdd23338d
Nothing <wwwaap@gmail.com>
2025-11-05 12:11:46 +0000

    Update 3464-maximize-the-distance-between-points-on-a-square.js

--- 3464-maximize-the-distance-between-points-on-a-square.js
+++ 3464-maximize-the-distance-between-points-on-a-square.js
@@ -4,6 +4,91 @@
  * @param {number} k
  * @return {number}
  */
+const maxDistance = function (side, points, k) {
+  const n = points.length
+  const arr = []
+
+  const next = new Array(15000)
+
+  for (const p of points) {
+    if (p[0] === 0) {
+      arr.push(p[1])
+    } else if (p[1] === side) {
+      arr.push(side + p[0])
+    } else if (p[0] === side) {
+      arr.push(2 * side + side - p[1])
+    } else if (p[1] === 0) {
+      arr.push(3 * side + side - p[0])
+    }
+  }
+
+  arr.sort((a, b) => a - b)
+
+  let low = 0,
+    high = side
+  while (low < high) {
+    const mid = high - Math.floor((high - low) / 2)
+    if (isOK(mid, k)) {
+      low = mid
+    } else {
+      high = mid - 1
+    }
+  }
+  return low
+
+  function isOK(dist, k) {
+    let j = 0
+    for (let i = 0; i < n; i++) {
+      while (pos(j) - arr[i] < dist) {
+        j++
+      }
+      next[i] = j
+    }
+
+    for (let i = 0; i < n; i++) {
+      let flag = true
+      let cur = i
+      for (let t = 0; t < k - 1; t++) {
+        if (cur < n) {
+          cur = next[cur]
+        } else {
+          cur = next[cur % n] + n
+        }
+        if (cur >= i + n) {
+          flag = false
+          break
+        }
+      }
+      if (pos(i) - pos(cur % n) < dist) {
+        flag = false
+      }
+      if (flag) {
+        return true
+      }
+    }
+    return false
+  }
+
+  function pos(j) {
+    if (j < n) {
+      return arr[j]
+    } else {
+      return arr[j % n] + side * 4
+    }
+  }
+
+}
+
+
+// another
+
+
+/**
+ * @param {number} side
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number}
+ */
 var maxDistance = function (side, points, k) {
   points = points
     .map(([x, y]) => {
