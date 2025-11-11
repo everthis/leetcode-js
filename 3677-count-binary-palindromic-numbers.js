c834c3aa82eff7bc575af8a4139ba0e5d1165397
Nothing <wwwaap@gmail.com>
2025-11-11 12:52:41 +0000

    Update 3677-count-binary-palindromic-numbers.js

--- 3677-count-binary-palindromic-numbers.js
+++ 3677-count-binary-palindromic-numbers.js
@@ -3,6 +3,68 @@
  * @return {number}
  */
 var countBinaryPalindromes = function (n) {
+  n = BigInt(n)
+  if (n === 0n) return 1
+  const maxLen = Math.floor(Math.log2(Number(n))) + 1
+
+  let ret = 1n
+  for (let L = 1; L < maxLen; L++) {
+    const h = Math.floor((L + 1) / 2)
+    const mn = 1n << BigInt(h - 1)
+    const mx = (1n << BigInt(h)) - 1n
+    ret += mx - mn + 1n
+  }
+
+  {
+    const L = maxLen
+    const h = Math.floor((L + 1) / 2)
+    const mn = 1n << BigInt(h - 1)
+    const mx = (1n << BigInt(h)) - 1n
+    let lo = mn,
+      hi = mx
+    while (lo < hi) {
+      const mid = hi - (hi - lo) / 2n
+      const pal = build(mid, L)
+      if (pal <= n) {
+        lo = mid
+      } else {
+        hi = mid - 1n
+      }
+    }
+    const pal = build(hi, L)
+    if (pal <= n) ret += hi - mn + 1n
+  }
+
+  return Number(ret)
+}
+function reverseBits(x) {
+  let r = 0n
+  while (x > 0n) {
+    r = r * 2n + (x & 1n)
+    x >>= 1n
+  }
+  return r
+}
+
+function build(half, L) {
+  const h = Math.floor((L + 1) / 2)
+  const k = L - h
+  if (L % 2 === 0) {
+    return (half << BigInt(k)) | reverseBits(half)
+  } else {
+    return (half << BigInt(k)) | reverseBits(half >> 1n)
+  }
+}
+
+
+// another
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countBinaryPalindromes = function (n) {
   let dp = []
   let built = false
   n = BigInt(n)
