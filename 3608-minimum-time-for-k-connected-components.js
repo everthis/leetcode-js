00e43eefa40e61471e9eb08e31f1570e696aeccc
Nothing <wwwaap@gmail.com>
2025-11-14 06:26:59 +0000

    Update 3608-minimum-time-for-k-connected-components.js

--- 3608-minimum-time-for-k-connected-components.js
+++ 3608-minimum-time-for-k-connected-components.js
@@ -4,36 +4,38 @@
  * @param {number} k
  * @return {number}
  */
-var minTime = function (n, edges, k) {
-  let f = Array.from({ length: n }, (_, i) => i)
-
+var minTime = function(n, edges, k) {
   edges.sort((a, b) => b[2] - a[2])
-  let count = n
-  for (const [u, v, t] of edges) {
-    if (union(u, v)) {
-      count -= 1
-    }
-    if (count < k) {
-      return t
-    }
+  const uf = new UF(n)
+  let cnt = n
+  
+  for(let i = 0; i < edges.length; i++) {
+    const [u, v, t] = edges[i]
+    if(uf.union(u, v)) cnt--
+    if(cnt < k) return t
   }
   return 0
+};
 
-  function find(x) {
-    if (x === f[x]) {
-      return x
-    }
-    f[x] = find(f[x])
-    return f[x]
+class UF {
+  constructor(n) {
+    this.root = Array(n).fill(null).map((_, i) => i)
   }
-
-  function union(x, y) {
-    x = find(x)
-    y = find(y)
-    if (x === y) {
-      return false
+  find(x) {
+    if (this.root[x] !== x) {
+      this.root[x] = this.find(this.root[x])
     }
-    f[x] = y
-    return true
+    return this.root[x]
+  }
+  union(x, y) {
+    const xr = this.find(x)
+    const yr = this.find(y)
+    if(xr === yr) {
+      return false
+    } else {
+      this.root[yr] = xr
+      return true
+    }
+
   }
 }
