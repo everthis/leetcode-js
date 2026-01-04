067cf316c5370c4a4958c289cf8e84e44297b119
Nothing <wwwaap@gmail.com>
2026-01-04 12:00:50 +0000

    Update 2335-minimum-amount-of-time-to-fill-cups.js

--- 2335-minimum-amount-of-time-to-fill-cups.js
+++ 2335-minimum-amount-of-time-to-fill-cups.js
@@ -2,6 +2,22 @@
  * @param {number[]} amount
  * @return {number}
  */
+var fillCups = function(amount) {
+  const { floor: flr, ceil, max} = Math
+  const ma = max(...amount)
+  const sum = amount.reduce((ac, e) => ac + e, 0)
+  const rest = sum - ma
+  if(ma > rest) return ma
+  return ceil(sum / 2)
+};
+
+
+// another
+
+/**
+ * @param {number[]} amount
+ * @return {number}
+ */
 const fillCups = function(amount) {
   amount.sort((a, b) => a- b)
   let res = 0;
