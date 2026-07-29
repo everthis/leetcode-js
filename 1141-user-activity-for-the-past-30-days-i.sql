c89d45111d17a1c40eeb8c409151ee5b5a240fad
Keval <208117821+kevalsondigala@users.noreply.github.com>
2026-07-29 12:46:08 +0530

    Modify SQL query to include date range condition
    The previous solution was to passing all test cases because of a bug where activity_date is greater than 2019-07-27 so actually it was checking both sides of 2019-07-27 we have to set a range or make proposed changes to pass all test cases

--- 1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select activity_date as day, count(distinct user_id) as active_users 
+select activity_date as day, count(distinct user_id) as active_users
 from Activity
-where datediff('2019-07-27', activity_date) < 30
-group by activity_date;
+where datediff('2019-07-27', activity_date) < 30 and activity_date <= '2019-07-27'
+group by activity_date
